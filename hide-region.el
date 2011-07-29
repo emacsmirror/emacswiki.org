640a6cc7394ab6c0e515b294fd53155ad34480dc
anybody <anybody@emacswiki.org>
2011-07-29 08:40:27 +0000

    updates

--- hide-region.el
+++ hide-region.el
@@ -2,11 +2,13 @@
 ;;
 ;; Copyright (C) 2001, 2005  Mathias Dahl
 ;;
-;; Version: 1.0.1
+;; Version: 1.0.2
 ;; Keywords: hide, region
 ;; Author: Mathias Dahl <mathias.rem0veth1s.dahl@gmail.com>
 ;; Maintainer: Mathias Dahl
 ;; URL: http://mathias.dahl.net/pgm/emacs/elisp/hide-region.el
+;; Last-Updated: Thu Sun Jul 28 16:10:15 2011 (+0200)
+;;           By: Deniz Dogan <deniz@dogan.se>
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
@@ -42,13 +44,18 @@
 ;; Test region 2
 ;; Test region 3
 ;;
-;; If you are annoyed by the text getting "stuck" inside the hidden
-;; regions, call the function `hide-region-setup-keybindings' to setup
-;; local keybindings to a couple of functions trying to be smart and
-;; guessing if the point is inside a hidden region and if so, move out
-;; of it in the correct direction.
+;; It can be useful to bind the commands to mnemonic keys, e.g.:
+;; (global-set-key (kbd "C-c h r") 'hide-region-hide)
+;; (global-set-key (kbd "C-c h u") 'hide-region-unhide)
 ;;
 ;;; Version history
+;;
+;; Version 1.0.2
+;;
+;; * Added defface for text properties.
+;;
+;; * Minor tweaks.
+;;
 ;;
 ;; Version 1.0.1
 ;;
@@ -83,19 +90,27 @@ property. The text is not affected."
 (defcustom hide-region-before-string "@["
   "String to mark the beginning of an invisible region. This string is
 not really placed in the text, it is just shown in the overlay"
-  :type '(string)
+  :type 'string
   :group 'hide-region)
 
 (defcustom hide-region-after-string "]@"
   "String to mark the beginning of an invisible region. This string is
 not really placed in the text, it is just shown in the overlay"
-  :type '(string)
+  :type 'string
   :group 'hide-region)
 
 (defcustom hide-region-propertize-markers t
   "If non-nil, add text properties to the region markers."
   :type 'boolean
   :group 'hide-region)
+
+(defface hide-region-before-string-face
+  '((t (:inherit region)))
+  "Face for the before string.")
+
+(defface hide-region-after-string-face
+  '((t (:inherit region)))
+  "Face for the after string.")
 
 (defvar hide-region-overlays nil
   "Variable to store the regions we put an overlay on.")
@@ -105,30 +120,27 @@ not really placed in the text, it is just shown in the overlay"
 overlay on the hide-region-overlays \"ring\""
   (interactive)
   (let ((new-overlay (make-overlay (mark) (point))))
-    (setq hide-region-overlays
-	  (append
-	   (list new-overlay) hide-region-overlays))
+    (push new-overlay hide-region-overlays)
     (overlay-put new-overlay 'invisible t)
     (overlay-put new-overlay 'intangible t)
     (overlay-put new-overlay 'before-string
                  (if hide-region-propertize-markers
                      (propertize hide-region-before-string
-                                 'font-lock-face 'region)
+                                 'font-lock-face 'hide-region-before-string-face)
                    hide-region-before-string))
     (overlay-put new-overlay 'after-string
                  (if hide-region-propertize-markers
                      (propertize hide-region-after-string
-                                 'font-lock-face 'region)
+                                 'font-lock-face 'hide-region-after-string-face)
                    hide-region-after-string))))
 
 (defun hide-region-unhide ()
   "Unhide a region at a time, starting with the last one hidden and
 deleting the overlay from the hide-region-overlays \"ring\"."
   (interactive)
-  (if (car hide-region-overlays)
-      (progn
-	(delete-overlay (car hide-region-overlays))
-	(setq hide-region-overlays (cdr hide-region-overlays)))))
+  (when (car hide-region-overlays)
+    (delete-overlay (car hide-region-overlays))
+    (setq hide-region-overlays (cdr hide-region-overlays))))
 
 (provide 'hide-region)
 
