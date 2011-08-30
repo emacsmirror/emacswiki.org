72c193a339c1e35d3c7ac0cf687997293b2b8b1e
anybody <anybody@emacswiki.org>
2011-08-30 08:40:20 +0000

    updates

--- buffer-stack-suppl.el
+++ buffer-stack-suppl.el
@@ -22,38 +22,74 @@
 
 ;;; Commentary:
 
-;;
+;; Change buffer-stack default behaviour to only bubble through buffers of the
+;; same major-mode.  Switching between all buffers is done with new *-thru-all
+;; functions.
 
 ;; Put this file into your load-path and the following into your ~/.emacs:
+;;
 ;;   (require 'buffer-stack-suppl)
+;;
+;;   (global-set-key [(f10)] 'buffer-stack-bury)
+;;   (global-set-key [(control f10)] 'buffer-stack-bury-and-kill)
+;;   (global-set-key [(f9)] 'buffer-stack-up)
+;;   (global-set-key [(f11)] 'buffer-stack-down)
+;;   (global-set-key [(shift f10)] 'buffer-stack-bury-thru-all)
+;;   (global-set-key [(shift f9)] 'buffer-stack-up-thru-all)
+;;   (global-set-key [(shift f11)] 'buffer-stack-down-thru-all)
+;;
 
 ;;; Code:
 
-(provide 'buffer-stack-suppl)
 (eval-when-compile
   (require 'cl))
+
+(require 'buffer-stack)
 
 (setq buffer-stack-filter 'buffer-stack-same-major-mode-filter)
 
 
-(defun buffer-stack-same-major-mode-filter (proposed-buffer)
-  "exclusive, valid filename, same (basis) major-mode  filter."
+(defun* buffer-stack-same-major-mode-filter (proposed-buffer)
+  "exclusive, valid filename, same (basis) major-mode  filter.
+
+addition: if the frame has a 'major-mode parameter, filter out
+all buffers that don't match it.
+
+Rinari minor-mode files can show different major mode based on
+context (mumamo).  We group all rinari minor mode buffers
+together.
+
+"
   (and (buffer-stack-filter-exclusive proposed-buffer)
-       (let ((proposed-buffer-major-mode (with-current-buffer proposed-buffer major-mode)))
-	 (or (equal major-mode proposed-buffer-major-mode)
-             ;; use substring search to group these modes
-	     (let((case-fold-search t)
-		  (mode-groups '("lisp" "ruby")))
-               (dolist (mode mode-groups nil)
-                 (when (and
-                        (string-match mode (format "%s" major-mode))
-                        (string-match mode (format "%s" proposed-buffer-major-mode)))
-                   (return t))))))))
+       (let ((proposed-buffer-major-mode (with-current-buffer proposed-buffer major-mode))
+             (frame-major-mode (frame-parameter nil 'major-mode)))
+         (cond (frame-major-mode
+                (if (eq frame-major-mode proposed-buffer-major-mode)
+                    t
+                  nil))
+               ((buffer-available-in-frame-local-p proposed-buffer)
+                nil)
+               ((and (boundp 'rinari-minor-mode)
+                     rinari-minor-mode)
+                (with-current-buffer proposed-buffer
+                  rinari-minor-mode))
+               ((eq major-mode proposed-buffer-major-mode)
+                t)
+               (t
+                ;; use substring search to group these modes
+                (let((case-fold-search t)
+                     (mode-groups '("lisp" "ruby")))
+                  (dolist (mode mode-groups nil)
+                    (when (and
+                           (string-match mode (symbol-name major-mode))
+                           (string-match mode (symbol-name proposed-buffer-major-mode)))
+                      (return t)))))))))
 
 
 (defun buffer-stack-no-compilation-filter (proposed-buffer)
   "specifically removes some major-modes, and specific buffers"
   (and (buffer-stack-filter-exclusive proposed-buffer)
+       (not (buffer-available-in-frame-local-p proposed-buffer))
        (let ((proposed-buffer-major-mode (with-current-buffer proposed-buffer major-mode))
              (skip-modes '(compilation-mode)))
          (not (memq proposed-buffer-major-mode skip-modes)))))
