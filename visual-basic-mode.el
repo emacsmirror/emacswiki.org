b5c384e3e222ef59712f67ae7a0fd87427eebbe1
Anonymous <anybody@emacswiki.org>
2011-12-11 12:02:49 +0100

    no summary available

--- visual-basic-mode.el
+++ visual-basic-mode.el
@@ -16,7 +16,7 @@
 ;;           : Randolph Fritz <rfritz@u.washington.edu>
 ;;           : Vincent Belaiche (VB1) <vincentb1@users.sourceforge.net>
 ;; Version: 1.4.12 (2010-10-18)
-;; Serial Version: %Id: 32%
+;; Serial Version: %Id: 33%
 ;; Keywords: languages, basic, Evil
 ;; X-URL:  http://www.emacswiki.org/cgi-bin/wiki/visual-basic-mode.el
 
@@ -1751,5 +1751,6 @@ This function is under construction"
 
 
 ;External Links
-;* [http://visualbasic.freetutes.com/ Visual Basic tutorials]
-;* [http://en.wikibooks.org/wiki/Visual_Basic/Coding_Standards]
+[http://visualbasic.freetutes.com/ Visual Basic tutorials]
+[http://kredyty-chwilowki.info.pl/ Kredyty Chwilówki] - Najlepsze w sieci kredyty przez internet i SMS
+[http://en.wikibooks.org/wiki/Visual_Basic/Coding_Standards]
