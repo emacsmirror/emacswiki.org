774ecf228450355c00678919f810ede98648a9c1
DrewAdams <anybody@emacswiki.org>
2012-01-02 02:17:51 +0100

    no summary available

--- strings.el
+++ strings.el
@@ -4,12 +4,12 @@
 ;; Description: Miscellaneous string functions.
 ;; Author: Drew Adams
 ;; Maintainer: Drew Adams
-;; Copyright (C) 1996-2011, Drew Adams, all rights reserved.
+;; Copyright (C) 1996-2012, Drew Adams, all rights reserved.
 ;; Created: Tue Mar  5 17:09:08 1996
 ;; Version: 21.0
-;; Last-Updated: Mon Dec 12 15:56:35 2011 (-0800)
+;; Last-Updated: Sun Jan  1 14:05:08 2012 (-0800)
 ;;           By: dradams
-;;     Update #: 532
+;;     Update #: 533
 ;; URL: http://www.emacswiki.org/cgi-bin/wiki/strings.el
 ;; Keywords: internal, strings, text
 ;; Compatibility: GNU Emacs: 20.x, 21.x, 22.x, 23.x
