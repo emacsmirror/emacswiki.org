22c00c1a60d4ae3fab0511edaf78bde964a45a9e
DrewAdams <anybody@emacswiki.org>
2012-01-02 02:31:22 +0100

    no summary available

--- unaccent.el
+++ unaccent.el
@@ -4,12 +4,12 @@
 ;; Description: Functions dealing with accented characters.
 ;; Author: Drew Adams
 ;; Maintainer: Drew Adams
-;; Copyright (C) 1999-2011, Drew Adams, all rights reserved.
+;; Copyright (C) 1999-2012, Drew Adams, all rights reserved.
 ;; Created: Fri Sep  3 11:02:14 1999
 ;; Version: 20.0
-;; Last-Updated: Tue Jan  4 14:50:09 2011 (-0800)
+;; Last-Updated: Sun Jan  1 14:05:07 2012 (-0800)
 ;;           By: dradams
-;;     Update #: 55
+;;     Update #: 56
 ;; URL: http://www.emacswiki.org/cgi-bin/wiki/unaccent.el
 ;; Keywords: i18n, language, accents
 ;; Compatibility: GNU Emacs 20.x
