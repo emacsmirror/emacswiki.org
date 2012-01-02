d72199a2f666738bb918e0713d3bc95528a5d7fb
DrewAdams <anybody@emacswiki.org>
2012-01-02 01:20:02 +0100

    no summary available

--- icicles-opt.el
+++ icicles-opt.el
@@ -4,12 +4,12 @@
 ;; Description: User options (variables) for Icicles
 ;; Author: Drew Adams
 ;; Maintainer: Drew Adams
-;; Copyright (C) 1996-2011, Drew Adams, all rights reserved.
+;; Copyright (C) 1996-2012, Drew Adams, all rights reserved.
 ;; Created: Mon Feb 27 09:22:14 2006
 ;; Version: 22.0
-;; Last-Updated: Wed Dec 28 10:49:38 2011 (-0800)
+;; Last-Updated: Sun Jan  1 14:05:16 2012 (-0800)
 ;;           By: dradams
-;;     Update #: 4750
+;;     Update #: 4751
 ;; URL: http://www.emacswiki.org/cgi-bin/wiki/icicles-opt.el
 ;; Keywords: internal, extensions, help, abbrev, local, minibuffer,
 ;;           keys, apropos, completion, matching, regexp, command
