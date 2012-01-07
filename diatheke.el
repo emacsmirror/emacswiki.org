580dbce242870db204f3650682b233b3dd39657c
dat <anybody@emacswiki.org>
2012-01-07 16:42:00 +0100

    Code snippet to grab a Bible verse using diatheke

---
+++ diatheke.el
@@ -0,0 +1,14 @@
+;;; diatheke.el
+
+;; DTK: grab and insert, into current buffer, a Bible verse
+;; - ensure diatheke is installed
+(defun dtk ()
+  (interactive)
+  (let* ((book
+	 (minibuffer-with-setup-hook 'minibuffer-complete 
+	   (completing-read (concat "Book: ")
+			    '("Ge" "Ex" "Le"))))
+	(ch (read-from-minibuffer "Ch: "))
+	(vs (read-from-minibuffer "Vs: "))
+	(ch-vs (concat ch ":" vs))) 
+    (call-process "diatheke" nil (current-buffer) t "-b" "KJV" "-k" book ch-vs)))
