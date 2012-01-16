331499e44fbd20ef13de9aac9e620a846be4190e
JochenHayek <anybody@emacswiki.org>
2012-01-16 13:36:19 +0100

    added note on Blogger.com

--- weblogger.el
+++ weblogger.el
@@ -1,1 +1,5 @@
 Please get a current version from https://launchpad.net/weblogger-el
+
+This software is not able to deal with blogs on Blogger.com,
+as you can read on this thread:
+http://thread.gmane.org/gmane.emacs.blogging/76
