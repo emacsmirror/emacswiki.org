e8401af8aaaa513dcd7c4fafb075405c02c205fa
JonasBernoulli <anybody@emacswiki.org>
2012-01-27 21:27:30 +0100

    fix/add header line; remove trailing whitespace

--- anything-adaptive.el
+++ anything-adaptive.el
@@ -1,9 +1,9 @@
-;;; anything-adaptive.el --- 
+;;; anything-adaptive.el --- Sort candidates by usage frequency
 
-;; Copyright (C) 2007  
+;; Copyright (C) 2007
 
 ;; Author:  Tamas Patrovics
-;; Keywords: 
+;; Keywords:
 
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -27,7 +27,7 @@
 (defvar anything-adaptive-history-file "~/.anything_adaptive_history"
   "Path of file where history information is stored.")
 
-(defvar anything-adaptive-history-length 50 
+(defvar anything-adaptive-history-length 50
   "Max number of candidates stored for a source.")
 
 
@@ -42,39 +42,39 @@
   "Contains the stored history information.
 Format: ((SOURCE-NAME (SELECTED-CANDIDATE (PATTERN . NUMBER-OF-USE) ...) ...) ...)")
 
-(defadvice anything-initialize (before anything-adaptive-initialize activate) 
+(defadvice anything-initialize (before anything-adaptive-initialize activate)
   "Advise `anything-initialize' to reset `anything-adaptive-done'
 when anything is started."
   (setq anything-adaptive-done nil))
 
-(defadvice anything-exit-minibuffer (before anything-adaptive-exit-minibuffer activate) 
+(defadvice anything-exit-minibuffer (before anything-adaptive-exit-minibuffer activate)
   "Advise `anything-exit-minibuffer' to store history information
 when a candidate is selected with ENTER."
-  (anything-adaptive-store-selection)) 
+  (anything-adaptive-store-selection))
 
-(defadvice anything-select-action (before anything-adaptive-select-action activate) 
+(defadvice anything-select-action (before anything-adaptive-select-action activate)
   "Advise `anything-select-action' to store history information
 when the user goes to the action list with TAB."
-  (anything-adaptive-store-selection)) 
+  (anything-adaptive-store-selection))
 
-(defun anything-adaptive-store-selection () 
+(defun anything-adaptive-store-selection ()
   "Store history information for the selected candidate."
-  (unless anything-adaptive-done 
-    (setq anything-adaptive-done t) 
+  (unless anything-adaptive-done
+    (setq anything-adaptive-done t)
     (let* ((source (anything-get-current-source))
            (source-name (or (assoc-default 'type source)
                             (assoc-default 'name source)))
-           (source-info (or (assoc source-name anything-adaptive-history) 
-                            (progn 
-                              (push (list source-name) anything-adaptive-history) 
-                              (car anything-adaptive-history)))) 
-           (selection (anything-get-selection)) 
+           (source-info (or (assoc source-name anything-adaptive-history)
+                            (progn
+                              (push (list source-name) anything-adaptive-history)
+                              (car anything-adaptive-history))))
+           (selection (anything-get-selection))
            (selection-info (progn
                              (setcdr source-info
-                                     (cons 
+                                     (cons
                                       (let ((found (assoc selection (cdr source-info))))
                                         (if (not found)
-                                            ;; new entry 
+                                            ;; new entry
                                             (list selection)
 
                                           ;; move entry to the
@@ -82,15 +82,15 @@ when the user goes to the action list with TAB."
                                           ;; that it doesn't get
                                           ;; trimmed when the history
                                           ;; is truncated
-                                          (setcdr source-info 
+                                          (setcdr source-info
                                                   (delete found (cdr source-info)))
                                           found))
                                       (cdr source-info)))
                              (cadr source-info)))
            (pattern-info (progn
                            (setcdr selection-info
-                                   (cons 
-                                    (let ((found (assoc anything-pattern (cdr selection-info)))) 
+                                   (cons
+                                    (let ((found (assoc anything-pattern (cdr selection-info))))
                                       (if (not found)
                                           ;; new entry
                                           (cons anything-pattern 0)
@@ -101,59 +101,59 @@ when the user goes to the action list with TAB."
                                         ;; number of times then the one
                                         ;; used last appears first in
                                         ;; the list
-                                        (setcdr selection-info 
+                                        (setcdr selection-info
                                                 (delete found (cdr selection-info)))
                                         found))
                                     (cdr selection-info)))
                            (cadr selection-info))))
 
       ;; increase usage count
-      (setcdr pattern-info (1+ (cdr pattern-info))) 
+      (setcdr pattern-info (1+ (cdr pattern-info)))
 
       ;; truncate hsitory if needed
-      (if (> (length (cdr selection-info)) anything-adaptive-history-length) 
-          (setcdr selection-info 
-                  (subseq (cdr selection-info) 0 anything-adaptive-history-length)))))) 
+      (if (> (length (cdr selection-info)) anything-adaptive-history-length)
+          (setcdr selection-info
+                  (subseq (cdr selection-info) 0 anything-adaptive-history-length))))))
 
 
 (if (file-readable-p anything-adaptive-history-file)
     (load-file anything-adaptive-history-file))
-(add-hook 'kill-emacs-hook 'anything-adaptive-save-history) 
+(add-hook 'kill-emacs-hook 'anything-adaptive-save-history)
 
 
-(defun anything-adaptive-save-history () 
+(defun anything-adaptive-save-history ()
   "Save history information to file pointed by
 `anything-adaptive-save-history'."
-  (interactive) 
-  (with-temp-buffer 
-    (insert 
-     ";; -*- mode: emacs-lisp -*-\n" 
-     ";; History entries used for anything adaptive display.\n") 
-    (prin1 `(setq anything-adaptive-history ',anything-adaptive-history) 
-           (current-buffer)) 
-    (insert ?\n) 
-    (write-region (point-min) (point-max) anything-adaptive-history-file nil 
-                  (unless (interactive-p) 'quiet)))) 
+  (interactive)
+  (with-temp-buffer
+    (insert
+     ";; -*- mode: emacs-lisp -*-\n"
+     ";; History entries used for anything adaptive display.\n")
+    (prin1 `(setq anything-adaptive-history ',anything-adaptive-history)
+           (current-buffer))
+    (insert ?\n)
+    (write-region (point-min) (point-max) anything-adaptive-history-file nil
+                  (unless (interactive-p) 'quiet))))
 
 
-(defun anything-adaptive-sort (candidates source) 
+(defun anything-adaptive-sort (candidates source)
   "Sort the CANDIDATES for SOURCE by usage frequency."
   (let* ((source-name (or (assoc-default 'type source)
                           (assoc-default 'name source)))
-         (source-info (assoc source-name anything-adaptive-history))) 
+         (source-info (assoc source-name anything-adaptive-history)))
     (if (not source-info)
         ;; if there is no information is stored for this source then
         ;; do nothing
         candidates
-      
-      (let ((usage 
+
+      (let ((usage
              ;; assemble a list containing the (CANIDATE . USAGE-COUNT) pairs
-             (mapcar (lambda (candidate-info) 
-                       (let ((count 0)) 
-                         (dolist (pattern-info (cdr candidate-info)) 
-                           (if (not (equal (car pattern-info) 
-                                           anything-pattern)) 
-                               (incf count (cdr pattern-info)) 
+             (mapcar (lambda (candidate-info)
+                       (let ((count 0))
+                         (dolist (pattern-info (cdr candidate-info))
+                           (if (not (equal (car pattern-info)
+                                           anything-pattern))
+                               (incf count (cdr pattern-info))
 
                              ;; if current pattern is equal to the
                              ;; previously used one then this
@@ -162,38 +162,38 @@ when the user goes to the action list with TAB."
                              ;; only has to compete with other
                              ;; candidates which were also selected
                              ;; with the same pattern
-                             (setq count (+ 10000 (cdr pattern-info))) 
-                             (return))) 
-                         (cons (car candidate-info) count))) 
-                     (cdr source-info))) 
-            sorted) 
+                             (setq count (+ 10000 (cdr pattern-info)))
+                             (return)))
+                         (cons (car candidate-info) count)))
+                     (cdr source-info)))
+            sorted)
 
         ;; sort the list in descending order, so candidates with
         ;; highest priorty come first
-        (setq usage (sort usage (lambda (first second) 
-                                  (> (cdr first) (cdr second))))) 
+        (setq usage (sort usage (lambda (first second)
+                                  (> (cdr first) (cdr second)))))
 
         ;; put those candidates first which have the highest usage
         ;; count
-        (dolist (info usage) 
-          (when (member* (car info) candidates 
-                         :test 'anything-adaptive-compare) 
-            (push (car info) sorted) 
-            (setq candidates (remove* (car info) candidates 
-                                      :test 'anything-adaptive-compare)))) 
+        (dolist (info usage)
+          (when (member* (car info) candidates
+                         :test 'anything-adaptive-compare)
+            (push (car info) sorted)
+            (setq candidates (remove* (car info) candidates
+                                      :test 'anything-adaptive-compare))))
 
         ;; and append the rest
-        (append (reverse sorted) candidates nil))))) 
+        (append (reverse sorted) candidates nil)))))
 
 
-(defun anything-adaptive-compare (x y) 
+(defun anything-adaptive-compare (x y)
   ;; Compare candidates X and Y taking into account that the candidate
   ;; can be in (DISPLAY . REAL) format.
-  (equal (if (listp x) 
-             (cdr x) 
-           x) 
-         (if (listp y) 
-             (cdr y) 
+  (equal (if (listp x)
+             (cdr x)
+           x)
+         (if (listp y)
+             (cdr y)
            y)))
 
 
