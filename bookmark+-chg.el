97efae89293ffc3c0d6306bbbca3c30137966c78
DrewAdams <anybody@emacswiki.org>
2012-02-08 19:48:13 +0100

    no summary available

--- bookmark+-chg.el
+++ bookmark+-chg.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
 ;; Copyright (C) 2000-2012, Drew Adams, all rights reserved.
 ;; Created: Fri Sep 15 07:58:41 2000
-;; Last-Updated: Tue Feb  7 16:19:04 2012 (-0800)
+;; Last-Updated: Wed Feb  8 10:41:03 2012 (-0800)
 ;;           By: dradams
-;;     Update #: 14247
+;;     Update #: 14248
 ;; URL: http://www.emacswiki.org/cgi-bin/wiki/bookmark+-chg.el
 ;; Keywords: bookmarks, bookmark+
 ;; Compatibility: GNU Emacs: 20.x, 21.x, 22.x, 23.x
@@ -145,6 +145,8 @@
  
 ;;;(@* "CHANGE LOG FOR `bookmark+-1.el'")
 ;;
+;; 2012/02/08 dadams
+;;     bmkp-remove-dups: Redefined to use a hash table.
 ;; 2012/02/07 dadams
 ;;     bmkp-same-file-p: Test compare-strings result using t, not non-nil.  Thx to Michael Heerdegen.
 ;; 2012/02/04 dadams
