08703c9a3e879d4365de3a3e249fb025d74045d3
Joseph <anybody@emacswiki.org>
2012-02-10 04:52:48 +0100

    add erlang-compile-dwim

--- erlang-dired-mode.el
+++ erlang-dired-mode.el
@@ -2,7 +2,7 @@
 
 ;; Description: erlang dired mode
 ;; Created: 2011-12-20 22:41
-;; Last Updated: Joseph 2011-12-20 23:11:15 星期二
+;; Last Updated: Joseph 2012-02-10 11:50:04 金曜日
 ;; Author: Joseph(纪秀峰)  jixiuf@gmail.com
 ;; Maintainer:  Joseph(纪秀峰)  jixiuf@gmail.com
 ;; Keywords: erlang dired Emakefile
@@ -26,16 +26,29 @@
 
 ;;; Commentary:
 
+;; when you open dired mode buffer ,if a Emakefile are in current or parent
+;; directory recursively , then `erlang-dired-mode' minor mode is enabled.
 ;;
+;;  I write a function named `erlang-create-project' ,it will create a Emakefile
+;;  and a Makefile(copide from mochiweb.) and erlang standard project .
 
+;; `erlang-compile-dwim'
+;;  if Emakefile exists in project root directory ,call "make:all[load]"
+;;  if not call `erlang-compile'
+;;  with  prefix `C-u':
+;;  if Makefile exists in project root directory ,all make --directory project-root-directory
+;;  if not call default make command (maybe ks "make -k")
+;;
 ;;; Commands:
 ;;
 ;; Below are complete command list:
 ;;
-;;  `inferior-erlang-emake'
-;;    run make:all(load) in project root of erlang application.
-;;  `erlang-dired-emake'
-;;    Compile Erlang module in current buffer.
+;;  `erlang-emake'
+;;    run make:all(load) in project root of erlang application,if Emakefile doesn't exists ,call `erlang-compile' instead
+;;  `erlang-make'
+;;    run make command at project root directory
+;;  `erlang-compile-dwim'
+;;    call `erlang-emake', if with prefix `C-u' then run call `erlang-make'.
 ;;  `erlang-dired-mode'
 ;;    Erlang application development minor mode.
 ;;
@@ -69,53 +82,142 @@
         (require 'tempo)
         (require 'compile))))
 
-(defun erlang-root ()
+;;;###autoload
+(defun erlang-create-project(root-dir)
+  (interactive "Fselect a directory to create erlang project")
+  (unless (file-exists-p root-dir)
+    (make-directory root-dir))
+  (unless (file-directory-p root-dir)
+    (error "%s is not a directory!"  root-dir))
+  (make-directory (expand-file-name  "src"  root-dir) t)
+  (make-directory (expand-file-name  "include" root-dir) t)
+  (make-directory (expand-file-name "ebin" root-dir) t)
+  (make-directory (expand-file-name  "test" root-dir) t)
+  (make-directory (expand-file-name  "deps" root-dir) t)
+  (make-directory (expand-file-name  "priv" root-dir) t)
+  (unless (or (file-exists-p (expand-file-name "Emakefile" root-dir))
+              (file-exists-p  (expand-file-name "Emakefile" root-dir)))
+    (with-temp-file (expand-file-name "Emakefile" root-dir)
+      (insert "{ 'src/*', [debug_info, {i ,\"include/\"} ,{outdir,\"ebin/\"}] }.\n")
+      (insert "{ 'test/*', [debug_info, {i ,\"src/\"}, {i ,\"include/\"}, {outdir,\"ebin\"} ] }.\n")
+      ))
+
+  (unless (or (file-exists-p (expand-file-name "makefile" root-dir))
+              (file-exists-p  (expand-file-name "Makefile" root-dir)))
+    (with-temp-file (expand-file-name "Makefile" root-dir)
+      (insert "PREFIX:=../\n")
+      (insert "DEST:=$(PREFIX)$(PROJECT)\n")
+      (insert "REBAR=./rebar\n")
+      (insert "\n")
+      (insert "all:\n")
+      (insert "	@$(REBAR) get-deps compile\n")
+      (insert "edoc:\n")
+      (insert "	@$(REBAR) doc\n")
+      (insert "test:\n")
+      (insert "	@rm -rf .eunit\n")
+      (insert "	@mkdir -p .eunit\n")
+      (insert "	@$(REBAR) skip_deps=true eunit\n")
+      (insert "clean:\n")
+      (insert "	@$(REBAR) clean\n")
+      (insert "build_plt:\n")
+      (insert "	@$(REBAR) build-plt\n")
+      (insert "dialyzer:\n")
+      (insert "	@$(REBAR) dialyze\n")
+      (insert "app:\n")
+      (insert "	@$(REBAR) create template=mochiwebapp dest=$(DEST) appid=$(PROJECT)\n")
+      )
+    )
+  (dired root-dir)
+  )
+
+(defun erlang-root (&optional Emakefile)
   "Look for Emakefile file to find project root of erlang application."
-  (locate-dominating-file default-directory "Emakefile"))
+  (let ((erlang-root (locate-dominating-file default-directory (or Emakefile  "Emakefile"))))
+    (if erlang-root
+        (expand-file-name erlang-root)
+      nil)))
 
-(defun inferior-erlang-emake (arg)
-  "run make:all(load) in project root of erlang application."
+(defun erlang-root-by-makefile ()
+  "Look for Makefile file to find project root of erlang application.
+if found return the directory or nil
+"
+  (let ((erlang-root (locate-dominating-file default-directory "Makefile")))
+    (if erlang-root
+        (expand-file-name erlang-root)
+      (setq erlang-root (locate-dominating-file default-directory "makefile"))
+      (if erlang-root
+          (expand-file-name erlang-root)
+        nil
+        ))))
+
+;;;###autoload
+(defun erlang-emake (arg)
+  "run make:all(load) in project root of erlang application,if Emakefile doesn't exists ,call `erlang-compile' instead"
   (interactive "P")
-  (save-some-buffers)
-  (inferior-erlang-prepare-for-input)
-  (let* ((default-dir default-directory)
-         end)
-    (with-current-buffer inferior-erlang-buffer
-      (compilation-forget-errors))
+  (let ((project-root (erlang-root)))
+    (if (not project-root)
+        (call-interactively 'erlang-compile)
+      (save-some-buffers)
+      (inferior-erlang-prepare-for-input)
+      (let* (end)
+        (with-current-buffer inferior-erlang-buffer
+          (compilation-forget-errors))
 
-    (inferior-erlang-send-command
-     (format "cd(\"%s\")." ( erlang-root)) nil)
-    (sit-for 0)
-    (inferior-erlang-wait-prompt)
+        (inferior-erlang-send-command
+         (format "cd(\"%s\")." project-root) nil)
+        (sit-for 0)
+        (inferior-erlang-wait-prompt)
 
-    (inferior-erlang-send-command
-     "make:all([load])." nil)
-    (sit-for 0)
-    (inferior-erlang-wait-prompt)
+        (inferior-erlang-send-command
+         "make:all([load])." nil)
+        (sit-for 0)
+        (inferior-erlang-wait-prompt)
 
-    (setq end (inferior-erlang-send-command
-               (format "cd(\"%s\")." default-dir) nil))
-    (sit-for 0)
-    (inferior-erlang-wait-prompt)
-    (with-current-buffer inferior-erlang-buffer
-      (setq compilation-error-list nil)
-      (set-marker compilation-parsing-end end))
-    (setq compilation-last-buffer inferior-erlang-buffer)))
+        (setq end (inferior-erlang-send-command
+                   "cd(\"ebin/\")." nil))
+        (sit-for 0)
+        (inferior-erlang-wait-prompt)
+        (with-current-buffer inferior-erlang-buffer
+          (setq compilation-error-list nil)
+          (set-marker compilation-parsing-end end))
+        (setq compilation-last-buffer inferior-erlang-buffer))
 
+      )
+    )
+  )
 
-(defun erlang-dired-emake ()
-  "Compile Erlang module in current buffer."
+;;;###autoload
+(defun erlang-make()
+  "run make command at project root directory"
   (interactive)
-  (call-interactively 'inferior-erlang-emake))
+  (let ((project-root ( erlang-root-by-makefile))
+        (compile-command compile-command))
+    (when project-root
+      (setq compile-command (concat "make --directory=" project-root))
+      )
+    (call-interactively 'compile)
+    )
+  )
+
+;;;###autoload
+(defun erlang-compile-dwim(&optional arg)
+  "call `erlang-emake', if with prefix `C-u' then run call `erlang-make'."
+  (interactive "P")
+  (if arg
+      (call-interactively 'erlang-make)      ;`C-u'
+    (call-interactively ' erlang-emake)
+    )
+  )
 
 (defvar erlang-dired-mode-map
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "C-c C-z") 'erlang-shell-display)
     (define-key map (kbd "C-c C-l") 'erlang-compile-display)
-    (define-key map (kbd "C-c C-k") 'erlang-dired-emake) ;run make:all([load])
+    (define-key map (kbd "C-c C-k") 'erlang-compile-dwim) ;run make:all([load])
+    (define-key map (kbd "C-c C-p") 'erlang-create-project) ; you should bind this fun
     map))
 
-;; (define-key erlang-dired-mode-map (kbd "C-z s") 'erlang-dired-emake)
+;; (define-key erlang-dired-mode-map (kbd "C-z s") 'erlang-emake)
 ;;;###autoload
 (define-minor-mode erlang-dired-mode
   "Erlang application development minor mode."
@@ -125,10 +227,19 @@
 
 ;;;###autoload
 (defun erlang-dired-mode-fun()
-  (when (erlang-root) (erlang-dired-mode t)))
+  (let ((erlang-project-root-dir (erlang-root)))
+    (when erlang-project-root-dir  (erlang-dired-mode t))
+    )
+  )
 
 ;;;###autoload
 (add-hook 'dired-mode-hook 'erlang-dired-mode-fun)
+
+;;;###autoload
+(defun erlang-mode-hook-1()
+  (define-key erlang-mode-map (kbd "C-c C-k") 'erlang-compile-dwim) ;C-cC-k
+  )
+(add-hook 'erlang-mode-hook 'erlang-mode-hook-1)
 
 (provide 'erlang-dired-mode)
 ;;; erlang-dired.el ends here
