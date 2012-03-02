4662e7843ac3c38e9404e433555eb30520aa6f4e
DrewAdams <anybody@emacswiki.org>
2012-03-02 17:31:08 +0100

    no summary available

--- misc-fns.el
+++ misc-fns.el
@@ -7,9 +7,9 @@
 ;; Copyright (C) 1996-2012, Drew Adams, all rights reserved.
 ;; Created: Tue Mar  5 17:21:28 1996
 ;; Version: 21.0
-;; Last-Updated: Fri Mar  2 08:01:03 2012 (-0800)
+;; Last-Updated: Fri Mar  2 08:30:24 2012 (-0800)
 ;;           By: dradams
-;;     Update #: 580
+;;     Update #: 581
 ;; URL: http://www.emacswiki.org/cgi-bin/wiki/misc-fns.el
 ;; Keywords: internal, unix, lisp, extensions, local
 ;; Compatibility: GNU Emacs: 20.x, 21.x, 22.x, 23.x
@@ -159,8 +159,7 @@
 ;;
 ;;; Code:
 
-(eval-when-compile (when (< emacs-major-version 21) (require 'cl)))
- ;; dolist, push, pop
+(eval-when-compile (when (< emacs-major-version 21) (require 'cl))) ;; dolist
 
 ;;;;;;;;;;;;;;;;;;;;;
 
