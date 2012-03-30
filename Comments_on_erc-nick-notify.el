75ec2ac4728e65d8dfcff125f6db462bb8900dd6
technomancy <anybody@emacswiki.org>
2012-03-31 00:42:42 +0200

    Sorry, I deleted this earlier because I noticed that the version on Marmalade has a vulnerability, but I didn't check to see whether the one here was . . .

---
+++ Comments_on_erc-nick-notify.el
@@ -0,0 +1,4 @@
+Sorry, I deleted this earlier because I noticed that the version on Marmalade has a vulnerability, but I didn't check to see whether the one here was fixed.
+
+-- technomancy 2012-03-30 22:42 UTC
+
