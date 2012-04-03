4de0dd9ce4fe03ec9f38a95b339279d305f4649e
AlexSchroeder <anybody@emacswiki.org>
2012-04-03 10:07:22 +0200

    no summary available

--- Comments_on_any-ini-mode.el
+++ Comments_on_any-ini-mode.el
@@ -1,9 +1,5 @@
 in xemacs this mode dies with the error 
 menu name (first element) must be a string: (any-ini-imenu-name:filter imenu-menu-filter)
 
-----
-
-Add your comment here.
-
 -- jistanidiot 2012-04-02 16:07 UTC
 
