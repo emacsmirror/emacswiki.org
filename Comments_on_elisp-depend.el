1a6a4c514278fbbc764512862336ecb75dcd16e1
DrewAdams <anybody@emacswiki.org>
2012-04-27 08:27:57 +0200

    IMO it's not a good idea to upload the file instead of pasting its text. Why? Because clicking Diff is useless in that case.  I click Diff to see . . .

---
+++ Comments_on_elisp-depend.el
@@ -0,0 +1,4 @@
+IMO it's not a good idea to upload the file instead of pasting its text. Why? Because clicking Diff is useless in that case.  I click Diff to see what you changed (even just to see if the change is spam), and all I see is the file name as the diff content.
+
+-- DrewAdams 2012-04-27 06:27 UTC
+
