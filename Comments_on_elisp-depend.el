bf7adc57920ac55c09a310d077a39be96774f117
DrewAdams <anybody@emacswiki.org>
2012-04-27 08:34:34 +0200

    The header says the file is compatible with GnuEmacs versions 20-23, but it is no longer compatible with GnuEmacs 20 or 21 because you use functions . . .

--- Comments_on_elisp-depend.el
+++ Comments_on_elisp-depend.el
@@ -2,3 +2,10 @@ IMO it's not a good idea to upload the file instead of pasting its text. Why? Be
 
 -- DrewAdams 2012-04-27 06:27 UTC
 
+
+----
+
+The header says the file is compatible with GnuEmacs versions 20-23, but it is no longer compatible with GnuEmacs 20 or 21 because you use functions `propertize' and `delete-dups'.  You can easily forego using these two functions and make it once again compatible with the older versions.
+
+-- DrewAdams 2012-04-27 06:34 UTC
+
