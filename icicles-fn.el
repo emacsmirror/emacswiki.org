7b1eaedfe4c3eaff7c6f352fb2bb6d8cf83c8537
DrewAdams <anybody@emacswiki.org>
2012-05-13 19:23:47 +0200

    no summary available

--- icicles-fn.el
+++ icicles-fn.el
@@ -7,9 +7,9 @@
 ;; Copyright (C) 1996-2012, Drew Adams, all rights reserved.
 ;; Created: Mon Feb 27 09:25:53 2006
 ;; Version: 22.0
-;; Last-Updated: Mon Apr 30 21:47:32 2012 (-0700)
+;; Last-Updated: Sun May 13 10:19:09 2012 (-0700)
 ;;           By: dradams
-;;     Update #: 12990
+;;     Update #: 12996
 ;; URL: http://www.emacswiki.org/cgi-bin/wiki/icicles-fn.el
 ;; Keywords: internal, extensions, help, abbrev, local, minibuffer,
 ;;           keys, apropos, completion, matching, regexp, command
@@ -545,9 +545,10 @@ so it is called after completion-list buffer text is written."
     (save-excursion
       (let* ((mainbuf        (current-buffer))
              (mbuf-contents  (icicle-input-from-minibuffer))
-             ;; $$$$$ Should we `expand-file-name' mbuf-contents first?
              (dir-of-input   (and minibuffer-completing-file-name
-                                  (icicle-file-name-directory mbuf-contents))))
+                                  (icicle-file-name-directory
+                                   (file-name-as-directory
+                                    (expand-file-name (substitute-in-file-name mbuf-contents)))))))
         ;; If reading file name and either `icicle-comp-base-is-default-dir-p' is nil or this is a
         ;; completion command, then set `default-directory' so it will be copied into `*Completions*'.
         (when (and dir-of-input
@@ -580,9 +581,10 @@ so it is called after completion-list buffer text is written."
     (save-excursion
       (let* ((mainbuf        (current-buffer))
              (mbuf-contents  (minibuffer-completion-contents)) ; Get contents only up to point.
-             ;; $$$$$ Should we `expand-file-name' mbuf-contents first?  Vanilla Emacs does that.
              (dir-of-input   (and minibuffer-completing-file-name
-                                  (icicle-file-name-directory mbuf-contents)))
+                                  (icicle-file-name-directory
+                                   (file-name-as-directory
+                                    (expand-file-name (substitute-in-file-name mbuf-contents))))))
              common-string-length)
         ;; If reading file name and either `icicle-comp-base-is-default-dir-p' is nil or this is a
         ;; completion command, then set `default-directory' so it will be copied into `*Completions*'.
@@ -636,13 +638,14 @@ so it is called after completion-list buffer text is written."
   (defun icicle-completion-setup-function ()
     "Set up for completion.  This goes in `completion-setup-hook'
 so it is called after completion-list buffer text is written."
-    ;; I could probably get rid of even more of the vanilla vestiges here...
+    ;; I could perhaps get rid of even more of the vanilla vestiges here...
     (save-excursion
-      (let* ((mainbuf        (current-buffer))
-             (mbuf-contents  (minibuffer-completion-contents)) ; Get contents only up to point.
-             ;; $$$$$ Should we `expand-file-name' mbuf-contents first?  Vanilla Emacs does that.
-             (dir-of-input   (and minibuffer-completing-file-name
-                                  (icicle-file-name-directory mbuf-contents))))
+      (let ((mainbuf       (current-buffer))
+            (dir-of-input  (and minibuffer-completing-file-name
+                                (icicle-file-name-directory
+                                 (file-name-as-directory
+                                  (expand-file-name
+                                   (substitute-in-file-name (minibuffer-completion-contents))))))))
         ;; If reading file name and either `icicle-comp-base-is-default-dir-p' is nil or this is a
         ;; completion command, then set `default-directory' so it will be copied into `*Completions*'.
         (when (and dir-of-input
