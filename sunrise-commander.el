cdba23b6ea324f2100981fbd94d2f68be87fcd31
José Alfredo Romero L <anybody@emacswiki.org>
2012-06-05 21:46:33 +0200

    +5R420 (bugfix: make sr-lock-window a bit more resilient -- thanks John Wiegley)

--- sunrise-commander.el
+++ sunrise-commander.el
@@ -7,7 +7,7 @@
 ;; Maintainer: José Alfredo Romero L. <escherdragon@gmail.com>
 ;; Created: 24 Sep 2007
 ;; Version: 5
-;; RCS Version: $Rev: 419 $
+;; RCS Version: $Rev: 420 $
 ;; Keywords: files, dired, midnight commander, norton, orthodox
 ;; URL: http://www.emacswiki.org/emacs/sunrise-commander.el
 ;; Compatibility: GNU Emacs 22+
@@ -1327,24 +1327,26 @@ buffer or window."
 (defun sr-lock-window (frame)
   "Resize the left Sunrise pane to have the \"right\" size."
   (when sr-running
-    (let ((sr-windows-locked sr-windows-locked))
-      (when (> window-min-height (- (frame-height)
-                                    (window-height sr-left-window)))
-        (setq sr-windows-locked nil))
-      (and sr-windows-locked
-           (not sr-ediff-on)
-           (not (eq sr-window-split-style 'vertical))
-           (window-live-p sr-left-window)
-           (save-selected-window
-             (select-window sr-left-window)
-             (let* ((my-style-factor
-                     (if (eq sr-window-split-style 'horizontal) 2 1))
-                    (my-delta (- sr-panes-height (window-height))))
-               (enlarge-window my-delta))
-             (scroll-right)
-             (when (window-live-p sr-right-window)
-               (select-window sr-right-window)
-               (scroll-right)))))))
+    (if (not (window-live-p sr-left-window))
+        (setq sr-running nil)
+      (let ((sr-windows-locked sr-windows-locked))
+        (when (> window-min-height (- (frame-height)
+                                      (window-height sr-left-window)))
+          (setq sr-windows-locked nil))
+        (and sr-windows-locked
+             (not sr-ediff-on)
+             (not (eq sr-window-split-style 'vertical))
+             (window-live-p sr-left-window)
+             (save-selected-window
+               (select-window sr-left-window)
+               (let* ((my-style-factor
+                       (if (eq sr-window-split-style 'horizontal) 2 1))
+                      (my-delta (- sr-panes-height (window-height))))
+                 (enlarge-window my-delta))
+               (scroll-right)
+               (when (window-live-p sr-right-window)
+                 (select-window sr-right-window)
+                 (scroll-right))))))))
 
 ;; This keeps the size of the Sunrise panes constant:
 (add-hook 'window-size-change-functions 'sr-lock-window)
