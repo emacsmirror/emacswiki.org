55c5ef9be24d43ce7c092be014c18d52e7a2a8e5
DrewAdams <anybody@emacswiki.org>
2012-06-13 20:37:55 +0200

    no summary available

--- bookmark+-chg.el
+++ bookmark+-chg.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
 ;; Copyright (C) 2000-2012, Drew Adams, all rights reserved.
 ;; Created: Fri Sep 15 07:58:41 2000
-;; Last-Updated: Tue Jun 12 15:50:47 2012 (-0700)
+;; Last-Updated: Wed Jun 13 11:29:50 2012 (-0700)
 ;;           By: dradams
-;;     Update #: 14951
+;;     Update #: 14955
 ;; URL: http://www.emacswiki.org/cgi-bin/wiki/bookmark+-chg.el
 ;; Keywords: bookmarks, bookmark+
 ;; Compatibility: GNU Emacs: 20.x, 21.x, 22.x, 23.x
@@ -786,6 +786,10 @@
  
 ;;;(@* "CHANGE LOG FOR `bookmark+-bmu.el'")
 ;;
+;; 2012/06/13 dadams
+;;     bookmark-bmenu-bookmark: forward-char bmkp-bmenu-marks-width, not 1+ that.
+;;     bmkp-bmenu-propertize-item: Allow a file bookmark to get buffer prop if file not yet saved.
+;;                                 Do not require buffer-name attribute for bmkp-non-file face.
 ;; 2012/06/11 dadams
 ;;     bmkp-bmenu-propertize-item: Use bmkp-non-file-filename also if filename is missing from bmk.
 ;; 2012/05/05 dadams
