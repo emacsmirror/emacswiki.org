d42ab3aa5e76e1de0aee605e4045956c585459cf
MaartenG <anybody@emacswiki.org>
2012-07-17 00:52:28 +0200

    This code allows you to search files using Tracker, from within emacs. The results are shown in a dired-like buffer. Analog to find-dired.

--- tracker-dired.el
+++ tracker-dired.el
@@ -1,123 +1,194 @@
-#FILE text/x-emacs-lisp 
-Ozs7IHRyYWNrZXItZGlyZWQuZWwgLS0tIHJ1biBhIHRyYWNrZXItc2VhcmNoIHF1ZXJ5IGFuZCBk
-aXJlZCB0aGUgb3V0cHV0Cgo7OyBDb3B5cmlnaHQgKEMpIDE5OTIsIDE5OTQsIDE5OTUgRnJlZSBT
-b2Z0d2FyZSBGb3VuZGF0aW9uLCBJbmMuCgo7OyBBdXRob3I6IFJvbGFuZCBNY0dyYXRoIDxyb2xh
-bmQgKGF0KSBnbnUuIGFpLiBtaXQuIGVkdT4sCjs7CSAgIFNlYmFzdGlhbiBLcmVtZXIgPHNrIChh
-dCkgdGhwLiB1bmkta29lbG4uIGRlPgo7OyAgICAgICAgIE1hYXJ0ZW4gR3JhY2h0ZW4gPG1hYXJ0
-ZW4uIGdyYWNodGVuIChhdCkgZ21haWwuIGNvbT4KOzsgTWFpbnRhaW5lcjogRlNGCjs7IEtleXdv
-cmRzOiB1bml4Cgo7OyBUaGlzIGZpbGUgaXMgcGFydCBvZiBHTlUgRW1hY3MuCgo7OyBHTlUgRW1h
-Y3MgaXMgZnJlZSBzb2Z0d2FyZTsgeW91IGNhbiByZWRpc3RyaWJ1dGUgaXQgYW5kL29yIG1vZGlm
-eQo7OyBpdCB1bmRlciB0aGUgdGVybXMgb2YgdGhlIEdOVSBHZW5lcmFsIFB1YmxpYyBMaWNlbnNl
-IGFzIHB1Ymxpc2hlZCBieQo7OyB0aGUgRnJlZSBTb2Z0d2FyZSBGb3VuZGF0aW9uOyBlaXRoZXIg
-dmVyc2lvbiAyLCBvciAoYXQgeW91ciBvcHRpb24pCjs7IGFueSBsYXRlciB2ZXJzaW9uLgoKOzsg
-R05VIEVtYWNzIGlzIGRpc3RyaWJ1dGVkIGluIHRoZSBob3BlIHRoYXQgaXQgd2lsbCBiZSB1c2Vm
-dWwsCjs7IGJ1dCBXSVRIT1VUIEFOWSBXQVJSQU5UWTsgd2l0aG91dCBldmVuIHRoZSBpbXBsaWVk
-IHdhcnJhbnR5IG9mCjs7IE1FUkNIQU5UQUJJTElUWSBvciBGSVRORVNTIEZPUiBBIFBBUlRJQ1VM
-QVIgUFVSUE9TRS4gIFNlZSB0aGUKOzsgR05VIEdlbmVyYWwgUHVibGljIExpY2Vuc2UgZm9yIG1v
-cmUgZGV0YWlscy4KCjs7IFlvdSBzaG91bGQgaGF2ZSByZWNlaXZlZCBhIGNvcHkgb2YgdGhlIEdO
-VSBHZW5lcmFsIFB1YmxpYyBMaWNlbnNlCjs7IGFsb25nIHdpdGggR05VIEVtYWNzOyBzZWUgdGhl
-IGZpbGUgQ09QWUlORy4gIElmIG5vdCwgd3JpdGUgdG8gdGhlCjs7IEZyZWUgU29mdHdhcmUgRm91
-bmRhdGlvbiwgSW5jLiwgNTkgVGVtcGxlIFBsYWNlIC0gU3VpdGUgMzMwLAo7OyBCb3N0b24sIE1B
-IDAyMTExLTEzMDcsIFVTQS4KCjs7OyBDb2RlOgoKKHJlcXVpcmUgJ2RpcmVkKQoocmVxdWlyZSAn
-ZGJ1cykKKHJlcXVpcmUgJ2dudXMtdXRpbCkKCihkZWZ1biBkamNiLWNhbGwtdHJhY2tlciAobWV0
-aG9kICZyZXN0IGFyZ3MpCiAgImNhbGwgdGhlIHRyYWNrZXIgbWV0aG9kIE1FVEhPRCB3aXRoIEFS
-R1Mgb3ZlciBkYnVzIgogIChhcHBseSAnZGJ1cy1jYWxsLW1ldGhvZCAKICAgIDpzZXNzaW9uICAg
-ICAgICAgICAgICAgICAgICAgICAgICAgIDsgdXNlIHRoZSBzZXNzaW9uIChub3Qgc3lzdGVtKSBi
-dXMKICAgICJvcmcuZnJlZWRlc2t0b3AuVHJhY2tlcjEiICAgICAgICAgICAgICAgICAgOyBzZXJ2
-aWNlIG5hbWUKICAgICIvb3JnL2ZyZWVkZXNrdG9wL1RyYWNrZXIxL1Jlc291cmNlcyIgICA7IHBh
-dGggbmFtZQogICAgIm9yZy5mcmVlZGVza3RvcC5UcmFja2VyMS5SZXNvdXJjZXMiICAgIDsgaW50
-ZXJmYWNlIG5hbWUKICAgIG1ldGhvZCBhcmdzKSkKCihkZWZ1biBzZWFyY2ggKHF1ZXJ5KQogIChs
-ZXQgKChyZXN1bHRzIChkamNiLWNhbGwtdHJhY2tlciAKCQkgICJTcGFycWxRdWVyeSIgCgkJICAo
-Y29uY2F0ICJTRUxFQ1QgbmllOnVybCg/ZikgV0hFUkUgeyA/ZiBmdHM6bWF0Y2ggJyIgCgkJCSAg
-cXVlcnkgCgkJCSAgIicgLiA/ZiBuaWU6bWltZVR5cGUgP20gLiBGSUxURVIgKCBmbjpjb250YWlu
-cyg/bSwndGV4dC8nKSApfSIpKSkpCgkobWFwY2FyICcobGFtYmRhICh4KSAKCQkgICAoY29uY2F0
-ICIgJyIgCgkJCSAgIChzdWJzdHJpbmcgKGdudXMtdXJsLXVuaGV4LXN0cmluZyAoY2FyIHgpIG5p
-bCkgNykgCgkJCSAgICInIikpCgkJcmVzdWx0cykpKQoKKGRlZmdyb3VwIHRyYWNrZXItZGlyZWQg
-bmlsCiAgIlJ1biBhIHRyYWNrZXItc2VhcmNoIHF1ZXJ5IGFuZCBkaXJlZCB0aGUgb3V0cHV0LiIK
-ICA6Z3JvdXAgJ2RpcmVkCiAgOnByZWZpeCAidHJhY2tlci0iKQoKOzsgZmluZCdzIC1scyBjb3Jy
-ZXNwb25kcyB0byB0aGVzZSBzd2l0Y2hlcy4KOzsgTm90ZSAtYiwgYXQgbGVhc3QgR05VIGZpbmQg
-cXVvdGVzIHNwYWNlcyBldGMuIGluIGZpbGVuYW1lcwo7OzsjIyNhdXRvbG9hZAooZGVmY3VzdG9t
-IHRyYWNrZXItbHMtb3B0aW9uCiAgKGlmIChlcSBzeXN0ZW0tdHlwZSAnYmVya2VsZXktdW5peCkg
-JygiLWxzIiAuICItZ2lsc2IiKQogICAgJygiLWV4ZWMgbHMgLWxkIHt9IFxcOyIgLiAiLWxkIikp
-CiAgIipEZXNjcmlwdGlvbiBvZiB0aGUgb3B0aW9uIHRvIGBmaW5kJyB0byBwcm9kdWNlIGFuIGBs
-cyAtbCctdHlwZSBsaXN0aW5nLgpUaGlzIGlzIGEgY29ucyBvZiB0d28gc3RyaW5ncyAoVFJBQ0tF
-Ui1PUFRJT04gLiBMUy1TV0lUQ0hFUykuICBUUkFDS0VSLU9QVElPTgpnaXZlcyB0aGUgb3B0aW9u
-IChvciBvcHRpb25zKSB0byBgZmluZCcgdGhhdCBwcm9kdWNlIHRoZSBkZXNpcmVkIG91dHB1dC4K
-TFMtU1dJVENIRVMgaXMgYSBsaXN0IG9mIGBscycgc3dpdGNoZXMgdG8gdGVsbCBkaXJlZCBob3cg
-dG8gcGFyc2UgdGhlIG91dHB1dC4iCiAgOnR5cGUgJyhjb25zIChzdHJpbmcgOnRhZyAiRmluZCBP
-cHRpb24iKQoJICAgICAgIChzdHJpbmcgOnRhZyAiTHMgU3dpdGNoZXMiKSkKICA6Z3JvdXAgJ3Ry
-YWNrZXItZGlyZWQpCgo7OzsjIyNhdXRvbG9hZAoKKGRlZnZhciB0cmFja2VyLXF1ZXJpZXMgbmls
-CiAgIkxhc3QgcXVlcmllcyBnaXZlbiB0byBUcmFja2VyIGJ5IFxcW3RyYWNrZXItZGlyZWRdLiIp
-Cgo7OyBIaXN0b3J5IG9mIHRyYWNrZXItcXVlcmllcyB2YWx1ZXMgZW50ZXJlZCBpbiB0aGUgbWlu
-aWJ1ZmZlci4KKGRlZnZhciB0cmFja2VyLXF1ZXJpZXMtaGlzdG9yeSBuaWwpCgo7OzsjIyNhdXRv
-bG9hZAooZGVmdW4gdHJhY2tlci1kaXJlZCAoc2VhcmNoLXRlcm0pCiAgIlJ1biBgZmluZCcgYW5k
-IGdvIGludG8gRGlyZWQgbW9kZSBvbiBhIGJ1ZmZlciBvZiB0aGUgb3V0cHV0LgpUaGUgY29tbWFu
-ZCBydW4gKGFmdGVyIGNoYW5naW5nIGludG8gRElSKSBpcwoKICAgIGZpbmQgLiBcXCggQVJHUyBc
-XCkgLWxzCgpleGNlcHQgdGhhdCB0aGUgdmFyaWFibGUgYHRyYWNrZXItbHMtb3B0aW9uJyBzcGVj
-aWZpZXMgd2hhdCB0byB1c2UKYXMgdGhlIGZpbmFsIGFyZ3VtZW50LiIKICAoaW50ZXJhY3RpdmUg
-KGxpc3QgKHJlYWQtc3RyaW5nICJTZWFyY2ggdGVybXM6ICIgdHJhY2tlci1xdWVyaWVzCgkJCQkg
-ICcodHJhY2tlci1xdWVyaWVzLWhpc3RvcnkgLiAxKSkpKQogIChsZXQgKChyZXN1bHRzIChzZWFy
-Y2ggc2VhcmNoLXRlcm0pKSkKCQogICAgKHN3aXRjaC10by1idWZmZXIgKGdldC1idWZmZXItY3Jl
-YXRlICIqVHJhY2tlci1SZXN1bHRzKiIpKQogICAgKHdpZGVuKQogICAgKGtpbGwtYWxsLWxvY2Fs
-LXZhcmlhYmxlcykKICAgIChzZXRxIGJ1ZmZlci1yZWFkLW9ubHkgbmlsKQogICAgKGVyYXNlLWJ1
-ZmZlcikKICAgIChzZXRxIHRyYWNrZXItcXVlcmllcyBzZWFyY2gtdGVybSkKCiAgICAoZGlyZWQt
-bW9kZSkKCiAgICAodXNlLWxvY2FsLW1hcCAoYXBwZW5kIChtYWtlLXNwYXJzZS1rZXltYXApIChj
-dXJyZW50LWxvY2FsLW1hcCkpKQogICAgKGRlZmluZS1rZXkgKGN1cnJlbnQtbG9jYWwtbWFwKSAi
-ZyIgJ3VuZGVmaW5lZCkKICAgIDs7IFNldCBzdWJkaXItYWxpc3Qgc28gdGhhdCBUcmVlIERpcmVk
-IHdpbGwgd29yazoKICAgIAogICAgKGlmIChmYm91bmRwICdkaXJlZC1zaW1wbGUtc3ViZGlyLWFs
-aXN0KQoJOzsgd2lsbCB3b3JrIGV2ZW4gd2l0aCBuZXN0ZWQgZGlyZWQgZm9ybWF0IChkaXJlZC1u
-c3RkLmVsLHYgMS4xNQoJOzsgYW5kIGxhdGVyKQoJKGRpcmVkLXNpbXBsZS1zdWJkaXItYWxpc3Qp
-CiAgICAgIDs7IGVsc2Ugd2UgaGF2ZSBhbiBhbmNpZW50IHRyZWUgZGlyZWQgKG9yIGNsYXNzaWMg
-ZGlyZWQsIHdoZXJlCiAgICAgIDs7IHRoaXMgZG9lcyBubyBoYXJtKSAKICAgICAgKHNldCAobWFr
-ZS1sb2NhbC12YXJpYWJsZSAnZGlyZWQtc3ViZGlyLWFsaXN0KQoJICAgKGxpc3QgKGNvbnMgZGVm
-YXVsdC1kaXJlY3RvcnkgKHBvaW50LW1pbi1tYXJrZXIpKSkpKQoKICAgIChzZXRxIGJ1ZmZlci1y
-ZWFkLW9ubHkgbmlsKQogICAgOzsgU3ViZGlyIGhlYWRsZXJsaW5lIG11c3QgY29tZSBmaXJzdCBi
-ZWNhdXNlIHRoZSBmaXJzdCBtYXJrZXIgaW4KICAgIDs7IHN1YmRpci1hbGlzdCBwb2ludHMgdGhl
-cmUuCiAgICA7OyBgYHdpbGRjYXJkJycgbGluZS4gCiAgICAoaW5zZXJ0ICJUcmFja2VyIHNlYXJj
-aCByZXN1bHRzIGZvciBcIiIgc2VhcmNoLXRlcm0gCgkgICAgIlwiICgiIChmb3JtYXQgIiVkIiAo
-bGVuZ3RoIHJlc3VsdHMpKSAiIG1hdGNoZXMpXG5cbiIpCiAgICA7OyBTdGFydCB0aGUgcHJvY2Vz
-cyB0aGF0IHBpcGVzIHRoZSByZXN1bHRzIG9mIHRoZSB0cmFja2VyIHNlYXJjaCB0aHJvdWdoIGxz
-IC1sZC4KICAgIChsZXQgKChwcm9jIChzdGFydC1wcm9jZXNzLXNoZWxsLWNvbW1hbmQgIlRyYWNr
-ZXIgU2VhcmNoIiAoY3VycmVudC1idWZmZXIpIAoJCQkJCSAgICAgKGNvbmNhdCAibHMgLWxkIiAo
-YXBwbHkgJ2NvbmNhdCByZXN1bHRzKSkpKSkKICAgICAgKHNldC1wcm9jZXNzLWZpbHRlciBwcm9j
-IChmdW5jdGlvbiB0cmFja2VyLWRpcmVkLWZpbHRlcikpCiAgICAgIChzZXQtcHJvY2Vzcy1zZW50
-aW5lbCBwcm9jIChmdW5jdGlvbiB0cmFja2VyLWRpcmVkLXNlbnRpbmVsKSkKICAgICAgOzsgSW5p
-dGlhbGl6ZSB0aGUgcHJvY2VzcyBtYXJrZXI7IGl0IGlzIHVzZWQgYnkgdGhlIGZpbHRlci4KICAg
-ICAgKG1vdmUtbWFya2VyIChwcm9jZXNzLW1hcmsgcHJvYykgMSAoY3VycmVudC1idWZmZXIpKSkK
-ICAgIChzZXRxIG1vZGUtbGluZS1wcm9jZXNzICcoIjolcyIpKSkpCgooZGVmdW4gdHJhY2tlci1k
-aXJlZC1maWx0ZXIgKHByb2Mgc3RyaW5nKQogIDs7IEZpbHRlciBmb3IgXFxbdHJhY2tlci1kaXJl
-ZF0gcHJvY2Vzc2VzLgogIChsZXQgKChidWYgKHByb2Nlc3MtYnVmZmVyIHByb2MpKSkKICAgIChp
-ZiAoYnVmZmVyLW5hbWUgYnVmKQkJOyBub3Qga2lsbGVkPwoJKHNhdmUtZXhjdXJzaW9uCgkgIChz
-ZXQtYnVmZmVyIGJ1ZikKCSAgKHNhdmUtcmVzdHJpY3Rpb24KCSAgICAod2lkZW4pCgkgICAgKHNh
-dmUtZXhjdXJzaW9uCgkgICAgICAobGV0ICgoYnVmZmVyLXJlYWQtb25seSBuaWwpCgkJICAgIChl
-bmQgKHBvaW50LW1heCkpKQoJCShnb3RvLWNoYXIgZW5kKQoJCShpbnNlcnQgc3RyaW5nKQoJCShn
-b3RvLWNoYXIgZW5kKQoJCShvciAobG9va2luZy1hdCAiXiIpCgkJICAgIChmb3J3YXJkLWxpbmUg
-MSkpCgkJKHdoaWxlIChsb29raW5nLWF0ICJeIikKCQkgIChpbnNlcnQgIiAgIikKCQkgIChmb3J3
-YXJkLWxpbmUgMSkpCgkJOzsgQ29udmVydCBgIC4vRklMRScgdG8gYCBGSUxFJwoJCTs7IFRoaXMg
-d291bGQgbG9zZSBpZiB0aGUgY3VycmVudCBjaHVuayBvZiBvdXRwdXQKCQk7OyBzdGFydHMgb3Ig
-ZW5kcyB3aXRoaW4gdGhlIGAgLi8nLCBzbyBiYWNrIHVwIGEgYml0OgoJCShnb3RvLWNoYXIgKC0g
-ZW5kIDMpKQk7IG5vIGVycm9yIGlmIDwgMAoJCSh3aGlsZSAoc2VhcmNoLWZvcndhcmQgIiAuLyIg
-bmlsIHQpCgkJICAoZGVsZXRlLXJlZ2lvbiAocG9pbnQpICgtIChwb2ludCkgMikpKQoJCTs7IEZp
-bmQgYWxsIHRoZSBjb21wbGV0ZSBsaW5lcyBpbiB0aGUgdW5wcm9jZXNzZWQKCQk7OyBvdXRwdXQg
-YW5kIHByb2Nlc3MgaXQgdG8gYWRkIHRleHQgcHJvcGVydGllcy4KCQkoZ290by1jaGFyIGVuZCkK
-CQkoaWYgKHNlYXJjaC1iYWNrd2FyZCAiXG4iIChwcm9jZXNzLW1hcmsgcHJvYykgdCkKCQkgICAg
-KHByb2duCgkJICAgICAgKGRpcmVkLWluc2VydC1zZXQtcHJvcGVydGllcyAocHJvY2Vzcy1tYXJr
-IHByb2MpCgkJCQkJCSAgICgxKyAocG9pbnQpKSkKCQkgICAgICAobW92ZS1tYXJrZXIgKHByb2Nl
-c3MtbWFyayBwcm9jKSAoMSsgKHBvaW50KSkpKSkKCQkpKSkpCiAgICAgIDs7IFRoZSBidWZmZXIg
-aGFzIGJlZW4ga2lsbGVkLgogICAgICAoZGVsZXRlLXByb2Nlc3MgcHJvYykpKSkKCihkZWZ1biB0
-cmFja2VyLWRpcmVkLXNlbnRpbmVsIChwcm9jIHN0YXRlKQogIDs7IFNlbnRpbmVsIGZvciBcXFt0
-cmFja2VyLWRpcmVkXSBwcm9jZXNzZXMuCiAgKGxldCAoKGJ1ZiAocHJvY2Vzcy1idWZmZXIgcHJv
-YykpKQogICAgKGlmIChidWZmZXItbmFtZSBidWYpCgkoc2F2ZS1leGN1cnNpb24KCSAgKHNldC1i
-dWZmZXIgYnVmKQoJICAobGV0ICgoYnVmZmVyLXJlYWQtb25seSBuaWwpKQoJICAgIChzYXZlLWV4
-Y3Vyc2lvbgoJICAgICAgKGdvdG8tY2hhciAocG9pbnQtbWF4KSkKCSAgICAgIChpbnNlcnQgIlxu
-VHJhY2tlciBzZWFyY2ggIiBzdGF0ZSkKCSAgICAgIChmb3J3YXJkLWNoYXIgLTEpCQk7QmFjayB1
-cCBiZWZvcmUgXG4gYXQgZW5kIG9mIFNUQVRFLgoJICAgICAgKGluc2VydCAiIGF0ICIgKHN1YnN0
-cmluZyAoY3VycmVudC10aW1lLXN0cmluZykgMCAxOSkpCgkgICAgICAoZm9yd2FyZC1jaGFyIDEp
-CgkgICAgICAoc2V0cSBtb2RlLWxpbmUtcHJvY2VzcwoJCSAgICAoY29uY2F0ICI6IgoJCQkgICAg
-KHN5bWJvbC1uYW1lIChwcm9jZXNzLXN0YXR1cyBwcm9jKSkpKQoJICAgICAgOzsgU2luY2UgdGhl
-IGJ1ZmZlciBhbmQgbW9kZSBsaW5lIHdpbGwgc2hvdyB0aGF0IHRoZQoJICAgICAgOzsgcHJvY2Vz
-cyBpcyBkZWFkLCB3ZSBjYW4gZGVsZXRlIGl0IG5vdy4gIE90aGVyd2lzZSBpdAoJICAgICAgOzsg
-d2lsbCBzdGF5IGFyb3VuZCB1bnRpbCBNLXggbGlzdC1wcm9jZXNzZXMuCgkgICAgICAoZGVsZXRl
-LXByb2Nlc3MgcHJvYykKCSAgICAgIChmb3JjZS1tb2RlLWxpbmUtdXBkYXRlKSkpCgkgIChtZXNz
-YWdlICJ0cmFja2VyLWRpcmVkICVzIGZpbmlzaGVkLiIgKGN1cnJlbnQtYnVmZmVyKSkpKSkpCgoo
-cHJvdmlkZSAndHJhY2tlci1kaXJlZCkKCjs7OyB0cmFja2VyLWRpcmVkLmVsIGVuZHMgaGVyZQoK
+;;; tracker-dired.el --- run a tracker-search query and dired the output
+
+;; Copyright (C) 1992, 1994, 1995 Free Software Foundation, Inc.
+
+;; Author: Roland McGrath <roland (at) gnu. ai. mit. edu>,
+;;	   Sebastian Kremer <sk (at) thp. uni-koeln. de>
+;;         Maarten Grachten <maarten. grachten (at) gmail. com>
+;; Maintainer: FSF
+;; Keywords: unix
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Code:
+
+(require 'dired)
+(require 'dbus)
+(require 'gnus-util)
+
+(defun djcb-call-tracker (method &rest args)
+  "call the tracker method METHOD with ARGS over dbus"
+  (apply 'dbus-call-method 
+    :session                            ; use the session (not system) bus
+    "org.freedesktop.Tracker1"                  ; service name
+    "/org/freedesktop/Tracker1/Resources"   ; path name
+    "org.freedesktop.Tracker1.Resources"    ; interface name
+    method args))
+
+(defun search (query)
+  (let ((results (djcb-call-tracker 
+		  "SparqlQuery" 
+		  (concat "SELECT nie:url(?f) WHERE { ?f fts:match '" 
+			  query 
+			  "' . ?f nie:mimeType ?m . FILTER ( fn:contains(?m,'text/') )}"))))
+	(mapcar '(lambda (x) 
+		   (concat " '" 
+			   (substring (gnus-url-unhex-string (car x) nil) 7) 
+			   "'"))
+		results)))
+
+(defgroup tracker-dired nil
+  "Run a tracker-search query and dired the output."
+  :group 'dired
+  :prefix "tracker-")
+
+;; find's -ls corresponds to these switches.
+;; Note -b, at least GNU find quotes spaces etc. in filenames
+;;;###autoload
+(defcustom tracker-ls-option
+  (if (eq system-type 'berkeley-unix) '("-ls" . "-gilsb")
+    '("-exec ls -ld {} \\;" . "-ld"))
+  "*Description of the option to `find' to produce an `ls -l'-type listing.
+This is a cons of two strings (TRACKER-OPTION . LS-SWITCHES).  TRACKER-OPTION
+gives the option (or options) to `find' that produce the desired output.
+LS-SWITCHES is a list of `ls' switches to tell dired how to parse the output."
+  :type '(cons (string :tag "Find Option")
+	       (string :tag "Ls Switches"))
+  :group 'tracker-dired)
+
+;;;###autoload
+
+(defvar tracker-queries nil
+  "Last queries given to Tracker by \\[tracker-dired].")
+
+;; History of tracker-queries values entered in the minibuffer.
+(defvar tracker-queries-history nil)
+
+;;;###autoload
+(defun tracker-dired (search-term)
+  "Run `find' and go into Dired mode on a buffer of the output.
+The command run (after changing into DIR) is
+
+    find . \\( ARGS \\) -ls
+
+except that the variable `tracker-ls-option' specifies what to use
+as the final argument."
+  (interactive (list (read-string "Search terms: " tracker-queries
+				  '(tracker-queries-history . 1))))
+  (let ((results (search search-term)))
+	
+    (switch-to-buffer (get-buffer-create "*Tracker-Results*"))
+    (widen)
+    (kill-all-local-variables)
+    (setq buffer-read-only nil)
+    (erase-buffer)
+    (setq tracker-queries search-term)
+
+    (dired-mode)
+
+    (use-local-map (append (make-sparse-keymap) (current-local-map)))
+    (define-key (current-local-map) "g" 'undefined)
+    ;; Set subdir-alist so that Tree Dired will work:
+    
+    (if (fboundp 'dired-simple-subdir-alist)
+	;; will work even with nested dired format (dired-nstd.el,v 1.15
+	;; and later)
+	(dired-simple-subdir-alist)
+      ;; else we have an ancient tree dired (or classic dired, where
+      ;; this does no harm) 
+      (set (make-local-variable 'dired-subdir-alist)
+	   (list (cons default-directory (point-min-marker)))))
+
+    (setq buffer-read-only nil)
+    ;; Subdir headlerline must come first because the first marker in
+    ;; subdir-alist points there.
+    ;; ``wildcard'' line. 
+    (insert "Tracker search results for \"" search-term 
+	    "\" (" (format "%d" (length results)) " matches)\n\n")
+    ;; Start the process that pipes the results of the tracker search through ls -ld.
+    (let ((proc (start-process-shell-command "Tracker Search" (current-buffer) 
+					     (concat "ls -ld" (apply 'concat results)))))
+      (set-process-filter proc (function tracker-dired-filter))
+      (set-process-sentinel proc (function tracker-dired-sentinel))
+      ;; Initialize the process marker; it is used by the filter.
+      (move-marker (process-mark proc) 1 (current-buffer)))
+    (setq mode-line-process '(":%s"))))
+
+(defun tracker-dired-filter (proc string)
+  ;; Filter for \\[tracker-dired] processes.
+  (let ((buf (process-buffer proc)))
+    (if (buffer-name buf)		; not killed?
+	(save-excursion
+	  (set-buffer buf)
+	  (save-restriction
+	    (widen)
+	    (save-excursion
+	      (let ((buffer-read-only nil)
+		    (end (point-max)))
+		(goto-char end)
+		(insert string)
+		(goto-char end)
+		(or (looking-at "^")
+		    (forward-line 1))
+		(while (looking-at "^")
+		  (insert "  ")
+		  (forward-line 1))
+		;; Convert ` ./FILE' to ` FILE'
+		;; This would lose if the current chunk of output
+		;; starts or ends within the ` ./', so back up a bit:
+		(goto-char (- end 3))	; no error if < 0
+		(while (search-forward " ./" nil t)
+		  (delete-region (point) (- (point) 2)))
+		;; Find all the complete lines in the unprocessed
+		;; output and process it to add text properties.
+		(goto-char end)
+		(if (search-backward "\n" (process-mark proc) t)
+		    (progn
+		      (dired-insert-set-properties (process-mark proc)
+						   (1+ (point)))
+		      (move-marker (process-mark proc) (1+ (point)))))
+		))))
+      ;; The buffer has been killed.
+      (delete-process proc))))
+
+(defun tracker-dired-sentinel (proc state)
+  ;; Sentinel for \\[tracker-dired] processes.
+  (let ((buf (process-buffer proc)))
+    (if (buffer-name buf)
+	(save-excursion
+	  (set-buffer buf)
+	  (let ((buffer-read-only nil))
+	    (save-excursion
+	      (goto-char (point-max))
+	      (insert "\nTracker search " state)
+	      (forward-char -1)		;Back up before \n at end of STATE.
+	      (insert " at " (substring (current-time-string) 0 19))
+	      (forward-char 1)
+	      (setq mode-line-process
+		    (concat ":"
+			    (symbol-name (process-status proc))))
+	      ;; Since the buffer and mode line will show that the
+	      ;; process is dead, we can delete it now.  Otherwise it
+	      ;; will stay around until M-x list-processes.
+	      (delete-process proc)
+	      (force-mode-line-update)))
+	  (message "tracker-dired %s finished." (current-buffer))))))
+
+(provide 'tracker-dired)
+
+;;; tracker-dired.el ends here
