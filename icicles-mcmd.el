3d64b0e2315b3ea3fae709a623d159642ea12164
DrewAdams <anybody@emacswiki.org>
2012-07-17 19:15:12 +0200

    no summary available

--- icicles-mcmd.el
+++ icicles-mcmd.el
@@ -7,13 +7,13 @@
 ;; Copyright (C) 1996-2012, Drew Adams, all rights reserved.
 ;; Created: Mon Feb 27 09:25:04 2006
 ;; Version: 22.0
-;; Last-Updated: Tue Jul 17 09:16:44 2012 (-0700)
+;; Last-Updated: Tue Jul 17 10:03:20 2012 (-0700)
 ;;           By: dradams
-;;     Update #: 18386
+;;     Update #: 18387
 ;; URL: http://www.emacswiki.org/cgi-bin/wiki/icicles-mcmd.el
 ;; Keywords: internal, extensions, help, abbrev, local, minibuffer,
 ;;           keys, apropos, completion, matching, regexp, command
-;; Compatibility: GNU Emacs: 20.x, 21.x, 22.x, 23.x
+;; Compatibility: GNU Emacs: 20.x, 21.x, 22.x, 23.x, 24.x
 ;;
 ;; Features that might be required by this library:
 ;;
