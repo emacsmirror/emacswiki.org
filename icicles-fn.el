9398e05bcaf304dab7187fc9800c59c309d676a4
DrewAdams <anybody@emacswiki.org>
2012-07-17 19:18:03 +0200

    no summary available

--- icicles-fn.el
+++ icicles-fn.el
@@ -7,13 +7,13 @@
 ;; Copyright (C) 1996-2012, Drew Adams, all rights reserved.
 ;; Created: Mon Feb 27 09:25:53 2006
 ;; Version: 22.0
-;; Last-Updated: Sat Jul 14 10:46:46 2012 (-0700)
+;; Last-Updated: Tue Jul 17 10:03:38 2012 (-0700)
 ;;           By: dradams
-;;     Update #: 13162
+;;     Update #: 13163
 ;; URL: http://www.emacswiki.org/cgi-bin/wiki/icicles-fn.el
 ;; Keywords: internal, extensions, help, abbrev, local, minibuffer,
 ;;           keys, apropos, completion, matching, regexp, command
-;; Compatibility: GNU Emacs: 20.x, 21.x, 22.x, 23.x
+;; Compatibility: GNU Emacs: 20.x, 21.x, 22.x, 23.x, 24.x
 ;;
 ;; Features that might be required by this library:
 ;;
