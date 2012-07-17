84f2b36193f2d88285060bee14d6fc1f495d1093
DrewAdams <anybody@emacswiki.org>
2012-07-17 19:27:34 +0200

    no summary available

--- icicles-doc2.el
+++ icicles-doc2.el
@@ -7,13 +7,13 @@
 ;; Copyright (C) 1996-2012, Drew Adams, all rights reserved.
 ;; Created: Tue Aug  1 14:21:16 1995
 ;; Version: 22.0
-;; Last-Updated: Tue Jul 17 09:14:31 2012 (-0700)
+;; Last-Updated: Tue Jul 17 10:03:56 2012 (-0700)
 ;;           By: dradams
-;;     Update #: 28947
+;;     Update #: 28948
 ;; URL: http://www.emacswiki.org/cgi-bin/wiki/icicles-doc2.el
 ;; Keywords: internal, extensions, help, abbrev, local, minibuffer,
 ;;           keys, apropos, completion, matching, regexp, command
-;; Compatibility: GNU Emacs: 20.x, 21.x, 22.x, 23.x
+;; Compatibility: GNU Emacs: 20.x, 21.x, 22.x, 23.x, 24.x
 ;;
 ;; Features that might be required by this library:
 ;;
