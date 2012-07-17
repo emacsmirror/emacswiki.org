6ef032eeee3ebcee0f6f8a2f314a229fd995c860
DrewAdams <anybody@emacswiki.org>
2012-07-17 19:12:52 +0200

    no summary available

--- icicles-mode.el
+++ icicles-mode.el
@@ -7,13 +7,13 @@
 ;; Copyright (C) 1996-2012, Drew Adams, all rights reserved.
 ;; Created: Mon Feb 27 10:21:10 2006
 ;; Version: 22.0
-;; Last-Updated: Fri Jul 13 14:46:54 2012 (-0700)
+;; Last-Updated: Tue Jul 17 10:03:06 2012 (-0700)
 ;;           By: dradams
-;;     Update #: 9026
+;;     Update #: 9027
 ;; URL: http://www.emacswiki.org/cgi-bin/wiki/icicles-mode.el
 ;; Keywords: internal, extensions, help, abbrev, local, minibuffer,
 ;;           keys, apropos, completion, matching, regexp, command
-;; Compatibility: GNU Emacs: 20.x, 21.x, 22.x, 23.x
+;; Compatibility: GNU Emacs: 20.x, 21.x, 22.x, 23.x, 24.x
 ;;
 ;; Features that might be required by this library:
 ;;
