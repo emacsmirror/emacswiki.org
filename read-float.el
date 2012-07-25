30c5915f4236853f461216c7a58539b465544fb2
TN <anybody@emacswiki.org>
2012-07-25 23:12:44 +0200

    Reading floating point numbers in binary format

---
+++ read-float.el
@@ -0,0 +1,117 @@
+;; Copyright (C) 2012  Tobias.Naehring -- Read float values in binary format from literal buffer
+
+;; Author: Tobias.Naehring <i@tn-home.de>
+;; Keywords: Search for mouse selection.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Function `read-float' reads floating point numbers in binary format
+;; from buffer. Leading most significant bit is assumed.
+
+;; Just experimental.
+;; 
+
+(defvar IEEE754-types
+  ;; number of bits:
+  ;; overall, mantissa-length
+  '(
+    (single 32 23) ;; 1bit sign, 8bits exponent
+    (double 64 52) ;; 1bit sign, 11bits exponent
+    )
+  "Number formats.
+Each entry is a list of the form (fmt size mantissa).
+fmt is a symbol denoting the floating point number format
+size is the overall size in bits
+mantissa is the length of the mantissa")
+;; additionally there is one sign bit
+;; the remainding number of bits are for the exponent
+
+(defun read-number-bits (v b n)
+  "Read binary number from vector V of bytes.
+\(most significant bit first)
+B is the begin of the bit sequence counting from zero
+N is the number of bits."
+  (let* ((b8 (/ b 8)) ;; first byte used (counting from zero)
+	 (e8 (/ (+ b n 7) 8)) ;; end byte: not used byte (counting from zero)
+	 (db8 (- (* (1+ b8) 8) b)) ;; significant bits in first byte
+	 (s (- n db8))
+	 (c (logand (aref v b8) (- (ash 1 db8) 1)))
+	 (x (ash c s)))
+    (setq b8 (1+ b8))
+    (while (< b8 e8)
+      (setq s (- s 8))
+      (setq c (aref v b8))
+      (setq x (+ x (ash c s)))
+      (setq b8 (1+ b8)))
+    x))
+
+(defun pow2 (n)
+  "Calculate 2^n as a floating point number."
+  (let ((x 1.0))
+  (if (> n 0)
+      (loop for i from 1 to n do (setq x (* x 2)))
+    (setq n (- n))
+    (loop for i from 1 to n do (setq x (/ x 2))))
+  x))
+
+(defun read-float (fmt &optional pos buf)
+  "Read floating point number in binary format FMT from
+buffer BUF at position POS (defaulting to the current point).
+After success point is behind the read bytes."
+  (unless buf
+    (setq buf (current-buffer)))
+  (with-current-buffer buf
+    (if pos
+	(goto-char pos))
+    ;; extract sign (highest bit)
+    (if (symbolp fmt)
+	(setq fmt (cdr (assoc fmt IEEE754-types))))
+    (let* ((size (car fmt))
+	   (sizeBytes (/ size 8))
+	   (mantissa-size (cadr fmt))
+	   (exp-size (- size mantissa-size 1))
+	   (bytes (buffer-substring-no-properties (point) (+ (point) sizeBytes)))
+	   (sign (logand #b10000000 (aref bytes 0)))
+	   (exp (read-number-bits bytes 1 exp-size))
+	   (NAN-exp (1- (ash 1 exp-size)))
+	   (digits (read-number-bits bytes (1+ exp-size) mantissa-size)))
+      (case exp
+	(0 ;; denormalized numbers
+	 (/ (float digits) (ash 1 mantissa-size)))
+	(NAN-exp ;; nan and inf
+	 ;; todo
+	 0.0)
+	(t ;; normalized number
+	 (* (1+ (/ (float digits) (ash 1 mantissa-size)))
+	    (pow2 (- exp (ash 1 (1- exp-size)) -1))
+	    (if (= sign 0) 1 -1)))
+	))))
+
+;; Test:
+(when nil
+  (with-temp-buffer
+    ;;........SEEEEEEE
+    (insert #b10111111
+	    ;;EMMMMMMM
+	    #b10000000
+	    ;;MMMMMMMM
+	    #b00000000
+	    ;;MMMMMMMM
+	    #b00000000)
+    (read-float 'single 0))
+  )
+
+(provide 'read-float)
