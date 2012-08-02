4c972713e682cc74627bcd8904feb5630561c312
antoineb <anybody@emacswiki.org>
2012-08-02 20:12:54 +0200

    no summary available

--- double-char.el
+++ double-char.el
@@ -1,18 +1,68 @@
-== Double char ==
+;;; double-char.el --- Get different insertion when you hit a char twice fast enought
 
-Double char is a way to insert two different string.
-One is inserted when you hit the char twice fast enough. 
+;;; Commentary:
+;; a silly usage but clear:
+;; (defdoublechar albatros "12" "abc")
+;; (local-set-key "ù" 'double-char---albatros)
+;; then press "ùù" in less than 0.3 second and get "abc"
+;; or "12" if more than 0.3 second
+;; change the delay with the var double-char-delay
+;; A less silly example:
+;; (defdoublechar great "<" ">")
+;; (global-set-key (kbd "<") 'double-char---great)
+;; now you have just one key for "<" and ">"
 
-Lisp:double-char.el
+;;; Code:
 
-typically on azerty keybord you can do:
-(defdoublechar great "<" ">")
-(global-set-key (kbd "<") 'double-char---great)
+(defvar double-char--last-time nil
+  "Keep track of the time we were last called.")
 
-Then type "<<" in less than 0.3 second to get ">"
-Or "<" if it exceed 0.3 second.
-In that example it will permit to skip the use of the modifier Shift (azerty keyborad).
+(defcustom double-char-delay 0.3
+  "Maximum delay between command invocations."
+  :type 'float
+  :group 'double-char)
 
-Change the 'double-char-delay to change the delay (0.3 second as default).
+(defvar double-char--list (make-hash-table :test 'equal)
+  "Association between the name of a function and the strings to insert.
+string to insert if the double press fail and the string to
+  insert it succed.")
 
-Notice that '(defdoublechar silly "12" "abc") work as well.
+(defun double-char--success (command)
+  "The success string associated with COMMAND."
+  (second (gethash command double-char--list)))
+
+(defun double-char--fail (command)
+  "The fail string associated with COMMAND."
+  (first (gethash command double-char--list)))
+
+(defmacro defdoublechar (name pressed-char string-insert)
+  "Produce a function which call 'double-char' with NAME.
+parameter and fill the 'double-char--list' hash.  Name the name you
+want for the function, PRESSED-CHAR the insert on fail,
+STRING-INSERT the insert on success."
+  (let ((fun-name (intern (concat "double-char---" (symbol-name name)))))
+    (puthash fun-name (list pressed-char string-insert) double-char--list)
+    `(defun ,fun-name ()
+       (interactive)
+       (double-char ',fun-name))))
+
+(defun double-char (command)
+  "Insert the fail or successstring of the COMMAND."
+  (let ((time (current-time))
+        (prev-cmd last-command))
+    (if (and (eq prev-cmd command)
+             (< (- (time-to-seconds time)
+                   (time-to-seconds double-char--last-time))
+                double-char-delay))
+        (progn
+	  (setq double-char--last-time '(0 0 0))
+          (delete-backward-char (string-width (double-char--fail command)))
+          (insert (double-char--success command)))
+      (progn
+	(insert (double-char--fail command))
+	(setq double-char--last-time (current-time))))))
+
+(provide 'double-char)
+(provide 'double-char)
+
+;;; double-char.el ends here
