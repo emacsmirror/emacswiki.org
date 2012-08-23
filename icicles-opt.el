b72ee1e3f353ac4f222c6a06145722f8d62b9298
DrewAdams <anybody@emacswiki.org>
2012-08-23 23:22:22 +0200

    no summary available

--- icicles-opt.el
+++ icicles-opt.el
@@ -7,10 +7,11 @@
 ;; Copyright (C) 1996-2012, Drew Adams, all rights reserved.
 ;; Created: Mon Feb 27 09:22:14 2006
 ;; Version: 22.0
-;; Last-Updated: Tue Aug 21 15:10:36 2012 (-0700)
+;; Last-Updated: Thu Aug 23 14:19:18 2012 (-0700)
 ;;           By: dradams
-;;     Update #: 5307
+;;     Update #: 5308
 ;; URL: http://www.emacswiki.org/cgi-bin/wiki/icicles-opt.el
+;; Doc URL: http://www.emacswiki.org/cgi-bin/wiki/Icicles
 ;; Keywords: internal, extensions, help, abbrev, local, minibuffer,
 ;;           keys, apropos, completion, matching, regexp, command
 ;; Compatibility: GNU Emacs: 20.x, 21.x, 22.x, 23.x, 24.x
