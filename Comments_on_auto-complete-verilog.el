d0a64d5ac3cdfdc81933c89a9d79c62e51d75937
Anonymous <anybody@emacswiki.org>
2012-09-11 01:27:06 +0200

    I wanted to use this with new auto-complete and I can't because new auto-complete doesn't have option to specify new dictionary the way this . . .

---
+++ Comments_on_auto-complete-verilog.el
@@ -0,0 +1,4 @@
+I wanted to use this with new auto-complete and I can't because new auto-complete doesn't have option to specify new dictionary the way this particular extension uses.
+
+-- Anonymous 2012-09-10 23:27 UTC
+
