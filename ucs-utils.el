7d2632e2b2c0e3bb9dbfd3715dd0f2699bf4e0b4
roland.walker <anybody@emacswiki.org>
2012-09-12 16:18:05 +0200

    documentation

--- ucs-utils.el
+++ ucs-utils.el
@@ -15,56 +15,80 @@
 ;;
 ;;; Commentary:
 ;;
-;; This library provides some utilities for manipulating Unicode
-;; characters.  There are three interactive commands
+;; Quickstart
 ;;
-;;    `ucs-utils-ucs-insert'       ; `ucs-insert' workalike using ido
-;;    `ucs-utils-eval'             ; the inverse of `ucs-insert'
-;;    `ucs-utils-install-aliases'  ; install shorter aliases
+;;      (require 'ucs-utils)
+;;
+;;      (ucs-utils-char "Middle Dot"         ; character to return
+;;                      ?.                   ; fallback if unavailable
+;;                      'char-displayable-p) ; test for character to pass
+;;
+;;      (ucs-utils-first-existing-char '("White Bullet"
+;;                                       "Bullet Operator"
+;;                                       "Circled Bullet"
+;;                                       "Middle Dot"
+;;                                       ?.) 'cdp)
+;;
+;;      (ucs-utils-string "Horizontal Ellipsis" '[["..."]])
+;;
+;; Explanation
+;;
+;; This library provides utilities for manipulating Unicode
+;; characters, with integrated ability to return fallback characters
+;; when Unicode display is not possible.
+;;
+;; Some ambiguities in Emacs' built-in Unicode data are resolved, and
+;; character support is updated to Unicode 6.1.
+;;
+;; There are three interactive commands
+;;
+;;     `ucs-utils-ucs-insert'       ; `ucs-insert' workalike using ido
+;;     `ucs-utils-eval'             ; the inverse of `ucs-insert'
+;;     `ucs-utils-install-aliases'  ; install shorter aliases
 ;;
 ;; The other functions are only useful from other Lisp code:
 ;;
-;;    `ucs-utils-char'
-;;    `ucs-utils-first-existing-char'
-;;    `ucs-utils-vector'
-;;    `ucs-utils-string'
-;;    `ucs-utils-intact-string'
-;;    `ucs-utils-pretty-name'
-;;    `ucs-utils-read-char-by-name'
-;;    `ucs-utils-subst-char-in-region'
+;;     `ucs-utils-char'
+;;     `ucs-utils-first-existing-char'
+;;     `ucs-utils-vector'
+;;     `ucs-utils-string'
+;;     `ucs-utils-intact-string'
+;;     `ucs-utils-pretty-name'
+;;     `ucs-utils-read-char-by-name'
+;;     `ucs-utils-subst-char-in-region'
 ;;
 ;; To use ucs-utils, place the ucs-utils.el library somewhere Emacs
 ;; can find it, and add the following to your ~/.emacs file:
 ;;
-;;    (require 'ucs-utils)
+;;     (require 'ucs-utils)
 ;;
 ;; and optionally
 ;;
-;;    (ucs-install-aliases)
+;;     (ucs-install-aliases)
 ;;
 ;; See Also
 ;;
-;;    M-x customize-group RET ucs-utils RET
-;;    http://en.wikipedia.org/wiki/Universal_Character_Set
+;;     M-x customize-group RET ucs-utils RET
+;;     http://en.wikipedia.org/wiki/Universal_Character_Set
 ;;
 ;; Notes
 ;;
 ;; Compatibility and Requirements
 ;;
-;;    Tested on GNU Emacs versions 23.3 and 24.1
+;;     Tested on GNU Emacs versions 23.3 and 24.1
 ;;
-;;    For full Emacs 23.x support, the library ucs-utils-6.0-delta.el
-;;    should also be installed.
+;;     For full Emacs 23.x support, the library ucs-utils-6.0-delta.el
+;;     should also be installed.
 ;;
-;;    Requires persistent-soft.el
+;;     Requires persistent-soft.el
 ;;
-;;    Uses if present: memoize.el
+;;     Uses if present: memoize.el
 ;;
 ;; Bugs
 ;;
 ;; TODO
 ;;
-;;    Accept synonyms on inputs? at least Tab would be nice.
+;;     Accept synonyms on inputs? at least Tab would be nice.
 ;;
 ;;; License
 ;;
