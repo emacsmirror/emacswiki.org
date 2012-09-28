43301657da4aa37f1e3b3e16df9f8c1686532e2f
DrewAdams <anybody@emacswiki.org>
2012-09-28 23:45:04 +0200

    no summary available

--- naked.el
+++ naked.el
@@ -7,9 +7,9 @@
 ;; Copyright (C) 2011-2012, Drew Adams, all rights reserved.
 ;; Created: Fri Oct  7 13:12:52 2011 (-0700)
 ;; Version: 21.0
-;; Last-Updated: Thu Sep 27 15:30:48 2012 (-0700)
+;; Last-Updated: Fri Sep 28 14:44:31 2012 (-0700)
 ;;           By: dradams
-;;     Update #: 119
+;;     Update #: 158
 ;; URL: http://www.emacswiki.org/emacs-en/naked.el
 ;; Doc URL: http://www.emacswiki.org/cgi-bin/wiki/NaKeD
 ;; Keywords: lisp, key, print, format, help
@@ -45,15 +45,14 @@
 ;;  stand for a function key named `forward', can you?
 ;;
 ;;  Well, yes you can, if the context makes things clear enough.  And
-;;  I for one (maybe the only one), find `C-M-<insert>' butt-ugly
-;;  (likewise `<C-M-insert>') - and, more importantly, insulting to
-;;  Occam.
+;;  I for one find `C-M-<insert>' butt-ugly (likewise `<C-M-insert>')
+;;  - and, more importantly, insulting to Occam and his razor.
 ;;
 ;;  So go ahead and go NaKeD - shed your angles.
 ;;
 ;;  Here's what you need.  It won't completely purge Emacs from
 ;;  insulting you with the occasional pair of angle brackets, but it
-;;  at least lets you DTRT in code you write:
+;;  at least lets you DTRT in code that you write:
 ;;
 ;;  * Use function `naked-key-description' instead of
 ;;    `key-description'.
@@ -67,7 +66,7 @@
 ;;
 ;;  * Use macro `naked' instead of macro `kbd'.
 ;;
-;;    The former expects its argument key-sequence description to use
+;;    The former allows its argument key-sequence description to use
 ;;    naked keys, not clothed in angle brackets.  E.g., (naked
 ;;    "C-M-delete") has the same effect as (kbd "C-M-<delete>").
 ;;
@@ -83,12 +82,21 @@
 ;;  function keys to be fully clothed in angle brackets.  E.g.: (naked
 ;;  "C-M-<delete>" t).
 ;;
+;;  In addition, even without an non-nil ANGLES argument, macro
+;;  `naked' does the right thing for keys expressed using angle
+;;  brackets.  IOW, (naked "M-<foobar>") returns the same thing that
+;;  (naked "M-foobar") does: [M-foobar].
+;;
 ;;  Enjoy!
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;; Change Log:
 ;;
+;; 2012/09/28 dadams
+;;     naked-edmacro-parse-keys:
+;;       Better fix for M-TAB.  Do not assume that function keys are lowercase.
+;;       Handle angle brackets even if ANGLES is nil.  But keep vanilla bug #12535 if non-nil.
 ;; 2012/09/27 dadams
 ;;     naked-edmacro-parse-keys: Fix handling of M-TAB.  Function keys are lowercase.
 ;;                               So M-TAB returns [134217737], but M-tab returns [M-tab].
@@ -132,11 +140,11 @@ Non-nil optional arg ANGLES means use angle brackets."
 ;; Differences are:
 ;;
 ;; 1. Addition of optional arg ANGLES.
-;; 2. Not handling (not expecting) angle brackets, unless ANGLES is non-nil.
-;; 3. Handling names without angle brackets, unless ANGLES is non-nil.
-;; 4. Thus, testing REM before [ACHMsS]- etc.
-;; 5. Expect symbols inside angle brackets to have at least two chars, the first of which is
-;;    a letter and the others alphanumeric or `-'.
+;; 2. Ensure same behavior as `edmacro-parse-keys', if ANGLES is non-nil.
+;; 2. Handle angle brackets, whether ANGLES is nil or non-nil.
+;; 3. Handle `TAB' correctly, if ANGLES is nil.
+;; 4. Handle names without angle brackets, if ANGLES is nil.
+;; 5. Works for all Emacs versions.
 ;;
 (defun naked-edmacro-parse-keys (string &optional need-vector angles)
   "Like `edmacro-parse-keys', but does not use angle brackets, by default.
@@ -153,7 +161,8 @@ ANGLES."
 	     (word      (substring string word-beg len))
 	     (times     1)
              (key       nil))
-        (if (and angles  (string-match "\\`<[^ <>\t\n\f][^>\t\n\f]*>" word))
+	;; Try to catch events of the form "<as df>".
+        (if (string-match "\\`<[^ <>\t\n\f][^>\t\n\f]*>" word)
             (setq word  (match-string 0 word)
                   pos   (+ word-beg (match-end 0)))
           (setq word  (substring string word-beg word-end)
@@ -162,28 +171,50 @@ ANGLES."
           (setq times  (string-to-number (substring word 0 (match-end 1)))
                 word   (substring word (1+ (match-end 1)))))
         (cond ((string-match "^<<.+>>$" word)
-               (setq key  (vconcat (if (eq (key-binding [?\M-x])
-                                           'execute-extended-command)
+               (setq key  (vconcat (if (eq (key-binding [?\M-x]) 'execute-extended-command)
                                        [?\M-x]
-                                     (or (car (where-is-internal
-                                               'execute-extended-command))
+                                     (or (car (where-is-internal 'execute-extended-command))
                                          [?\M-x]))
                                    (substring word 2 -2) "\r")))
+
+              ;; Must test this before [ACHMsS]- etc., to prevent match.
               ((or (equal word "REM") (string-match "^;;" word))
                (setq pos  (string-match "$" string pos)))
-              ((and (string-match (if angles
-                                      "^\\(\\([ACHMsS]-\\)*\\)<\\(..+\\)>$"
-                                    ;; Rely on the fact that function keys are lowercase.
-                                    "^\\(\\([ACHMsS]-\\)*\\)\\([a-z][-a-z0-9]+\\)$")
-                                  word)
-                    (or angles
-                        ;; Do not count `C-' etc. when at end of string.
-                        (save-match-data (not (string-match "\\([ACHMsS]-.\\)+$" word))))
+
+              ;; Straight `edmacro-parse-keys' case - ensure same behavior.
+              ;; Includes same bugged handling of `TAB'.  That is Emacs bug #12535.
+              ;; The bug fix is to add `TAB' to the list in this clause.
+	      ((and angles  (string-match "^\\(\\([ACHMsS]-\\)*\\)<\\(.+\\)>$" word)
+		    (progn
+		      (setq word  (concat (substring word (match-beginning 1)
+                                                     (match-end 1))
+                                          (substring word (match-beginning 3)
+                                                     (match-end 3))))
+		      (not (string-match "\\<\\(NUL\\|RET\\|LFD\\|ESC\\|SPC\\|DEL\\)$" word))))
+	       (setq key  (list (intern word))))
+
+              ;; NaKeD handling of <...>.  Recognize it anyway, even without non-nil ANGLES.
+              ;; But unlike `edmacro-parse-keys', include <TAB>, to handle it correctly.
+              ((and (string-match "^\\(\\([ACHMsS]-\\)*\\)<\\(..+\\)>$" word)
                     (progn
                       (setq word  (concat (substring word (match-beginning 1) (match-end 1))
                                           (substring word (match-beginning 3) (match-end 3))))
-                      (not (string-match "\\<\\(NUL\\|RET\\|LFD\\|ESC\\|SPC\\|DEL\\)$" word))))
+                      (not (string-match "\\<\\(NUL\\|RET\\|LFD\\|ESC\\|SPC\\|DEL\\|TAB\\)$"
+                                         word))))
                (setq key  (list (intern word))))
+
+              ;; NaKeD handling of names without <...>.
+              ((and (not angles)
+                    (string-match "^\\(\\([ACHMsS]-\\)*\\)\\([^ \t\f\n][^ \t\f\n]+\\)$" word)
+                    ;; Do not count `C-' etc. when at end of string.
+                    (save-match-data (not (string-match "\\([ACHMsS]-.\\)+$" word)))
+                    (progn
+                      (setq word  (concat (substring word (match-beginning 1) (match-end 1))
+                                          (substring word (match-beginning 3) (match-end 3))))
+                      (not (string-match "\\<\\(NUL\\|RET\\|LFD\\|ESC\\|SPC\\|DEL\\|TAB\\)$"
+                                         word))))
+               (setq key  (list (intern word))))
+              
               (t
                (let ((orig-word  word)
                      (prefix     0)
@@ -198,10 +229,9 @@ ANGLES."
                    (incf bits ?\C-\^@)
                    (incf prefix)
                    (callf substring word 1))
-                 (let ((found  (assoc word '(("NUL" . "\0") ("RET" . "\r")
-                                             ("LFD" . "\n") ("TAB" . "\t")
-                                             ("ESC" . "\e") ("SPC" . " ")
-                                             ("DEL" . "\177")))))
+                 (let ((found  (assoc word '(("NUL" . "\0") ("RET" . "\r") ("LFD" . "\n")
+                                             ("ESC" . "\e") ("SPC" . " ") ("DEL" . "\177")
+                                             ("TAB" . "\t")))))
                    (when found (setq word  (cdr found))))
                  (when (string-match "^\\\\[0-7]+$" word)
                    (loop for ch across word
