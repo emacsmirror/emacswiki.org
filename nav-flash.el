b02006d48cf75133ee7bf54bd4a4552520ae3c6e
roland.walker <anybody@emacswiki.org>
2012-10-14 00:58:03 +0200

    updated version

--- nav-flash.el
+++ nav-flash.el
@@ -5,8 +5,8 @@
 ;; Author: Roland Walker <walker@pobox.com>
 ;; Homepage: http://github.com/rolandwalker/nav-flash
 ;; URL: http://raw.github.com/rolandwalker/nav-flash/master/nav-flash.el
-;; Version: 1.0.1
-;; Last-Updated: 27 Aug 2012
+;; Version: 1.0.2
+;; Last-Updated: 14 Sep 2012
 ;; EmacsWiki: NavFlash
 ;; Keywords: extensions, navigation, interface
 ;;
@@ -17,6 +17,7 @@
 ;; Quickstart
 ;;
 ;;     (require 'nav-flash)
+;;
 ;;     (nav-flash-show)
 ;;
 ;; Explanation
@@ -27,52 +28,55 @@
 ;; To use nav-flash, place the nav-flash.el library somewhere Emacs
 ;; can find it, and add the following to your ~/.emacs file:
 ;;
-;;    (require 'nav-flash)
+;;     (require 'nav-flash)
 ;;
 ;; There is no user-level interface for this library; it is only used
 ;; by other Lisp libraries.  However, you might find it useful to call
 ;; `nav-flash-show' in your ~/.emacs file.  For example, the following
 ;; hook causes a flash to appear after navigating via imenu:
 ;;
-;;    (add-hook 'imenu-after-jump-hook 'nav-flash-show nil t)
+;;     (add-hook 'imenu-after-jump-hook 'nav-flash-show nil t)
 ;;
 ;; See Also
 ;;
-;;    M-x customize-group RET nav-flash RET
-;;    M-x customize-group RET pulse RET
+;;     M-x customize-group RET nav-flash RET
+;;     M-x customize-group RET pulse RET
 ;;
 ;; Notes
 ;;
-;;    This library reuses a timer and overlay defined in compile.el,
-;;    but should not affect the normal use of compile.el / `next-error'.
+;;     This library reuses a timer and overlay defined in compile.el,
+;;     but should not affect the normal use of compile.el / `next-error'.
 ;;
-;;    Pulse.el provides similar functionality and is included with
-;;    Emacs.  This library can use pulse.el, but does not do so by
-;;    default, because pulse.el uses `sit-for', breaking this type
-;;    of construction:
+;;     Pulse.el provides similar functionality and is included with
+;;     Emacs.  This library can use pulse.el, but does not do so by
+;;     default, because pulse.el uses `sit-for', breaking this type
+;;     of construction:
 ;;
-;;       (nav-flash-show)
-;;       (with-temp-message "message here"
-;;          (sit-for 2))
+;;         (nav-flash-show)
+;;         (with-temp-message "message here"
+;;            (sit-for 2))
 ;;
-;;    When using an overlay and timer for cleanup (as nav-flash does
-;;    by default) the flash and message appear simultaneously.
+;;     When using an overlay and timer for cleanup (as nav-flash does
+;;     by default) the flash and message appear simultaneously.
 ;;
-;;    Nav-flash.el is also simpler than pulse.el.
+;;     Nav-flash.el is also simpler than pulse.el.
 ;;
 ;; Compatibility and Requirements
 ;;
-;;    Tested on GNU Emacs versions 23.3 and 24.1
+;;     GNU Emacs version 24.3-devel     : yes, at the time of writing
+;;     GNU Emacs version 24.1 & 24.2    : yes
+;;     GNU Emacs version 23.3           : yes
+;;     GNU Emacs version 22.3 and lower : no
 ;;
-;;    No external dependencies
+;;     No external dependencies
 ;;
 ;; Bugs
 ;;
-;;    No known bugs.
+;;     No known bugs.
 ;;
 ;; TODO
 ;;
-;;    Check pulse period on other platforms.
+;;     Check pulse period on other platforms.
 ;;
 ;;; License
 ;;
@@ -115,8 +119,7 @@
 ;;; requires
 
 ;; for callf
-(eval-when-compile
-  (require 'cl))
+(require 'cl)
 
 ;; may use pulse.el if available
 (require 'pulse nil t)
@@ -127,7 +130,7 @@
 ;;;###autoload
 (defgroup nav-flash nil
   "Briefly highlight the current line."
-  :version "1.0.1"
+  :version "1.0.2"
   :link '(emacs-commentary-link "nav-flash")
   :prefix "nav-flash-"
   :group 'navigation
@@ -155,6 +158,14 @@ Setting this to nil or 0 will turn off the indicator."
   :group 'nav-flash)
 
 ;;; utility functions
+
+(defun nav-flash-use-pulse-p ()
+  "Return t if pulse.el should be used."
+  (and (fboundp 'pulse-available-p)
+       (pulse-available-p)
+       (or (eq nav-flash-use-pulse t)
+           (and (eq nav-flash-use-pulse 'gui-only)
+                (display-graphic-p)))))
 
 ;;;###autoload
 (defun nav-flash-show (&optional pos end-pos face delay)
@@ -203,14 +214,6 @@ this function a no-op."
        (setq next-error-highlight-timer
              (run-at-time delay nil 'compilation-goto-locus-delete-o)))))
 
-(defun nav-flash-use-pulse-p ()
-  "Return t if pulse.el should be used."
-  (and (fboundp 'pulse-available-p)
-       (pulse-available-p)
-       (or (eq nav-flash-use-pulse t)
-           (and (eq nav-flash-use-pulse 'gui-only)
-                (display-graphic-p)))))
-
 (provide 'nav-flash)
 
 ;;
@@ -221,6 +224,7 @@ this function a no-op."
 ;; mangle-whitespace: t
 ;; require-final-newline: t
 ;; coding: utf-8
+;; byte-compile-warnings: (not cl-functions redefine)
 ;; End:
 ;;
 ;; LocalWords:  NavFlash imenu callf
