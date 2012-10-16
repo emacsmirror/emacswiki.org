07e9135b9ded72e86510cc164fe27e0a2b30822c
MaartenG <anybody@emacswiki.org>
2012-10-16 21:35:10 +0200

    no summary available

--- tracker-dired.el
+++ tracker-dired.el
@@ -1,194 +1,123 @@
-;;; tracker-dired.el --- run a tracker-search query and dired the output
-
-;; Copyright (C) 1992, 1994, 1995 Free Software Foundation, Inc.
-
-;; Author: Roland McGrath <roland (at) gnu. ai. mit. edu>,
-;;	   Sebastian Kremer <sk (at) thp. uni-koeln. de>
-;;         Maarten Grachten <maarten. grachten (at) gmail. com>
-;; Maintainer: FSF
-;; Keywords: unix
-
-;; This file is part of GNU Emacs.
-
-;; GNU Emacs is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 2, or (at your option)
-;; any later version.
-
-;; GNU Emacs is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING.  If not, write to the
-;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-;; Boston, MA 02111-1307, USA.
-
-;;; Code:
-
-(require 'dired)
-(require 'dbus)
-(require 'gnus-util)
-
-(defun djcb-call-tracker (method &rest args)
-  "call the tracker method METHOD with ARGS over dbus"
-  (apply 'dbus-call-method 
-    :session                            ; use the session (not system) bus
-    "org.freedesktop.Tracker1"                  ; service name
-    "/org/freedesktop/Tracker1/Resources"   ; path name
-    "org.freedesktop.Tracker1.Resources"    ; interface name
-    method args))
-
-(defun search (query)
-  (let ((results (djcb-call-tracker 
-		  "SparqlQuery" 
-		  (concat "SELECT nie:url(?f) WHERE { ?f fts:match '" 
-			  query 
-			  "' . ?f nie:mimeType ?m . FILTER ( fn:contains(?m,'text/') )}"))))
-	(mapcar '(lambda (x) 
-		   (concat " '" 
-			   (substring (gnus-url-unhex-string (car x) nil) 7) 
-			   "'"))
-		results)))
-
-(defgroup tracker-dired nil
-  "Run a tracker-search query and dired the output."
-  :group 'dired
-  :prefix "tracker-")
-
-;; find's -ls corresponds to these switches.
-;; Note -b, at least GNU find quotes spaces etc. in filenames
-;;;###autoload
-(defcustom tracker-ls-option
-  (if (eq system-type 'berkeley-unix) '("-ls" . "-gilsb")
-    '("-exec ls -ld {} \\;" . "-ld"))
-  "*Description of the option to `find' to produce an `ls -l'-type listing.
-This is a cons of two strings (TRACKER-OPTION . LS-SWITCHES).  TRACKER-OPTION
-gives the option (or options) to `find' that produce the desired output.
-LS-SWITCHES is a list of `ls' switches to tell dired how to parse the output."
-  :type '(cons (string :tag "Find Option")
-	       (string :tag "Ls Switches"))
-  :group 'tracker-dired)
-
-;;;###autoload
-
-(defvar tracker-queries nil
-  "Last queries given to Tracker by \\[tracker-dired].")
-
-;; History of tracker-queries values entered in the minibuffer.
-(defvar tracker-queries-history nil)
-
-;;;###autoload
-(defun tracker-dired (search-term)
-  "Run `find' and go into Dired mode on a buffer of the output.
-The command run (after changing into DIR) is
-
-    find . \\( ARGS \\) -ls
-
-except that the variable `tracker-ls-option' specifies what to use
-as the final argument."
-  (interactive (list (read-string "Search terms: " tracker-queries
-				  '(tracker-queries-history . 1))))
-  (let ((results (search search-term)))
-	
-    (switch-to-buffer (get-buffer-create "*Tracker-Results*"))
-    (widen)
-    (kill-all-local-variables)
-    (setq buffer-read-only nil)
-    (erase-buffer)
-    (setq tracker-queries search-term)
-
-    (dired-mode)
-
-    (use-local-map (append (make-sparse-keymap) (current-local-map)))
-    (define-key (current-local-map) "g" 'undefined)
-    ;; Set subdir-alist so that Tree Dired will work:
-    
-    (if (fboundp 'dired-simple-subdir-alist)
-	;; will work even with nested dired format (dired-nstd.el,v 1.15
-	;; and later)
-	(dired-simple-subdir-alist)
-      ;; else we have an ancient tree dired (or classic dired, where
-      ;; this does no harm) 
-      (set (make-local-variable 'dired-subdir-alist)
-	   (list (cons default-directory (point-min-marker)))))
-
-    (setq buffer-read-only nil)
-    ;; Subdir headlerline must come first because the first marker in
-    ;; subdir-alist points there.
-    ;; ``wildcard'' line. 
-    (insert "Tracker search results for \"" search-term 
-	    "\" (" (format "%d" (length results)) " matches)\n\n")
-    ;; Start the process that pipes the results of the tracker search through ls -ld.
-    (let ((proc (start-process-shell-command "Tracker Search" (current-buffer) 
-					     (concat "ls -ld" (apply 'concat results)))))
-      (set-process-filter proc (function tracker-dired-filter))
-      (set-process-sentinel proc (function tracker-dired-sentinel))
-      ;; Initialize the process marker; it is used by the filter.
-      (move-marker (process-mark proc) 1 (current-buffer)))
-    (setq mode-line-process '(":%s"))))
-
-(defun tracker-dired-filter (proc string)
-  ;; Filter for \\[tracker-dired] processes.
-  (let ((buf (process-buffer proc)))
-    (if (buffer-name buf)		; not killed?
-	(save-excursion
-	  (set-buffer buf)
-	  (save-restriction
-	    (widen)
-	    (save-excursion
-	      (let ((buffer-read-only nil)
-		    (end (point-max)))
-		(goto-char end)
-		(insert string)
-		(goto-char end)
-		(or (looking-at "^")
-		    (forward-line 1))
-		(while (looking-at "^")
-		  (insert "  ")
-		  (forward-line 1))
-		;; Convert ` ./FILE' to ` FILE'
-		;; This would lose if the current chunk of output
-		;; starts or ends within the ` ./', so back up a bit:
-		(goto-char (- end 3))	; no error if < 0
-		(while (search-forward " ./" nil t)
-		  (delete-region (point) (- (point) 2)))
-		;; Find all the complete lines in the unprocessed
-		;; output and process it to add text properties.
-		(goto-char end)
-		(if (search-backward "\n" (process-mark proc) t)
-		    (progn
-		      (dired-insert-set-properties (process-mark proc)
-						   (1+ (point)))
-		      (move-marker (process-mark proc) (1+ (point)))))
-		))))
-      ;; The buffer has been killed.
-      (delete-process proc))))
-
-(defun tracker-dired-sentinel (proc state)
-  ;; Sentinel for \\[tracker-dired] processes.
-  (let ((buf (process-buffer proc)))
-    (if (buffer-name buf)
-	(save-excursion
-	  (set-buffer buf)
-	  (let ((buffer-read-only nil))
-	    (save-excursion
-	      (goto-char (point-max))
-	      (insert "\nTracker search " state)
-	      (forward-char -1)		;Back up before \n at end of STATE.
-	      (insert " at " (substring (current-time-string) 0 19))
-	      (forward-char 1)
-	      (setq mode-line-process
-		    (concat ":"
-			    (symbol-name (process-status proc))))
-	      ;; Since the buffer and mode line will show that the
-	      ;; process is dead, we can delete it now.  Otherwise it
-	      ;; will stay around until M-x list-processes.
-	      (delete-process proc)
-	      (force-mode-line-update)))
-	  (message "tracker-dired %s finished." (current-buffer))))))
-
-(provide 'tracker-dired)
-
-;;; tracker-dired.el ends here
+#FILE text/x-emacs-lisp 
+Ozs7IHRyYWNrZXItZGlyZWQuZWwgLS0tIHJ1biBhIHRyYWNrZXItc2VhcmNoIHF1ZXJ5IGFuZCBk
+aXJlZCB0aGUgb3V0cHV0Cgo7OyBDb3B5cmlnaHQgKEMpIDE5OTIsIDE5OTQsIDE5OTUgRnJlZSBT
+b2Z0d2FyZSBGb3VuZGF0aW9uLCBJbmMuCgo7OyBBdXRob3I6IFJvbGFuZCBNY0dyYXRoIDxyb2xh
+bmQgKGF0KSBnbnUuIGFpLiBtaXQuIGVkdT4sCjs7CSAgIFNlYmFzdGlhbiBLcmVtZXIgPHNrIChh
+dCkgdGhwLiB1bmkta29lbG4uIGRlPgo7OyAgICAgICAgIE1hYXJ0ZW4gR3JhY2h0ZW4gCjs7IE1h
+aW50YWluZXI6IEZTRgo7OyBLZXl3b3JkczogdW5peAoKOzsgVGhpcyBmaWxlIGlzIHBhcnQgb2Yg
+R05VIEVtYWNzLgoKOzsgR05VIEVtYWNzIGlzIGZyZWUgc29mdHdhcmU7IHlvdSBjYW4gcmVkaXN0
+cmlidXRlIGl0IGFuZC9vciBtb2RpZnkKOzsgaXQgdW5kZXIgdGhlIHRlcm1zIG9mIHRoZSBHTlUg
+R2VuZXJhbCBQdWJsaWMgTGljZW5zZSBhcyBwdWJsaXNoZWQgYnkKOzsgdGhlIEZyZWUgU29mdHdh
+cmUgRm91bmRhdGlvbjsgZWl0aGVyIHZlcnNpb24gMiwgb3IgKGF0IHlvdXIgb3B0aW9uKQo7OyBh
+bnkgbGF0ZXIgdmVyc2lvbi4KCjs7IEdOVSBFbWFjcyBpcyBkaXN0cmlidXRlZCBpbiB0aGUgaG9w
+ZSB0aGF0IGl0IHdpbGwgYmUgdXNlZnVsLAo7OyBidXQgV0lUSE9VVCBBTlkgV0FSUkFOVFk7IHdp
+dGhvdXQgZXZlbiB0aGUgaW1wbGllZCB3YXJyYW50eSBvZgo7OyBNRVJDSEFOVEFCSUxJVFkgb3Ig
+RklUTkVTUyBGT1IgQSBQQVJUSUNVTEFSIFBVUlBPU0UuICBTZWUgdGhlCjs7IEdOVSBHZW5lcmFs
+IFB1YmxpYyBMaWNlbnNlIGZvciBtb3JlIGRldGFpbHMuCgo7OyBZb3Ugc2hvdWxkIGhhdmUgcmVj
+ZWl2ZWQgYSBjb3B5IG9mIHRoZSBHTlUgR2VuZXJhbCBQdWJsaWMgTGljZW5zZQo7OyBhbG9uZyB3
+aXRoIEdOVSBFbWFjczsgc2VlIHRoZSBmaWxlIENPUFlJTkcuICBJZiBub3QsIHdyaXRlIHRvIHRo
+ZQo7OyBGcmVlIFNvZnR3YXJlIEZvdW5kYXRpb24sIEluYy4sIDU5IFRlbXBsZSBQbGFjZSAtIFN1
+aXRlIDMzMCwKOzsgQm9zdG9uLCBNQSAwMjExMS0xMzA3LCBVU0EuCgo7OzsgQ29kZToKCihyZXF1
+aXJlICdkaXJlZCkKKHJlcXVpcmUgJ2RidXMpCihyZXF1aXJlICdnbnVzLXV0aWwpCgooZGVmdW4g
+ZGpjYi1jYWxsLXRyYWNrZXIgKG1ldGhvZCAmcmVzdCBhcmdzKQogICJjYWxsIHRoZSB0cmFja2Vy
+IG1ldGhvZCBNRVRIT0Qgd2l0aCBBUkdTIG92ZXIgZGJ1cyIKICAoYXBwbHkgJ2RidXMtY2FsbC1t
+ZXRob2QgCiAgICA6c2Vzc2lvbiAgICAgICAgICAgICAgICAgICAgICAgICAgICA7IHVzZSB0aGUg
+c2Vzc2lvbiAobm90IHN5c3RlbSkgYnVzCiAgICAib3JnLmZyZWVkZXNrdG9wLlRyYWNrZXIxIiAg
+ICAgICAgICAgICAgICAgIDsgc2VydmljZSBuYW1lCiAgICAiL29yZy9mcmVlZGVza3RvcC9UcmFj
+a2VyMS9SZXNvdXJjZXMiICAgOyBwYXRoIG5hbWUKICAgICJvcmcuZnJlZWRlc2t0b3AuVHJhY2tl
+cjEuUmVzb3VyY2VzIiAgICA7IGludGVyZmFjZSBuYW1lCiAgICBtZXRob2QgYXJncykpCgooZGVm
+dW4gc2VhcmNoIChxdWVyeSkKICAobGV0ICgocmVzdWx0cyAoZGpjYi1jYWxsLXRyYWNrZXIgCgkJ
+ICAiU3BhcnFsUXVlcnkiIAoJCSAgKGNvbmNhdCAiU0VMRUNUIG5pZTp1cmwoP2YpIFdIRVJFIHsg
+P2YgZnRzOm1hdGNoICciIAoJCQkgIHF1ZXJ5IAoJCQkgICInIC4gP2YgbmllOm1pbWVUeXBlID9t
+IC4gRklMVEVSICggZm46Y29udGFpbnMoP20sJ3RleHQvJykgKX0iKSkpKQoJKG1hcGNhciAnKGxh
+bWJkYSAoeCkgCgkJICAgKGNvbmNhdCAiICciIAoJCQkgICAoc3Vic3RyaW5nIChnbnVzLXVybC11
+bmhleC1zdHJpbmcgKGNhciB4KSBuaWwpIDcpIAoJCQkgICAiJyIpKQoJCXJlc3VsdHMpKSkKCihk
+ZWZncm91cCB0cmFja2VyLWRpcmVkIG5pbAogICJSdW4gYSB0cmFja2VyLXNlYXJjaCBxdWVyeSBh
+bmQgZGlyZWQgdGhlIG91dHB1dC4iCiAgOmdyb3VwICdkaXJlZAogIDpwcmVmaXggInRyYWNrZXIt
+IikKCjs7IGZpbmQncyAtbHMgY29ycmVzcG9uZHMgdG8gdGhlc2Ugc3dpdGNoZXMuCjs7IE5vdGUg
+LWIsIGF0IGxlYXN0IEdOVSBmaW5kIHF1b3RlcyBzcGFjZXMgZXRjLiBpbiBmaWxlbmFtZXMKOzs7
+IyMjYXV0b2xvYWQKKGRlZmN1c3RvbSB0cmFja2VyLWxzLW9wdGlvbgogIChpZiAoZXEgc3lzdGVt
+LXR5cGUgJ2JlcmtlbGV5LXVuaXgpICcoIi1scyIgLiAiLWdpbHNiIikKICAgICcoIi1leGVjIGxz
+IC1sZCB7fSBcXDsiIC4gIi1sZCIpKQogICIqRGVzY3JpcHRpb24gb2YgdGhlIG9wdGlvbiB0byBg
+ZmluZCcgdG8gcHJvZHVjZSBhbiBgbHMgLWwnLXR5cGUgbGlzdGluZy4KVGhpcyBpcyBhIGNvbnMg
+b2YgdHdvIHN0cmluZ3MgKFRSQUNLRVItT1BUSU9OIC4gTFMtU1dJVENIRVMpLiAgVFJBQ0tFUi1P
+UFRJT04KZ2l2ZXMgdGhlIG9wdGlvbiAob3Igb3B0aW9ucykgdG8gYGZpbmQnIHRoYXQgcHJvZHVj
+ZSB0aGUgZGVzaXJlZCBvdXRwdXQuCkxTLVNXSVRDSEVTIGlzIGEgbGlzdCBvZiBgbHMnIHN3aXRj
+aGVzIHRvIHRlbGwgZGlyZWQgaG93IHRvIHBhcnNlIHRoZSBvdXRwdXQuIgogIDp0eXBlICcoY29u
+cyAoc3RyaW5nIDp0YWcgIkZpbmQgT3B0aW9uIikKCSAgICAgICAoc3RyaW5nIDp0YWcgIkxzIFN3
+aXRjaGVzIikpCiAgOmdyb3VwICd0cmFja2VyLWRpcmVkKQoKOzs7IyMjYXV0b2xvYWQKCihkZWZ2
+YXIgdHJhY2tlci1xdWVyaWVzIG5pbAogICJMYXN0IHF1ZXJpZXMgZ2l2ZW4gdG8gVHJhY2tlciBi
+eSBcXFt0cmFja2VyLWRpcmVkXS4iKQoKOzsgSGlzdG9yeSBvZiB0cmFja2VyLXF1ZXJpZXMgdmFs
+dWVzIGVudGVyZWQgaW4gdGhlIG1pbmlidWZmZXIuCihkZWZ2YXIgdHJhY2tlci1xdWVyaWVzLWhp
+c3RvcnkgbmlsKQoKOzs7IyMjYXV0b2xvYWQKKGRlZnVuIHRyYWNrZXItZGlyZWQgKHNlYXJjaC10
+ZXJtKQogICJSdW4gYGZpbmQnIGFuZCBnbyBpbnRvIERpcmVkIG1vZGUgb24gYSBidWZmZXIgb2Yg
+dGhlIG91dHB1dC4KVGhlIGNvbW1hbmQgcnVuIChhZnRlciBjaGFuZ2luZyBpbnRvIERJUikgaXMK
+CiAgICBmaW5kIC4gXFwoIEFSR1MgXFwpIC1scwoKZXhjZXB0IHRoYXQgdGhlIHZhcmlhYmxlIGB0
+cmFja2VyLWxzLW9wdGlvbicgc3BlY2lmaWVzIHdoYXQgdG8gdXNlCmFzIHRoZSBmaW5hbCBhcmd1
+bWVudC4iCiAgKGludGVyYWN0aXZlIChsaXN0IChyZWFkLXN0cmluZyAiU2VhcmNoIHRlcm1zOiAi
+IHRyYWNrZXItcXVlcmllcwoJCQkJICAnKHRyYWNrZXItcXVlcmllcy1oaXN0b3J5IC4gMSkpKSkK
+ICAobGV0ICgocmVzdWx0cyAoc2VhcmNoIHNlYXJjaC10ZXJtKSkpCgkKICAgIChzd2l0Y2gtdG8t
+YnVmZmVyIChnZXQtYnVmZmVyLWNyZWF0ZSAiKlRyYWNrZXItUmVzdWx0cyoiKSkKICAgICh3aWRl
+bikKICAgIChraWxsLWFsbC1sb2NhbC12YXJpYWJsZXMpCiAgICAoc2V0cSBidWZmZXItcmVhZC1v
+bmx5IG5pbCkKICAgIChlcmFzZS1idWZmZXIpCiAgICAoc2V0cSB0cmFja2VyLXF1ZXJpZXMgc2Vh
+cmNoLXRlcm0pCgogICAgKGRpcmVkLW1vZGUpCgogICAgKHVzZS1sb2NhbC1tYXAgKGFwcGVuZCAo
+bWFrZS1zcGFyc2Uta2V5bWFwKSAoY3VycmVudC1sb2NhbC1tYXApKSkKICAgIChkZWZpbmUta2V5
+IChjdXJyZW50LWxvY2FsLW1hcCkgImciICd1bmRlZmluZWQpCiAgICA7OyBTZXQgc3ViZGlyLWFs
+aXN0IHNvIHRoYXQgVHJlZSBEaXJlZCB3aWxsIHdvcms6CiAgICAKICAgIChpZiAoZmJvdW5kcCAn
+ZGlyZWQtc2ltcGxlLXN1YmRpci1hbGlzdCkKCTs7IHdpbGwgd29yayBldmVuIHdpdGggbmVzdGVk
+IGRpcmVkIGZvcm1hdCAoZGlyZWQtbnN0ZC5lbCx2IDEuMTUKCTs7IGFuZCBsYXRlcikKCShkaXJl
+ZC1zaW1wbGUtc3ViZGlyLWFsaXN0KQogICAgICA7OyBlbHNlIHdlIGhhdmUgYW4gYW5jaWVudCB0
+cmVlIGRpcmVkIChvciBjbGFzc2ljIGRpcmVkLCB3aGVyZQogICAgICA7OyB0aGlzIGRvZXMgbm8g
+aGFybSkgCiAgICAgIChzZXQgKG1ha2UtbG9jYWwtdmFyaWFibGUgJ2RpcmVkLXN1YmRpci1hbGlz
+dCkKCSAgIChsaXN0IChjb25zIGRlZmF1bHQtZGlyZWN0b3J5IChwb2ludC1taW4tbWFya2VyKSkp
+KSkKCiAgICAoc2V0cSBidWZmZXItcmVhZC1vbmx5IG5pbCkKICAgIDs7IFN1YmRpciBoZWFkbGVy
+bGluZSBtdXN0IGNvbWUgZmlyc3QgYmVjYXVzZSB0aGUgZmlyc3QgbWFya2VyIGluCiAgICA7OyBz
+dWJkaXItYWxpc3QgcG9pbnRzIHRoZXJlLgogICAgOzsgYGB3aWxkY2FyZCcnIGxpbmUuIAogICAg
+KGluc2VydCAiVHJhY2tlciBzZWFyY2ggcmVzdWx0cyBmb3IgXCIiIHNlYXJjaC10ZXJtIAoJICAg
+ICJcIiAoIiAoZm9ybWF0ICIlZCIgKGxlbmd0aCByZXN1bHRzKSkgIiBtYXRjaGVzKVxuXG4iKQog
+ICAgOzsgU3RhcnQgdGhlIHByb2Nlc3MgdGhhdCBwaXBlcyB0aGUgcmVzdWx0cyBvZiB0aGUgdHJh
+Y2tlciBzZWFyY2ggdGhyb3VnaCBscyAtbGQuCiAgICAobGV0ICgocHJvYyAoc3RhcnQtcHJvY2Vz
+cy1zaGVsbC1jb21tYW5kICJUcmFja2VyIFNlYXJjaCIgKGN1cnJlbnQtYnVmZmVyKSAKCQkJCQkg
+ICAgIChjb25jYXQgImxzIC1sZCIgKGFwcGx5ICdjb25jYXQgcmVzdWx0cykpKSkpCiAgICAgIChz
+ZXQtcHJvY2Vzcy1maWx0ZXIgcHJvYyAoZnVuY3Rpb24gdHJhY2tlci1kaXJlZC1maWx0ZXIpKQog
+ICAgICAoc2V0LXByb2Nlc3Mtc2VudGluZWwgcHJvYyAoZnVuY3Rpb24gdHJhY2tlci1kaXJlZC1z
+ZW50aW5lbCkpCiAgICAgIDs7IEluaXRpYWxpemUgdGhlIHByb2Nlc3MgbWFya2VyOyBpdCBpcyB1
+c2VkIGJ5IHRoZSBmaWx0ZXIuCiAgICAgIChtb3ZlLW1hcmtlciAocHJvY2Vzcy1tYXJrIHByb2Mp
+IDEgKGN1cnJlbnQtYnVmZmVyKSkpCiAgICAoc2V0cSBtb2RlLWxpbmUtcHJvY2VzcyAnKCI6JXMi
+KSkpKQoKKGRlZnVuIHRyYWNrZXItZGlyZWQtZmlsdGVyIChwcm9jIHN0cmluZykKICA7OyBGaWx0
+ZXIgZm9yIFxcW3RyYWNrZXItZGlyZWRdIHByb2Nlc3Nlcy4KICAobGV0ICgoYnVmIChwcm9jZXNz
+LWJ1ZmZlciBwcm9jKSkpCiAgICAoaWYgKGJ1ZmZlci1uYW1lIGJ1ZikJCTsgbm90IGtpbGxlZD8K
+CShzYXZlLWV4Y3Vyc2lvbgoJICAoc2V0LWJ1ZmZlciBidWYpCgkgIChzYXZlLXJlc3RyaWN0aW9u
+CgkgICAgKHdpZGVuKQoJICAgIChzYXZlLWV4Y3Vyc2lvbgoJICAgICAgKGxldCAoKGJ1ZmZlci1y
+ZWFkLW9ubHkgbmlsKQoJCSAgICAoZW5kIChwb2ludC1tYXgpKSkKCQkoZ290by1jaGFyIGVuZCkK
+CQkoaW5zZXJ0IHN0cmluZykKCQkoZ290by1jaGFyIGVuZCkKCQkob3IgKGxvb2tpbmctYXQgIl4i
+KQoJCSAgICAoZm9yd2FyZC1saW5lIDEpKQoJCSh3aGlsZSAobG9va2luZy1hdCAiXiIpCgkJICAo
+aW5zZXJ0ICIgICIpCgkJICAoZm9yd2FyZC1saW5lIDEpKQoJCTs7IENvbnZlcnQgYCAuL0ZJTEUn
+IHRvIGAgRklMRScKCQk7OyBUaGlzIHdvdWxkIGxvc2UgaWYgdGhlIGN1cnJlbnQgY2h1bmsgb2Yg
+b3V0cHV0CgkJOzsgc3RhcnRzIG9yIGVuZHMgd2l0aGluIHRoZSBgIC4vJywgc28gYmFjayB1cCBh
+IGJpdDoKCQkoZ290by1jaGFyICgtIGVuZCAzKSkJOyBubyBlcnJvciBpZiA8IDAKCQkod2hpbGUg
+KHNlYXJjaC1mb3J3YXJkICIgLi8iIG5pbCB0KQoJCSAgKGRlbGV0ZS1yZWdpb24gKHBvaW50KSAo
+LSAocG9pbnQpIDIpKSkKCQk7OyBGaW5kIGFsbCB0aGUgY29tcGxldGUgbGluZXMgaW4gdGhlIHVu
+cHJvY2Vzc2VkCgkJOzsgb3V0cHV0IGFuZCBwcm9jZXNzIGl0IHRvIGFkZCB0ZXh0IHByb3BlcnRp
+ZXMuCgkJKGdvdG8tY2hhciBlbmQpCgkJKGlmIChzZWFyY2gtYmFja3dhcmQgIlxuIiAocHJvY2Vz
+cy1tYXJrIHByb2MpIHQpCgkJICAgIChwcm9nbgoJCSAgICAgIChkaXJlZC1pbnNlcnQtc2V0LXBy
+b3BlcnRpZXMgKHByb2Nlc3MtbWFyayBwcm9jKQoJCQkJCQkgICAoMSsgKHBvaW50KSkpCgkJICAg
+ICAgKG1vdmUtbWFya2VyIChwcm9jZXNzLW1hcmsgcHJvYykgKDErIChwb2ludCkpKSkpCgkJKSkp
+KQogICAgICA7OyBUaGUgYnVmZmVyIGhhcyBiZWVuIGtpbGxlZC4KICAgICAgKGRlbGV0ZS1wcm9j
+ZXNzIHByb2MpKSkpCgooZGVmdW4gdHJhY2tlci1kaXJlZC1zZW50aW5lbCAocHJvYyBzdGF0ZSkK
+ICA7OyBTZW50aW5lbCBmb3IgXFxbdHJhY2tlci1kaXJlZF0gcHJvY2Vzc2VzLgogIChsZXQgKChi
+dWYgKHByb2Nlc3MtYnVmZmVyIHByb2MpKSkKICAgIChpZiAoYnVmZmVyLW5hbWUgYnVmKQoJKHNh
+dmUtZXhjdXJzaW9uCgkgIChzZXQtYnVmZmVyIGJ1ZikKCSAgKGxldCAoKGJ1ZmZlci1yZWFkLW9u
+bHkgbmlsKSkKCSAgICAoc2F2ZS1leGN1cnNpb24KCSAgICAgIChnb3RvLWNoYXIgKHBvaW50LW1h
+eCkpCgkgICAgICAoaW5zZXJ0ICJcblRyYWNrZXIgc2VhcmNoICIgc3RhdGUpCgkgICAgICAoZm9y
+d2FyZC1jaGFyIC0xKQkJO0JhY2sgdXAgYmVmb3JlIFxuIGF0IGVuZCBvZiBTVEFURS4KCSAgICAg
+IChpbnNlcnQgIiBhdCAiIChzdWJzdHJpbmcgKGN1cnJlbnQtdGltZS1zdHJpbmcpIDAgMTkpKQoJ
+ICAgICAgKGZvcndhcmQtY2hhciAxKQoJICAgICAgKHNldHEgbW9kZS1saW5lLXByb2Nlc3MKCQkg
+ICAgKGNvbmNhdCAiOiIKCQkJICAgIChzeW1ib2wtbmFtZSAocHJvY2Vzcy1zdGF0dXMgcHJvYykp
+KSkKCSAgICAgIDs7IFNpbmNlIHRoZSBidWZmZXIgYW5kIG1vZGUgbGluZSB3aWxsIHNob3cgdGhh
+dCB0aGUKCSAgICAgIDs7IHByb2Nlc3MgaXMgZGVhZCwgd2UgY2FuIGRlbGV0ZSBpdCBub3cuICBP
+dGhlcndpc2UgaXQKCSAgICAgIDs7IHdpbGwgc3RheSBhcm91bmQgdW50aWwgTS14IGxpc3QtcHJv
+Y2Vzc2VzLgoJICAgICAgKGRlbGV0ZS1wcm9jZXNzIHByb2MpCgkgICAgICAoZm9yY2UtbW9kZS1s
+aW5lLXVwZGF0ZSkpKQoJICAobWVzc2FnZSAidHJhY2tlci1kaXJlZCAlcyBmaW5pc2hlZC4iIChj
+dXJyZW50LWJ1ZmZlcikpKSkpKQoKKHByb3ZpZGUgJ3RyYWNrZXItZGlyZWQpCgo7OzsgdHJhY2tl
+ci1kaXJlZC5lbCBlbmRzIGhlcmUK
