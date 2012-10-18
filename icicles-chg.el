bd5f4586ec63ad1b7cd54e1b2c9ef7eb8d065aa4
DrewAdams <anybody@emacswiki.org>
2012-10-18 23:07:39 +0200

    no summary available

--- icicles-chg.el
+++ icicles-chg.el
@@ -7,9 +7,9 @@
 ;; Copyright (C) 2007-2012, Drew Adams, all rights reserved.
 ;; Created: Tue Nov 27 07:47:53 2007
 ;; Version: 22.0
-;; Last-Updated: Thu Oct 18 08:26:13 2012 (-0700)
+;; Last-Updated: Thu Oct 18 14:01:25 2012 (-0700)
 ;;           By: dradams
-;;     Update #: 9102
+;;     Update #: 9103
 ;; URL: http://www.emacswiki.org/cgi-bin/wiki/icicles-chg.el
 ;; Doc URL: http://www.emacswiki.org/cgi-bin/wiki/Icicles
 ;; Keywords: extensions, help, abbrev, local, minibuffer,
@@ -88,6 +88,7 @@
 ;;
 ;; 2012/10/18 dadams
 ;;     icicle-locate-file-1: Ensure correct prefix arg treatment for icicle-locate (vs *-locate-file*).
+;;     icicle-string-list: Last code: reset icicle-proxy-candidates to ().
 ;; 2012/10/09 dadams
 ;;     icicle-customize-face*, icicle(-customize)-apropos-options-of-type,
 ;;       icicle-describe-option-of-type, icicle-dired-saved-file-candidates*, icicle-bookmark*,
