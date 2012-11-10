b30239bd39d073015d0dd56a37ddecc4a8b8696e
DrewAdams <anybody@emacswiki.org>
2012-11-10 16:43:56 +0100

    no summary available

--- icicles-chg.el
+++ icicles-chg.el
@@ -7,9 +7,9 @@
 ;; Copyright (C) 2007-2012, Drew Adams, all rights reserved.
 ;; Created: Tue Nov 27 07:47:53 2007
 ;; Version: 22.0
-;; Last-Updated: Thu Nov  8 13:48:47 2012 (-0800)
+;; Last-Updated: Sat Nov 10 07:38:53 2012 (-0800)
 ;;           By: dradams
-;;     Update #: 9257
+;;     Update #: 9258
 ;; URL: http://www.emacswiki.org/cgi-bin/wiki/icicles-chg.el
 ;; Doc URL: http://www.emacswiki.org/Icicles
 ;; Keywords: extensions, help, abbrev, local, minibuffer,
@@ -1530,6 +1530,8 @@
  
 ;;;(@* "CHANGE LOG FOR `icicles-fn.el'")
 ;;
+;; 2012/11/10 dadams
+;;     icicle-fuzzy-candidates: Show all (not no) candidates for empty input.
 ;; 2012/11/08 dadams
 ;;     Added: icicle-get-safe.
 ;;     icicle-completion-setup-function, icicle-var-(is-of|matches|inherits)-type-p,
