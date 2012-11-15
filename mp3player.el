5a1ac95db3f13cbeb59c233a04b434cdd56f85a1
AlexSchroeder <anybody@emacswiki.org>
2012-11-15 11:36:06 +0100

    encoding

--- mp3player.el
+++ mp3player.el
@@ -1280,7 +1280,7 @@ and replace a sub-expression, e.g.
 				   (buffer-substring 
 				    (+ (point) start) 
 				    (+ (point) end))
-				   "\x0")))))
+				   (char-to-string 0))))))
 	    (make-id3
 	     :song (retrieve-info 3 33)
 	     :artist (retrieve-info 33 63)
