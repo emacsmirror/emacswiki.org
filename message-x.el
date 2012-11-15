de9c45d4863d7f032b04248901ef0704cd9f86fb
AlexSchroeder <anybody@emacswiki.org>
2012-11-15 11:19:43 +0100

    encoding

--- message-x.el
+++ message-x.el
@@ -1,5 +1,5 @@
 ;;; message-x.el -- customizable completion in message headers
-;; Copyright (C) 1998 Kai Gro\xDFjohann
+;; Copyright (C) 1998 Kai Großjohann
 
 ;; $Id: message-x.el,v 1.23 2001/05/30 21:04:47 grossjoh Exp $
 
