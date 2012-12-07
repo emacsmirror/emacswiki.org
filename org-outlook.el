eb0a8aead4dd2cf814eaa1313efe4bc5c31f2c84
MatthewL.Fidler <anybody@emacswiki.org>
2012-12-07 18:03:37 +0100

    no summary available

--- org-outlook.el
+++ org-outlook.el
@@ -5,7 +5,7 @@
 ;; Author: Matthew L. Fidler
 ;; Maintainer:
 ;; Created: Mon May 10 09:44:59 2010 (-0500)
-;; Version: 0.3
+;; Version: 0.7
 ;; Last-Updated: Tue May 29 22:21:06 2012 (-0500)
 ;;           By: Matthew L. Fidler
 ;;     Update #: 166
@@ -17,14 +17,86 @@
 ;; 
 ;;; Commentary: 
 ;; 
-;; Adapted from http://superuser.com/questions/71786/can-i-create-a-link-to-a-specific-email-message-in-outlook
-;; Allows selecting then inserting
-;;
-;; org-outlook-task creates task(s) from the selected item(s).
-;;
+;; * Introduction:
+;; Org mode lets you organize your tasks. However, sometimes you may wish
+;; to integrate org-mode with outlook since your company forces you to
+;; use Microsoft Outlook.  [[file:org-outlook.el][org-outlook.el]] allows: 
+;; - Creating Tasks from outlook items:
+;;   - org-outlook-task. All selected items in outlook will be added to a
+;;     task-list at current point. This version requires org-protocol and   
+;;     org-protocol.vbs.  The org-protocol.vbs has can be generated with
+;;     the interactive function `org-outlook-create-vbs'.
+;; 
+;; - Open Outlook Links in org-mode
+;; 
+;;   - Requires org-outlook-location to be customized when using Outlook
+;;     2007 (this way you don’t have to edit the registry).
+;; 
+;; This is based loosely on:
+;; http://superuser.com/questions/71786/can-i-create-a-link-to-a-specific-email-message-in-outlook
+;; 
+;; 
+;; Note that you may also add tasks using visual basic directly. The script below performs the following actions:
+;; 
+;;    - Move email to Personal Folders under folder "@ActionTasks" (changes GUID)
+;;    - Create a org-mode task under heading "* Tasks" for the file `f:\Documents\org\gtd.org'
+;;    - Note by replacing "@ActionTasks", "* Tasks" and
+;;      `f:\Documents\org\gtd.org' you can modify this script to your
+;;      personal needs.
+;; 
+;; The visual basic script for outlook is:
+;; 
+;; 
+;; 
+;; Sub CreateTaskFromItem()
+;;     Dim T As Variant
+;;     Dim Outlook As New Outlook.Application
+;;     Dim orgfile As Variant
+;;     Dim Pos As Integer
+;;     Dim taskf As Object
+;;     
+;;     Set myNamespace = Outlook.GetNamespace("MAPI")
+;;     Set myPersonalFolder = myNamespace.Folders.item("Personal Folders")
+;;     Set allPersonalFolders = myPersonalFolder.Folders
+;;     
+;;     T = ""
+;;     For Each Folder In allPersonalFolders
+;;         If Folder.Name = "@ActionTasks" Then
+;;             Set taskf = Folder
+;;             Exit For
+;;         End If
+;;     Next
+;;     
+;;     If Outlook.Application.ActiveExplorer.Selection.Count > 0 Then
+;;         For I = 1 To Outlook.Application.ActiveExplorer.Selection.Count
+;;                 Set objMail = Outlook.ActiveExplorer.Selection.item(I)
+;;                 Set objMail = objMail.Move(taskf)
+;;                 objMail.Save 'Maybe this will update EntryID
+;;                 T = T + "** TODO " + objMail.Subject + " :OFFICE:" + vbCrLf
+;;                 T = T + "[[outlook:" + objMail.EntryID + "][MESSAGE: " + objMail.Subject + " (" + objMail.SenderName + ")]]"
+;;                 T = T + vbCrLf + vbCrLf
+;;                 T = T + objMail.Body
+;;                 T = T + vbCrLf + vbCrLf
+;;         Next
+;;         ' Now that we have the org-mode tasks, add to org-mode file
+;;         orgfile = GetFile("f:\Documents\org\gtd.org")
+;;         Pos = InStr(1, orgfile, "* Tasks", vbTextCompare)
+;;         orgfile = Mid(orgfile, 1, Pos + Len("* Tasks") + 1) + vbCrLf + T + Mid(orgfile, Pos + Len("* Tasks") + 1, Len(orgfile))
+;;         orgfile = Replace(orgfile, vbCrLf, Chr(10)) ' Change to unix line endings.
+;;         WriteFile "f:\Documents\org\gtd.org", orgfile
+;;     Else
+;;         MsgBox "No Message(s) Selected"
+;;     End If
+;;  
+;; End Sub
+;; 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; 
 ;;; Change log:
+;; 07-Dec-2012    Matthew L. Fidler  
+;;    Last-Updated: Tue May 29 22:21:06 2012 (-0500) #166 (Matthew L. Fidler)
+;;    Should fix Issue #1.  Also added org-outlook-create-vbs to create the
+;;    VBS code based on a user's setup.
 ;; 26-May-2012    Matthew L. Fidler  
 ;;    Last-Updated: Sat May 26 11:13:22 2012 (-0500) #163 (Matthew L. Fidler)
 ;;    Added (require 'cl), Thanks Robert Pluim
@@ -106,8 +178,124 @@
                          (concat "/select \"outlook:" id "\""))
     (w32-shell-execute "open" (concat "outlook:" id))))
 
+(defvar org-outlook-dir (file-name-directory
+                         (or
+                          load-file-name
+                          (buffer-file-name)))
+  "Org outlook directory")
+
 ;;;###autoload
-(org-add-link-type "outlook" 'org-outlook-open)
+(defun org-outlook-create-vbs ()
+  "Creates Visual Basic Code for Org-protocol"
+  (interactive)
+  (let ((script (concat 
+                 "'**************************************
+' Name: URLEncode Function
+' Description:Encodes a string to create legally formatted
+'QueryString for URL. This function is more flexible
+'than the IIS Server.Encode function because you can
+'pass in the WHOLE URL and only the QueryString data
+'will be converted. IIS strangely converts EVERYTHING
+'(ie \"http://\" becomes \"http%3A%2F%2F\").
+' By: Markus Diersbock
+'
+' Inputs:sRawURL - String to Encode
+'
+' Returns:Encoded String
+'
+'This code is copyrighted and has' limited warranties.
+'Please see http://www.Planet-Source-Code.com/vb/scripts/ShowCode.asp?txtCodeId=43806&lngWId=1'for details.
+'**************************************
+
+' Changed by Matthew Fidler to have http:// become http%3A%2F%2F
+' Also changed to have spaces be %20 instead of +
+
+
+Public Function URLEncode(sRawURL As String) As String
+    On Error GoTo Catch
+    Dim iLoop As Integer
+    Dim sRtn As String
+    Dim sTmp As String
+    Const sValidChars = \"1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz\"
+    If Len(sRawURL) > 0 Then
+        ' Loop through each char
+        For iLoop = 1 To Len(sRawURL)
+            sTmp = Mid(sRawURL, iLoop, 1)
+            If InStr(1, sValidChars, sTmp, vbBinaryCompare) = 0 Then
+                ' If not ValidChar, convert to HEX and prefix with %
+                sTmp = Hex(Asc(sTmp))
+                If Len(sTmp) = 1 Then
+                    sTmp = \"%0\" & sTmp
+                Else
+                    sTmp = \"%\" & sTmp
+                End If
+            End If
+            sRtn = sRtn & sTmp
+        Next iLoop
+        URLEncode = sRtn
+    End If
+Finally:
+    Exit Function
+Catch:
+    URLEncode = \"\"
+    Resume Finally
+End Function
+
+
+Sub CreateTaskFromItem()
+    Dim T As Variant
+    Dim Outlook As New Outlook.Application
+    Dim ie As Object
+    Set ie = CreateObject(\"InternetExplorer.Application\")
+
+    
+    Dim orgfile As Variant
+    Dim Pos As Integer
+    Dim taskf As Object
+    
+    Set myNamespace = Outlook.GetNamespace(\"MAPI\")
+    Set myPersonalFolder = myNamespace.Folders.Item(\""
+                 (read-from-minibuffer "Personal Folder Name: ")
+  "\")
+    Set allPersonalFolders = myPersonalFolder.Folders
+    
+    T = ""
+    For Each Folder In allPersonalFolders
+        If Folder.Name = \"" (read-from-minibuffer "Subfolder to put tasks in: " "@ActionTasks") "\" Then
+            Set taskf = Folder
+            Exit For
+        End If
+    Next
+    
+    ' Send selected text to clipboard.
+    SendKeys (\"%E\")
+    SendKeys (\"C\")
+    DoEvents
+    
+    
+    Set objWeb = CreateObject(\"InternetExplorer.Application\")
+    
+        
+    If Outlook.Application.ActiveExplorer.Selection.Count > 0 Then
+        For i = 1 To Outlook.Application.ActiveExplorer.Selection.Count
+                Set objMail = Outlook.ActiveExplorer.Selection.Item(i)
+                Set objMail = objMail.Move(taskf)
+                objMail.Save 'Maybe this will update EntryID
+                T = \"org-protocol:/outlook:/o/\" + URLEncode(objMail.EntryID) _
+                    + \"/\" + URLEncode(objMail.Subject) _
+                    + \"/\" + URLEncode(objMail.SenderName) _
+                    + \"/\" + URLEncode(objMail.SenderEmailAddress)
+                    '+ \"/\" + URLEncode(objMail.Body)
+        objWeb.Navigate T
+        objWeb.Visible = True
+        Next
+    End If
+End Sub")))
+    (with-temp-file (expand-file-name "org-protocol.vbs" org-outlook-dir)
+      (insert script))))
+                
+;;;###autoload
+(eval-after-load "org" '(org-add-link-type "outlook" 'org-outlook-open))
 
 ;;;###autoload
 (defun org-protocol-outlook (info)
@@ -306,13 +494,14 @@ CAPTURE-FUNC is either the symbol `org-remember' or `org-capture'."
     (funcall capture-func nil template)))
 
 ;;;###autoload
-(progn
-  (if (not (boundp 'org-protocol-protocol-alist))
-      (setq org-protocol-protocol-alist nil))
-  (add-to-list 'org-protocol-protocol-alist
-               '("outlook" :protocol "outlook"
-                 :function org-protocol-outlook :kill-client t)))
-
+(eval-after-load "org-protocol"
+  '(progn 
+    (if (not (boundp 'org-protocol-protocol-alist))
+        (setq org-protocol-protocol-alist nil))
+    (add-to-list 'org-protocol-protocol-alist
+                 '("outlook" :protocol "outlook"
+                   :function org-protocol-outlook :kill-client t))))
+  
 (provide 'org-outlook)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; org-outlook.el ends here
