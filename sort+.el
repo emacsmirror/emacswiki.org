a5b19256f87290e6293afd47937222dc037d3c22
DrewAdams <anybody@emacswiki.org>
2012-12-28 20:28:17 +0100

    no summary available

--- sort+.el
+++ sort+.el
@@ -4,13 +4,13 @@
 ;; Description: Extensions to `sort.el'.
 ;; Author: Drew Adams
 ;; Maintainer: Drew Adams
-;; Copyright (C) 1996-2012, Drew Adams, all rights reserved.
+;; Copyright (C) 1996-2013, Drew Adams, all rights reserved.
 ;; Created: Thu Apr 18 10:16:50 1996
 ;; Version: 20.0
-;; Last-Updated: Thu Aug 23 16:57:08 2012 (-0700)
+;; Last-Updated: Fri Dec 28 10:26:07 2012 (-0800)
 ;;           By: dradams
-;;     Update #: 94
-;; URL: http://www.emacswiki.org/emacs-en/sort%2b.el
+;;     Update #: 95
+;; URL: http://www.emacswiki.org/sort%2b.el
 ;; Keywords: unix, tools, sorting
 ;; Compatibility: GNU Emacs 20.x
 ;;
