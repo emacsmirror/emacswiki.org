023090967ccd9898ed7cd04d9cb2e10b5432aff2
BozhidarBatsov <anybody@emacswiki.org>
2013-01-29 20:18:49 +0100

    Add a basic basic page about nREPL.el

---
+++ nREPL.el
@@ -0,0 +1,5 @@
+*nrepl.el* is an Emacs client for nREPL, the Clojure networked REPL server. It's a great alternative to the now deprecated combination of [[SLIME]] + swank-clojure.
+
+nrepl.el is available for installation from both [[Marmalade]] and [[MELPA]].
+
+It's source code can be found at [[https://github.com/kingtim/nrepl.el GitHub]].
