b08a16d78b4824cdc6bd029c2a9ed3c078dcade9
rubikitch <anybody@emacswiki.org>
2013-01-30 22:51:34 +0100

    Fix header(no code change)

--- time-ext.el
+++ time-ext.el
@@ -1,5 +1,6 @@
-;;;; time-ext.el --- more function for time/date
-;; Time-stamp: <2010-05-16 09:34:43 rubikitch>
+;;; time-ext.el --- more function for time/date
+
+;; Time-stamp: <2013-01-31 06:48:11 rubikitch>
 
 ;; Copyright (C) 2010  rubikitch
 
@@ -24,7 +25,29 @@
 
 ;;; Commentary:
 ;;
-;; 
+;; More easy-to-use time/date functions. Using polymorphism,
+;; TIME accepts (T1 T2 T3), (SEC MINUTE HOUR DAY MONTH YEAR DOW DST ZONE) and
+;; float time.
+
+;;; Bug Report:
+;;
+;; If you have problem, send a bug report via M-x time-ext-send-bug-report.
+;; The step is:
+;;  0) Setup mail in Emacs, the easiest way is:
+;;       (setq user-mail-address "your@mail.address")
+;;       (setq user-full-name "Your Full Name")
+;;       (setq smtpmail-smtp-server "your.smtp.server.jp")
+;;       (setq mail-user-agent 'message-user-agent)
+;;       (setq message-send-mail-function 'message-smtpmail-send-it)
+;;  1) Be sure to use the LATEST version of time-ext.el.
+;;  2) Enable debugger. M-x toggle-debug-on-error or (setq debug-on-error t)
+;;  3) Use Lisp version instead of compiled one: (load "time-ext.el")
+;;  4) Do it!
+;;  5) If you got an error, please do not close *Backtrace* buffer.
+;;  6) M-x time-ext-send-bug-report and M-x insert-buffer *Backtrace*
+;;  7) Describe the bug using a precise recipe.
+;;  8) Type C-c C-c to send.
+;;  # If you are a Japanese, please write in Japanese:-)
 
 ;;; Commands:
 ;;
@@ -101,6 +124,31 @@ WEEKNUM is the numeric day of week from 0 (Sunday) to 6."
 (defun next-monday (&optional time)
   "Time of Next monday."
   (next-nweek 1 time))
+
+;;;; Bug report
+(defvar time-ext-maintainer-mail-address
+  (concat "rubiki" "tch@ru" "by-lang.org"))
+(defvar time-ext-bug-report-salutation
+  "Describe bug below, using a precise recipe.
+
+When I executed M-x ...
+
+How to send a bug report:
+  1) Be sure to use the LATEST version of time-ext.el.
+  2) Enable debugger. M-x toggle-debug-on-error or (setq debug-on-error t)
+  3) Use Lisp version instead of compiled one: (load \"time-ext.el\")
+  4) If you got an error, please paste *Backtrace* buffer.
+  5) Type C-c C-c to send.
+# If you are a Japanese, please write in Japanese:-)")
+(defun time-ext-send-bug-report ()
+  (interactive)
+  (reporter-submit-bug-report
+   time-ext-maintainer-mail-address
+   "time-ext.el"
+   nil
+   nil nil
+   time-ext-bug-report-salutation))
+
 
 ;;;; unit test
 ;; (install-elisp "http://www.emacswiki.org/cgi-bin/wiki/download/el-expectations.el")
