31a63466bd075c72d0b1c7c3d15547404a1ef5f2
JonasBernoulli <anybody@emacswiki.org>
2013-01-30 19:31:51 +0100

    Could someone with admin rights please remove all provided features except the correct one. It seems I cannot edit this because there are three . . .

---
+++ Comments_on_cfml-helper-mode.el
@@ -0,0 +1,4 @@
+Could someone with admin rights please remove all provided features except the correct one. It seems I cannot edit this because there are three consecutive lines of whitespace. Is it that really a good idea?
+
+-- JonasBernoulli 2013-01-30 18:31 UTC
+
