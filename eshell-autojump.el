d81d9a48dff8cd87da85ee5202b8ff61491ec794
AlexSchroeder <anybody@emacswiki.org>
2013-01-31 08:22:58 +0100

    (provide 'eshell-autojump)

--- eshell-autojump.el
+++ eshell-autojump.el
@@ -1,17 +1,18 @@
 ;;; eshell-autojump.el -- autojump command for Eshell
 ;; Copyright 2013  Alex Schroeder
 
-;; This program is free software: you can redistribute it and/or modify it under
-;; the terms of the GNU General Public License as published by the Free Software
-;; Foundation, either version 3 of the License, or (at your option) any later
-;; version.
+;; This program is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the Free
+;; Software Foundation, either version 3 of the License, or (at your option)
+;; any later version.
 ;;
 ;; This program is distributed in the hope that it will be useful, but WITHOUT
-;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-;; FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+;; more details.
 ;;
-;; You should have received a copy of the GNU General Public License along with
-;; this program. If not, see <http://www.gnu.org/licenses/>.
+;; You should have received a copy of the GNU General Public License along
+;; with this program. If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
@@ -116,5 +117,7 @@ Otherwise, call `eshell/cd' with the result."
 	    (setq result (car candidates))
 	  (setq candidates (cdr candidates))))
       (eshell/cd result))))
+
+(provide 'eshell-autojump)
     
 ;;; eshell-autojump.el ends here
