d750c76860fe208afe9dde09049bd55505189f64
DrewAdams <anybody@emacswiki.org>
2013-02-08 04:55:53 +0100

    Hi Rubikitch.  FYI, <tt>replace+.el</tt> has something similar that might fulfill your need.  Non-`nil' option . . .

--- Comments_on_replace-from-region.el
+++ Comments_on_replace-from-region.el
@@ -1,1 +1,8 @@
-DeletedPage
+
+
+----
+
+Hi Rubikitch.  FYI, <tt>[[replace+.el]]</tt> has something similar that might fulfill your need.  Non-`nil' option `search/replace-region-as-default-flag' means that query-replacing uses the active region text as the default (for both old and new, so you can easily choose which or edit for both).  There is also a toggle command for the option.
+
+-- DrewAdams 2013-02-08 03:55 UTC
+
