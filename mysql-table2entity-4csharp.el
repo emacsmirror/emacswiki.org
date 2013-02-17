3fbffc54ec5d4863a67d82509f8cc43336da09bc
Joseph <anybody@emacswiki.org>
2013-02-17 13:54:48 +0100

    no summary available

--- mysql-table2entity-4csharp.el
+++ mysql-table2entity-4csharp.el
@@ -1,7 +1,7 @@
 ;;; mysql-table2entity-4csharp.el --- mysql table2entity for csharp   -*- coding:utf-8 -*-
 
 ;; Description:mysql table2entity for csharp
-;; Last Updated: Joseph 2012-01-15 23:56:06 星期日
+;; Last Updated: 纪秀峰 2013-02-17 20:53:56 星期日
 ;; Created: 2012-01-15 23:55
 ;; Author: 纪秀峰(Joseph)  jixiuf@gmail.com
 ;; Keywords: mysql csharp entity
@@ -94,7 +94,7 @@
 (defun camelize (s &optional separator )
   "Convert under_score string S to CamelCase string."
   (mapconcat 'identity (mapcar
-                        '(lambda (word) (capitalize (downcase word)))
+                        #'(lambda (word) (capitalize (downcase word)))
                         (if separator (split-string s "_") (list s))
                         ) ""))
 
