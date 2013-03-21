aac6d30bd78d37e681a15fa06a96384352056392
BrandonBetances <anybody@emacswiki.org>
2013-03-21 05:53:36 +0100

    =Introduction= pacman.el is an emacs mode that's written to interact with the Archlinux package manager, pacman. =Installation= . . .

---
+++ pacman.el
@@ -0,0 +1,15 @@
+=Introduction=
+pacman.el is an emacs mode that's written to interact with the Archlinux package manager, pacman.
+
+=Installation=
+https://github.com/bbetances/conf-scripts/blob/master/emacs-dir/custom-modes/pacman.el
+
+wget that file, or clone the base repo, put it in your .emacs.d, and require it in .emacs
+
+Sooner or later, I'll move it to it's own repo, and streamline the installation process.
+
+=Usage=
+Once loaded, M-x pacman-mode to start it. The keybindings are all listed in the main buffer. 
+
+----
+Archlinux | CategoryModes
