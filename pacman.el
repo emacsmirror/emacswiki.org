f15b78f9830d482f71ac9b77b88aa9b47844f8d7
TrippLilley <anybody@emacswiki.org>
2013-03-27 18:35:49 +0100

    fix grammo

--- pacman.el
+++ pacman.el
@@ -6,7 +6,7 @@ https://github.com/bbetances/conf-scripts/blob/master/emacs-dir/custom-modes/pac
 
 wget that file, or clone the base repo, put it in your .emacs.d, and require it in .emacs
 
-Sooner or later, I'll move it to it's own repo, and streamline the installation process.
+Sooner or later, I'll move it to its own repo, and streamline the installation process.
 
 =Usage=
 Once loaded, M-x pacman-mode to start it. The keybindings are all listed in the main buffer. 
