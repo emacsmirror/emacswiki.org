3aae60aa7c26f7865ac9d9cab55cf57a71aa89d8
DrewAdams <anybody@emacswiki.org>
2013-03-28 06:29:39 +0100

    no summary available

--- icicles-doc2.el
+++ icicles-doc2.el
@@ -7,9 +7,9 @@
 ;; Copyright (C) 1996-2013, Drew Adams, all rights reserved.
 ;; Created: Tue Aug  1 14:21:16 1995
 ;; Version: 22.0
-;; Last-Updated: Sat Mar 23 21:39:27 2013 (-0700)
+;; Last-Updated: Wed Mar 27 22:26:32 2013 (-0700)
 ;;           By: dradams
-;;     Update #: 29274
+;;     Update #: 29276
 ;; URL: http://www.emacswiki.org/icicles-doc2.el
 ;; Doc URL: http://www.emacswiki.org/Icicles
 ;; Keywords: internal, extensions, help, abbrev, local, minibuffer,
@@ -57,8 +57,9 @@
 ;;    (@file :file-name "icicles-doc1.el" :to "README for Non-Readers")
 ;;    (@file :file-name "icicles-doc1.el" :to "Flashy Demo to Peak Your Curiosity")
 ;;      (@file :file-name "icicles-doc1.el" :to "First Example: Multi-Inputs")
-;;      (@file :file-name "icicles-doc1.el" :to "Second Example: Multi-Completion")
-;;      (@file :file-name "icicles-doc1.el" :to "Third Example: Tagged Files")
+;;      (@file :file-name "icicles-doc1.el" :to "Second Example: Multi-Completions")
+;;      (@file :file-name "icicles-doc1.el" :to "Third Example: Narrowing a Manual")
+;;      (@file :file-name "icicles-doc1.el" :to "Fourth Example: Tagged Files")
 ;;    (@file :file-name "icicles-doc1.el" :to "Toggle Options on the Fly")
 ;;    (@file :file-name "icicles-doc1.el" :to "Cycle Completion Candidates")
 ;;    (@file :file-name "icicles-doc1.el" :to "Display Completion Candidates")
@@ -272,6 +273,7 @@
 ;;  (@> "Icicles Info Enhancements")
 ;;    (@> "Icicles Completion for Info")
 ;;      (@> "Virtual Info Books")
+;;      (@> "Using Icicle-Search With Info")
 ;;
 ;;    (@> "Using Icicle-Search With Info")
 ;;
