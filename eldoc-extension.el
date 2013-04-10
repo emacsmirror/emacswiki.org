97ac6783ef702bf167bdd4ac99ea5aa77f4ec2ba
rrt <anybody@emacswiki.org>
2013-04-10 02:17:41 +0200

    Add an autoload for eldoc-argument-list, required by init-eldoc.el.

--- eldoc-extension.el
+++ eldoc-extension.el
@@ -4,7 +4,7 @@
 ;; Description: Some extension for eldoc
 ;; Author: Andy Stewart <lazycat.manatee@gmail.com>
 ;; Maintainer: rubikitch <rubikitch@ruby-lang.org>
-;; Time-stamp: <2013-04-10 00:12:00 rrt@sc3d.org>
+;; Time-stamp: <2013-04-10 01:16:00 rrt@sc3d.org>
 ;; Copyright (C) 2008, 2009, Andy Stewart, all rights reserved.
 ;; Copyright (C) 2010, rubikitch, all rights reserved.
 ;; Created: 2008-12-07 21:44:29
@@ -105,6 +105,7 @@
 
 ;;; Code:
 
+;;;###autoload
 (defun eldoc-argument-list (string)
   "Down case and fontify STRING for use with `eldoc-mode'."
   (propertize (downcase string)
