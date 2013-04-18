b7a7c567af3fc255098a6287b42605ef0f4cccfb
OleArndt <anybody@emacswiki.org>
2013-04-18 16:07:54 +0200

    v0.5

--- slime-company.el
+++ slime-company.el
@@ -1,12 +1,12 @@
 ;;; slime-company.el --- slime completion backend for company mode
 ;;
-;; Copyright (C) 2009  Ole Arndt
+;; Copyright (C) 2009-2013  Ole Arndt
 ;;
 ;; Author: Ole Arndt <ole@sugarshark.com>
 ;; Keywords: convenience, lisp, abbrev
-;; Version: 0.4
+;; Version: 0.5
 ;;
-;; This program is free software; you can redistribute it and/or modify
+;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
@@ -26,29 +26,12 @@
 ;; is available at http://nschum.de/src/emacs/company-mode
 ;; Company-mode is also available at the ELPA http://tromey.com/elpa
 ;;
-;; The backend is installed into the list of company-backends, but
-;; you have to enable company-mode manually (or in lisp-mode-hook).
-;;
 ;;; Installation:
 ;;
-;; Add this to your .emacs:
+;;  Put this file somewhere into your load-path
+;;  (or just into slime-path/contribs) and then call
 ;;
-;;   (add-to-list 'load-path "<directory-of-this-file>")
-;;   (add-hook 'slime-load-hook (lambda () (require 'slime-company)))
-;;
-;; or use:
-;;
-;;   (slime-setup 'slime-fancy 'slime-company)
-;;
-;; For the completion to work, you need to enable company mode in
-;; slime and set the appropriate company backends. This can be done
-;; via a hook function:
-;;
-;;   (add-hook 'slime-mode-hook
-;;             (lambda ()
-;;               (company-mode 1)
-;;               (set (make-local-variable 'company-backends)
-;;                    '(slime-company-backend))))
+;;   (slime-setup '(slime-company))
 ;;
 ;; I also have the following, IMO more convenient key bindings for
 ;; company mode in my .emacs:
@@ -65,17 +48,51 @@
 
 (require 'company)
 
+(define-slime-contrib slime-company
+  "Interaction between slime and the company completion mode."
+  (:license "GPL")
+  (:authors "Ole Arndt <anwyn@sugarshark.com>")
+  (:slime-dependencies slime-autodoc)
+  (:swank-dependencies swank-arglists)
+  (:on-load
+   (dolist (h '(slime-mode-hook slime-repl-mode-hook sldb-mode-hook))
+     (add-hook h 'slime-company-maybe-enable)))
+  (:on-unload
+   (dolist (h '(slime-mode-hook slime-repl-mode-hook sldb-mode-hook))
+     (remove-hook h 'slime-company-maybe-enable))
+   (slime-company-disable)))
+
+(defsubst slime-company-active-p ()
+  "Test if the slime-company backend should be active in the current buffer."
+  (derived-mode-p 'lisp-mode 'clojure-mode 'slime-repl-mode))
+
+(defun slime-company-maybe-enable ()
+  (when (slime-company-active-p)
+    (company-mode 1)
+    (add-hook 'company-completion-finished-hook 'slime-company-echo-arglist)
+    (add-to-list 'company-backends 'slime-company-backend)))
+
+(defun slime-company-disable ()
+  (setq company-backends (remove 'slime-company-backend company-backends))
+  (remove-hook 'company-completion-finished-hook 'slime-company-echo-arglist))
+
+(defun slime-company-echo-arglist (_)
+  (when (slime-company-active-p)
+    (slime-echo-arglist)))
+
 (defun slime-company-backend (command &optional arg &rest ignored)
+  "Company mode backend for slime."
   (case command
     ('prefix
-     (if (derived-mode-p 'lisp-mode 'clojure-mode 'slime-repl-mode)
-         (company-grab-lisp-symbol)))
+     (if (slime-company-active-p)
+         (company-grab-symbol)))
     ('candidates
      (first (slime-simple-completions (substring-no-properties arg))))
     ('meta
-     (let ((arglist (slime-retrieve-arglist arg)))
-       (when arglist
-         (slime-fontify-string arglist))))
+     (let ((arglist (slime-eval `(swank:operator-arglist ,arg ,(slime-current-package)))))
+       (if arglist
+           (slime-fontify-string arglist)
+         :not-available)))
     ('doc-buffer
      (let ((doc (slime-eval `(swank:describe-symbol ,arg))))
        (with-current-buffer (company-doc-buffer)
@@ -93,8 +110,6 @@
              (cons buffer (with-current-buffer buffer
                             (point))))))))
     ('sorted nil)))
-
-(add-to-list 'company-backends 'slime-company-backend)
 
 (provide 'slime-company)
 
