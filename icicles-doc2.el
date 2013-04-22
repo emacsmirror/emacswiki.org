4dac9ff482098839de90980b7bd99da6cd232181
DrewAdams <anybody@emacswiki.org>
2013-04-22 16:46:29 +0200

    no summary available

--- icicles-doc2.el
+++ icicles-doc2.el
@@ -7,9 +7,9 @@
 ;; Copyright (C) 1996-2013, Drew Adams, all rights reserved.
 ;; Created: Tue Aug  1 14:21:16 1995
 ;; Version: 22.0
-;; Last-Updated: Thu Apr 18 19:27:40 2013 (-0700)
+;; Last-Updated: Sun Apr 21 12:44:45 2013 (-0700)
 ;;           By: dradams
-;;     Update #: 29296
+;;     Update #: 29297
 ;; URL: http://www.emacswiki.org/icicles-doc2.el
 ;; Doc URL: http://www.emacswiki.org/Icicles
 ;; Keywords: internal, extensions, help, abbrev, local, minibuffer,
@@ -57,7 +57,7 @@
 ;;    (@file :file-name "icicles-doc1.el" :to "README for Non-Readers")
 ;;    (@file :file-name "icicles-doc1.el" :to "Menus")
 ;;    (@file :file-name "icicles-doc1.el" :to "README")
-;;    (@file :file-name "icicles-doc1.el" :to "Flashy Demo to Peak Your Curiosity")
+;;    (@file :file-name "icicles-doc1.el" :to "Flashy Demo to Pique Your Curiosity")
 ;;      (@file :file-name "icicles-doc1.el" :to "First Example: Multi-Inputs")
 ;;      (@file :file-name "icicles-doc1.el" :to "Second Example: Multi-Completions")
 ;;      (@file :file-name "icicles-doc1.el" :to "Third Example: Narrowing a Manual")
