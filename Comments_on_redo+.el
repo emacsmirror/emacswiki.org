66f27e7e1c6efe577f2f265ac3efaf1624773593
HenryVIII <anybody@emacswiki.org>
2013-04-24 03:17:17 +0200

    Broke with Emacs 24.2.50, see: GNU bug report logs - #12581 24.2.50: redo+ fails to modify edit menu

--- Comments_on_redo+.el
+++ Comments_on_redo+.el
@@ -2,3 +2,12 @@ Looks like redo+ 1.15 breaks under GNU Emacs 24.3.1
 
 -- Anonymous 2013-04-24 00:53 UTC
 
+
+----
+
+Broke with Emacs 24.2.50, see:
+GNU bug report logs - #12581
+24.2.50: redo+ fails to modify edit menu
+
+-- [http://https://github.com/HenryVIII HenryVIII] 2013-04-24 01:17 UTC
+
