b64c11f4b00b6d3347dc5fae249a1ede7252bd9e
GregLucas <anybody@emacswiki.org>
2013-04-30 18:58:59 +0200

    cleanup

--- Comments_on_move-text.el
+++ Comments_on_move-text.el
@@ -1,4 +1,3 @@
-Add your comment here.
 The special handling for Emacs 24.3 breaks move-text for me, though I am running Gnu Emacs 24.3.1. I also haven't found anything the describes a change in the behavior of transpose-lines in that version of Emacs. What am I missing here?
 
 -- GregLucas 2013-04-30 16:58 UTC
