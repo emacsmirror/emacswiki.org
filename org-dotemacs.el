9cec7c080fe59a70a5d9a9114fe8bf93a9816d41
JoeBloggs <anybody@emacswiki.org>
2013-05-03 19:58:27 +0200

    update

--- org-dotemacs.el
+++ org-dotemacs.el
@@ -42,28 +42,33 @@
 ;; Bitcoin donations gratefully accepted: 1Ph9srQBspJCDS9CnGUyWJPTrU4ydX9Aa3
 ;;
 ;; Keeping your emacs config in an org file makes it easier for you to keep your .emacs under control,
-;; and avoid dotemacs bankruptcy (http://www.emacswiki.org/emacs/DotEmacsBankruptcy).
+;; and avoid DotEmacsBankruptcy.
 ;; With your config code stored in an org file you can easily edit the structure and keep notes.
+;; 
 ;; This library allows you to load elisp code from an org file on emacs startup.
 ;; You can also limit the code that is loaded to certain tagged headers using an org tag match,
 ;; and specify dependencies between code blocks.
+;; Using tag matches you can also reuse the same org file for different emacs setups by specifying different
+;; tag matches for each setup, or load parts of the file on demand.
 ;; 
-;; First you need to create an org file ~/.dotemacs.org and add your config code to emacs-lisp code blocks
-;; in this file. You can use another file location if you wish, see `org-dotemacs-default-file' below.
-;;
-;; To aid debugging you can name the code blocks by adding NAME properties to the corresponding org headers
-;; (see the "Properties and columns" in the org manual). You can also introduce dependencies between the
-;; blocks by creating DEPENDS properties containing space separated lists of block names. org-dotemacs will
-;; only load a block if all its dependencies have already been successfully loaded.
-;; You may also decide to tag the headers so that you can filter out which code blocks to load with a tag match
-;; (see "Matching tags and properties" in the org manual). 
-;; After doing that your .dotemacs.org file might look something like this:
-
-;; * Display settings code                :settings:
-;;   :PROPERTIES:
-;;   :DEPENDS: 
-;;   :NAME:     display_settings
-;;   :END:
+;;; Commands/Usage 
+;; 
+;; The main command is `org-dotemacs-load-default' which loads your default org-dotemacs file (~/.dotemacs.org)
+;; and prompts for a tag match to specify which code blocks to load. 
+;; In this way you can load bits of config code when you need them.
+;; 
+;; You can also put this command in your InitFile (see Installation below) to load the code on startup.
+;; To change the default org file use the `org-dotemacs-default-file' option.
+;; If you want to load a different org file from your default one, use `org-dotemacs-load-file'.
+;; 
+;; For faster loading you may prefer to keep your config code in a separate elisp file, and just update this file now and again
+;; by exporting the code from the org file.
+;; Use the `org-dotemacs-load-file' command for this and specify a target file when prompted.
+;; 
+;;; Structure of the org file 
+;; 
+;; The elisp code should be contained in emacs-lisp code blocks, e.g:
+;; 
 ;; #+BEGIN_SRC emacs-lisp
 ;; (setq line-number-mode t)
 ;; (setq column-number-mode t)
@@ -72,75 +77,100 @@
 ;; (set-foreground-color "White")
 ;; (set-cursor-color "White")
 ;; #+END_SRC
-;; * Scrolling settings code              :settings:mouse:
-;;   :PROPERTIES:
-;;   :DEPENDS:  display_settings other_settings
-;;   :NAME:     scrolling_settings
-;;   :END:
-;; #+BEGIN_SRC emacs-lisp
-;; (mouse-wheel-mode t)
-;; (setq scroll-step 1)
-;; (setq scroll-conservatively 5)
-;; #+END_SRC
-
-;; You can get a nice columns view of your code blocks by adding the following line to the top of the org file:
-;;
+;; 
+;; Ideally you should have each code block under a separate org subtree, then you can use properties to
+;; name the blocks and define dependencies, and tags and todo states to specify which blocks
+;; should be loaded (see below).
+;; 
+;; I prefer to keep all my code block subtrees under a single header, and use other headers for keeping notes,
+;; defining buffer-wide properties, etc. This way I can get a nice column view of the code blocks
+;; (see the columns view section below).
+;; 
+;;;  Block dependencies 
+;; 
+;; You can enforce dependencies between code blocks by defining NAME & DEPENDS properties for the subtrees containing the
+;; blocks (preferred). The NAME property should contain the name of the block, and the DEPENDS property should contain a space
+;; separated list of block names that this block depends on.
+;; These properties will be applied to all code blocks in the subtree (see "Properties and Columns" in the org manual for
+;; more details).
+;; 
+;; The NAME property can be overridden on a per block basis by adding a :name header arg to a block, and dependencies can be
+;; augmented by adding a :depends header arg (see "Header arguments" in the org manual).
+;; However it is recommended to keep a separate subtree for each code block and use properties for defining headers and names
+;; since then you can get a column view of the dependencies (see below).
+;; 
+;; A block will not be loaded until all of its dependencies have been loaded.
+;; 
+;;;  Tags and TODO states 
+;; 
+;; You can tag your subtrees and use tag matches to specify which blocks to evaluate in calls to `org-dotemacs-load-file'
+;; and `org-dotemacs-load-default'. See "Matching tags and properties" in the org manual for more information on tag matches.
+;; 
+;; Also, by default any blocks in a subtree marked with a todo state of BROKEN will not be evaluated.
+;; You can specify which TODO states to include/exclude for evaluation by customizing the `org-dotemacs-include-todo' and
+;; `org-dotemacs-exclude-todo' options.
+;; 
+;; To add the BROKEN state to the list of todo states for the file you need to add buffer-wide todo states by adding a line
+;; like this somewhere in your org file (see "Per file keywords" in the org manual).
+;; 
+;; #+TODO: BROKEN CHECK TODO
+;; 
+;;;  Columns View 
+;; 
+;; If you use properties for defining names and dependencies then you can get a nice column view of your code subtrees
+;; with the following columns view specification:
+;; 
 ;; #+COLUMNS: %35ITEM %15NAME %35DEPENDS %15TAGS %TODO
-;;
-;; To load code blocks on startup you need to ensure that this file is loaded before calling
-;; `org-dotemacs-load-default', see installation below.
-;; If you do M-x org-dotemacs-load-default, you will be prompted for a tag match and the corresponding
-;; code blocks will be loaded (enter an empty tag match string to load all blocks). In this way you can load
-;; just the parts of your config file that you need, when you need them.
-;; If you have another org dotemacs file you can use `org-dotemacs-load-file' to load the code blocks from
-;; this file and optionally save them to an elisp file. This can be loaded from your .emacs faster than
-;; the org file.
-;;
+;; 
+;; This can be placed anywhere in your dotemacs org file.
+;; Then if you press C-c C-x C-c on the toplevel header for your code blocks you'll get a column view that allows you
+;; to easily change the names, dependencies, tags and todo states.
+;; 
+;;;  Error handling 
+;; 
 ;; Error handling can be controlled by customizing `org-dotemacs-error-handling' or by setting the error-handling
 ;; command line option when starting emacs.
 ;; By default code blocks with unmet dependencies or errors are skipped over as soon as an error is encountered,
 ;; but you can also specify that org-dotemacs should halt or try to reload the blocks.
 ;; In the latter case each time a new block is successfully loaded, any unsuccessful blocks will be retried.
-;;
-;; Command line options:
-;;
+;; 
+;;;  Command line options 
+;; 
 ;; org-dotemacs.el will look for two command line options when loaded: error-handling (for setting the value of
 ;; `org-dotemacs-error-handling') and tag-match (for specifying which headers to load).
 ;; For example if you enter the following at the command line:
-;;
+;; 
 ;;        emacs --error-handling retry --tag-match "settings-mouse"
-;;
+;; 
 ;; Then only code blocks tagged "settings" but not "mouse" will be loaded, and org-dotemacs will try to reload any
 ;; blocks that have errors.
-
-
-;;; Installation:
-;;
+;; 
+;;;  Installation 
+;; 
 ;; Put org-dotemacs.el in a directory in your load-path, e.g. ~/.emacs.d/
 ;; You can add a directory to your load-path with the following line in ~/.emacs
 ;; (add-to-list 'load-path (expand-file-name "~/elisp"))
 ;; where ~/elisp is the directory you want to add 
 ;; (you don't need to do this for ~/.emacs.d - it's added by default).
-;;
+;; 
 ;; Then make sure you have an ~/.dotemacs.org file and add the following lines to
 ;; the end of your .emacs file:
-;;
+;; 
 ;; (load-file "~/.emacs.d/org-dotemacs.el")
 ;; (org-dotemacs-load-default)
-;;
+;; 
 ;; or if you want to just load code blocks matching a tag match:
-;;
+;; 
 ;; (load-file "~/.emacs.d/org-dotemacs.el")
 ;; (org-dotemacs-load-default "<TAG-MATCH>")
-;;
-;; See the org manual "Matching tags and properties" section for more details on tag matches.
-
+;; 
 ;; To load a different org file either customize `org-dotemacs-default-file' or use the
 ;; `org-dotemacs-load-file' function, e.g:
-;;
+;; 
 ;; (load-file "~/.emacs.d/org-dotemacs.el")
 ;; (org-dotemacs-load-file "~/.emacs.d/my_emacs_config.org" "<TAG-MATCH>")
-;;
+;; 
+
 
 ;;; Customize:
 ;;
