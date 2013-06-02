64c1c18ed29f279f272ec65be4803e3e167dac80
DrewAdams <anybody@emacswiki.org>
2013-06-02 20:37:58 +0200

    no summary available

--- bookmark+-chg.el
+++ bookmark+-chg.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
 ;; Copyright (C) 2000-2013, Drew Adams, all rights reserved.
 ;; Created: Fri Sep 15 07:58:41 2000
-;; Last-Updated: Fri May 31 15:16:06 2013 (-0700)
+;; Last-Updated: Sun Jun  2 11:28:29 2013 (-0700)
 ;;           By: dradams
-;;     Update #: 15319
+;;     Update #: 15320
 ;; URL: http://www.emacswiki.org/bookmark+-chg.el
 ;; Doc URL: http://www.emacswiki.org/BookmarkPlus
 ;; Keywords: bookmarks, bookmark+
@@ -146,6 +146,8 @@
  
 ;;;(@* "CHANGE LOG FOR `bookmark+-1.el'")
 ;;
+;; 2013/06/02 dadams
+;;     bmkp-set-sequence-bookmark: Forgot to bind FUN.  Thx to Michael Heerdegen.
 ;; 2013/05/31 dadams
 ;;     bmkp-save-menu-list-state: Display warning, do not raise error, if write-file fails.
 ;;     bookmark-write-file: Use display-warning, if fboundp.
