d81d941e061680c19a6a5e206cbfc4e75feb4cf2
christopherbalz <anybody@emacswiki.org>
2013-06-15 06:37:40 +0200

    How best to avoid the pop-up menus that come up and say, "Desktop file is more recent than the one loaded.  Save anyway?" ? Those dialogs don't . . .

---
+++ Comments_on_desktop-menu.el
@@ -0,0 +1,6 @@
+How best to avoid the pop-up menus that come up and say, "Desktop file is more recent than the one loaded.  Save anyway?" ?
+
+Those dialogs don't have meaning when the current desktop is one you purposely switched to.
+
+-- christopherbalz 2013-06-15 04:37 UTC
+
