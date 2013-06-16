eee68a332ad1f45680ab1f6d199e4010e0eb0a11
JoeBloggs <anybody@emacswiki.org>
2013-06-16 20:04:44 +0200

    update

--- jb-misc-macros.el
+++ jb-misc-macros.el
@@ -84,7 +84,7 @@
 
 ;;; TODO
 ;;
-;; More macros.
+;; Macro that works like apply-partially but allowing partial application of args in any position.
 ;;
 
 ;;; Require
@@ -115,6 +115,71 @@ between successive evaluations of NEXTFORM."
 (defmacro list-subset (indices list)
   "Return elements of LIST corresponding to INDICES."
   `(mapcar (lambda (i) (nth i ,list)) ,indices))
+
+(defun number-list (start end &optional length)
+  "Return a sequential list of numbers from START to END.
+If END is nil and LENGTH is provided then return a list from START to (1- (+ START LENGTH))."
+  (if end
+      (loop for i from start to end collect i)
+    (assert length)
+    (loop for i from start to (1- (+ start length)) collect i)))
+
+;; This might be better as a function but I wanted to practice writing macros.
+;; Also this way we can use gensyms to minimize the number of variables bound in the let
+;; form surrounding the evaluation of FORMS.
+(defmacro* read-key-menu (prompts forms &optional startstr endstr keys)
+  "Prompt the user for a key and return the results of evaluating the corresponding form in the list FORMS.
+If the corresponding form is a symbol just return that symbol unevaluated.
+If KEYS is supplied then it should be a list (of the same length as PROMPTS & FORMS) of keys to be prompted for.
+Each element of KEYS should be a string or vector as returned by `read-key-sequence'. 
+If the KEYS list is not long enough to cover all PROMPTS, or if there are nil values in the list then any missing
+values will be replaced by unused keys starting with the \"1\" key.
+
+The prompt string for `read-key-sequence' will be formed from PROMPTS, KEYS and the optional STARTSTR and ENDSTR in
+the following way:
+
+STARTSTR
+K1) PROMPT1
+K2) PROMPT2
+...
+KN) PROMPT3
+ENDSTR
+
+Where K1-KN are key descriptions of the keys in KEYS, and PROMPT1-PROMPTN are the corresponding prompt strings in the
+list PROMPTS.
+
+The macro arguments will be evaluated once before expanding the macro."
+  (with-gensyms (newprompts prompt prompts2 retval newkeys keystrs maxlen)
+                `(let* ((,prompts2 ,prompts)
+                        (,newkeys (let* ((origkeys ,keys)
+                                         (uniqkeys (remove 'nil origkeys))
+                                         (nextkey 48))
+                                    (mapcar (lambda (key)
+                                              (or key (progn
+                                                        (setq nextkey (1+ nextkey))
+                                                        (while (member (char-to-string nextkey) uniqkeys)
+                                                          (setq nextkey (1+ nextkey)))
+                                                        (char-to-string nextkey))))
+                                            (nconc origkeys (make-list (- (length ,prompts2) (length origkeys)) nil)))))
+                        (,keystrs (mapcar 'key-description ,newkeys))
+                        (,maxlen (loop for keystr in ,keystrs maximize (length keystr)))
+                        (,newprompts (mapcar* (lambda (k p)
+                                                (let ((len (- ,maxlen (length k))))
+                                                  (concat k ") " (make-string len ? ) p)))
+                                              ,keystrs ,prompts2))
+                        (,prompt (concat (and ,(eval startstr) (concat ,(eval startstr) "\n"))
+                                         (mapconcat 'identity ,newprompts "\n")
+                                         "\nC-g) Quit"
+                                         (and ,(eval endstr) (concat "\n" ,(eval endstr)))))
+                        (,retval 'again))
+                   (while (eq ,retval 'again)
+                     (let (key)
+                       (while (not (or (member key ,newkeys) (equal key " ")))
+                         (setq key (read-key-sequence ,prompt nil t nil t)))
+                       (if (equal key " ")
+                           (keyboard-quit)
+                         (setq ,retval (nth (position key ,newkeys :test 'equal) ,forms)))))
+                   (if (symbolp ,retval) ,retval (eval ,retval)))))
 
 (provide 'jb-misc-macros)
 
