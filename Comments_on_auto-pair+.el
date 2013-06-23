2ed56c751c94261b7cf87f82c66be425d84fd0f3
RaffaeleRicciardi <anybody@emacswiki.org>
2013-06-23 12:50:45 +0200

    auto-pair+.el extends and modifies autopair.el  (see AutoPairs). : I've contacted the author asking him for more information, and he has . . .

---
+++ Comments_on_auto-pair+.el
@@ -0,0 +1,6 @@
+[[auto-pair+.el]] extends and modifies autopair.el  (see [[AutoPairs]]).
+
+: I've contacted the author asking him for more information, and he has replied that he hadn't used it for a while and therefore he wasn't sure it did work with the most recent versions of autopair.el. This applies to auto-pair+.el "Last-Updated: Tue Nov 30 14:05:00 2010".
+
+-- RaffaeleRicciardi 2013-06-23 10:50 UTC
+
