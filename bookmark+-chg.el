919d81ea982a78ee888f5a18d4f708eea808149c
DrewAdams <anybody@emacswiki.org>
2013-07-02 01:20:55 +0200

    no summary available

--- bookmark+-chg.el
+++ bookmark+-chg.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
 ;; Copyright (C) 2000-2013, Drew Adams, all rights reserved.
 ;; Created: Fri Sep 15 07:58:41 2000
-;; Last-Updated: Sun Jun 30 17:00:10 2013 (-0700)
+;; Last-Updated: Mon Jul  1 16:17:27 2013 (-0700)
 ;;           By: dradams
-;;     Update #: 15375
+;;     Update #: 15379
 ;; URL: http://www.emacswiki.org/bookmark+-chg.el
 ;; Doc URL: http://www.emacswiki.org/BookmarkPlus
 ;; Keywords: bookmarks, bookmark+
@@ -146,6 +146,8 @@
  
 ;;;(@* "CHANGE LOG FOR `bookmark+-1.el'")
 ;;
+;; 2013/07/01 dadams
+;;     bookmark-make-record-default: Fixed typo for end-position (listify).
 ;; 2013/06/30 dadams
 ;;     Added: bmkp-set-snippet-bookmark, bmkp-snippet-to-kill-ring, bmkp-jump-snippet,
 ;;            bmkp-snippet-alist-only, bmkp-snippet-bookmark-p, bmkp-snippet-history.
