ee24937c9ac1f461a00deb780bf58c6833434e95
tuxdna <anybody@emacswiki.org>
2013-07-20 15:18:35 +0200

    This code doens't work with Fedora 19 with GNOME 3.8.1. However I could use following command from within a shell in Emacs: {{{ wmctrl -r . . .

---
+++ Comments_on_fullscreen.el
@@ -0,0 +1,10 @@
+This code doens't work with Fedora 19 with GNOME 3.8.1.
+
+However I could use following command from within a shell in Emacs:
+
+{{{
+wmctrl -r :ACTIVE: -btoggle,fullscreen
+}}}
+
+-- [http://https://tuxdna.wordpress.com/ tuxdna] 2013-07-20 13:18 UTC
+
