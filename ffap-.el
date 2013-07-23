cd5628448ae0b194baf3c78db82bcd39bd2f335c
DrewAdams <anybody@emacswiki.org>
2013-07-24 01:07:13 +0200

    no summary available

--- ffap-.el
+++ ffap-.el
@@ -1,28 +1,29 @@
 ;;; ffap-.el --- Extensions to library `ffap.el'
-;; 
+;;
 ;; Filename: ffap-.el
 ;; Description: Extensions to library `ffap.el'
 ;; Author: Drew Adams
 ;; Maintainer: Drew Adams
 ;; Copyright (C) 2005-2013, Drew Adams, all rights reserved.
 ;; Created: Wed Feb 08 10:47:56 2006
-;; Version: 20
-;; Last-Updated: Fri Dec 28 09:41:27 2012 (-0800)
+;; Version: 0
+;; Package-Requires: ()
+;; Last-Updated: Tue Jul 23 16:03:09 2013 (-0700)
 ;;           By: dradams
-;;     Update #: 112
+;;     Update #: 115
 ;; URL: http://www.emacswiki.org/ffap-.el
 ;; Doc URL: http://emacswiki.org/FindFileAtPoint
 ;; Keywords: files, hypermedia, matching, mouse, convenience
 ;; Compatibility: GNU Emacs: 20.x, 21.x, 22.x, 23.x, 24.x
-;; 
+;;
 ;; Features that might be required by this library:
 ;;
 ;;   `ffap'.
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Commentary:
-;; 
+;;
 ;;  Extensions to library `ffap.el'.
 ;;
 ;;  To use this library, add this to your initialization file
@@ -44,7 +45,7 @@
 ;;  the cursor is.  Instead, I want to use completion to provide the
 ;;  file name.  This inhibition is done by setting option
 ;;  `ffap-inhibit-ffap-flag' to t in Dired buffers:
-;; 
+;;
 ;;    (add-hook 'dired-mode-hook 'ffap-inhibit-here)
 ;;
 ;;  If you do *not* want to inhibit ffap in Dired, then do this after
@@ -81,33 +82,33 @@
 ;;     "Wrong type argument: stringp, nil."
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Change Log:
 ;;
 ;; 2011/01/04 dadams
 ;;     Added autoload cookies for defcustom.
-;; 2006/12/29 dadams 
+;; 2006/12/29 dadams
 ;;     Removed top-level call to ffap-bindings - users do it explicitly or not.
-;; 
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2, or (at your option)
 ;; any later version.
-;; 
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-;; 
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program; see the file COPYING.  If not, write to the
 ;; Free Software Foundation, Inc., 51 Franklin Street, Fifth
 ;; ;; Floor, Boston, MA 02110-1301, USA.
-;; 
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Code:
 
 ;;;###autoload
