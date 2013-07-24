32e311f3b5abf44b578185c325efddbc0d6d9760
alkhimov <anybody@emacswiki.org>
2013-07-24 14:38:28 +0200

    On "GNU Emacs 24.2.1 (i386-mingw-nt5.1.2600)  of 2012-08-27 on MARVIN" I had to comment out the following block to make the code work properly: . . .

--- Comments_on_move-text.el
+++ Comments_on_move-text.el
@@ -16,3 +16,17 @@ Okay, give that a try.
 
 -- StevePurcell 2013-05-06 18:26 UTC
 
+
+----
+
+On "GNU Emacs 24.2.1 (i386-mingw-nt5.1.2600)
+ of 2012-08-27 on MARVIN" I had to comment out the following block to make the code work properly:
+
+          ;; (when (and (eval-when-compile
+          ;;              '(and (>= emacs-major-version 24)
+          ;;                    (>= emacs-minor-version 3)))
+          ;;            (< arg 0))
+          ;;   (forward-line -1))
+
+-- [http://emacs-sbcl-slime.blogspot.com alkhimov] 2013-07-24 12:38 UTC
+
