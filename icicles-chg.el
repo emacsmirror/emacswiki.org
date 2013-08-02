7d093d7a387a3fa007ac3bbade8eea21f8f11631
DrewAdams <anybody@emacswiki.org>
2013-08-02 20:32:29 +0200

    no summary available

--- icicles-chg.el
+++ icicles-chg.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Drew Adams
 ;; Copyright (C) 2007-2013, Drew Adams, all rights reserved.
 ;; Created: Tue Nov 27 07:47:53 2007
-;; Last-Updated: Thu Aug  1 13:41:24 2013 (-0700)
+;; Last-Updated: Fri Aug  2 11:25:27 2013 (-0700)
 ;;           By: dradams
-;;     Update #: 10137
+;;     Update #: 10147
 ;; URL: http://www.emacswiki.org/icicles-chg.el
 ;; Doc URL: http://www.emacswiki.org/Icicles
 ;; Keywords: extensions, help, abbrev, local, minibuffer,
@@ -3868,6 +3868,9 @@
  
 ;;;(@* "CHANGE LOG FOR `icicles-mcmd.el'")
 ;;
+;; 2013/08/02 dadams
+;;     Added: icicle-complete-current-candidate-as-input, icicle-dispatch-C-M-/.
+;;     icicle-change-sort-order: Bind icicle-show-Completions-initially-flag to t.
 ;; 2013/08/01 dadams
 ;;     icicle-choose-completion: Use icicle-choose-completion-string, not choose-completion-string.
 ;; 2013/07/24 dadams
@@ -6722,6 +6725,9 @@
  
 ;;;(@* "CHANGE LOG FOR `icicles-opt.el'")
 ;;
+;; 2013/08/02 dadams
+;;     icicle-top-level-key-bindings: Bind C-M-/ to icicle-dispatch-C-M-/.
+;;     icicle-completion-key-bindings: Do not bind C-M-/ (overridden by icicle-mode binding anyway).
 ;; 2013/07/09 dadams
 ;;     icicle-top-level-key-bindings: Bind S-f10 to icicle-complete-menu-bar (new).
 ;; 2013/07/04 dadams
