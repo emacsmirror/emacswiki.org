448b63f46fb5b45bb6ab6b5e46f301301100aa6a
DrewAdams <anybody@emacswiki.org>
2013-08-18 01:01:39 +0200

    no summary available

--- highlight-fns.el
+++ highlight-fns.el
@@ -1,5 +1,5 @@
 ;;; highlight-fns.el --- Highlight called Lisp functions.
-;; 
+;;
 ;; Filename: highlight-fns.el
 ;; Description: Highlight called Lisp functions.
 ;; Author: Drew Adams
@@ -8,22 +8,22 @@
 ;; Created: Sat Aug 17 13:59:36 2013 (-0700)
 ;; Version: 0
 ;; Package-Requires: ()
-;; Last-Updated: Sat Aug 17 15:32:11 2013 (-0700)
+;; Last-Updated: Sat Aug 17 15:56:40 2013 (-0700)
 ;;           By: dradams
-;;     Update #: 38
+;;     Update #: 42
 ;; URL: http://www.emacswiki.org/highlight-fns.el
 ;; Doc URL: http://emacswiki.org/HighlightLispFunctions
 ;; Keywords: highlight, lisp, functions
 ;; Compatibility: GNU Emacs: 22.x, 23.x, 24.x
-;; 
+;;
 ;; Features that might be required by this library:
 ;;
 ;;   None
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
-;;; Commentary: 
-;; 
+;;
+;;; Commentary:
+;;
 ;;    A minor mode that highlights function calls in Emacs-Lisp mode.
 ;;
 ;;  Put this in your init file:
@@ -40,20 +40,12 @@
 ;;    (require 'highlight-fns)
 ;;    (add-hook 'emacs-lisp-mode-hook 'highlight-lisp-fns-mode 'APPEND)
 ;;
-;;  The highlighting works by identifying a known function in the car
-;;  of a list.  This means that it can sometimes highlight when it
-;;  should not and fail to highlight when it should, as follows:
+;;  The names of known functions are highlighted.  This is so even if
+;;  some occurrence of the name is not used for a function.  For
+;;  example, it might be used as a variable name:
 ;;
-;;  * The name of a known function as the car of a list that is not
-;;    a function call is highlighted.  For example:
-;;
-;;    (let ((setq  (foobar)))...) ; `setq' is a variable here.
-;;
-;;  * A function argument that is invoked by a higher-order function
-;;    is not highlighted.  Only functions in the car of a list are
-;;    highlighted.  For example:
-;;
-;;    (funcall #'max '(41 42 3 4 52)) ; `max' is not highlighted.
+;;    ;; `setq' is a variable here, but it is highlighted anyway.
+;;    (let ((setq  (foobar)))...)
 ;;
 ;;
 ;;  Faces defined here:
@@ -67,33 +59,33 @@
 ;;  Non-interactive functions defined here:
 ;;
 ;;    `highlight-lisp-fns'.
-;; 
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Change Log:
 ;;
 ;; 2013/08/17 dadams
 ;;     Created.
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
 ;; published by the Free Software Foundation; either version 3, or
 ;; (at your option) any later version.
-;; 
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;; General Public License for more details.
-;; 
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program; see the file COPYING.  If not, write to
 ;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
 ;; Floor, Boston, MA 02110-1301, USA.
-;; 
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Code:
 
 (defface highlight-lisp-fns
