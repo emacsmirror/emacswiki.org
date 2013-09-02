0b8afae8b5552da77ac57ed6e8e772e68054bc4c
JoeBloggs <anybody@emacswiki.org>
2013-09-02 17:13:39 +0200

    update

--- helm-helm-commands.el
+++ helm-helm-commands.el
@@ -45,6 +45,8 @@
 ;; containing a list of helm commands and short descriptions. You can press C-z on an item
 ;; to see a longer description of the command, and RET to execute the command.
 ;;
+;; Note: you can achieve the same thing with the `helm-M-x' command which comes with helm,
+;; but that requires a few more keystrokes and doesn't show descriptions by default.
 ;;
 ;;;;
 
