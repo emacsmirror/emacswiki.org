07828d469a0d4d5ea8a653696a285e6dc32b4bae
AlexSchroeder <anybody@emacswiki.org>
2013-09-04 14:20:53 +0200

    Rollback to 2013-09-04 09:10 UTC

--- Comments_on_cdb-gud.el
+++ Comments_on_cdb-gud.el
@@ -4,10 +4,3 @@ Also gud mangles syntax for connection to remote host.
 Here is my hacked version of cdb-gud that resolves these issues with kd.exe
 
 https://github.com/4DA/kd-gud
-
-----
-
-Add your comment here.
-
--- 4DA 2013-09-04 09:11 UTC
-
