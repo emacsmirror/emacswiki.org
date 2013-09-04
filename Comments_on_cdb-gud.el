f107ebabb317e0c16085944b924b275dcdcca3f2
Geats az <anybody@emacswiki.org>
2013-09-04 11:10:35 +0200

    Added a note about kd.exe problems (and a solution)

---
+++ Comments_on_cdb-gud.el
@@ -0,0 +1,6 @@
+Although cdb-gud is nice it fails to show source and callstack when using with kd.exe
+Also gud mangles syntax for connection to remote host.
+
+Here is my hacked version of cdb-gud that resolves these issues with kd.exe
+
+https://github.com/4DA/kd-gud
