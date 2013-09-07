9f40dd3c0587251ef07387d0ab6c03f15df1a097
DrewAdams <anybody@emacswiki.org>
2013-09-07 06:40:53 +0200

    no summary available

--- icicles-chg.el
+++ icicles-chg.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Drew Adams
 ;; Copyright (C) 2007-2013, Drew Adams, all rights reserved.
 ;; Created: Tue Nov 27 07:47:53 2007
-;; Last-Updated: Mon Sep  2 16:24:10 2013 (-0700)
+;; Last-Updated: Fri Sep  6 21:31:48 2013 (-0700)
 ;;           By: dradams
-;;     Update #: 10199
+;;     Update #: 10204
 ;; URL: http://www.emacswiki.org/icicles-chg.el
 ;; Doc URL: http://www.emacswiki.org/Icicles
 ;; Keywords: extensions, help, abbrev, local, minibuffer,
@@ -939,6 +939,9 @@
  
 ;;;(@* "CHANGE LOG FOR `icicles-cmd2.el'")
 ;;
+;; 2013/009/06 dadams
+;;     icicle-imenu-*-full: First arg to icicle-imenu-1 should just be non-nil.
+;;     icicle-imenu-non-interactive-function: Emacs Lisp only, not Lisp mode too.
 ;; 2013/08/13 dadams
 ;;     icicle-where-is: Bind icicle-highlight-input-completion-failure to nil if no prefix arg.
 ;; 2013/07/24 dadams
