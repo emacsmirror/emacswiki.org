4a7e1f2ff713ccdd70b6e053e70ee762453c4137
Bauke Jan Douma <anybody@emacswiki.org>
2013-10-06 14:36:53 +0200

    This is an addition to my previous comment here. I forgot to give the (admittedly non-impressive) source code of the notrail script: #!/bin/bash . . .

--- Comments_on_setnu+.el
+++ Comments_on_setnu+.el
@@ -22,3 +22,32 @@ bjd
 
 -- Bauke Jan Douma 2013-10-06 12:28 UTC
 
+
+----
+
+This is an addition to my previous comment here.
+I forgot to give the (admittedly non-impressive) source
+code of the notrail script:
+
+#!/bin/bash
+# remove trailing whitespace from lines
+sed 's/[ 	]*$//g'
+
+Also, there was another problem with setnu+ that surfaced
+even before the notrail bug;  I often paste code example
+snippets into my emacs buffer.  I was doing that with a
+code example of two lines (cut from the web) which I pasted
+into a buffer with setnu+ line numbers.  In this case I pasted
+them at the final line (I have the option to number the last
+line, after the last newline, also numbered).  What happened
+was that (I have to write this from memory) somehow the two
+pasted lines got pasted on one line in emacs, with the line
+number that used to be the final line number in between
+them.  After a quick wtf I tried to remove the line number there,
+but that didn't work.
+
+Again I'm sorry to say setnu/setnu+ doesn't sufficiently 'work'
+for me, because I presently badly need the lineno's!
+
+-- Bauke Jan Douma 2013-10-06 12:36 UTC
+
