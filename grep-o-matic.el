f3d18faaacba1a9f18c81a1241b6974f92bc820b
AviRozen <anybody@emacswiki.org>
2013-10-13 10:54:30 +0200

    new minor version: fixed compilation warnings, added support for git-grep

--- grep-o-matic.el
+++ grep-o-matic.el
@@ -1,11 +1,11 @@
 ;;; grep-o-matic.el --- auto grep word under cursor
 
-;; Copyright (C) 2008-2012 Avi Rozen
+;; Copyright (C) 2008-2013 Avi Rozen
 
 ;; Author: Avi Rozen <avi.rozen@gmail.com>
 ;; Keywords: tools, processes, search
 ;; URL: https://github.com/ZungBang/emacs-grep-o-matic
-;; Version: 1.0.4
+;; Version: 1.0.6
 
 ;; This file is NOT part of GNU Emacs.
 
@@ -26,11 +26,17 @@
 
 ;;; Commentary:
 
-;; This package lets the user launch a search, with a single key combination,
-;; for the word under the cursor, in the current repository, or the current
-;; directory, or in the set of currently open files.
+;; This package lets the user launch a search, with a single key
+;; combination, for the word under the cursor, in the current
+;; repository, or the current directory, or in the set of currently
+;; open files.
 ;;
-;; Works nicely in combination with grep-a-lot and repository-root libraries.
+;; Repository-wide search should work out-of-the-box for vc backends
+;; known to Emacs, and may be further enhanced with the
+;; repository-root library. Git grep may optionally be used instead of
+;; grep in git repositories.
+;;
+;; Works nicely in combination with grep-a-lot.
 ;;
 ;; Installation:
 ;;
@@ -38,13 +44,9 @@
 ;;    byte-compile it (e.g. with `M-x byte-compile-file') for better
 ;;    performance.
 ;; 2. Add the following to your ~/.emacs:
-;;    ;(require 'repository-root) ; optional: needed for repository-wide search
 ;;    (require 'grep-o-matic)
 ;; 3. Customize grep-o-matic with `M-x customize-group grep-o-matic'
-;;
-;; Repository-wide search requires customization of the repository root function,
-;; unless the repository-root libarary is loaded before grep-o-matic.
-;;
+;;    
 ;; Default Key Bindings:
 ;;
 ;; M-] M-/         Search for current word in current repository
@@ -56,6 +58,12 @@
 ;;; Code:
 
 (require 'grep)
+(require 'vc)
+
+(eval-when-compile
+  (unless (featurep 'repository-root)
+    (defsubst repository-root (filename)
+      nil)))
 
 (defgroup grep-o-matic nil
   "Automation layer for grep.el"
@@ -63,23 +71,11 @@
   :group 'convenience
   :prefix "grep-o-matic-")
 
-
 (defcustom grep-o-matic-search-patterns
   (list "*.cpp" "*.c" "*.h" "*.awk" "*.sh" "*.py" "*.pl" "[Mm]akefile" "*.el")
   "*Search file patterns for use with grep-o-matic-* commands."
   :group 'grep-o-matic
   :type '(repeat string))
-
-(defcustom grep-o-matic-repository-root-function (if (featurep 'repository-root)
-                                                     'repository-root
-                                                   nil)
-  "*If non-nil, a function that returns the current file's repository root directory.
-The function is called with a single string argument (a file name) and should
-return either nil, or a string, which is the root directory of that file's repository.
-The default value is nil, unless the repository-root library is loaded before
-loading grep-o-matic, in which case the default value is `repository-root'."
-  :group 'grep-o-matic
-  :type '(choice (const nil) (function)))
 
 (defcustom grep-o-matic-ask-about-save t
   "*If non-nil ask which buffers to save before performing a search.
@@ -87,18 +83,32 @@ Otherwise, all modified buffers are saved without asking."
   :group 'grep-o-matic
   :type 'boolean)
 
+(defcustom grep-o-matic-use-git-grep nil
+  "*If non-nil use git grep to perfrom search in git
+repositories."
+  :group 'grep-o-matic
+  :type 'boolean)
+
+(defcustom grep-o-matic-git-grep-template "git grep <C> -n -e <R> -- <F>"
+  "Template for git grep command. See `grep-template' for
+more details."
+  :group 'grep-o-matic
+  :type 'string)
+
 (defun grep-o-matic-repository-root (filename)
-  "Attempt to deduce the current file's repository root directory.
-You should customize `grep-o-matic-repository-root-function' and provide a function that
-does the actual work, based of the type of SCM tool that you're using."
+  "Attempt to deduce the current file's repository root directory."
   (if (null filename)
-      nil
+      default-directory
     (let* ((directory (file-name-directory filename))
-           (repository-root (if (and grep-o-matic-repository-root-function
-                                     (functionp grep-o-matic-repository-root-function))
-                                (apply grep-o-matic-repository-root-function (list filename))
-                              nil)))
-      (or repository-root directory))))
+	   (backend (vc-backend filename))
+	   (vc_rootdir (if backend
+                           (ignore-errors
+                             (vc-call-backend backend 'root directory))
+			 nil))
+	   (rr_rootdir (if (featurep 'repository-root)
+			   (repository-root filename)
+			 nil)))
+      (or vc_rootdir rr_rootdir directory))))
 
 (defun grep-o-matic-get-regexp (prompt)
   "Get the default regexp or query the user for it."
@@ -109,9 +119,8 @@ does the actual work, based of the type of SCM tool that you're using."
 	  regexp)
       (grep-read-regexp))))
 
-(defun grep-o-matic-directory (prompt directory)
-  "Search directory for word at point.
-Optionaly prompt for regexp to search."
+(defun grep-o-matic-compute-search-patterns ()
+  "Compute file search glob patterns."
   (let ((patterns grep-o-matic-search-patterns)
 	(extension (if buffer-file-name
 		       (file-name-extension buffer-file-name)
@@ -134,20 +143,55 @@ Optionaly prompt for regexp to search."
 	(setq patterns (list (if extension
 				 (concat "*." extension)
 			       "*"))))
-      (grep-compute-defaults)
-      (save-some-buffers (not grep-o-matic-ask-about-save) nil)
-      (rgrep (grep-o-matic-get-regexp prompt)
-	     (mapconcat (lambda (s) s) patterns " ")
-	     (if directory
-		 directory
-	       default-directory)))))
+      (mapconcat (lambda (s) s) patterns " "))))
 
+
+(defun grep-o-matic-directory (prompt directory)
+  "Search directory for word at point.
+Optionaly prompt for regexp to search."
+  (let ((patterns (grep-o-matic-compute-search-patterns)))
+    (grep-compute-defaults)
+    (save-some-buffers (not grep-o-matic-ask-about-save) nil)
+    (rgrep (grep-o-matic-get-regexp prompt)
+	   patterns
+	   (if directory
+	       directory
+	     default-directory))))
+
+(defun grep-o-matic-git-repository (prompt repository-root)
+  "Search a git repository for word at point.
+Optionaly prompt for regexp to search."
+  (interactive "P")
+  (let ((regexp (grep-o-matic-get-regexp prompt))
+	(patterns (grep-o-matic-compute-search-patterns)))
+    (grep-compute-defaults)
+    (save-some-buffers (not compilation-ask-about-save) nil)
+    (let ((default-directory repository-root))
+      ; Running git grep with no pager (as is necessary) does not play
+      ; well with font-locking, so that next/previous-error do not
+      ; work at all. So we pipe the output of 'git grep' through a
+      ; dummy 'cat'.
+      (grep (grep-expand-template
+	     (concat (grep-expand-template 
+		      grep-o-matic-git-grep-template
+		      regexp
+		      patterns)
+		      " | cat"))))))
+
+;;;###autoload
 (defun grep-o-matic-repository (&optional prompt)
   "Search repository for word at point.
 Optionaly prompt for regexp to search."
   (interactive "P")
-  (grep-o-matic-directory prompt (grep-o-matic-repository-root buffer-file-name)))
+  (let ((repository-root (grep-o-matic-repository-root buffer-file-name)))
+    (if (and grep-o-matic-use-git-grep
+	     (string-equal "git"
+			   (downcase (symbol-name (vc-backend
+						   buffer-file-name)))))
+	(grep-o-matic-git-repository prompt repository-root)
+      (grep-o-matic-directory prompt repository-root))))
 
+;;;###autoload
 (defun grep-o-matic-current-directory (&optional prompt)
   "Search current directory for word at point.
 Optionaly prompt for regexp to search."
@@ -156,6 +200,7 @@ Optionaly prompt for regexp to search."
                                      (file-name-directory buffer-file-name)
                                    nil)))
 
+;;;###autoload
 (defun grep-o-matic-visited-files (&optional prompt)
   "Search currently visited files for word at point.
 Optionaly prompt for regexp to search."
@@ -168,11 +213,7 @@ Optionaly prompt for regexp to search."
                                    (mapcar '(lambda (buffer)
                                               (let ((file (buffer-file-name buffer)))
                                                 (if (and file
-                                                         (cond ((featurep 'ange-ftp)
-                                                                (not (ange-ftp-ftp-name file)))
-                                                               ((featurep 'efs)
-                                                                (not (efs-ftp-path file)))
-                                                               (t t)))
+                                                         (not (file-remote-p file)))
                                                     (list file))))
                                            (buffer-list)))
                             " ")))
@@ -185,12 +226,12 @@ Optionaly prompt for regexp to search."
 
 ;; key bindings
 (define-prefix-command 'grep-o-matic-map)
-(define-key grep-o-matic-map "\M-/" 'grep-o-matic-repository)
-(define-key grep-o-matic-map "/" 'grep-o-matic-repository)
-(define-key grep-o-matic-map "\M-." 'grep-o-matic-current-directory)
-(define-key grep-o-matic-map "." 'grep-o-matic-current-directory)
-(define-key grep-o-matic-map "\M-," 'grep-o-matic-visited-files)
-(define-key grep-o-matic-map "," 'grep-o-matic-visited-files)
+(define-key 'grep-o-matic-map "\M-/" 'grep-o-matic-repository)
+(define-key 'grep-o-matic-map "/" 'grep-o-matic-repository)
+(define-key 'grep-o-matic-map "\M-." 'grep-o-matic-current-directory)
+(define-key 'grep-o-matic-map "." 'grep-o-matic-current-directory)
+(define-key 'grep-o-matic-map "\M-," 'grep-o-matic-visited-files)
+(define-key 'grep-o-matic-map "," 'grep-o-matic-visited-files)
 (global-set-key "\M-]" 'grep-o-matic-map)
 
 (provide 'grep-o-matic)
