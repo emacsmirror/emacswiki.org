0f68840b36fa68a7795ef0f29051a769cb476da4
TunaFish <anybody@emacswiki.org>
2013-10-21 17:34:21 +0200

    minor warning on byte-compile: Compiling file c:/Program Files/gnu/emacs-24.3/site-lisp/setup-cygwin.el at Mon Oct 21 11:25:52 2013 . . .

---
+++ Comments_on_setup-cygwin.el
@@ -0,0 +1,9 @@
+minor warning on byte-compile:
+Compiling file c:/Program Files/gnu/emacs-24.3/site-lisp/setup-cygwin.el at Mon Oct 21 11:25:52 2013
+setup-cygwin.el:82:1:Warning: defgroup for `setup-cygwin' fails to specify containing group
+
+Although the current help info for defgroup (24.3.1) doesn't specify this as a requirement.
+(defgroup SYMBOL MEMBERS DOC &rest ARGS)
+
+-- TunaFish 2013-10-21 15:34 UTC
+
