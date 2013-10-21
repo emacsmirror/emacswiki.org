85a01478d3aa335fbb816f5c61a06d8365fe3c9d
TunaFish5 <anybody@emacswiki.org>
2013-10-21 17:45:22 +0200

    minor fix:    custom-group defined here needs to be placed in emacs custom-group heirarchy

--- Comments_on_setup-cygwin.el
+++ Comments_on_setup-cygwin.el
@@ -1,9 +1,15 @@
 minor warning on byte-compile:
+<pre>
 Compiling file c:/Program Files/gnu/emacs-24.3/site-lisp/setup-cygwin.el at Mon Oct 21 11:25:52 2013
 setup-cygwin.el:82:1:Warning: defgroup for `setup-cygwin' fails to specify containing group
+</pre>
 
 Although the current help info for defgroup (24.3.1) doesn't specify this as a requirement.
+
+<pre>
 (defgroup SYMBOL MEMBERS DOC &rest ARGS)
+</pre>
 
--- TunaFish 2013-10-21 15:34 UTC
+cygwin-mount.el defgroups <code>cygwin-mount</code> and places it in the <code>files</code> group.
 
+setup-cygwin, however, seems more appropriately placed in the <code>Processes</code> group.
