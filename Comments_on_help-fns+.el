995d657c26effadc73a29f7bec30e36fd061fb21
PuercoPop <anybody@emacswiki.org>
2013-11-17 19:44:41 +0100

    Add your comment here. Hi, it after installing from melpa help-fns+ I got the warning that interactive-p is an obsolete function that one should use . . .

---
+++ Comments_on_help-fns+.el
@@ -0,0 +1,5 @@
+Add your comment here.
+Hi, it after installing from melpa help-fns+ I got the warning that interactive-p is an obsolete function that one should use called-interactively-p instead. Do you mind if code?
+
+-- PuercoPop 2013-11-17 18:44 UTC
+
