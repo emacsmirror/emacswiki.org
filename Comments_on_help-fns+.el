e1c878340c26a0d77cdf673f95f0cd0997ef5d23
DrewAdams <anybody@emacswiki.org>
2013-11-17 20:16:19 +0100

    You can ignore all such compiler warnings.  The byte-compiler assumes that the code being compiled is written only for the same Emacs version as the . . .

--- Comments_on_help-fns+.el
+++ Comments_on_help-fns+.el
@@ -3,3 +3,10 @@ Hi, it after installing from melpa help-fns+ I got the warning that interactive-
 
 -- PuercoPop 2013-11-17 18:44 UTC
 
+
+----
+
+You can ignore all such compiler warnings.  The byte-compiler assumes that the code being compiled is written only for the same Emacs version as the byte-compiler. For many third-party libraries the code is designed to work with multiple Emacs versions. And that is the case here.
+
+-- DrewAdams 2013-11-17 19:16 UTC
+
