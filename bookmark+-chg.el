51881a3f5c3c6732acc5682dffe20f83cf8b7659
DrewAdams <anybody@emacswiki.org>
2013-11-22 08:05:19 +0100

    no summary available

--- bookmark+-chg.el
+++ bookmark+-chg.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
 ;; Copyright (C) 2000-2013, Drew Adams, all rights reserved.
 ;; Created: Fri Sep 15 07:58:41 2000
-;; Last-Updated: Tue Nov  5 16:01:56 2013 (-0800)
+;; Last-Updated: Thu Nov 21 22:58:40 2013 (-0800)
 ;;           By: dradams
-;;     Update #: 15448
+;;     Update #: 15452
 ;; URL: http://www.emacswiki.org/bookmark+-chg.el
 ;; Doc URL: http://www.emacswiki.org/BookmarkPlus
 ;; Keywords: bookmarks, bookmark+
@@ -146,6 +146,9 @@
  
 ;;;(@* "CHANGE LOG FOR `bookmark+-1.el'")
 ;;
+;; 2013/11/21 dadams
+;;    bmkp-url-target-set: Added argument NO-OVERWRITE-P.
+;;    bmkp-(url|file)-target-set: Numeric prefix arg means do not overwrite.  N<0 means use full name.
 ;; 2013/11/05 dadams
 ;;     Added: bmkp-create-dired-bookmarks-recursive.
 ;;     bookmark-set: Added optional arg NO-UPDATE-P - pass it to bookmark-store.
