fbedf5565170f1ca15740e9c8dcc830dc2a4b627
Anonymous <anybody@emacswiki.org>
2013-11-25 02:37:28 +0100

    no summary available

--- image-dimensions-minor-mode.el
+++ image-dimensions-minor-mode.el
@@ -1,6 +1,6 @@
  ;; Adds the image dimensions in the mode line, when viewing an image.
  ;; Compatibility: GNU Emacs 24.x
-
+ 
  ;; To also see this information in the frame title, your `frame-title-format'
  ;; variable might be something like the following:
  ;;
@@ -10,10 +10,10 @@
  ;;         (dired-directory
  ;;          (:eval (concat (buffer-name) " (Emacs) " dired-directory))
  ;;          ("%b (Emacs)"))))
-
+ 
  (defvar-local image-dimensions-minor-mode-dimensions nil
    "Buffer-local image dimensions for `image-dimensions-minor-mode'")
-
+ 
  (define-minor-mode image-dimensions-minor-mode
    "Displays the image dimensions in the mode line."
    :init-value nil
@@ -27,10 +27,10 @@
 			   (destructuring-bind (width . height)
 				   (image-size image :pixels)
 				 (format " (%dx%d)" width height)))))))
-
+ 
  (defun image-mode-hook ()
    (image-dimensions-minor-mode 1))
-
+ 
  (add-hook 'image-mode-hook 'image-mode-hook)
-
+ 
  (provide 'image-dimensions-minor-mode)
