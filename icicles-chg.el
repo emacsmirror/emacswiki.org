764718c2fd47943c079e4ebcc6b5a18984e1d55f
DrewAdams <anybody@emacswiki.org>
2013-11-29 00:16:23 +0100

    no summary available

--- icicles-chg.el
+++ icicles-chg.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Drew Adams
 ;; Copyright (C) 2007-2013, Drew Adams, all rights reserved.
 ;; Created: Tue Nov 27 07:47:53 2007
-;; Last-Updated: Tue Nov 26 09:26:52 2013 (-0800)
+;; Last-Updated: Thu Nov 28 09:56:23 2013 (-0800)
 ;;           By: dradams
-;;     Update #: 10315
+;;     Update #: 10316
 ;; URL: http://www.emacswiki.org/icicles-chg.el
 ;; Doc URL: http://www.emacswiki.org/Icicles
 ;; Keywords: extensions, help, abbrev, local, minibuffer,
@@ -1844,6 +1844,8 @@
  
 ;;;(@* "CHANGE LOG FOR `icicles-fn.el'")
 ;;
+;; 2013/11/28 dadams
+;;     icicle-read-face-name: Better fix for brain-dead vanilla Emacs PROMPT.
 ;; 2013/11/24 dadams
 ;;     icicle-read-face-name: Updated for Emacs 24.
 ;; 2013/11/19 dadams
