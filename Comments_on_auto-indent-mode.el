b11b8cb534d486f85123c7f17d30c216ffe46ae3
kornfactory <anybody@emacswiki.org>
2013-11-29 12:22:34 +0100

    Hi great job with this .el I have a question. Is it normal that this automatically happens? for(i=0; i<n; i++)     {         blabla; . . .

---
+++ Comments_on_auto-indent-mode.el
@@ -0,0 +1,25 @@
+Hi
+great job with this .el
+I have a question.
+Is it normal that this automatically happens?
+
+for(i=0; i<n; i++)
+    {
+        blabla;
+    }
+
+emacs autoindents the parenthesis, and again the ode inside the block.
+Shouldn't it be like this?
+
+for(i=0; i<n; i++)
+{
+    blabla;
+}
+
+How could I make this happen?
+THanks in advance.
+
+Federico
+
+-- kornfactory 2013-11-29 11:22 UTC
+
