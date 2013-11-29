55411f42e665580b15d05704171d6b22faea3f71
kornfactory <anybody@emacswiki.org>
2013-11-29 12:25:17 +0100

    i'm sorry, it didin't show well for(i=0; i<n; i++) ->  { ->  ->  blabla;     } emacs autoindents the parenthesis, and again the . . .

--- Comments_on_auto-indent-mode.el
+++ Comments_on_auto-indent-mode.el
@@ -23,3 +23,28 @@ Federico
 
 -- kornfactory 2013-11-29 11:22 UTC
 
+
+----
+
+i'm sorry, it didin't show well
+
+for(i=0; i<n; i++)
+->  {
+->  ->  blabla;
+    }
+
+emacs autoindents the parenthesis, and again the code inside the block.
+Shouldn't it be like this?
+
+for(i=0; i<n; i++)
+{
+->  blabla;
+}
+
+How can I make this happen?
+Thanks in advance.
+
+Federico
+
+-- kornfactory 2013-11-29 11:25 UTC
+
