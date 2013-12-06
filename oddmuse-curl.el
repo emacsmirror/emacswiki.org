c39d7d58d8ca901001fe4d75254f78b0d683242d
AlexSchroeder <anybody@emacswiki.org>
2013-12-06 11:21:46 +0100

    git

--- oddmuse-curl.el
+++ oddmuse-curl.el
@@ -1,1 +1,3 @@
+This file is hosted [http://git.savannah.gnu.org/cgit/oddmuse.git/tree/contrib/oddmuse-curl.el on Savannah] (git).
+
 <include text "http://git.savannah.gnu.org/cgit/oddmuse.git/plain/contrib/oddmuse-curl.el">
