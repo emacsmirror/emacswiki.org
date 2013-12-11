347bc260dadde8ebb55b2ca47415e834f0d9081a
DrewAdams <anybody@emacswiki.org>
2013-12-11 01:59:18 +0100

    no summary available

--- icicles-chg.el
+++ icicles-chg.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Drew Adams
 ;; Copyright (C) 2007-2013, Drew Adams, all rights reserved.
 ;; Created: Tue Nov 27 07:47:53 2007
-;; Last-Updated: Mon Dec  9 14:10:01 2013 (-0800)
+;; Last-Updated: Tue Dec 10 16:53:52 2013 (-0800)
 ;;           By: dradams
-;;     Update #: 10493
+;;     Update #: 10497
 ;; URL: http://www.emacswiki.org/icicles-chg.el
 ;; Doc URL: http://www.emacswiki.org/Icicles
 ;; Keywords: extensions, help, abbrev, local, minibuffer,
@@ -1030,6 +1030,11 @@
  
 ;;;(@* "CHANGE LOG FOR `icicles-cmd2.el'")
 ;;
+;; 2013/12/10 dadams
+;;     Added: icicle-search-regexp.
+;;     icicle-search-define-candidates-1: Set icicle-search-regexp.
+;;     icicle-search-highlight-and-maybe-replace:
+;;       Search for main regexp, to set match data for replace-match (via icicle-search-replace-match).
 ;; 2013/12/01 dadams
 ;;     icicle-this-command-keys-prefix: Use [] as prefix key if this command is icicle-complete-keys.
 ;; 2013/11/17 dadams
