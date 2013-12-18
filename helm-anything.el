7fcfd1f0eeb0af5f2f6628d7c48c78d0f14d5e3b
rubikitch <anybody@emacswiki.org>
2013-12-18 03:34:03 +0100

    Update

--- helm-anything.el
+++ helm-anything.el
@@ -5,9 +5,10 @@
 ;; Author: rubikitch <rubikitch@ruby-lang.org>
 ;; Maintainer: rubikitch <rubikitch@ruby-lang.org>
 ;; Copyright (C) 2013, rubikitch, all rights reserved.
-;; Time-stamp: <2013-03-29 13:41:30 rubikitch>
+;; Time-stamp: <2013-12-18 11:32:24 rubikitch>
 ;; Created: 2013-03-15 08:59:37
-;; Version: 0.1
+;; Version: 0.2
+;; Package-Requires: ((helm "20130406") (anything "20120101"))
 ;; URL: http://www.emacswiki.org/emacs/download/helm-anything.el
 ;; Keywords: helm, anything, convenience, search, tools
 ;; Compatibility: GNU Emacs 24.2.2
@@ -40,8 +41,15 @@
 ;; 
 ;; Bridge between anything and helm.
 ;;
-;; * M-x `helm-anything-resume' replaces M-x `anything-resume' and M-x `helm-resume'.
-;; 
+;; M-x `helm-anything-resume' replaces M-x `anything-resume' and M-x
+;; `helm-resume'.
+;;
+;; `helm-anything/funcall', `helm-anything/set' and
+;; `helm-anything/get' handles helm or anything functions and
+;; variables.
+;;
+;; `helm-anything/define-key' binds key to `helm-map' and `anything-map'.
+;;
 ;;; Installation:
 ;;
 ;; Put helm-anything.el to your load-path.
@@ -57,6 +65,21 @@
 ;;
 ;; No need more.
 ;;
+;;; Examples:
+;;
+;; (defun helm-anything-occur-search-forward ()
+;;   (interactive)
+;;   (helm-anything/funcall 'next-line) ;; or (anything-previous-line)
+;;   (helm-anything/funcall 'execute-persistent-action))
+;;
+;; (setq helm-testvar 0)
+;; (setq anything-testvar 100)
+;; (defun helm-anything-test ()
+;;   (interactive)
+;;   (helm-anything/set 'testvar (1+ (helm-anything/value 'testvar)))
+;;   (message "%s" (helm-anything/value 'testvar)))
+;; (define-key helm-map (kbd "C-x C-z") 'helm-anything-test)
+;; (define-key anything-map (kbd "C-x C-z") 'helm-anything-test)
 
 ;;; Require
 (require 'helm)
@@ -102,6 +125,24 @@ Called from lisp, you can specify a buffer-name as a string with PARG."
   (interactive)
   (global-set-key [remap anything-resume] 'helm-anything-resume)
   (global-set-key [remap helm-resume] 'helm-anything-resume))
+
+;;;; Compatibility code.
+(defun helm-anything/funcall (func &rest args)
+  "Call helm or anything function FUNC with ARGS.
+If you are using helm, call helm-FUNC, otherwise anything-FUNC."
+  (apply (intern (format "%s-%s" (if (helm-alive-p) 'helm 'anything) func)) args))
+(defun helm-anything/set (varname value)
+  "Set helm or anything variable VARNAME to VALUE.
+If you are using helm, set helm-VARNAME, otherwise anything-VARNAME."
+  (set (intern (format "%s-%s" (if (helm-alive-p) 'helm 'anything) varname)) value))
+(defun helm-anything/value (varname)
+  "Get helm or anything variable VARNAME.
+If you are using helm, set helm-VARNAME, otherwise anything-VARNAME."
+  (symbol-value (intern (format "%s-%s" (if (helm-alive-p) 'helm 'anything) varname))))
+(defun helm-anything/define-key (key command)
+  "Define KEY to COMMAND both `helm-map' and `anything-map'."
+  (define-key helm-map key command)
+  (define-key anything-map key command))
 
 (provide 'helm-anything)
 
