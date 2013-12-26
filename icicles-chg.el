ff6e20edc7d2208e9de515a7c0470be317fa107e
DrewAdams <anybody@emacswiki.org>
2013-12-26 20:41:10 +0100

    no summary available

--- icicles-chg.el
+++ icicles-chg.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
 ;; Copyright (C) 2007-2014, Drew Adams, all rights reserved.
 ;; Created: Tue Nov 27 07:47:53 2007
-;; Last-Updated: Thu Dec 26 11:29:33 2013 (-0800)
+;; Last-Updated: Thu Dec 26 11:35:55 2013 (-0800)
 ;;           By: dradams
-;;     Update #: 10505
+;;     Update #: 10507
 ;; URL: http://www.emacswiki.org/icicles-chg.el
 ;; Doc URL: http://www.emacswiki.org/Icicles
 ;; Keywords: extensions, help, abbrev, local, minibuffer,
@@ -4037,6 +4037,8 @@
  
 ;;;(@* "CHANGE LOG FOR `icicles-mcmd.el'")
 ;;
+;; 2013/12/26 dadams
+;;     icicle-narrow-candidates: Get back original candidates, via text property icicle-orig-cand.
 ;; 2013/12/01 dadams
 ;;     icicle-top-level: Throw to the catch icicle-top-level, which is now defined for multi-commands.
 ;; 2013/09/21 dadams
