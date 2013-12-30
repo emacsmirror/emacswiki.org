4d093706b9e2dd065011749fab62f5bd6f635cfb
AndyStewart <anybody@emacswiki.org>
2013-12-30 17:34:55 +0100

    Update init-backup.el

--- init-backup.el
+++ init-backup.el
@@ -76,13 +76,13 @@
 
 ;;; Code:
 
-(setq backup-directory-alist '(("." . "~/MyEmacs/Backup"))) ;备份目录
-(setq make-backup-files t)                                  ;开启备份
-(setq kept-old-versions 5)                                  ;备份老版本的数量
-(setq kept-new-versions 10)                                 ;备份新版本的数量
-(setq delete-old-versions t)                                ;自动删除老版本
-(setq backup-by-copying t)                                  ;拷贝时自动备份
-(setq version-control t)                                    ;多版本备份
+(setq backup-directory-alist `((".*" . ,temporary-file-directory))) ;让所有 ~ 备份文件全部放在 /tmp 目录
+(setq make-backup-files t)                                          ;开启备份
+(setq kept-old-versions 10)                                         ;备份老版本的数量
+(setq kept-new-versions 20)                                         ;备份新版本的数量
+(setq delete-old-versions t)                                        ;自动删除老版本
+(setq backup-by-copying t)                                          ;拷贝时自动备份
+(setq version-control t)                                            ;多版本备份
 
 (provide 'init-backup)
 
