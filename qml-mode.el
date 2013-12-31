38ccdd73285d3fc954c7d6754d2b832842ef3291
AndyStewart <anybody@emacswiki.org>
2013-12-31 14:27:20 +0100

    Upload qml-mode.el, sorry forgot elisp header

--- qml-mode.el
+++ qml-mode.el
@@ -1,3 +1,89 @@
+;;; qml-mode.el --- Mode for Qt QML file
+
+;; Filename: qml-mode.el
+;; Description: Mode for Qt QML file
+;; Author: Andy Stewart <lazycat.manatee@gmail.com>
+;; Maintainer: Andy Stewart <lazycat.manatee@gmail.com>
+;; Copyright (C) 2013, Andy Stewart, all rights reserved.
+;; Created: 2013-12-31 21:23:56
+;; Version: 0.1
+;; Last-Updated: 2013-12-31 21:23:56
+;;           By: Andy Stewart
+;; URL: http://www.emacswiki.org/emacs/download/qml-mode.el
+;; Keywords: 
+;; Compatibility: GNU Emacs 24.3.50.1
+;;
+;; Features that might be required by this library:
+;;
+;; 
+;;
+
+;;; This file is NOT part of GNU Emacs
+
+;;; License
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
+;;; Commentary: 
+;; 
+;; Mode for Qt QML file
+;; 
+
+;;; Installation:
+;;
+;; Put qml-mode.el to your load-path.
+;; The load-path is usually ~/elisp/.
+;; It's set in your ~/.emacs like this:
+;; (add-to-list 'load-path (expand-file-name "~/elisp"))
+;;
+;; And the following to your ~/.emacs startup file.
+;;
+;; (require 'qml-mode)
+;;
+;; No need more.
+
+;;; Customize:
+;;
+;; 
+;;
+;; All of the above can customize by:
+;;      M-x customize-group RET qml-mode RET
+;;
+
+;;; Change log:
+;;	
+;; 2013/12/31
+;;      * First released.
+;; 
+
+;;; Acknowledgements:
+;;
+;; 
+;;
+
+;;; TODO
+;;
+;; 
+;;
+
+;;; Require
+
+
+;;; Code:
+
 (defcustom qml-mode-hook '()
   "Called upon entry into term mode.
 This is run before the process is cranked up."
