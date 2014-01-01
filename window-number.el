ac18142395fe0105ea6a630ff760b48a757d19de
AndyStewart <anybody@emacswiki.org>
2014-01-01 15:49:03 +0100

    Add new function `window-number-switch', and recommand binding 'C-x o' on it for fast switch in windows.

--- window-number.el
+++ window-number.el
@@ -1,68 +1,132 @@
-;;; window-number.el --- Select windows by numbers
+;;; window-number.el --- Select windows by numbers.
 
-;; Copyright (C) 2004 Johann "Myrkraverk" Oskarsson
-;; <myrkraverk@users.sourceforge.net>
+;; Filename: window-number.el
+;; Description: Select windows by numbers.
+;; Author: Johann "Myrkraverk" Oskarsson <myrkraverk@users.sourceforge.net>
+;; Maintainer: Johann "Myrkraverk" Oskarsson <myrkraverk@users.sourceforge.net>
+;;             Andy Stewart <lazycat.manatee@gmail.com>
+;; Copyright (C) 2004, Johann "Myrkraverk" Oskarsson, all rights reserved.
+;; Copyright (C) 2014, Andy Stewart, all rights reserved.
+;; Created: 2004
+;; Version: 0.1
+;; Last-Updated: 2014-01-01 22:25:03
+;;           By: Andy Stewart
+;; URL: http://www.emacswiki.org/emacs/download/window-number.el
+;; Keywords:
+;; Compatibility: GNU Emacs 24.3.50.1
+;;
+;; Features that might be required by this library:
+;;
+;; `cl'
+;;
 
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation; either version 2 of
-;; the License, or (at your option) any later version.
+;;; This file is NOT part of GNU Emacs
 
-;; This program is distributed in the hope that it will be
-;; useful, but WITHOUT ANY WARRANTY; without even the implied
-;; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-;; PURPOSE.  See the GNU General Public License for more details.
+;;; License
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
 
-;; You should have received a copy of the GNU General Public
-;; License along with this program; if not, write to the Free
-;; Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-;; MA 02111-1307 USA
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
 
 ;;; Commentary:
+;;
+;; Window number mode allows you to select windows by numbers.
+;;
+;; `window-number-switch' same as `other-window' (C-x o) when windows less than three.
+;; If have three windows (or more) showing, `window-number-switch' will
+;; highlight window number at mode-line then prompt you input window number.
+;;
+;; I binding `window-number-switch' on 'C-x o' to instead `other-window'.
+;;
 
-;; Introduction
-;; ============
+;;; Installation:
+;;
+;; Put window-number.el to your load-path.
+;; The load-path is usually ~/elisp/.
+;; It's set in your ~/.emacs like this:
+;; (add-to-list 'load-path (expand-file-name "~/elisp"))
+;;
+;; And the following to your ~/.emacs startup file.
+;;
+;; (require 'window-number)
+;; (window-number-mode 1)
+;;
+;; No need more.
 
-;; Window number mode allows you to select windows by numbers.  This
-;; edition now works with XEmacs as well as GNU Emacs.  The window
-;; numbers do not show up in the mode-line in XEmacs yet, instead a
-;; -?- is displayed.  Hopefully this can be fixed soon, but really
-;; depends on XEmacs developers.
+;;; Customize:
+;;
+;; `window-number-active-foreground'
+;; `window-number-active-background'
+;; `window-number-inactive-foreground'
+;; `window-number-inactive-background'
+;;
+;; All of the above can customize by:
+;;      M-x customize-group RET window-number RET
+;;
 
-;; Installation
-;; ============
+;;; Change log:
+;;
+;; 2014/01/01
+;;      * Add new function `window-number-switch'
+;;      * Add group `window-number' and customize colors.
+;;      * Highlight window number on mode-line when `window-number-switch' prompt input.
+;;
+;; 2004
+;;      * First released.
+;;
 
-;; Drop this file into your load path.  C-h v load-path RET or F1 v
-;; load-path RET will help.  Then place the following lines into your
-;; .emacs or ~/.xemacs/init.el and uncomment them.
+;;; Acknowledgements:
+;;
+;;
+;;
 
-;; ----------------------------------------------------------------------------
+;;; TODO
+;;
+;;
+;;
 
-;; (autoload 'window-number-mode "window-number"
-;;   "A global minor mode that enables selection of windows according to
-;; numbers with the C-x C-j prefix.  Another mode,
-;; `window-number-meta-mode' enables the use of the M- prefix."
-;;   t)
+;;; Require
 
-;; (autoload 'window-number-meta-mode "window-number"
-;;   "A global minor mode that enables use of the M- prefix to select
-;; windows, use `window-number-mode' to display the window numbers in
-;; the mode-line."
-;;   t)
+(require 'cl)                           ; for set-difference and loop
 
-;; ----------------------------------------------------------------------------
+;;; Code:
 
-;; Then you can use M-x window-number-mode RET to turn the mode on, or
-;; place (window-number-mode 1) and (window-number-meta-mode 1) into
-;; your .emacs or ~/.xemacs/init.el.
+(defgroup window-number nil
+  "Window number group")
 
-;; ----------------------------------------------------------------------------
+(defcustom window-number-active-foreground "black"
+  "The foreground color when window number active."
+  :type 'string
+  :group 'window-number)
 
-;; Code starts here.
+(defcustom window-number-active-background "gold"
+  "The background color when window number active."
+  :type 'string
+  :group 'window-number)
 
-;; ----------------------------------------------------------------------------
+(defcustom window-number-inactive-foreground "white"
+  "The foreground color when window number inactive."
+  :type 'string
+  :group 'window-number)
 
-(require 'cl); for set-difference and loop
+(defcustom window-number-inactive-background "darkred"
+  "The background color when window number inactive."
+  :type 'string
+  :group 'window-number)
+
+(defface window-number-face nil
+  "The face used for the window number in the mode-line.")
 
 (defun window-number-list ()
   "Returns a list of the windows, in fixed order and the
@@ -80,6 +144,21 @@ minibuffer (even if not active) last."
              (not (eq walk-windows-current walk-windows-start))))
     (reverse (cons (car list) (cdr list)))))
 
+(defun window-number-switch ()
+  "Call `other-window' when just two windows.
+Prompt user input window number if have more windows."
+  (interactive)
+  (if (< (length (window-list)) 3)
+      (call-interactively 'other-window)
+    (window-number-set-active-color)
+    (unwind-protect
+        (window-number-select (read-number "Window number: "))
+      ;; Reset to inactive color if interactive is intercept by Ctrl+g
+      (window-number-set-inactive-color)
+      ))
+  ;; Always reset to inactive color at end.
+  (window-number-set-inactive-color))
+
 (defun window-number-select (number)
   "Selects the nth window."
   (interactive "P")
@@ -90,6 +169,16 @@ minibuffer (even if not active) last."
             (select-window window)
           (error "No such window.")))))
 
+(defun window-number-set-inactive-color ()
+  (set-face-foreground 'window-number-face "white")
+  (set-face-background 'window-number-face "darkred")
+  (force-mode-line-update))
+
+(defun window-number-set-active-color ()
+  (set-face-foreground 'window-number-face "black")
+  (set-face-background 'window-number-face "gold")
+  (force-mode-line-update))
+
 (defun window-number ()
   "Returns the the number of the current window."
   (length
@@ -99,7 +188,7 @@ minibuffer (even if not active) last."
 (defun window-number-string ()
   "Returns the string containing the number of the current window"
   (propertize
-   (concat " -" (number-to-string (window-number)) "-")
+   (concat " [" (number-to-string (window-number)) "] ")
    'face
    'window-number-face))
 
@@ -110,21 +199,21 @@ minibuffer (even if not active) last."
   "Keymap for the window number meta mode.")
 
 (defmacro window-number-define-keys (mode-map prefix)
-  `(progn 
+  `(progn
      ,@(loop for number from 1 to 10 collect
-             `(define-key ,mode-map 
-                (kbd ,(concat prefix (number-to-string 
+             `(define-key ,mode-map
+                (kbd ,(concat prefix (number-to-string
                                       (if (>= number 10) 0 number))))
                 (lambda nil (interactive)
                   (window-number-select ,number))))))
 
-; define C-x C-j 1 to switch to win 1, etc (C-x C-j 0 = win 10)
+                                        ; define C-x C-j 1 to switch to win 1, etc (C-x C-j 0 = win 10)
 (unless window-number-mode-map
   (setq window-number-mode-map (make-sparse-keymap))
-  ; space after C-j is important
+                                        ; space after C-j is important
   (window-number-define-keys window-number-mode-map "C-x C-j "))
 
-; define M-1 to switch to win 1, etc (M-0 = win 10)
+                                        ; define M-1 to switch to win 1, etc (M-0 = win 10)
 (unless window-number-meta-mode-map
   (setq window-number-meta-mode-map (make-sparse-keymap))
   (window-number-define-keys window-number-meta-mode-map "M-"))
@@ -136,7 +225,8 @@ according to numbers with the C-x C-j prefix.  Another mode,
 `window-number-meta-mode' enables the use of the M- prefix."
       :global t
       :init-value nil
-      :lighter " -?-")
+      :lighter " -?-"
+      (window-number-set-inactive-color))
 
   (define-minor-mode window-number-mode
     "A global minor mode that enables selection of windows
@@ -144,7 +234,9 @@ according to numbers with the C-x C-j prefix.  Another mode,
 `window-number-meta-mode' enables the use of the M- prefix."
     :global t
     :init-value nil
-    :lighter (:eval (window-number-string))))
+    :lighter (:eval (window-number-string))
+    (window-number-set-inactive-color)
+    ))
 
 (define-minor-mode window-number-meta-mode
   "A global minor mode that enables use of the M- prefix to
@@ -153,19 +245,6 @@ numbers in the mode-line."
   :global t
   :init-value nil)
 
-;;(push (cons 'my-window-number-meta-mode my-window-number-mode-map)
-;;       minor-mode-map-alist)
-
-
-(defface window-number-face
-  '((((type tty) (class color))
-     (:background "red"))
-    (((type tty) (class mono))
-     (:inverse-video t))
-    (((type x w32 mac))
-     (:foreground "red")))
-  "The face used for the window number in the mode-line.")
-
 (provide 'window-number)
 
-;;; window-number.el ends here.
+;;; window-number.el ends here
