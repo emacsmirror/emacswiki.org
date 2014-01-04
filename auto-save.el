84da8d7c461c8eab8e3e2b14b07acd433a23bd9c
AndyStewart <anybody@emacswiki.org>
2014-01-04 07:53:50 +0100

    Add new options: `auto-save-slient' and `auto-save-idle'

--- auto-save.el
+++ auto-save.el
@@ -4,10 +4,10 @@
 ;; Description: Auto save files when idle
 ;; Author: Andy Stewart lazycat.manatee@gmail.com
 ;; Maintainer: Andy Stewart lazycat.manatee@gmail.com
-;; Copyright (C) 2008, 2009, Andy Stewart, all rights reserved.
-;; Created: 2008-10-20 09:14:21
+;; Copyright (C) 2013 ~ 2014, Andy Stewart, all rights reserved.
+;; Created: 2013-12-31 00:32:00
 ;; Version: 0.1
-;; Last-Updated: 2008-10-20 09:14:25
+;; Last-Updated: 2014-01-04 14:23:05
 ;;           By: Andy Stewart
 ;; URL:
 ;; Keywords: autosave
@@ -39,7 +39,7 @@
 
 ;;; Commentary:
 ;;
-;; 自动保存的设置设置
+;; Auto save file when emacs idle
 ;;
 
 ;;; Installation:
@@ -52,10 +52,18 @@
 ;; And the following to your ~/.emacs startup file.
 ;;
 ;; (require 'init-auto-save)
+;; (auto-save-enable)
+;;
+;; Set `auto-save-slient' with non-nil if want emacs save files slient:
+;; (setq auto-save-slient t)
 ;;
 ;; No need more.
 
 ;;; Change log:
+;;
+;; 2014/01/04
+;;      * Add new function `auto-save-enable' to enable auto-save in user config file.
+;;      * Add options: `auto-save-idle' and `auto-save-slient'
 ;;
 ;; 2008/10/20
 ;;      First released.
@@ -76,31 +84,53 @@
 
 ;;; Code:
 
+(defgroup auto-save nil
+  "Auto save file when emacs idle."
+  :group 'auto-save)
+
+(defcustom auto-save-idle 1
+  "The idle seconds to auto save file."
+  :type 'integer
+  :group 'auto-save)
+
+(defcustom auto-save-slient nil
+  "Nothing to dirty minibuffer if this option is non-nil."
+  :type 'boolean
+  :group 'auto-save)
+
 ;; Emacs' default auto-save is stupid to generate #foo# files!
 (setq auto-save-default nil)
 
-(defun autosave-buffers ()
+(defun auto-save-buffers ()
   (interactive)
   (let ((autosave-buffer-list))
-	(save-excursion
-	 (dolist (buf (buffer-list))
-	   (set-buffer buf)
-	   (if (and (buffer-file-name) (buffer-modified-p))
-		   (progn
-			 (push (buffer-name) autosave-buffer-list)
-			 (basic-save-buffer))))
-	 ;; Tell user when auto save files.
-	 (cond
-	  ;; It's stupid tell user if nothing to save.
-	  ((= (length autosave-buffer-list) 1)
-	   (message "# Saved %s" (car autosave-buffer-list)))
-	  ((> (length autosave-buffer-list) 1)
-	   (message "# Saved %d files: %s"
-		     (length autosave-buffer-list)
-		     (mapconcat 'identity autosave-buffer-list ", "))))
-	 )))
+    (save-excursion
+      (dolist (buf (buffer-list))
+        (set-buffer buf)
+        (if (and (buffer-file-name) (buffer-modified-p))
+            (progn
+              (push (buffer-name) autosave-buffer-list)
+              (if auto-save-slient
+                  (with-temp-message ""
+                    (basic-save-buffer))
+                (basic-save-buffer))
+              )))
+      ;; Tell user when auto save files.
+      (unless auto-save-slient
+        (cond
+         ;; It's stupid tell user if nothing to save.
+         ((= (length autosave-buffer-list) 1)
+          (message "# Saved %s" (car autosave-buffer-list)))
+         ((> (length autosave-buffer-list) 1)
+          (message "# Saved %d files: %s"
+                   (length autosave-buffer-list)
+                   (mapconcat 'identity autosave-buffer-list ", ")))))
+      )))
 
-(run-with-idle-timer 1 t #'autosave-buffers)
+(defun auto-save-enable ()
+  (interactive)
+  (run-with-idle-timer auto-save-idle t #'auto-save-buffers)
+  )
 
 (provide 'auto-save)
 
