d6f5675239c605b0270391899e958d322b7daea2
AlexSchroeder <anybody@emacswiki.org>
2014-01-06 07:44:06 +0100

    Hi, is anyone getting some error like that after facebook-login?

---
+++ Comments_on_facebook.el
@@ -0,0 +1,9 @@
+[[Exal]]: Hi, is anyone getting some error like that after facebook-login?
+
+##Wrong type argument: characterp, (request_args . [((value . "118ffc451a20ce4072\
+2d8f961e0608bd") (key . "sig")) ((value . "") (key . "api_key")) ((value . "198\
+0728433329945") (key . "call_id")) ((value . "JSON") (key . "format")) ((value \
+. "auth.createToken") (key . "method")) ((value . "1.0") (key . "v"))])##
+
+-- Anonymous
+
