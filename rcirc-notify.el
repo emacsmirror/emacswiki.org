f5f497cb3f37d5038ce5e9364c09c44d41c79084
fgallina <anybody@emacswiki.org>
2014-01-14 00:10:15 +0100

    Fix the summary separator to use triple dashes, so elpa based repositories get the summary correctly.

--- rcirc-notify.el
+++ rcirc-notify.el
@@ -1,4 +1,4 @@
-;;; rcirc-notify.el -- libnotify popups
+;;; rcirc-notify.el --- libnotify popups
 ;;
 ;; Copyright (c) 2008 Will Farrington
 ;; Copyright (c) 2009, 2011 Alex Schroeder <alex@gnu.org>
