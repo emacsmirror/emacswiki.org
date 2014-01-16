ea886093074a51294aef7c36d65b5c95f19cff45
Syohei YOSHIDA <anybody@emacswiki.org>
2014-01-16 05:53:46 +0100

    Please add autoload cookie to open-junk-file interactive function. It makes lazy loading if users install open-jumk-file with package.el or . . .

---
+++ Comments_on_open-junk-file.el
@@ -0,0 +1,7 @@
+Please add autoload cookie to open-junk-file interactive function.
+
+It makes lazy loading if users install open-jumk-file with package.el or el-get.
+Users need not to load open-junk-file.el until it is used.
+
+-- [http://syohex.org Syohei YOSHIDA] 2014-01-16 04:53 UTC
+
