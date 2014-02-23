bd9a7ac14daf98491aeb4554d97a8fcc513dfdcb
TakeshiBanse <anybody@emacswiki.org>
2014-02-23 02:52:18 +0100

    Update

--- bracketed-paste.el
+++ bracketed-paste.el
@@ -24,6 +24,9 @@
 
 ;;; Commentary:
 
+;; Thank you very much Josh Triplett, this code is a child of your Vim script:
+;; http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=504244
+
 ;; This package provides bracketed paste mode support within emacs -nw.
 ;; - http://invisible-island.net/xterm/ctlseqs/ctlseqs.html (DECSET/DECRST)
 ;; - http://invisible-island.net/xterm/ctlseqs/ctlseqs.html#Bracketed Paste Mode
@@ -35,7 +38,9 @@
 ;; - http://stackoverflow.com/a/7053522
 ;;
 ;; Thank you very much for Josh Triplett, IWAMOTO Kouichi, Chris Page and
-;; Rainer Müller to open these settings to the public!
+;; Rainer Müller to open these settings to the public! I've learned how to
+;; get the advantage of Bracketed Paste Mode on Vim by reading your
+;; articles.
 
 ;; Please put the following to your ~/.emacs file then restart Emacs:
 ;;
