c6548fd58cdd4491b6a03956b9a927467ddcbbe1
Anonymous <anybody@emacswiki.org>
2014-03-06 15:10:20 +0100

    update 80 extensions from deepin-emacs

--- init-erc.el
+++ init-erc.el
@@ -73,9 +73,19 @@
 
 ;;; Require
 
+(require 'erc)
+(require 'erc-highlight-nicknames)
+(require 'erc-extension)
+(require 'erc-nick-notify)
+(require 'init-doi)
+(require 'init-irc)
+(require 'init-git)
+(require 'paste2)
+(require 'doi-extension)
 
 ;;; Code:
 
+(setq erc-user-full-name (get-git-user-name))                         ;设置全名
 (setq erc-autojoin-mode t)                                            ;自动加入
 (setq erc-join-buffer 'bury)                                          ;在隐藏的Buffer中加入
 (setq erc-autoaway-mode t)                                            ;开启自动离开模块
@@ -91,13 +101,21 @@
 (setq erc-insert-timestamp-function 'erc-insert-timestamp-right)      ;插入时间戳的方式(右边)
 (setq erc-insert-away-timestamp-function 'erc-insert-timestamp-right) ;插入离开时间戳的方式(右边)
 (setq erc-track-position-in-mode-line t)                              ;在Mode-line显示频道信息
+(setq erc-truncate-mode t)                                            ;开启截断模式
 (setq erc-enable-logging nil)                                         ;禁止日志
 (setq erc-log-mode nil)                                               ;关闭日志模式
-(setq erc-log-channels-directory                                      ;日志的记录目录
-      "~/MyEmacs/Configure-File/ERC/logs/")
-(add-hook 'erc-mode-hook
-          'turn-on-eldoc-mode           ;开启elisp参数提醒模式
-          )
+(setq erc-header-line-uses-tabbar-p t)                                ;默认开启 Tabbar
+(setq erc-nick-notify-delay '(0 1 0))                                 ;设置延迟为1分钟
+(setq erc-nickserv-passwords                                          ;设置自动登录时需要的密码
+      '((freenode (((eval erc-nick) . (eval erc-password))))))
+(setq erc-autojoin-channels-alist       ;自动加入的服务器和频道
+      `(,(cons "freenode.net" '("#emacs"))))
+(setq erc-log-channels-directory        ;日志的记录目录
+      "~/.emacs.d/deepin-emacs/Configure-File/ERC/logs/")
+(dolist (hooked (list
+                 'turn-on-eldoc-mode    ;开启elisp参数提醒模式
+                 ))
+  (add-hook 'erc-mode-hook hooked))
 (setq erc-modules                       ;加载的模块
       '(
         autojoin                        ;自动加入
@@ -120,16 +138,24 @@
         sound                           ;声音
         stamp                           ;时间戳
         track                           ;跟踪
-        unmorse                         ;转换莫尔斯码
+        ;; unmorse                         ;转换莫尔斯码
         highlight-nicknames
         ))
 (setq erc-hide-list                     ;需要隐藏的IRC消息的类型
       '(
         "353"                           ;忽略昵称列表
-        "JOIN"                          ;加入
-        "PART"                          ;离开
-        "QUIT"                          ;退出
+        ;; "JOIN"                          ;加入
+        ;; "PART"                          ;离开
+        ;; "QUIT"                          ;退出
         ))
+(lazy-set-mode-autoload-key
+ '(
+   ("C-c C-y" . paste2-buffer-create)   ;粘贴大段内容
+   ("/" . doi-erc-command)              ;erc命令
+   )
+ erc-mode-map nil "init-erc"
+ )
+(lazy-set-mode-autoload-key doi-key-alist erc-mode-map nil "init-erc") ;doi 的局部按键
 
 (provide 'init-erc)
 
