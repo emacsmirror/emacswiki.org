80562d9fba31383ae3b33da4f51930c8a0ec103a
Anonymous <anybody@emacswiki.org>
2014-03-06 15:18:48 +0100

    update 80 extensions from deepin-emacs

--- init-paredit.el
+++ init-paredit.el
@@ -7,8 +7,8 @@
 ;; Copyright (C) 2008, 2009, Andy Stewart, all rights reserved.
 ;; Created: 2008-10-20 09:41:55
 ;; Version: 0.1
-;; Last-Updated: 2013-04-10 01:25:00
-;;           By: Reuben Thomas <rrt@sc3d.org>
+;; Last-Updated: 2008-10-20 09:41:55
+;;           By: Andy Stewart
 ;; URL:
 ;; Keywords: paredit
 ;; Compatibility: GNU Emacs 23.0.60.1
@@ -86,9 +86,46 @@
                'lisp-mode-hook
                'maxima-mode-hook
                'ielm-mode-hook
-               'scheme-mode-hook
+               'sh-mode-hook
+               'makefile-gmake-mode-hook
+               'python-mode-hook
+               'js-mode-hook
+               'go-mode-hook
+               'qml-mode-hook
+               'web-mode-hook
                ))
   (add-hook hook '(lambda () (paredit-mode 1))))
+
+(defvar one-key-menu-paredit-alist nil
+  "The `one-key' menu alist for PAREDIT.")
+
+(setq one-key-menu-paredit-alist
+      '(
+        ;; Wrap.
+        (("h" . "Wrap Left Object") . paredit-backward-slurp-sexp)
+        (("l" . "Wrap Right Object") . paredit-forward-slurp-sexp)
+        ;; Free.
+        (("," . "Free Left Object") . paredit-backward-barf-sexp)
+        (("." . "Free Right Object") . paredit-forward-barf-sexp)
+        ;; Remove.
+        (("<" . "Remove Paren And Left Object") . paredit-splice-sexp-killing-backward)
+        ((">" . "Remove Paren And Right Object") . paredit-splice-sexp-killing-forward)
+        ;; Join or Split.
+        (("j" . "Join Sexps") . paredit-join-sexps)
+        (("k" . "Split Sexps") . paredit-split-sexp)
+        (("n" . "Join next list") . paredit-join-with-next-list)
+        (("p" . "Join previous list") . paredit-join-with-previous-list)
+        (("N" . "Add next list") . paredit-add-to-next-list)
+        (("P" . "Add previous list") . paredit-add-to-previous-list)
+        ;; Move.
+        (("u" . "Move list forward") . paredit-move-list-forward)
+        (("i" . "Move list backward") . paredit-move-list-backward)
+        ))
+
+(defun one-key-menu-paredit ()
+  "The `one-key' menu for PAREDIT."
+  (interactive)
+  (one-key-menu "PAREDIT" one-key-menu-paredit-alist t t nil nil t))
 
 (provide 'init-paredit)
 
