0aa7b356c26b7e4fcec11a48d823c5a3e919e874
Anonymous <anybody@emacswiki.org>
2014-03-06 15:19:39 +0100

    update 80 extensions from deepin-emacs

---
+++ init-startup.el
@@ -0,0 +1,95 @@
+;;; init-startup.el --- Config load when startup
+
+;; Filename: init-startup.el
+;; Description: Config load when startup
+;; Author: Andy Stewart <lazycat.manatee@gmail.com>
+;; Maintainer: Andy Stewart <lazycat.manatee@gmail.com>
+;; Copyright (C) 2014, Andy Stewart, all rights reserved.
+;; Created: 2014-01-20 23:58:38
+;; Version: 0.1
+;; Last-Updated: 2014-01-20 23:58:38
+;;           By: Andy Stewart
+;; URL: http://www.emacswiki.org/emacs/download/init-startup.el
+;; Keywords: 
+;; Compatibility: GNU Emacs 24.3.50.1
+;;
+;; Features that might be required by this library:
+;;
+;; 
+;;
+
+;;; This file is NOT part of GNU Emacs
+
+;;; License
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
+;;; Commentary: 
+;; 
+;; Config load when startup
+;; 
+
+;;; Installation:
+;;
+;; Put init-startup.el to your load-path.
+;; The load-path is usually ~/elisp/.
+;; It's set in your ~/.emacs like this:
+;; (add-to-list 'load-path (expand-file-name "~/elisp"))
+;;
+;; And the following to your ~/.emacs startup file.
+;;
+;; (require 'init-startup)
+;;
+;; No need more.
+
+;;; Customize:
+;;
+;; 
+;;
+;; All of the above can customize by:
+;;      M-x customize-group RET init-startup RET
+;;
+
+;;; Change log:
+;;	
+;; 2014/01/20
+;;      * First released.
+;; 
+
+;;; Acknowledgements:
+;;
+;; 
+;;
+
+;;; TODO
+;;
+;; 
+;;
+
+;;; Require
+
+
+;;; Code:
+
+(setq ad-redefinition-action 'accept)   ;不要烦人的 redefine warning
+(toggle-frame-fullscreen)               ;全屏
+(tool-bar-mode -1)                      ;禁用工具栏
+(menu-bar-mode -1)                      ;禁用菜单栏
+(scroll-bar-mode -1)                    ;禁用滚动条
+
+(provide 'init-startup)
+
+;;; init-startup.el ends here
