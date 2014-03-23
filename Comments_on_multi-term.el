97190aec58fd4dbdf208b7ac675544671465dd75
Anonymous <anybody@emacswiki.org>
2014-03-23 09:49:03 +0100

    After pulling in latest from melpa, edit history is broken. I am not longer able to edit history properly as it is meddling with previous command.

---
+++ Comments_on_multi-term.el
@@ -0,0 +1,4 @@
+After pulling in latest from melpa, edit history is broken. I am not longer able to edit history properly as it is meddling with previous command.
+
+-- Anonymous 2014-03-23 08:49 UTC
+
