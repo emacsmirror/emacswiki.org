7adfbf312a899253729a0a1d80e285bf45bb32f5
DrewAdams <anybody@emacswiki.org>
2014-03-24 02:10:04 +0100

    no summary available

--- bookmark+-chg.el
+++ bookmark+-chg.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
 ;; Copyright (C) 2000-2014, Drew Adams, all rights reserved.
 ;; Created: Fri Sep 15 07:58:41 2000
-;; Last-Updated: Sun Mar 23 17:43:12 2014 (-0700)
+;; Last-Updated: Sun Mar 23 17:55:59 2014 (-0700)
 ;;           By: dradams
-;;     Update #: 15505
+;;     Update #: 15507
 ;; URL: http://www.emacswiki.org/bookmark+-chg.el
 ;; Doc URL: http://www.emacswiki.org/BookmarkPlus
 ;; Keywords: bookmarks, bookmark+
@@ -147,6 +147,7 @@
 ;;;(@* "CHANGE LOG FOR `bookmark+-1.el'")
 ;;
 ;; 2014/03/23 dadams
+;;     bmkp-file-target-set: Fix interactive spec (parens).
 ;;     bmkp-file-target-set, bmkp-autofile-set, bmkp-autofile-(add|remove)-tags:
 ;;       Soft-require ffap.el before using ffap-guesser.
 ;; 2014/03/10 dadams
