300350c827cb4f3189a42e185ac7de9d477524ea
StefanKamphausen <anybody@emacswiki.org>
2014-03-31 08:41:46 +0200

    nrepl.el was renamed cider

--- nREPL.el
+++ nREPL.el
@@ -1,5 +1,3 @@
-*nrepl.el* is an Emacs client for nREPL, the Clojure networked REPL server. It's a great alternative to the now deprecated combination of [[SLIME]] + swank-clojure.
+*nrepl.el* was the first Emacs client for nREPL, the Clojure networked REPL server, deprecating the combination of [[SLIME]] + swank-clojure.
 
-nrepl.el is available for installation from both [[Marmalade]] and [[MELPA]].
-
-Its source code can be found on [[https://github.com/kingtim/nrepl.el GitHub]].
+It is obsolete, since the project was renamed to [[cider]] to avoid confusion with the nREPL Clojure backend.
