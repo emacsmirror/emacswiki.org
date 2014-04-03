7550334aa555dbbcd50b6d0faf2ac195284640ac
DrewAdams <anybody@emacswiki.org>
2014-04-03 05:07:25 +0200

    no summary available

--- icicles-chg.el
+++ icicles-chg.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
 ;; Copyright (C) 2007-2014, Drew Adams, all rights reserved.
 ;; Created: Tue Nov 27 07:47:53 2007
-;; Last-Updated: Tue Apr  1 13:26:16 2014 (-0700)
+;; Last-Updated: Wed Apr  2 20:02:44 2014 (-0700)
 ;;           By: dradams
-;;     Update #: 10757
+;;     Update #: 10759
 ;; URL: http://www.emacswiki.org/icicles-chg.el
 ;; Doc URL: http://www.emacswiki.org/Icicles
 ;; Keywords: extensions, help, abbrev, local, minibuffer,
@@ -5846,6 +5846,9 @@
  
 ;;;(@* "CHANGE LOG FOR `icicles-mode.el'")
 ;;
+;; 2014/04/02 dadams
+;;     icicle-define-icicle-maps:
+;;       (Typos) icicle-search-menu-map -> icicle-search-goto-menu-map; [search] -> [goto].
 ;; 2014/03/08 dadams
 ;;     icicle-top-level-prep:
 ;;       Test minibuffer-depth, not recursion-depth.  Do not set icicle-last-top-level-command for
