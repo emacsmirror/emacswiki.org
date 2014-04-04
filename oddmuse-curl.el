5c4291464c5b47c3d98f554e5644f2540bbed107
AlexSchroeder <anybody@emacswiki.org>
2014-04-04 18:17:49 +0200

    Deleted

--- oddmuse-curl.el
+++ oddmuse-curl.el
@@ -1,3 +1,1 @@
-This file is hosted [http://git.savannah.gnu.org/cgit/oddmuse.git/tree/contrib/oddmuse-curl.el on Savannah] (git).
 
-<include text "http://git.savannah.gnu.org/cgit/oddmuse.git/plain/contrib/oddmuse-curl.el">
