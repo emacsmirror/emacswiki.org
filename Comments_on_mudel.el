75d2a30822f6269539c6f0a1d2f3aacbfa2cfd43
Anonymous <anybody@emacswiki.org>
2014-04-14 03:47:05 +0200

    I have an issue with this, no matter what MUD I connect to I get telnet escape characters such as \377 and \371. This is both on 24.3 and the current . . .

---
+++ Comments_on_mudel.el
@@ -0,0 +1,4 @@
+I have an issue with this, no matter what MUD I connect to I get telnet escape characters such as \377 and \371. This is both on 24.3 and the current git source, on Arch linux x64.
+
+-- Anonymous 2014-04-14 01:47 UTC
+
