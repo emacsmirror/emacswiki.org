3e494a6b819a345750c9ab5beb7c4eea3d607c5b
Anonymous <anybody@emacswiki.org>
2014-05-01 19:56:10 +0200

    Thank you so much for this awesome package.  It is in my top5 favorite emacs packages!  The feedback and error-catching that this mitigates make it . . .

---
+++ Comments_on_volatile-highlights.el
@@ -0,0 +1,4 @@
+Thank you so much for this awesome package.  It is in my top5 favorite emacs packages!  The feedback and error-catching that this mitigates make it an extremely useful addon.
+
+-- Anonymous 2014-05-01 17:56 UTC
+
