67cc892ed76992faff6e4b229a270f4e369d208e
Anonymous <anybody@emacswiki.org>
2014-05-04 05:18:05 +0200

    no summary available

--- delight.el
+++ delight.el
@@ -1,6 +1,7 @@
 ;;; delight.el - A dimmer switch for your lighter text.
 ;;
 ;; Author: Phil S.
+;; Version: 1.01
 
 ;; Commentary:
 ;;
@@ -19,7 +20,7 @@
 ;;            (smart-tab-mode " \\t" "smart-tab")
 ;;            (eldoc-mode nil "eldoc")
 ;;            (rainbow-mode)
-;;            (emacs-lisp-mode "Elisp" "lisp-mode")))
+;;            (emacs-lisp-mode "Elisp" :major)))
 ;;
 ;; Important note:
 ;;
@@ -38,7 +39,7 @@
 ;;
 ;; (delight 'emacs-lisp-mode
 ;;          '("Elisp" (lexical-binding ":Lex" ":Dyn"))
-;;          'lisp-mode)
+;;          :major)
 ;;
 ;; See `mode-line-format' for information about mode-line constructs,
 ;; and M-: (info "(elisp) Mode Line Format") for further details.
@@ -47,6 +48,11 @@
 ;; values themselves (for instance dired-mode updates mode-name if
 ;; you change the sorting criteria) in which cases this library may
 ;; prove inadequate.
+
+;;; Changelog:
+;;
+;; 1.01 - Added support for using the keyword :major as the FILE argument
+;;        for major modes, to avoid also processing them as minor modes.
 
 ;;; Code:
 
@@ -61,7 +67,8 @@ See `delight'.")
 if and when the mode is loaded.
 
 SPEC can be either a mode symbol, or a list containing multiple elements of
-the form (MODE VALUE FILE).
+the form (MODE VALUE FILE). In the latter case the two optional arguments are
+omitted, as they are instead specified for each element of the list.
 
 For minor modes, VALUE is the replacement lighter value (or nil to disable)
 to set in the `minor-mode-alist' variable. For major modes VALUE is the
@@ -72,17 +79,22 @@ In both cases VALUE is commonly a string, but may in fact contain any valid
 mode-line construct. See `mode-line-format' for details.
 
 The FILE argument is passed through to `eval-after-load'. If FILE is nil then
-the mode symbol is passed as the required feature."
+the mode symbol is passed as the required feature. Both of these cases are
+relevant to minor modes only.
+
+For major modes you should specify the keyword :major as the value of FILE,
+to prevent the mode being treated as a minor mode."
   (add-hook 'after-change-major-mode-hook 'delight-major-mode)
   (let ((glum (if (consp spec) spec (list (list spec value file)))))
     (while glum
       (destructuring-bind (mode &optional value file) (pop glum)
         (assq-delete-all mode delighted-modes)
         (add-to-list 'delighted-modes (list mode value file))
-        (eval-after-load (or file mode)
-          `(let ((minor-delight (assq ',mode minor-mode-alist)))
-             (when minor-delight
-               (setcar (cdr minor-delight) ',value))))))))
+        (unless (eq file :major)
+          (eval-after-load (or file mode)
+            `(let ((minor-delight (assq ',mode minor-mode-alist)))
+               (when minor-delight
+                 (setcar (cdr minor-delight) ',value)))))))))
 
 (defun delight-major-mode ()
   "Delight the 'pretty name' of the current buffer's major mode
