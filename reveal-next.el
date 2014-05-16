0bd7fdbadc7416d753dc770572b6fbcff0a8e882
DrewAdams <anybody@emacswiki.org>
2014-05-16 17:06:41 +0200

    no summary available

--- reveal-next.el
+++ reveal-next.el
@@ -1,5 +1,5 @@
 ;;; reveal-next.el --- Progressively reveal text after the cursor.
-;; 
+;;
 ;; Filename: reveal-next.el
 ;; Description: Progressively reveal text after the cursor.
 ;; Author: Drew Adams
@@ -8,22 +8,22 @@
 ;; Created: Fri May 16 07:28:05 2014 (-0700)
 ;; Version: 0
 ;; Package-Requires: ()
-;; Last-Updated: Fri May 16 07:58:14 2014 (-0700)
+;; Last-Updated: Fri May 16 08:05:42 2014 (-0700)
 ;;           By: dradams
-;;     Update #: 16
+;;     Update #: 20
 ;; URL: http://www.emacswiki.org/reveal-next.el
 ;; Doc URL: http://www.emacswiki.org/RevealNextTextMode
 ;; Keywords: hide show invisible learning
 ;; Compatibility: GNU Emacs: 22.x, 23.x, 24.x
-;; 
+;;
 ;; Features that might be required by this library:
 ;;
 ;;   None
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
-;;; Commentary: 
-;; 
+;;
+;;; Commentary:
+;;
 ;;    Progressively reveal text after the cursor as you move it.
 ;;
 ;; You can use this during a presentation, such as in a class, to
@@ -36,33 +36,51 @@
 ;; the level, but you can also switch levels using command
 ;; `reveal-next-char/line' or `reveal-next-cycle'.  The latter command
 ;; cycles among line-level, char-level, and off.
-;; 
+;;
+;;
+;; User options defined here:
+;;
+;;    `reveal-next-char-level'.
+;;
+;; Commands defined here:
+;;
+;;    `reveal-next-char/line', `reveal-next-cycle',
+;;    `reveal-next-mode'.
+;;
+;; Non-interactive functions defined here:
+;;
+;;    `reveal-next-update'.
+;;
+;; Internal variables defined here:
+;;
+;;    `reveal-next-overlay'.
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Change Log:
 ;;
 ;; 2014/05/16 dadams
 ;;     Created.
-;; 
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
 ;; published by the Free Software Foundation; either version 3, or
 ;; (at your option) any later version.
-;; 
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;; General Public License for more details.
-;; 
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program; see the file COPYING.  If not, write to
 ;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
 ;; Floor, Boston, MA 02110-1301, USA.
-;; 
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Code:
 
 (defcustom reveal-next-char-level nil
