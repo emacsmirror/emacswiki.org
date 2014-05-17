2379cb5b143ff994297ef7cf46f4d636f1861529
Anonymous <anybody@emacswiki.org>
2014-05-17 16:27:02 +0200

    I have the same problem. It seems to be caused by ("C-m" . term-send-input) in term-bind-key-alist - if you get rid of that, everything works fine . . .

--- Comments_on_multi-term.el
+++ Comments_on_multi-term.el
@@ -9,3 +9,12 @@ I just maintain multi-term.el on EmacsWiki, i'm sure melpa is older than EmacsWi
 
 -- AndyStewart 2014-03-23 13:49 UTC
 
+
+----
+
+I have the same problem. It seems to be caused by ("C-m" . term-send-input) in term-bind-key-alist - if you get rid of that, everything works fine (i.e. C-m and RET are bound to term-send-raw again).
+
+Melpa pulls in updates automatically, I think, so it should always match the version on the wiki
+
+-- Anonymous 2014-05-17 14:27 UTC
+
