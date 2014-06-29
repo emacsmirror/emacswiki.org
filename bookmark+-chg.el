b8f2df3a2a5a7a8a935a442ebd52529d6232c8b8
DrewAdams <anybody@emacswiki.org>
2014-06-29 21:21:27 +0200

    no summary available

--- bookmark+-chg.el
+++ bookmark+-chg.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
 ;; Copyright (C) 2000-2014, Drew Adams, all rights reserved.
 ;; Created: Fri Sep 15 07:58:41 2000
-;; Last-Updated: Sun Jun 29 11:32:43 2014 (-0700)
+;; Last-Updated: Sun Jun 29 12:17:24 2014 (-0700)
 ;;           By: dradams
-;;     Update #: 15594
+;;     Update #: 15595
 ;; URL: http://www.emacswiki.org/bookmark+-chg.el
 ;; Doc URL: http://www.emacswiki.org/BookmarkPlus
 ;; Keywords: bookmarks, bookmark+
@@ -152,6 +152,7 @@
 ;;     bmkp-jump-1: Run bmkp-before-jump-hook.
 ;;     bmkp-set-desktop-bookmark: Use bmkp-desktop-save.
 ;;     bmkp-desktop-jump: If bmkp-desktop-jump-save-before-flag then bmkp-desktop-save-as-last.
+;;     bmkp-desktop-kill: Updated for Emacs 24.4+.
 ;; 2014/06/21 dadams
 ;;     Added: bmkp-icicle-search-hits-alist-only, bmkp-icicle-search-hits-bookmark-p,
 ;;       bmkp-icicle-search-hits-retrieve-more, bmkp-jump-icicle-search-hits,
