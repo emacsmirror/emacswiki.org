9e80b491374491f5fbb79d3793ca972c11a3da7d
DrewAdams <anybody@emacswiki.org>
2014-07-23 17:25:34 +0200

    no summary available

--- header2.el
+++ header2.el
@@ -11,9 +11,9 @@
 ;; Created: Tue Aug  4 17:06:46 1987
 ;; Version: 0
 ;; Package-Requires: ()
-;; Last-Updated: Mon Jan 13 08:16:09 2014 (-0800)
+;; Last-Updated: Wed Jul 23 08:22:12 2014 (-0700)
 ;;           By: dradams
-;;     Update #: 1878
+;;     Update #: 1888
 ;; URL: http://www.emacswiki.org/header2.el
 ;; Doc URL: http://emacswiki.org/AutomaticFileHeaders
 ;; Keywords: tools, docs, maint, abbrev, local
@@ -170,7 +170,8 @@
 ;;
 ;;; Change Log:
 ;;
-;;
+;; 2014/07/23 dadams
+;;     header-free-software: Updated per latest GNU boilerplate.
 ;; 2014/01/13 dadams
 ;;     Added: nonempty-comment-start, nonempty-comment-end.
 ;;     Removed variables comment-start-p, comment-end-p.
@@ -465,20 +466,18 @@ file `header2.el' to do this."
   :type 'string :group 'Automatic-File-Header)
 
 (defcustom header-free-software
-  "This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License as
-published by the Free Software Foundation; either version 3, or
-\(at your option) any later version.
+  "This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program; see the file COPYING.  If not, write to
-the Free Software Foundation, Inc., 51 Franklin Street, Fifth
-Floor, Boston, MA 02110-1301, USA."
+along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>."
 
   "*Text saying that this is free software"
   :type 'string :group 'Automatic-File-Header)
