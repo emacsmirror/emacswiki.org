0b03224305513821d8f37010e45bc39fcc65bde2
DrewAdams <anybody@emacswiki.org>
2014-08-14 00:36:00 +0200

    no summary available

--- icicles-doc1.el
+++ icicles-doc1.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
 ;; Copyright (C) 1996-2014, Drew Adams, all rights reserved.
 ;; Created: Tue Aug  1 14:21:16 1995
-;; Last-Updated: Mon Aug 11 19:18:11 2014 (-0700)
+;; Last-Updated: Wed Aug 13 08:50:40 2014 (-0700)
 ;;           By: dradams
-;;     Update #: 28270
+;;     Update #: 28274
 ;; URL: http://www.emacswiki.org/icicles-doc1.el
 ;; Doc URL: http://www.emacswiki.org/Icicles
 ;; Keywords: internal, extensions, help, abbrev, local, minibuffer,
@@ -6549,6 +6549,7 @@
 ;;                                       single-line hits)
 ;;  * `icicle-occur-dired-marked-recursive' - Search marked in Dired
 ;;  * `icicle-recent-file'             - Trip among recent files
+;;
 ;;  * `icicle-search' (`C-c `')        - Trip among regexp search hits
 ;;  * `icicle-search-bookmarks-together' (`C-u C-c `'),
 ;;    `icicle-search-bookmark',        - Search multiple bookmarks
@@ -6572,7 +6573,7 @@
 ;;  * `icicle-search-keywords' (`C-c ^') - Trip among keyword search
 ;;                                       hits.
 ;;  * `icicle-search-overlay-property' - Trip among buffer strings
-;;    with some overlay property.
+;;                                       with some overlay property.
 ;;  * `icicle-search-pages'            - Search Emacs pages
 ;;  * `icicle-search-paragraphs'       - Search Emacs paragraphs
 ;;  * `icicle-search-sentences'        - Search sentences as contexts
@@ -6585,11 +6586,14 @@
 ;;  * `icicle-select-frame' (`C-x 5 o') - Trip among frames, by name
 ;;  * `icicle-select-window' (`C-0 C-x o') - Trip among windows, by
 ;;                                       buffer name
+;;  * `icicle-wide-n'                  - Trip among narrowings
+;;                                       (buffer restrictions)
 ;;
 ;;  (You need library library `Bookmark+' for
 ;;  `icicle-search-bookmark-list-marked'.  You need library `Dired+'
 ;;  for `icicle-occur-dired-marked-recursive' and
-;;  `icicle-search-dired-marked-recursive'.)
+;;  `icicle-search-dired-marked-recursive'.  You need library
+;;  `wide-n.el' for `icicle-wide-n'.)
 ;;
 ;;  Note: Icicles search commands and commands `icicle-find-tag',
 ;;  `icicle-goto-marker', and `icicle-goto-global-marker' effectively
