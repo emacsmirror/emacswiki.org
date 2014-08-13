5677a21bd1df6cb2b6060786510ee70304848834
mcandre <anybody@emacswiki.org>
2014-08-13 21:03:47 +0200

    I tried emailing the author of findr.el to suggest a feature: "When searching through files, skip binaries." But the email bounced, as cadet@bu.edu . . .

---
+++ Comments_on_findr.el
@@ -0,0 +1,4 @@
+I tried emailing the author of findr.el to suggest a feature: "When searching through files, skip binaries." But the email bounced, as cadet@bu.edu is no longer valid. What does EmacsWiki typically do in such cases?
+
+-- [http://www.yellosoft.us/ mcandre] 2014-08-13 19:03 UTC
+
