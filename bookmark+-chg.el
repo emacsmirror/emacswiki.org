f045e9e32a24b451c81dc38e773189bde23d71f0
DrewAdams <anybody@emacswiki.org>
2014-08-20 01:31:47 +0200

    no summary available

--- bookmark+-chg.el
+++ bookmark+-chg.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
 ;; Copyright (C) 2000-2014, Drew Adams, all rights reserved.
 ;; Created: Fri Sep 15 07:58:41 2000
-;; Last-Updated: Mon Aug 18 22:04:57 2014 (-0700)
+;; Last-Updated: Tue Aug 19 10:38:41 2014 (-0700)
 ;;           By: dradams
-;;     Update #: 15697
+;;     Update #: 15699
 ;; URL: http://www.emacswiki.org/bookmark+-chg.el
 ;; Doc URL: http://www.emacswiki.org/BookmarkPlus
 ;; Keywords: bookmarks, bookmark+
@@ -146,6 +146,8 @@
  
 ;;;(@* "CHANGE LOG FOR `bookmark+-1.el'")
 ;;
+;; 2014/08/19 dadams
+;;     Added: bmkp-dired-wildcards-alist-only, bmkp-navlist-bookmark-p.
 ;; 2014/08/18 dadams
 ;;     bmkp-this-file-p: Return nil (instead of raising error) if THIS-FILE is nil.
 ;; 2014/07/11 dadams
