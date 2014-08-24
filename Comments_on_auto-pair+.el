1d76f4f8e4b7689b3981daab234be48a0c69cb46
MnZrK <anybody@emacswiki.org>
2014-08-24 14:06:38 +0200

    Since this package is abandoned, I have implemented the functionality of automatic escaping of quotes and escapes in region as a separate package, . . .

--- Comments_on_auto-pair+.el
+++ Comments_on_auto-pair+.el
@@ -4,3 +4,10 @@
 
 -- RaffaeleRicciardi 2013-06-23 10:50 UTC
 
+
+----
+
+Since this package is abandoned, I have implemented the functionality of automatic escaping of quotes and escapes in region as a separate package, check it out https://github.com/MnZrK/autopair-escape-region.
+
+-- MnZrK 2014-08-24 12:06 UTC
+
