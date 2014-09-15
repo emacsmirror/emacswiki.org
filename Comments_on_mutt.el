4ed45d5bc917b91081dcaca05cf18b96b638bdc7
Nate <anybody@emacswiki.org>
2014-09-15 16:48:11 +0200

    no summary available

--- Comments_on_mutt.el
+++ Comments_on_mutt.el
@@ -4,5 +4,5 @@ To make this work with more modern versions of Mutt I had to extend the regex fo
 
 as an additional numeric field has apparently been added to the file name.
 
--- [http://www.n0nb.us Nate] 2014-09-15 14:46 UTC
+-- [http://www.n0nb.us Nate Bargmann] 2014-09-15 14:46 UTC
 
