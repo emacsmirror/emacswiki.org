44ab1fd3edac0dc9d672257810a0656d99f1f299
Nate <anybody@emacswiki.org>
2014-09-15 16:46:19 +0200

    To make this work with more modern versions of Mutt I had to extend the regex for the mutt file name to: "mutt-[a-z]+-[0-9]+-[0-9]+-[0-9]+\\'" . . .

---
+++ Comments_on_mutt.el
@@ -0,0 +1,8 @@
+To make this work with more modern versions of Mutt I had to extend the regex for the mutt file name to:
+
+"mutt-[a-z]+-[0-9]+-[0-9]+-[0-9]+\\'"
+
+as an additional numeric field has apparently been added to the file name.
+
+-- [http://www.n0nb.us Nate] 2014-09-15 14:46 UTC
+
