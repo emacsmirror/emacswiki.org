d06beafdea6026e1077fe4b756a2274114c20b7f
denishowe <anybody@emacswiki.org>
2014-09-22 14:16:33 +0200

    When I do (find-file "C:/test.lnk"), it tries to read the .lnk file but throws an error because there is no autosave file called "C:\#test.lnk#".

---
+++ Comments_on_w32-symlinks.el
@@ -0,0 +1,4 @@
+When I do (find-file "C:/test.lnk"), it tries to read the .lnk file but throws an error because there is no autosave file called "C:\#test.lnk#".
+
+-- [http://foldoc.org/ denishowe] 2014-09-22 12:16 UTC
+
