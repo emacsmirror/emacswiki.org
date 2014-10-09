c5ada4eeb060533a5036609cf691f4ca9c85e329
cmcmahan <anybody@emacswiki.org>
2014-10-09 16:39:05 +0200

    Rollback to 2014-09-02 10:04 UTC

--- sqlplus.el
+++ sqlplus.el
@@ -3380,7 +3380,22 @@ not detected."
 (defun sqlplus-draw-table (lst &optional slips-count)
   "SLIPS-COUNT (nil means compute automatically)."
   ;; current buffer: SQL*Plus output buffer
-    (let* ((column-infos (car lst))
+  (when window-system
+    (if (>= (sqlplus-color-percentage (face-background 'default)) 50)
+	(progn
+	  (set-face-attribute 'sqlplus-table-head-face nil
+			      :background (sqlplus-shine-color (face-background 'default) -70) :foreground (face-background 'default))
+	  (set-face-attribute 'sqlplus-table-even-rows-face nil
+			      :background (sqlplus-shine-color (face-background 'default) -20) :overline (face-background 'default))
+	  (set-face-attribute 'sqlplus-table-odd-rows-face nil
+			      :background (sqlplus-shine-color (face-background 'default) -30) :overline (face-background 'default)))
+      (set-face-attribute 'sqlplus-table-head-face nil
+			  :background (sqlplus-shine-color (face-background 'default) +70) :foreground (face-background 'default))
+      (set-face-attribute 'sqlplus-table-even-rows-face nil
+			  :background (sqlplus-shine-color (face-background 'default) +20) :overline (face-background 'default))
+      (set-face-attribute 'sqlplus-table-odd-rows-face nil
+			  :background (sqlplus-shine-color (face-background 'default) +30) :overline (face-background 'default))))
+  (let* ((column-infos (car lst))
          (rows (cadr lst))
          (slip-width 0)
          (table-header-height 1)
