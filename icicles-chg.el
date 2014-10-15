463a32adaaf31ed33d2bb219011550e76e4b67e1
DrewAdams <anybody@emacswiki.org>
2014-10-15 20:28:45 +0200

    no summary available

--- icicles-chg.el
+++ icicles-chg.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
 ;; Copyright (C) 2007-2014, Drew Adams, all rights reserved.
 ;; Created: Tue Nov 27 07:47:53 2007
-;; Last-Updated: Wed Sep 24 07:12:26 2014 (-0700)
+;; Last-Updated: Wed Oct 15 11:24:53 2014 (-0700)
 ;;           By: dradams
-;;     Update #: 11285
+;;     Update #: 11288
 ;; URL: http://www.emacswiki.org/icicles-chg.el
 ;; Doc URL: http://www.emacswiki.org/Icicles
 ;; Keywords: extensions, help, abbrev, local, minibuffer,
@@ -1131,6 +1131,8 @@
  
 ;;;(@* "CHANGE LOG FOR `icicles-cmd2.el'")
 ;;
+;; 2014/10/15 dadams
+;;     icicle-imenu-1: Add contents of imenu-after-jump-hook to icicle-search-hook.
 ;; 2014/09/02 dadams
 ;;     icicle-search-replace-match: Temporary (?) fix for braindead Emacs 24.4 regression (bug #18388).
 ;; 2014/08/31 dadams
