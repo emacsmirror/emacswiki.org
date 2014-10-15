a3bf1a453976a8a1a06caefd5aaa9e6057b8d08d
DrewAdams <anybody@emacswiki.org>
2014-10-15 18:05:05 +0200

    no summary available

--- dired+.el
+++ dired+.el
@@ -8,9 +8,9 @@
 ;; Created: Fri Mar 19 15:58:58 1999
 ;; Version: 2013.07.23
 ;; Package-Requires: ()
-;; Last-Updated: Wed Oct 15 08:47:40 2014 (-0700)
+;; Last-Updated: Wed Oct 15 09:03:15 2014 (-0700)
 ;;           By: dradams
-;;     Update #: 8450
+;;     Update #: 8451
 ;; URL: http://www.emacswiki.org/dired+.el
 ;; Doc URL: http://www.emacswiki.org/DiredPlus
 ;; Keywords: unix, mouse, directories, diredp, dired
@@ -69,10 +69,10 @@
 ;;  Starting with Emacs 24.4, listing details are hidden by default.
 ;;  Use `(' anytime to toggle this hiding.  You can use option
 ;;  `diredp-hide-details-initially-flag' to change the default/initial
-;;  state.  See also option `diredp-hide-details-propagate-flag'.
-;;  However, if you do not want to hide details initially then you
-;;  must set at least one of these option values to `nil' *BEFORE*
-;;  loading `dired+.el'.
+;;  state.  However, if you do not want to hide details initially then
+;;  you must set `diredp-hide-details-initially-flag' to `nil'
+;;  *BEFORE* loading `dired+.el'.  See also option
+;;  `diredp-hide-details-propagate-flag'.
 ;;
 ;;  If you have an Emacs version older than 24.4, you can use library
 ;;  `dired-details+.el' (plus `dired-details.el') to get similar
