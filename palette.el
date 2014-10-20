a7525afa70af684abcd6a4640f77e5b7bc730f03
DrewAdams <anybody@emacswiki.org>
2014-10-20 04:14:08 +0200

    no summary available

--- palette.el
+++ palette.el
@@ -8,9 +8,9 @@
 ;; Created: Sat May 20 07:56:06 2006
 ;; Version: 0
 ;; Package-Requires: ((hexrgb "0"))
-;; Last-Updated: Tue Sep  2 13:58:23 2014 (-0700)
+;; Last-Updated: Sun Oct 19 19:12:49 2014 (-0700)
 ;;           By: dradams
-;;     Update #: 772 4
+;;     Update #: 777 4
 ;; URL: http://www.emacswiki.org/palette.el
 ;; Doc URL: http://emacswiki.org/ColorPalette
 ;; Keywords: color, rgb, hsv, hexadecimal, face, frame
@@ -18,7 +18,8 @@
 ;;
 ;; Features that might be required by this library:
 ;;
-;;   `hexrgb'.
+;;   `avoid', `frame-fns', `hexrgb', `misc-cmds', `misc-fns',
+;;   `strings', `thingatpt', `thingatpt+'.
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -312,6 +313,9 @@
 ;;
 ;;; Change Log:
 ;;
+;; 2014/10/19 dadams
+;;     palette-where-is-color, palette-brightness-scale:
+;;       Fixed typos: condition-case error handlers.
 ;; 2014/09/02 dadams
 ;;     Added: palette-list-colors-nearest.
 ;;     Soft-require misc-cmds.el.
@@ -1559,10 +1563,10 @@ option `palette-update-cursor-color-flag' is non-nil."
         (goto-char (- (point-max) 50)) ; Grayscale color (saturation=0).
       (while (and (not (eobp)) (setq bg  (palette-background-at-point))
                   (setq sat  (hexrgb-saturation bg)) (< target-sat sat))
-        (condition-case nil (forward-line 1) (goto-char (point-max))))
+        (condition-case nil (forward-line 1) (end-of-buffer nil)))
       (while (and (not (bobp)) (setq bg  (palette-background-at-point))
                   (setq sat  (hexrgb-saturation bg)) (> target-sat sat))
-        (condition-case nil (forward-line -1) (goto-char (point-min))))
+        (condition-case nil (forward-line -1) (beginning-of-buffer nil)))
       (while (and (not (eolp)) (setq bg  (palette-background-at-point))
                   (setq hue  (hexrgb-hue bg)) (< target-hue hue))
         (forward-char))
@@ -1977,11 +1981,11 @@ informative message."
       (while (and (not (eobp))
                   (setq val  (hexrgb-value (palette-background-at-point)))
                   (< target-val val))
-        (condition-case nil (forward-line 1) (goto-char (point-max))))
+        (condition-case nil (forward-line 1) (end-of-buffer nil)))
       (while (and (not (bobp))
                   (setq val  (hexrgb-value (palette-background-at-point)))
                   (> target-val val))
-        (condition-case nil (forward-line -1) (goto-char (point-min))))
+        (condition-case nil (forward-line -1) (beginning-of-buffer nil)))
       (save-excursion  ; Place horizontal line over the current value.
         (let ((buffer-read-only  nil)
               (cells             (make-string 5 ?e))
