5cff375eae12c6b03c23282643c8c8d2470ac775
DrewAdams <anybody@emacswiki.org>
2014-10-29 00:19:30 +0100

    no summary available

--- bookmark+-chg.el
+++ bookmark+-chg.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
 ;; Copyright (C) 2000-2014, Drew Adams, all rights reserved.
 ;; Created: Fri Sep 15 07:58:41 2000
-;; Last-Updated: Mon Oct 27 11:14:40 2014 (-0700)
+;; Last-Updated: Tue Oct 28 16:06:35 2014 (-0700)
 ;;           By: dradams
-;;     Update #: 15713
+;;     Update #: 15720
 ;; URL: http://www.emacswiki.org/bookmark+-chg.el
 ;; Doc URL: http://www.emacswiki.org/BookmarkPlus
 ;; Keywords: bookmarks, bookmark+
@@ -146,6 +146,13 @@
  
 ;;;(@* "CHANGE LOG FOR `bookmark+-1.el'")
 ;;
+;; 2014/10/28 dadams
+;;     Added: option bmkp-edit-annotation-mode-inherit-from.
+;;     bookmark-edit-annotation-mode: Redefined to use define-derived-mode (per Emacs 25).
+;;                                    Use new option bmkp-edit-annotation-mode-inherit-from.
+;;     bookmark-send-edited-annotation: Allow derived mode (per Emacs 25).
+;;     bookmark-edit-annotation:
+;;       Call bookmark-insert-annotation.  Set local var bookmark-annotation-name to arg (per E25).
 ;; 2014/10/27 dadams
 ;;     Added: Redefinition of bookmark-insert-annotation.
 ;;     bookmark-edit-annotation-mode: Use bookmark-insert-annotation.
