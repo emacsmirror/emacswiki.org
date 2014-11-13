02153ecd091dc1851980ec24b2e966f960b7eb10
SamuelTonini <anybody@emacswiki.org>
2014-11-13 18:52:28 +0100

    Interface to work with git-config variables. More information and installation instructions are available on . . .

---
+++ gitconfig.el
@@ -0,0 +1,6 @@
+Interface to work with [http://git-scm.com/docs/git-config git-config] variables.
+
+More information and installation instructions are available on [https://github.com/tonini/gitconfig.el GitHub].
+
+----
+CategoryProgrammerUtils SamuelTonini
