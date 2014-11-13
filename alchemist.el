ed64f2ebf5eb9a76fde73fa688357181dee02b8c
SamuelTonini <anybody@emacswiki.org>
2014-11-13 17:06:10 +0100

    no summary available

--- alchemist.el
+++ alchemist.el
@@ -3,4 +3,4 @@ Elixir Tooling Integration Into Emacs
 More information and installation instructions are available on [http://github.com/tonini/alchemist.el GitHub].
 
 ----
-ElixirLanguage ProgrammingModes
+ElixirLanguage ProgrammingModes SamuelTonini
