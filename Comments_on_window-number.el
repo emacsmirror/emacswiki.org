7c6c2a5aaebe6a64a4202d564b4a51aa2c2abcbf
Anonymous <anybody@emacswiki.org>
2014-11-13 14:32:44 +0100

    Also, numbers do not display in mode line.  Maybe not compatible with xemacs?

--- Comments_on_window-number.el
+++ Comments_on_window-number.el
@@ -2,3 +2,10 @@ Doesn't work for me - I get error "Symbol's function definition is void: number-
 
 -- Anonymous 2014-11-13 13:03 UTC
 
+
+----
+
+Also, numbers do not display in mode line.  Maybe not compatible with xemacs?
+
+-- Anonymous 2014-11-13 13:32 UTC
+
