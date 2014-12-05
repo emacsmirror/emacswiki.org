effd148aea4f085d003e1f9f64c27aa2dc79a8d2
luis <anybody@emacswiki.org>
2014-12-05 16:53:13 +0100

    Hi. I've made a few changes and pushed this to github at https://github.com/luismbo/wn-mode. I you know who the author or authors of this code are, . . .

---
+++ Comments_on_wn-mode.el
@@ -0,0 +1,4 @@
+Hi. I've made a few changes and pushed this to github at https://github.com/luismbo/wn-mode. I you know who the author or authors of this code are, please leave me a note so I can add proper attribution. Thanks!
+
+-- luis 2014-12-05 15:53 UTC
+
