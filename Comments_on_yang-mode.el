b5e3de8f647abc97baa13241184e008056e2c87b
Anonymous <anybody@emacswiki.org>
2014-12-09 17:22:24 +0100

    no summary available

--- Comments_on_yang-mode.el
+++ Comments_on_yang-mode.el
@@ -1,8 +1,1 @@
-Will not work with Emacs 24.4.1
-Fails with:
-
-Eager macro-expansion failure: (void-function cl-macroexpand-all)
-Symbol's function definition is void: cl-macroexpand-all
-
--- Anonymous 2014-12-09 16:22 UTC
 
