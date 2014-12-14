e50996c385ecec1fb1b15747d4849905ca57f0b5
Anonymous <anybody@emacswiki.org>
2014-12-14 22:18:41 +0100

    Hi Drew, Thanks for providing the bookmark+ package. I am using it to save desktops and switch between them. But it seems like all the buffers . . .

---
+++ Comments_on_bookmark+.el
@@ -0,0 +1,7 @@
+Hi Drew,
+Thanks for providing the bookmark+ package. I am using it to save desktops and switch between them. But it seems like all the buffers associated with the previous desktop remain open when I switch to the next desktop. Is there an option to close the buffers on switching?
+
+Rob
+
+-- Anonymous 2014-12-14 21:18 UTC
+
