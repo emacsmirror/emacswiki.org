65104f445ff79775ecc1e5489a863d0453fb7a95
DrewAdams <anybody@emacswiki.org>
2014-12-15 03:29:53 +0100

    Bookmark+ does not change what DeskTop saves when you save a desktop. Or, consequently, what gets restored when you restore a desktop. All Bookmark+ . . .

--- Comments_on_bookmark+.el
+++ Comments_on_bookmark+.el
@@ -5,3 +5,10 @@ Rob
 
 -- Anonymous 2014-12-14 21:18 UTC
 
+
+----
+
+Bookmark+ does not change what DeskTop saves when you save a desktop. Or, consequently, what gets restored when you restore a desktop. All Bookmark+ does is let you easily restore any desktop you have saved. You would need to look to DeskTop itself to see how to control what buffer information etc. is saved (and hence restored).
+
+-- DrewAdams 2014-12-15 02:29 UTC
+
