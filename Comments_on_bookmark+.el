8ae2e3cb0d92e85d9fd51593e4de781bbb3b9891
DrewAdams <anybody@emacswiki.org>
2014-12-15 03:30:55 +0100

    no summary available

--- Comments_on_bookmark+.el
+++ Comments_on_bookmark+.el
@@ -1,14 +1,12 @@
 Hi Drew,
 Thanks for providing the bookmark+ package. I am using it to save desktops and switch between them. But it seems like all the buffers associated with the previous desktop remain open when I switch to the next desktop. Is there an option to close the buffers on switching?
 
-Rob
-
--- Anonymous 2014-12-14 21:18 UTC
+Rob -- Anonymous 2014-12-14 21:18 UTC
 
 
 ----
 
-Bookmark+ does not change what DeskTop saves when you save a desktop. Or, consequently, what gets restored when you restore a desktop. All Bookmark+ does is let you easily restore any desktop you have saved. You would need to look to DeskTop itself to see how to control what buffer information etc. is saved (and hence restored).
+Rob: [[Bookmark+]] does not change what DeskTop saves when you save a desktop. Or, consequently, what gets restored when you restore a desktop. All Bookmark+ does is let you easily restore any desktop you have saved. You would need to look to DeskTop itself to see how to control what buffer information etc. is saved (and hence restored).
 
 -- DrewAdams 2014-12-15 02:29 UTC
 
