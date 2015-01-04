2cfb1909158d1d2ec2440fe9fdb8294bc2929d4d
DrewAdams <anybody@emacswiki.org>
2015-01-04 22:34:55 +0100

    no summary available

--- col-highlight.el
+++ col-highlight.el
@@ -4,17 +4,17 @@
 ;; Description: Highlight the current column.
 ;; Author: Drew Adams
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
-;; Copyright (C) 2006-2014, Drew Adams, all rights reserved.
+;; Copyright (C) 2006-2015, Drew Adams, all rights reserved.
 ;; Created: Fri Sep 08 11:06:35 2006
 ;; Version: 0
 ;; Package-Requires: ((vline "0"))
-;; Last-Updated: Thu Dec 26 08:51:27 2013 (-0800)
+;; Last-Updated: Thu Jan  1 10:27:40 2015 (-0800)
 ;;           By: dradams
-;;     Update #: 420
+;;     Update #: 424
 ;; URL: http://www.emacswiki.org/cgi-bin/wiki/col-highlight.el
 ;; Doc URL: http://emacswiki.org/emacs/HighlightCurrentColumn
 ;; Keywords: faces, frames, emulation, highlight, cursor, accessibility
-;; Compatibility: GNU Emacs: 22.x, 23.x
+;; Compatibility: GNU Emacs: 22.x, 23.x, 24.x, 25.x
 ;;
 ;; Features that might be required by this library:
 ;;
