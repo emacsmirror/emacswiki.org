0308f488abef303a09b53d444026cd9cda5622ff
DrewAdams <anybody@emacswiki.org>
2015-01-04 22:54:35 +0100

    no summary available

--- doremi.el
+++ doremi.el
@@ -4,17 +4,17 @@
 ;; Description: Incremental change using arrow keys or mouse wheel.
 ;; Author: Drew Adams
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
-;; Copyright (C) 2004-2014, Drew Adams, all rights reserved.
+;; Copyright (C) 2004-2015, Drew Adams, all rights reserved.
 ;; Created: Thu Sep 02 08:21:37 2004
 ;; Version: 0
 ;; Package-Requires: ()
-;; Last-Updated: Thu Dec 26 08:48:54 2013 (-0800)
+;; Last-Updated: Thu Jan  1 10:36:14 2015 (-0800)
 ;;           By: dradams
-;;     Update #: 1617
+;;     Update #: 1619
 ;; URL: http://www.emacswiki.org/doremi.el
 ;; Doc URL: http://www.emacswiki.org/DoReMi
 ;; Keywords: keys, cycle, repeat, higher-order
-;; Compatibility: GNU Emacs: 20.x, 21.x, 22.x, 23.x, 24.x
+;; Compatibility: GNU Emacs: 20.x, 21.x, 22.x, 23.x, 24.x, 25.x
 ;;
 ;; Features that might be required by this library:
 ;;
