53272d7840e3daab43eb9824cc0ed6f1ff724fb5
DrewAdams <anybody@emacswiki.org>
2015-01-12 00:16:21 +0100

    no summary available

--- ucs-cmds.el
+++ ucs-cmds.el
@@ -8,9 +8,9 @@
 ;; Created: Tue Oct  4 07:32:20 2011 (-0700)
 ;; Version: 0
 ;; Package-Requires: ()
-;; Last-Updated: Sun Jan 11 09:59:23 2015 (-0800)
+;; Last-Updated: Sun Jan 11 15:12:34 2015 (-0800)
 ;;           By: dradams
-;;     Update #: 196
+;;     Update #: 263
 ;; URL: http://www.emacswiki.org/ucs-cmds.el
 ;; Doc URL: http://emacswiki.org/UnicodeEncoding
 ;; Keywords: unicode, characters, encoding, commands, ucs-names
@@ -24,33 +24,46 @@
 ;;
 ;;; Commentary:
 ;;
-;;  This library defines a macro, `ucsc-make-commands', and a command
-;;  `ucsc-insert' that can define simple commands that insert Unicode
-;;  characters.
+;;  This library defines a macro, `ucsc-make-commands', and two
+;;  commands:
+;;
+;;  * `ucsc-define-char-insert-cmd' - Define a command to insert a
+;;                                    Unicode character.
+;;
+;;  * `ucsc-insert' - Insert a Unicode character and possibly define a
+;;                    command to insert it.
+;;
+;;  You can use `ucsc-make-commands' or `ucsc-define-char-insert-cmd'
+;;  in Emacs-Lisp code (such as in your init file) to define
+;;  character-inserting commands.  In all cases:
+;;
+;;  * The names of the character-inserting commands created are the
+;;    same as the char names, except that they are lowercase and any
+;;    `SPC' chars in the character name are replaced by hyphens (`-').
+;;
+;;  * You can use a numeric prefix argument with the command, to
+;;    insert multiple copies of the given character.
+;;
+;;  The commands are tailor-made to insert a given Unicode character.
+;;  You can bind such a command to a key sequence, effectively adding
+;;  Unicode characters to your keyboard.
 ;;
 ;;  Command `ucsc-insert' is a replacement for vanilla command
-;;  `insert-char' (called `ucs-insert' prior to Emacs 24), which is
-;;  bound by default to `C-x 8 RET' and which lets you type input to
+;;  `insert-char' (called `ucs-insert' prior to Emacs 24), which Emacs
+;;  binds by default to `C-x 8 RET' and which lets you type input to
 ;;  complete against a Unicode character name and then inserts that
 ;;  character.
 ;;
 ;;  The behavior and code of `ucsc-insert' are identical to those of
-;;  `insert-char' except for what happens when you use a negative
-;;  prefix argument:
+;;  `insert-char' (`ucs-insert') except for what happens when you use
+;;  a negative prefix argument:
 ;;
 ;;  1. It acts as if the prefix-arg value was positive.  So a value of
 ;;     -3 inserts three copies of the character, just as 3 does.
 ;;
-;;  2. In addition to inserting the character, it defines a command
-;;     that you can use thereafter to insert that character.  You can
-;;     use a prefix argument with that command to insert multiple
-;;     copies of the given character.  The command name is the same as
-;;     the character name, except it is lowercase and any `SPC'
-;;     characters in the character name are replaced by hyphens (`-').
-;;
-;;  This gives you a command that is tailor-made to insert a given
-;;  Unicode character.  You can then bind the command to a key
-;;  sequence, effectively adding Unicode characters to your keyboard.
+;;  2. In addition to inserting the character, it uses
+;;     `ucsc-define-char-insert-cmd' to define a command that you can
+;;     use thereafter to insert that character.
 ;;
 ;;  Whenever `insert-char' does anything (it does nothing for a
 ;;  negative prefix arg), `ucsc-insert' does the same thing.  Because
@@ -59,19 +72,20 @@
 ;;
 ;;    (define-key global-map [remap insert-char] 'ucsc-insert)
 ;;
-;;  If you only need a few such commands for inserting particular
-;;  Unicode characters, then using `ucsc-insert' to define them is
-;;  sufficiently convenient.  But it, like `insert-char', can be a bit
-;;  slow if you use completion, because there are many, *MANY*
-;;  completion candidates.
+;;  If you need only a few such commands for inserting particular
+;;  Unicode characters, and you do not want to look up their code
+;;  points, then using `ucsc-insert' or `ucsc-define-char-insert-cmd'
+;;  to define them interactively is sufficiently convenient.  But
+;;  these commands, like `insert-char', can be a bit slow if you use
+;;  completion, because there are many, *MANY* completion candidates.
 ;;
-;;  As an alternative, you can use macro `ucsc-make-commands' to
-;;  quickly create a whole set of such commands for characters whose
-;;  names are similar.
+;;  You can use macro `ucsc-make-commands' to quickly create a whole
+;;  set of such commands for characters whose names are similar.
 ;;
-;;  You provide a regexp as the macro argument.  It is matched against
-;;  all Unicode character names (in `ucs-names').  An insertion
-;;  command is created for each of the characters whose name matches.
+;;  You provide a regexp as the argument to `ucsc-make-commands'.  It
+;;  is matched against all Unicode character names (in `ucs-names').
+;;  An insertion command is created for each of the characters whose
+;;  name matches.
 ;;
 ;;  For example, here are some tests to try.  You need a Unicode font.
 ;;  One of these fonts might help:
@@ -95,12 +109,12 @@
 ;;  Icicles Can Help
 ;;  ----------------
 ;;
-;;  The commands created using macro `ucsc-make-commands' or the more
-;;  general command `ucsc-insert' (or `insert-char') are enhanced if
-;;  you use `Icicles'
+;;  Use of the commands created using `ucsc-define-char-insert-cmd',
+;;  `ucsc-make-commands', and `ucsc-insert' is enhanced by `Icicles'
 ;;  (http://www.emacswiki.org/icicles.el).
 ;;
-;;  For both, you can use more powerful completion with `Icicles',
+;;  When you enter the command name or code point interactively, you
+;;  can take advantage of the more powerful completion of `Icicles',
 ;;  including regexp, substring (a subset of regexp), and various
 ;;  kinds of fuzzy matching.
 ;;
@@ -109,11 +123,11 @@
 ;;  the non-elephant", removing whole sets of candidates that match
 ;;  patterns that you are *not* interested in.
 ;;
-;;  With `Icicles', `ucsc-insert' (but not the individual insertion
-;;  commands defined using it or `ucsc-make-commands') has the
-;;  additional advantage that it displays the characters themselves
-;;  next to their names, in `*Completions*'.  So you see immediately
-;;  what the names represent: WYSIWYG.
+;;  With `Icicles', commands, such as `ucsc-define-char-insert-cmd'
+;;  and `ucsc-insert', which use function `read-char-by-name' have the
+;;  additional advantage that the characters themselves are displayed
+;;  next to their names in buffer `*Completions*'.  So you see
+;;  immediately what the names represent: WYSIWYG.
 ;;
 ;;
 ;;  Macros defined here:
@@ -122,15 +136,17 @@
 ;;
 ;;  Commands defined here:
 ;;
-;;    `ucsc-insert'.
+;;    `ucsc-define-char-insert-cmd', `ucsc-insert'.
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;; Change Log:
 ;;
 ;; 2015/01/11 dadams
+;;     Added: ucsc-define-char-insert-cmd.  Use it in ucsc-make-commands and ucsc-insert.
 ;;     ucsc-make-commands, ucsc-insert: Ensure non-nil ARG for non-interactive use.
 ;;                                      Mention hex and octal in doc string.
+;;     Added autoload cookie for ucsc-insert.
 ;; 2012/12/15 dadams
 ;;     ucsc-insert: Raise error if CHARACTER is not characterp.
 ;; 2012/10/06 dadams
@@ -165,32 +181,37 @@
 ;;; Code:
 
 ;;;###autoload
-(defmacro ucsc-make-commands (regexp)
-  "Create commands to insert Unicode characters whose names match REGEXP.
-Letter case is ignored for matching.
+(defun ucsc-define-char-insert-cmd (character &optional msgp)
+  "Define a command that inserts CHARACTER.
+You are prompted for the CHARACTER name or code point, just as for `insert-char'.
+The command symbol is returned.
 
-The set of char names used is taken from `ucs-names'.  There are
-*many* such chars, so consider using a tighter regexp to limit the
-number of commands created.
+The command has the same name as the character itself, but lowercase
+and with any spaces replaced by hyphens.
 
-The commands created have the same names as the chars they insert,
-except that `SPC' chars in the character names are replaced by
-hyphens (`-'), and the command names are lowercase."
-  (dolist (name.code  (ucs-names))
-    (when (let ((case-fold-search  t))
-            (string-match (upcase regexp) (car name.code)))
-      (eval `(defun ,(intern
-                      (downcase
-                       (replace-regexp-in-string " " "-" (car name.code) nil t)))
-                 (arg)
-               ,(format "Insert Unicode character `%s'.
+For example, if the character is named `GREEK CAPITAL LETTER DELTA',
+then the command, which inserts one or more such chars, is named
+`greek-capital-letter-delta'.
+
+Non-interactively:
+ CHARACTER is a character - it must satisfy `characterp'.
+ MSGP non-nil means echo the name of the created command."
+  (interactive (list (read-char-by-name "Unicode (name or hex): ") t))
+  (let* ((char-name  (car (rassq character (ucs-names))))
+         (cmd-name   (and char-name
+                          (downcase (replace-regexp-in-string " " "-" char-name nil t))))
+         (cmd        (and cmd-name  (intern cmd-name))))
+    (unless char-name (error "No such Unicode character: `%s'" character)) ; Impossible?
+    (eval `(defun ,cmd (arg)
+             ,(format "Insert Unicode character `%s'.
 This char has code point %d decimal \(%X hex, %o octal)."
-                        (car name.code)
-                        (cdr name.code) (cdr name.code) (cdr name.code))
-               (interactive "*p")
-               (unless arg (setq arg  1))
-               (insert (make-string arg ,(cdr name.code))))))))
-
+                      char-name character character character)
+             (interactive "*p")
+             (unless arg (setq arg  1))
+             (insert (make-string arg ,character))))
+    (when msgp (message "Created command `%s'"
+                        (downcase (replace-regexp-in-string " " "-" char-name nil t))))
+    cmd))
 
 ;; Same as `insert-char' (aka `ucs-insert'), except:
 ;;
@@ -204,6 +225,7 @@ This char has code point %d decimal \(%X hex, %o octal)."
 ;;
 ;; 4) Better error message if input CHARACTER is not a Unicode character.
 ;;
+;;;###autoload
 (defun ucsc-insert (character &optional count inherit msgp)
   "Insert COUNT copies of CHARACTER of the given Unicode code point.
 Interactively, prompts for a Unicode character name or a hex number
@@ -242,29 +264,30 @@ command creation."
 	 (prefix-numeric-value current-prefix-arg)
 	 t
          t))
-  (unless (characterp character)        ; Protect `insert-char' from low-level err.
+  (unless (characterp character) ; Protect `insert-char' from low-level err.
     (error "No such Unicode character: `%s'" character))
   (let ((create-cmd-p  (< count 0)))
     (setq count  (abs count))
-    (if (commandp 'insert-char)         ; Emacs 24.  Handle the renaming this way.
+    (if (commandp 'insert-char) ; Emacs 24.  Handle the renaming this way.
         (insert-char character count inherit)
       (ucs-insert character count inherit))
-    (when create-cmd-p
-      (let* ((char-name  (car (rassq character (ucs-names))))
-             (cmd-name   (downcase
-                          (replace-regexp-in-string " " "-" char-name nil t))))
-        (unless char-name
-          (error "No such Unicode character: `%s'" character)) ; Impossible?
-        (eval `(defun ,(intern cmd-name) (arg)
-                 ,(format "Insert Unicode character `%s'.
-This char has code point %d decimal \(%X hex, %o octal)."
-                          char-name character character character)
-                 (interactive "*p")
-                 (unless arg (setq arg  1))
-                 (insert (make-string arg ,character))))
-        (when msgp (message "Created command `%s'"
-                            (downcase (replace-regexp-in-string
-                                       " " "-" char-name nil t))))))))
+    (when create-cmd-p (ucsc-define-char-insert-cmd character msgp))))
+
+;;;###autoload
+(defmacro ucsc-make-commands (regexp)
+  "Create commands to insert Unicode characters whose names match REGEXP.
+Letter case is ignored for matching.
+
+The set of char names used is taken from `ucs-names'.  There are
+*many* such chars, so consider using a tighter regexp to limit the
+number of commands created.
+
+The commands created have the same names as the chars they insert,
+except that `SPC' chars in the character names are replaced by
+hyphens (`-'), and the command names are lowercase."
+  (dolist (name.code  (ucs-names))
+    (when (let ((case-fold-search  t)) (string-match (upcase regexp) (car name.code)))
+      (ucsc-define-char-insert-cmd (cdr name.code)))))
 
 ;;;;;;;;;;;;;;;
 
