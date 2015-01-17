7246693c5ba9b33d9f785f87b8c66b5703874617
DrewAdams <anybody@emacswiki.org>
2015-01-17 21:16:58 +0100

    no summary available

--- bookmark+-chg.el
+++ bookmark+-chg.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
 ;; Copyright (C) 2000-2015, Drew Adams, all rights reserved.
 ;; Created: Fri Sep 15 07:58:41 2000
-;; Last-Updated: Sat Jan 17 11:23:34 2015 (-0800)
+;; Last-Updated: Sat Jan 17 12:15:51 2015 (-0800)
 ;;           By: dradams
-;;     Update #: 15806
+;;     Update #: 15807
 ;; URL: http://www.emacswiki.org/bookmark+-chg.el
 ;; Doc URL: http://www.emacswiki.org/BookmarkPlus
 ;; Keywords: bookmarks, bookmark+
@@ -147,6 +147,7 @@
 ;;;(@* "CHANGE LOG FOR `bookmark+-1.el'")
 ;;
 ;; 2015/01/17 dadams
+;;     Added bmkp-annotate-bookmark.
 ;;     bookmark-show-annotation, bookmark-show-all-annotations: Made it interactive.
 ;; 2015/01/01 dadams
 ;;     bookmark-default-handler, bmkp-goto-position:
