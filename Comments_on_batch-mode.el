3ec66fea7bdd20dc624095e5d00ab52bd42ffe18
Gillyweed <anybody@emacswiki.org>
2015-03-19 21:47:50 +0100

    Thanks, this is super helpful.  I'm not much of a lisp programmer anymore since I was forced of it in 1990, but  I still love emacs and I love the . . .

---
+++ Comments_on_batch-mode.el
@@ -0,0 +1,4 @@
+Thanks, this is super helpful.  I'm not much of a lisp programmer anymore since I was forced of it in 1990, but  I still love emacs and I love the internet for allowing everyone to share tools.
+
+-- Gillyweed 2015-03-19 20:47 UTC
+
