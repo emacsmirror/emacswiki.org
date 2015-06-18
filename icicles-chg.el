dea8b3fe2bc3b36a0daf90b83ee44afee48e7b3e
DrewAdams <anybody@emacswiki.org>
2015-06-18 18:01:24 +0200

    no summary available

--- icicles-chg.el
+++ icicles-chg.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
 ;; Copyright (C) 2007-2015, Drew Adams, all rights reserved.
 ;; Created: Tue Nov 27 07:47:53 2007
-;; Last-Updated: Wed Jun 17 07:25:18 2015 (-0700)
+;; Last-Updated: Thu Jun 18 09:00:35 2015 (-0700)
 ;;           By: dradams
-;;     Update #: 11443
+;;     Update #: 11447
 ;; URL: http://www.emacswiki.org/icicles-chg.el
 ;; Doc URL: http://www.emacswiki.org/Icicles
 ;; Keywords: extensions, help, abbrev, local, minibuffer,
@@ -85,6 +85,9 @@
  
 ;;;(@* "CHANGE LOG FOR `icicles-cmd1.el'")
 ;;
+;; 2015/06/18 dadams
+;;     icicle-make-bookmark-candidate: Favor property location over property buffer name for non-file.
+;;     icicle-bookmark-help-string: Use bookmark-prop-get, not bmkp-get-buffer-name, for buffer name.
 ;; 2015/06/17 dadams
 ;;     icicle-make-bookmark-candidate, icicle-bookmark-help-string:
 ;;       Updated to handle location field in bookmarks.  Thx to Martin Oppegaard.
