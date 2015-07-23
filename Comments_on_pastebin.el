2064a0de4aa2e98abc4c9548c7dcb7a470b0a86f
jspaypal <anybody@emacswiki.org>
2015-07-23 06:58:36 +0200

    My products for sale: Sell high quality CVV, Dumps,track1&2, Physical Dump Card, Paypal Accounts, Amazon t card & itunes t card, Gift Code, . . .

--- Comments_on_pastebin.el
+++ Comments_on_pastebin.el
@@ -1,1 +1,369 @@
-DeletedPage
+
+
+----
+
+My products for sale: Sell high quality CVV, Dumps,track1&2, Physical Dump Card, Paypal Accounts, Amazon t card & itunes t card, Gift Code, MOL, WU Transfer MTCN, Paypal transfer, Exchange PM, WMZ, BTC Bitcoin, Skirll; Passport, Driver License, ID Card, Certificate, Diploma, Degree, Counterfeit Dollars, euro I am verified vendor, supply good & quality CVV for shipping; booking airline ticket; shopping online clothes, hat,shoes, sport equipment, watch,... ; ordering online electronic products: Laptop,Iphone;... In last 7 years my Job Is This. PRESTIGE is my first motto. Not easy to build the good PRESTIGE. My motto is Alway make customers satisfied & happy ! To be a real & verified cvv vendor is not easy...
+
+*YMail:j.spaypal@yahoo.com 
+*Y!: j.spaypal 
+*Gmail:j.s.wu.trf@gmial 
+*Cell::::::+1 (347) 956-3393
+ 
+*** I have unlocked many softwares make good money, example: 
+-Software to make the bug and crack MTCN of the Western Union. 
+Version : 2.0.1.1 new update 
+-Software to open balance in PayPal and Bank Login 
+-Software hacking credit card, debit card Version 1.0 
+*** I only sell it for my good customers, and my familiarity 
+*** I update more than 200 CC + CVV everyday. Fresh + good valid + Strong,private + high balance with best price 
+*** Our products are checked by a partner who works in a bank. 
+Our products are better than 5-7 days after they dead. 
+They are raised mainly for money atm. 
+Can be used in most countries. 
+*** IF You are a SERIOUS BUYER, LET CONTACT Vendor VIA : 
+
+*YMail:j.spaypal@yahoo.com 
+*Y!: j.spaypal 
+*Gmail:j.s.wu.trf@gmial 
+*Cell::::::+1 (347) 956-3393
+
+ 
+__________List cc's __________
+- Us:
+visa,master = 6$
+amex,discover = 8$
+bin = 20$
+dob = 25$
+fullz = 45$
+- UK:
+visa,master = 10$
+amex,discover = 12$
+bin = 25$
+dob= 30$
+fullz = 60$
+- CA:
+visa,master = 15$
+amex = 18$
+bin= 25$
+dob = 30$
+fullz = 75$
+- AU:
+visa,master = 18$
+amex = 20$
+bin = 25$
+dob = 30$
+fullz = 75$
+- Italy = 20$ / 1
+- Spain = 20$ / 1
+- Denmark = 25$ / 1
+- Sweden = 20$ / 1
+- France = 20$ / 1
+- Germany = 19$ / 1
+- Ireland = 18$ / 1
+_ Brasil: 25$/ 1
+- New zeland = 18$ / 1
+- Switherland = 18$ / 1
+- Slovakia = 20$ / 1
+- Netherlands = 18$ / 1
+- Mexico=15 / 1
+- Middle East = 18$ / 1
+- Asia = 18$
+
+=========PRICE LIST ALL CVV CVV2.
+1 US ( visa,master) = 3$/ ( buy > 50 Price $1.2/ 1cvv)
+1 US (Amex,dis) = 5$/ ( buy > 50 price $3 /1cvv)
+1UK = 5$/ ( Buy > 50 price 4$/1cvv)
+1UK CVV with DOB = 15$/CVV ( Buy > 50 CVV Price 12$ = 1CVV)
+1 Ca CVV = 5$/CVV
+1 CA CVV(Amex,dis) = 7$/cvv
+1 EU CVV = 15$/CVV
+1 EU CVV(Amex,dis) = 5$/cvv
+1 US CVV full info = 35$/CVV
+1 UK CVV full info = 40$/CoVV
+Australia (AU) 10.00 $
+Switzerland (VE) 14.00 $
+France (FR) 15.00 $
+Germany (GE) 15.00 $
+Mexico (MX) 12.00 $
+New Zealand (NZ) 13.00 $
+ITALY 15$. And many country orther
+
+Demo US
+
+VISA | 4039959177849993 | 08 | 2011 | 104 | Schnell | Collins | 2411
+BROOKDALE DR | ARLINGTON | TX | 76014 | 2145524581 | United States
+MASTER | 5458831003207226 | 10 | 2011 | 938 | Lee Beam | 110 Flint
+Hill Rd. | Cherryville | NC | 28021 | United States
+Also sell SMTP , ORANGE ,MAILER ,SOFTWARES AT good take away prices
+
+Demo UK
+165430 | 134884 | 2 | 4921817934747226 | 4 | 2008 | 3 | 2010 | | 662
+MR ALAN D HOWELLS | 10 | Fairlead Drive | Gosport | PO139UX | | Hants
+01943 468442/si...@cambrig.co.uk
+165337 | 134815 | 2 | 4921817809597243 | 3 | 2008 | 2 | 2010 | | 185
+peter i hodgson | 10 | chapel close | wesham | PR4 3HB | | lancs
+01436672207/mpdmaxmy...@aol.com
+
+Demo au
+1124:1720176 |7:33PM | HUW AP REES | 5583886596112173| 03 |2011 | 937
+10 STUART ST | | LONGUEVILLE NSW | 00 | 2066 | Australia
+61404058007
+4779:1723315 |7:06AM | william oldjohn| 4601843780060933| 07 |2010
+325 | 137 Baden Powell Drive | | Mount Eliza | 00 | 3194 | Australia
+61395855116
+
+demo FR
+rezig philippe 4978130033603767 122007 685 lieu dit bourrieu 31470 STE
+FOY DE PEYROLIERES FR
+domingo felix 4976650000000426 102008 048 21 impasse du cdt espinadel
+
+demo japan
+tabita...@h9.dion.ne.jp | 4685175895770463 | 07 | 08 | 729
+2005-06-05 | 2005-07-05 | 16 | $35.00 | 222.13.57.221/32 | | UEMOTO
+SHUNSAKU | 3900221 | 1 |
+k...@tcp-ip.or.jp | 4986053198241050 | 06 | 07 | 035 | 2005-06-05
+2005-09-03 | 9 | $68.00 | 219.102.11.28/32 | | kusa | toshihiro
+4760002 | 1 |
+
+demo ger
+4489:1878065 | Mar 18 2009 3:20PM | Hanna Held | 4408499000897252| 01
+2013| 649 | Hohenstaufenring 62 | | Cologne | 00 | 50674 | Germany
+492212775820
+
+If you want to have deals with good quality please contact me with
+
+__________Email and pass , Email leads ( US , UK, CA, Eu, Au.....)__________
+ 
+1 Million Emails Leads Assorted = 10$
+2 Million Emails Leads Job Seekers = 35$
+1 Million Email Leads Businesses and large companies=30$
+1 Emails Leads Upscale & Affluent People = 25$
+2 Million Emails Leads Businessman' Emails = 35$
+1 Million Emails Leads Women Executives = 25$
+1 Million Emails Leads Work At Home = 25$
+1 Million Emails Leads Home Owners = 25$
+1 Million Emails Leads Consumers = 25$
+- Unlimited Webmail Server inbox : $15
+Note:
+We are committed to ensuring our emails provide you with a value of 100%. Email our regular updates
+We update leads according to fresh database not sold before to another and are cheap with best replies for your business.
+ 
+And more country ....
+
+*YMail:j.spaypal@yahoo.com 
+*Y!: j.spaypal 
+*Gmail:j.s.wu.trf@gmial 
+*Cell::::::+1 (347) 956-3393
+
+
+___________ RDP(Remote Desktop Protocal) ____________
+ 
+- RDP Australia :$10 ( Warranty: 30 day )
+- RDP Europe :$10 (Europe`s Best Quality Remote Desktop Protocol Tool )
+- RDP ASIA :$10 ( High Quality Remote Desktop Protocol 2003/2008 Tool )
+- RDP Netherlands :$10 ( Warranty: 20 day )
+- RDP With AMS & PHP Mailer Unlimited : $20
+- RDP With AMS & Unlimited SMTP : $20
+- RDP WIth AMS ( Germany ) : $10 ( Warranty: 30 day )
+- UK RDP WITH AMS : $10 ( Warranty: 30 day )
+- USA RDP WITH AMS : $10 ( Warranty: 15 day )
+- AMS 4.3 REGISTRATION CODE/ Advance Mail Sender Portable software : $3
+ 
+___________ Shell C99 ____________
+ 
+- Shell C99 : $10
+ 
+___________Cpanel Hosting__________
+ 
+- Cpanel Hosting : $10
+ 
+____________ SCAMPAGES ___________
+ 
+-All types of scampages for cheap rates.
+ 
+___________SMTP (Simple Mail Transfer Protocol)________
+ 
+- 5 INBOX SMTP PACK : $15( IP Inbox SMTP )
+- Smtp Scanner (Scan SMTP fresh.Daily 500smtp )
+- Unlimited Domain Smtp Server inbox : $15
+- IP SMTP unlimited Inbox : $15 (Unlimited for the day )
+ 
+___________ Mailer _____________
+ 
+- Limited PHP Mailer : $10 (Duration 7 Days Guaranty)
+( Can send upto 2k per shot and 100k per day )
+- Unlimited PHP Mailer Server : $15 ( 21 days guaranty Inbox 100% )
+___________ Webmail ______________
+ 
+- Random Webmail spam/inbox : $8
+- Unlimited Webmail Server inbox : $15
+ 
+__________ SSH Tunnelier _________
+ 
+- SSH Tunnelier (Root IP USA) : $8
+- SSH Tunnel( CA IP) : 10$
+- SSH Tunnel(AU IP) : 10$
+- SSH Tunnel(FR IP) : 10$
+- SSH Tunnel(UK IP) : 10$
+ 
+_________ VPN __________
+ 
+- U.S. VPN : $15
+ 
+- Canada VPN : $15
+ 
+-U.K VPN : $15
+- China VPN :$15
+- Deutsch VPN :$15
+- And more countries...
+______________SELL ACCT DATING SITES______________
+-Match
+-Zoosk
+-PlentyOfFish
+-OkCupid
+-ChristianMingle
+-eHarmony
+-DateHookup
+-OurTime
+-SeniorPeopleMeet
+-BlackPeopleMeet
+-SpeedDate
+-Chemistry
+-HowAboutWe
+-JDate
+-MeetLocals
+- And more sites...
+______________SELL TUTORIALS HACKING______________
+ 
+      - Carding Expensive Watches ( up to 3000$ ) , Drop in USA needed . 	
+      -Hack Up to 500 Business E-mails per Day with PDF Exploit - 2015 	
+      -Hack everyday up to 50 RDP , Tools Included !
+      -Hack cPanels from Shells - Up to 100 per Shell - Tools Included 		
+      -Carding eBay - The easiest way - Ship order up to 800$ - 2015 Method
+      -Verify Your Leads , Extract Only the valid e-mails without paying
+      - How To Get EarthLink & CenturyLink RDP and Send Unlimited Spam
+      -Hack Shop Admins and Get Unlimited CVV
+      - Western union transfer method-success 100%
+ 
+*** SOFTWARE WU BUG V3.1-Transfer Western Union / Moneygram/bank (ALL COUNTRIES and very easy to cashout African) :
+ 
+- 800$ you will have mtcn : 10000$
+ 
+- 700$ you will have mtcn : 8000$
+ 
+- 400$ you will have mtcn : 5500$
+ 
+- 300$ you will have mtcn : 3500$
+ 
+- 150$ you will have mtcn : 1500$
+
+
+------------ Bank / Western Union transfer ----------------
+*** Transfer Western Union / bank (Eu,Uk,Asia,Canada,Us,France,Germany,Italy,Nigeria and very easy to cashout African) :
+500$ you will have mtcn : 10000$400$ you will have mtcn : 8000$300$ you will have mtcn : 5500$200$ you will have mtcn : 3500$100$ you will have mtcn : 1500$
+
+------------ Bank / Western Union transfer ---------------- 
+*** Transfer Western Union / bank (Eu,Uk,Asia,Canada,Us,France,Germany,Italy,Nigeria and very easy to cashout African) : 
+500$ you will have mtcn : 10000$ 
+400$ you will have mtcn : 8000$ 
+300$ you will have mtcn : 5500$ 
+200$ you will have mtcn : 3500$ 
+100$ you will have mtcn : 1500$ 
+- We are Very PROFESSIONAL in WESTION UNION. Our Special Job Is this 
+- We Have big Western Union Service for everywhere and every when for you. 
+- We transfer money to all country in world. 
+- We can transfer big amount. And you can receive this money from your country. 
+- Our service accept payment 15% of transfer amount for small transfer . And 10% of big transfer. 
+- For large transfer . We make is very safe. And this service is very fast. 
+- We start to run software to make transfer to your WU info very fast,without delay and immediately. 
+- We give you MTCN and sender info and all cashout info, 15 mins after your payment completed. 
+- Sell BUG WESTERN UNION TRANSFER 
+--------------------- Login Bank (all bank )----------------------- 
+Sell BANK LOGIN, BANK TRANSFER = 
+- HSBC, Barclays, Welsfargo, BOA, Chase, Credit union, Halifax 
+- HSBC CC, Barclays CC, Welsfargo CVV, BOA Debit, Chase CC, Credit Union CVV, Halifax CC 
+Bank BOA Us : 
+1 . Balance 3000$ = 100$ 
+2 . Balance 11000$ = 300$ 
+Bank HSBC US : 
+1 . Balance 6000$ = 200$ 
+2 . Balance 14000$ = 400$ 
+Bank HSBC UK : 
+1 . Balance 6000 GBP = 200$ 
+2 . Balance 14000 GBP = 400$ 
+...I ALSO DO BANK TRANSFER OF ANY AMOUNT TO ANY BANK
+
+-------------------- SHIPPING(Laptop + Iphone + Ipad) ------------- 
+Ship iphone : 4g 150$ per 1 
+Ship iphone : 3gs 100$ per 1 
+Ship ipad : 150$ per 1 
+Ship Laptop : Vaio 200$ per 1 
+Ship Laptop : Dell 150$ per 1 
+Ship Laptop : HP 100$ per 1 
+SHIP LAPTOP APPLE = 150$ 
+SHIP LAPTOP HP + DELL = 120$ 
+SHIP LAPTOP TOSHIBA = 80$ 
+SHIP LAPTOP LENOVO = 100$ 
+(2-3 days have tracking number) 
+--------------------ALL FOR With PAYPAL ------------------------- 
+Sell Paypal with pass email = 15 $ /paypal 
+Sell Paypal don't have pass email = 10 $ /Paypal 
+Sell Visa Debit US : 120$ 
+Sell Paypal Veritified with balance 3000$ = 200$ 
+Sell Paypal Veritified with balance 8000$ = 400$ 
+Sell Paypal Veritified with balance 13000$= 600$
+
+________ Mistery Shopper Reply 10$/1_________
+ 
+***** Provide PriSock SSH with Bulk Private 100%, Cheapest price + donation Hosting Unix
+-Sock Create from Server like 1 hosting
+Should you buy I will send full info, including: IP/user/Pass
+Link Login ConTrolPanel Hosting
+Disk space Unlimited
+Bandwidth Transfer 8 TB
+-You use SSH for work, to run forum hosting and your website also
+-Transactions in 3 minutes will receive
+-Competitive price may renew the request
+ 
+**Costing: SSH 1month : 5$
+SSH 3month : 12$
+SSH 6month : 25$
+SSH 1 year : 40$
+ 
+-Paypal Us Verifiled + Confirmed SSN: 15$/1month
+-Bank US to Verified Paypal : 20$/1bank
+-Sell Fileserve Premium : 1year 30$
+_________DUMPS PRICE LIST_________
+- USA
+* VISA CLASSIC | MASTERCARD STANDART
+• 40$/1 for order till 10 pcs
+• 30$/1 for order over 10 pcs
+* VISA GOLD/PLATINUM | DISCOVER | AMEX | MC GOLD
+• 50$/1 for orders till 10 pcs
+• 40$/1 for orders over 10 pcs
+ 
+-CANADA & EUROPE & ASIA & LATIN & OTHERS
+* CANADA
+• VISA CLASSIC | MASTERCARD STANDART - $45
+• VISA GOLD/PLATINUM | DISCOVER | AMEX | MC GOLD - $70
+* EUROPE
+• VISA CLASSIC | MASTERCARD STANDART - $70
+• VISA GOLD/PLATINUM | DISCOVER | AMEX | MC GOLD - $80
+* ASIA:
+• VISA CLASSIC | MASTERCARD STANDART - $80
+• VISA GOLD/PLATINUM | DISCOVER | AMEX | MC GOLD - $100
+_________MY BUSINESS RULE _________
+-Can you buy small cc's or stuff then buy more later,NO FREE,if you dont trust can you dont buy.
+- All my stuff and the cc's has tested good before sell so dont ask for free test,send first.
+- If not good then i replace and long time for guarantee stuff.
+- And buy in bulk will you get the discount.
+_________ CONTACT_________
+Contact to me via
+ 
+*YMail:j.spaypal@yahoo.com 
+*Y!: j.spaypal 
+*Gmail:j.s.wu.trf@gmial 
+*Cell::::::+1 (347) 956-3393
+
+-- [http://vpaste.net/39OTj jspaypal] 2015-07-23 04:58 UTC
+
