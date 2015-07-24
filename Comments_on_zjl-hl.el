16fedf84c6234a344f3ac6130625564dee6710ee
DrewAdams <anybody@emacswiki.org>
2015-07-24 19:54:06 +0200

    DeletedPage

--- Comments_on_zjl-hl.el
+++ Comments_on_zjl-hl.el
@@ -1,18 +1,1 @@
-hi rda horses oper day
-
--- [http://horses Anonymous] 2015-07-24 10:53 UTC
-
-
-----
-
-hi rda horses oper day
-
--- [http://horses Anonymous] 2015-07-24 10:55 UTC
-
-
-----
-
 DeletedPage
-
--- DrewAdams 2015-07-24 17:53 UTC
-
