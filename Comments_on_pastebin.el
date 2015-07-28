67f71c4556c553449ac2b6f507f1f736db7c4c64
MichaelPaulukonis <anybody@emacswiki.org>
2015-07-28 22:52:06 +0200

    spam

--- Comments_on_pastebin.el
+++ Comments_on_pastebin.el
@@ -1,8 +1,1 @@
 
-
-----
-
-I Have good drops to receive packages US OR UK. I also have Company Bank Account available to receive good monies minimum 200k$ upward. contact me bode_jones@yahoo.com for deals.
-
--- bode_jones 2015-07-28 16:55 UTC
-
