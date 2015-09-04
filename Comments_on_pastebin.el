05d2bb7710d6d86fa1e6062309c9e0374a117e08
quickcash <anybody@emacswiki.org>
2015-09-04 05:14:04 +0200

    IF you really want to make really  good money in no time and  without stress and effort then read this.I'm an American hacker-  i have team . . .

--- Comments_on_pastebin.el
+++ Comments_on_pastebin.el
@@ -1,1 +1,640 @@
-DeletedPage
+
+
+----
+
+IF you really want to make really  good money in no time and 
+
+without stress and effort then read this.I'm an American hacker- 
+
+i have team living UK US Asian - We are hacker We have a lot of 
+
+stuff need sell,we are well specialized in WESTERN 
+
+UNION,PAYPAL,BANK TRANSFER & LOGINS.Also  My products for sale: 
+
+Sell high quality CVV, Dumps,track1&2, Physical Dump Card, Paypal 
+
+Accounts, Amazon t card & itunes t card, Gift Code, MOL, WU 
+
+Transfer MTCN, Paypal transfer, Exchange PM, WMZ, BTC Bitcoin, 
+
+Skirll; Passport, Driver License, ID Card, Certificate, Diploma, 
+
+Degree, Counterfeit Dollars, euro I am verified vendor, supply 
+
+good & quality CVV for shipping; booking airline ticket; shopping 
+
+online clothes, hat,shoes, sport equipment, watch,... ; ordering 
+
+online electronic products: Laptop,Iphone;... In last 7 years my 
+
+Job Is This. PRESTIGE is my first motto. Not easy to build the 
+
+good PRESTIGE. My motto is Alway make customers satisfied & happy 
+
+! To be a real & verified cvv vendor is not easy...
+
+PLEASE EMAIL OR TEXT ME FOR FAST REPLY AND RESPONSE 
+
+*YMail:j.spaypal@yahoo.com
+ 
+*Y!: j.spaypal
+ 
+*Gmail:j.s.wu.trf@gmail 
+
+*Cell::::::+1 (512) 309-5117
+ 
+*** I have unlocked many softwares make good money, example: 
+-Software to make the bug and crack MTCN of the Western Union. 
+Version : 2.0.1.1 new update 
+-Software to open balance in PayPal and Bank Login 
+-Software hacking credit card, debit card Version 1.0 
+*** I only sell it for my good customers, and my familiarity 
+*** I update more than 200 CC + CVV everyday. Fresh + good valid 
+
++ Strong,private + high balance with best price 
+*** Our products are checked by a partner who works in a bank. 
+Our products are better than 5-7 days after they dead. 
+They are raised mainly for money atm. 
+Can be used in most countries. 
+*** IF You are a SERIOUS BUYER, CONTACT Vendor VIA : 
+
+*YMail:j.spaypal@yahoo.com
+ 
+*Y!: j.spaypal
+ 
+*Gmail:j.s.wu.trf@gmail 
+
+*Cell::::::+1 (512) 309-5117
+
+------------ Western Union transfer ---------------- 
+*** Transfer Western Union / bank 
+
+(Eu,Uk,Asia,Canada,Us,France,Germany,Italy,Nigeria and very easy 
+
+to cashout African) : 
+500$ you will have mtcn : 10000$ 
+400$ you will have mtcn : 8000$ 
+300$ you will have mtcn : 5500$ 
+200$ you will have mtcn : 3500$ 
+150$ you will have mtcn : 1500$ 
+- We are Very PROFESSIONAL in WESTION UNION. Our Special Job Is 
+
+this 
+- We Have big Western Union Service for everywhere and every when 
+
+for you. 
+- We transfer money to all country in world. 
+- We can transfer big amount. And you can receive this money from 
+
+your country. 
+- Our service accept payment 15% of transfer amount for small 
+
+transfer . And 10% of big transfer. 
+- For large transfer . We make is very safe. And this service is 
+
+very fast. 
+- We start to run software to make transfer to your WU info very 
+
+fast,without delay and immediately. 
+- We give you MTCN and sender info and all cashout info, 15 mins 
+
+after your payment completed. 
+
+For Western Union Transfer Info Needed
+1: First Name
+2: Last Name
+3: City
+4: Country
+5: Email for confirmatio
+- Sell BUG WESTERN UNION TRANSFER 
+
+*** BANK LOGIN and TRANFER BANK : COUNTRY : US,UK,CA,EU,ASIAN...
+
+Bank Us : HALIFAX,BOA,CHASE,Wells Fargo...
+. Balance 6000$ = 500$
+
+. Balance 8000$ = 600$
+
+. Balance 12000$ = 800$
+
+. Balance 15000$ = 1000$
+
+. Balance 20000$ = 1200$
+
+Bank UK : LLOYDS TSB,BARCLAYS,Standard Chartered,HSBC...
+. Balance 10000 GBP = 700$
+
+. Balance 12000 GBP = 800$
+
+. Balance 16000 GBP = 900$
+
+. Balance 20000 GBP = 1400$
+
+. Balance 30000 GBP = 1600$
+
+You can contact me for more and many Bank Logins you need.
+Have all details for login and I can transfer balance to your 
+
+account if you want
+Bank To Bank Transfer To Any Usa Bank
+
+Bank To Bank Transfer To Any Uk Bank
+
+Bank To Bank Transfer To Any Euro Country Bank
+
+Amount To Pay For That Depend On Amount You Want To Transfer
+
+chat with me for know more details: === yahoo YM / 
+
+j.spaypal@yahoo.com
+----DUMP Services:-------
+
++ skimmer ATM with pin 
+
++ POS tracks with pin
+Codes
+101-521-201
+121-221-110
+100-90-80
+122-91-120
+226-106-81
+71-70-206
+
+Dumps,Tracks 1&2 Us = 90$ per 1
+
+Dumps,Tracks 1&2 Uk = 110$ per 1
+
+Dumps,Tracks 1&2 Ca = 120$ per 1
+
+Dumps,Tracks 1&2 Au = 120$ per 1
+
+Dumps,Tracks 1&2 Eu = 130$ per 1
+Will check with hight balance 
+
+Other countries: BRAZIL , AUSTRALIA , CHINA , JAPAN , etc
+Classic – $90
+Gold / Platinum - $100
+Corporate / Signature - $ 120
+Business - 120
+
+==================
+Sample : 
+
+CardNumber=4190004341231284
+HolderName1=DALAS/WILLIAMS
+HolderName2=
+CardExp=08/15
+Carrier=101
+Track1=B4190004341231284^FRANK/JEFFREY 
+
+R/^1302101184710000000000426000000
+Track2=4190004341231284=13021011847142600001
+pin : 2451
+
+sample of dumps :
+MC: 5424180*****509=0906101085010581 ; 
+
+B5424180*****09^LISA/MICHAEL D ^090610100000008501000000581000000
+
+542418*****6583=0909101226010498 ; B542418*****6583^PETERS/ANGELA 
+
+M ^090910100000022601000000498000000
+VISA:
+41144******501329=091110114013216 ; B41144******501329^BALANGERO/ 
+
+JUSTIN M^091110100000014013216000000
+
+41212******22456=09091010000092900000 ; 
+
+B41212******22456^CORNELL/LORI^09091010000000000 092900000000000 
+__________List cc's __________
+- Us:
+visa,master = 6$
+amex,discover = 8$
+bin = 20$
+dob = 25$
+fullz = 45$
+- UK:
+visa,master = 10$
+amex,discover = 12$
+bin = 25$
+dob= 30$
+fullz = 60$
+- CA:
+visa,master = 15$
+amex = 18$
+bin= 25$
+dob = 30$
+fullz = 75$
+- AU:
+visa,master = 18$
+amex = 20$
+bin = 25$
+dob = 30$
+fullz = 75$
+- Italy = 20$ / 1
+- Spain = 20$ / 1
+- Denmark = 25$ / 1
+- Sweden = 20$ / 1
+- France = 20$ / 1
+- Germany = 19$ / 1
+- Ireland = 18$ / 1
+_ Brasil: 25$/ 1
+- New zeland = 18$ / 1
+- Switherland = 18$ / 1
+- Slovakia = 20$ / 1
+- Netherlands = 18$ / 1
+- Mexico=15 / 1
+- Middle East = 18$ / 1
+- Asia = 18$
+__________Email and pass , Email leads ( US , UK, CA, Eu, 
+
+Au.....)__________
+ 
+1 Million Emails Leads Assorted = 10$
+2 Million Emails Leads Job Seekers = 35$
+1 Million Email Leads Businesses and large companies=30$
+1 Emails Leads Upscale & Affluent People = 25$
+2 Million Emails Leads Businessman' Emails = 35$
+1 Million Emails Leads Women Executives = 25$
+1 Million Emails Leads Work At Home = 25$
+1 Million Emails Leads Home Owners = 25$
+1 Million Emails Leads Consumers = 25$
+- Unlimited Webmail Server inbox : $15
+Note:
+We are committed to ensuring our emails provide you with a value 
+
+of 100%. Email our regular updates
+We update leads according to fresh database not sold before to 
+
+another and are cheap with best replies for your business.
+And more country ....
+
+*YMail:j.spaypal@yahoo.com
+ 
+*Y!: j.spaypal
+ 
+*Gmail:j.s.wu.trf@gmial 
+
+*Cell::::::+1 (512) 309-5117
+
+
+___________ RDP(Remote Desktop Protocal) ____________
+ 
+- RDP Australia :$10 ( Warranty: 30 day )
+- RDP Europe :$10 (Europe`s Best Quality Remote Desktop Protocol 
+
+Tool )
+- RDP ASIA :$10 ( High Quality Remote Desktop Protocol 2003/2008 
+
+Tool )
+- RDP Netherlands :$10 ( Warranty: 20 day )
+- RDP With AMS & PHP Mailer Unlimited : $20
+- RDP With AMS & Unlimited SMTP : $20
+- RDP WIth AMS ( Germany ) : $10 ( Warranty: 30 day )
+- UK RDP WITH AMS : $10 ( Warranty: 30 day )
+- USA RDP WITH AMS : $10 ( Warranty: 15 day )
+- AMS 4.3 REGISTRATION CODE/ Advance Mail Sender Portable 
+
+software : $3
+ 
+___________ Shell C99 ____________
+ 
+- Shell C99 : $10
+ 
+___________Cpanel Hosting__________
+ 
+- Cpanel Hosting : $10
+ 
+____________ SCAMPAGES ___________
+ 
+-All types of scampages for cheap rates.
+ 
+___________SMTP (Simple Mail Transfer Protocol)________
+ 
+- 5 INBOX SMTP PACK : $15( IP Inbox SMTP )
+- Smtp Scanner (Scan SMTP fresh.Daily 500smtp )
+- Unlimited Domain Smtp Server inbox : $15
+- IP SMTP unlimited Inbox : $15 (Unlimited for the day )
+ 
+___________ Mailer _____________
+ 
+- Limited PHP Mailer : $10 (Duration 7 Days Guaranty)
+( Can send upto 2k per shot and 100k per day )
+- Unlimited PHP Mailer Server : $15 ( 21 days guaranty Inbox 100% 
+
+)
+___________ Webmail ______________
+ 
+- Random Webmail spam/inbox : $8
+- Unlimited Webmail Server inbox : $15
+ 
+__________ SSH Tunnelier _________
+ 
+- SSH Tunnelier (Root IP USA) : $8
+- SSH Tunnel( CA IP) : 10$
+- SSH Tunnel(AU IP) : 10$
+- SSH Tunnel(FR IP) : 10$
+- SSH Tunnel(UK IP) : 10$
+ 
+_________ VPN __________
+ 
+- U.S. VPN : $15
+ 
+- Canada VPN : $15
+ 
+-U.K VPN : $15
+- China VPN :$15
+- Deutsch VPN :$15
+- And more countries...
+______________SELL ACCT DATING SITES______________
+-Match
+-Zoosk
+-PlentyOfFish
+-OkCupid
+-ChristianMingle
+-eHarmony
+-DateHookup
+-OurTime
+-SeniorPeopleMeet
+-BlackPeopleMeet
+-SpeedDate
+-Chemistry
+-HowAboutWe
+-JDate
+-MeetLocals
+- And more sites...
+______________SELL TUTORIALS HACKING______________
+ 
+      - Carding Expensive Watches ( up to 3000$ ) , Drop in USA 
+
+needed . 	
+      -Hack Up to 500 Business E-mails per Day with PDF Exploit - 
+
+2015 	
+      -Hack everyday up to 50 RDP , Tools Included !
+      -Hack cPanels from Shells - Up to 100 per Shell - Tools 
+
+Included 		
+      -Carding eBay - The easiest way - Ship order up to 800$ - 
+
+2015 Method
+      -Verify Your Leads , Extract Only the valid e-mails without 
+
+paying
+      - How To Get EarthLink & CenturyLink RDP and Send Unlimited 
+
+Spam
+      -Hack Shop Admins and Get Unlimited CVV
+      - Western union transfer method-success 100%
+ 
+*** SOFTWARE WU BUG V3.1-Transfer Western Union / Moneygram/bank 
+
+(ALL COUNTRIES and very easy to cashout African) :
+
+******************** SOFTWARES **************************** 
+1 .Comersus Software With Bank Login And Bank Credit Card Code : 
+
+500$ 
+2 .Comersus Software Without Bank Login And Bank Credit Card Code 
+
+: 200$ 
+3. New Western Union Hacking Bug For World Wide Transfer : 300$ 
+4 .New Paypal Login Hackware For Hacking Fresh Paypal : 250$ 
+5. New Shop Admin Hackware For Hacking Online Shop For Credit 
+
+Card : 250$ 
+6 .New Credit Card Amount Checker Software For Peoples Wanting To 
+
+Know Balance on Cc : 150$ 
+7 .New Credit Card Validator For Validation Any Full CC Info : 
+
+120$ 
+-And more price and more discount, bonus .. IF u real a big buyer 
+
+let's contact me to have good info about CC and price . 
+
+ATM SKIMMERS PRICE LIST : 
+http://krebsonsecurity.com/2010/01/would-you-have-spotted-the-
+
+fraud/ 
+
+ATM Skimmer Wincor Nixdorf : $ 3000 
+ATM Skimmer Wincor : $ 3000 
+ATM Skimmer Slimm : $ 3000 
+ATM Skimmer Slim : $ 3000 
+ATM Skimmer NCR : $ 3000 
+ATM Skimmer Diebold Opteva : $ 2500 
+ATM Skimmer Diebold : $ 2000 
+ATM Skimmer Universal : $ 4000 
+ATM Skimmer Small : $ 2500 
+Chip POS ingenico&amigo : $ 1500 
+
+MSR300 : $ 189 
+MSR500 : $ 199 
+MSR500m : $ 255 
+Mini400u : $ 329 
+Mini600 : $ 349 
+MSR2000 : $ 259 
+MSR3000 : $ 339 
+TA-48 : $ 299 
+TA-32 : $ 359 
+PMR-232 : $ 349 
+
+Writer Specials 
+As Low As 
+MSE750 : $ 359 
+MSR206 : $ 444 
+MSR505 : $ 359 
+Bundles 
+
+MSR505 / MSR2000 : $ 549 
+MSR505 / MSR300* : $ 499 
+MSR505 / TA-48 : $ 639 
+MSR206 / MSR3000 : $ 729 
+MSR206 / MSR300 : $ 549 
+
+Best Buy 
+MSR206 + 2x MSR400 : $ 900 
+
+MSR206 + 2x MSR500m (Mini123) : $ 875 
+MSR206 + 2x TA-32 : $ 990 
+MSR206 + 2x CRM42 : $ 869 
+MSR206 + 2xCRM41 : $ 929 
+Fresh cvvs fulls dumps track1 and 2 banks logins (wu)verified 
+
+seller 
+Fresh cvvs fulls dumps track1 and 2 banks logins (wu)verified 
+
+seller 
+I am a legit seller of skimmed dumps + bank logins + verified 
+
+paypal (Track 1 + Track 2 + Pin) 
+
+
+-------------------- SHIPPING(Laptop + Iphone + Ipad) 
+
+------------- 
+Ship iphone : 4g 150$ per 1 
+Ship iphone : 3gs 100$ per 1 
+Ship ipad : 150$ per 1 
+Ship Laptop : Vaio 200$ per 1 
+Ship Laptop : Dell 150$ per 1 
+Ship Laptop : HP 100$ per 1 
+SHIP LAPTOP APPLE = 150$ 
+SHIP LAPTOP HP + DELL = 120$ 
+SHIP LAPTOP TOSHIBA = 80$ 
+SHIP LAPTOP LENOVO = 100$ 
+(2-3 days have tracking number) 
+--------------------ALL FOR With PAYPAL ------------------------- 
+Sell Paypal with pass email = 15 $ /paypal 
+Sell Paypal don't have pass email = 10 $ /Paypal 
+Sell Visa Debit US : 120$ 
+Sell Paypal Veritified with balance 3000$ = 200$ 
+Sell Paypal Veritified with balance 8000$ = 400$ 
+Sell Paypal Veritified with balance 13000$= 600$
+
+________ Mistery Shopper Reply 10$/1_________
+ 
+***** Provide PriSock SSH with Bulk Private 100%, Cheapest price 
+
++ donation Hosting Unix
+-Sock Create from Server like 1 hosting
+Should you buy I will send full info, including: IP/user/Pass
+Link Login ConTrolPanel Hosting
+Disk space Unlimited
+Bandwidth Transfer 8 TB
+-You use SSH for work, to run forum hosting and your website also
+-Transactions in 3 minutes will receive
+-Competitive price may renew the request
+ 
+**Costing: SSH 1month : 5$
+SSH 3month : 12$
+SSH 6month : 25$
+SSH 1 year : 40$
+ 
+-Paypal Us Verifiled + Confirmed SSN: 15$/1month
+-Bank US to Verified Paypal : 20$/1bank
+-Sell Fileserve Premium : 1year 30$
+
+-----------------Ebay/Electronic Carding 
+
+Service------------------------ 
+
+1) add me or pm me y/h: pass.king 
+2) tell me what you want to be carded, or link me specific link 
+
+on ebay 
+The item must be atleast 200$ 
+3) i confirm to be able and give you the price ( you should be 
+
+able to know the price with the rate) 
+4) you pay to my bitcoin or perfect money  
+5) You pm me or email : Batch number, if you want direct carding 
+
+or by my drop , and provide your adress like this: 
+Name: 
+Adress: 
+City: 
+State: 
+Postal: 
+Country: 
+6) and you wait patiently 
+
+Dont pm for free method or bins or something else. All i give 
+
+free are cc and i share always in Accounts and Data Dumps. So if 
+
+you want free stuff, just go there 
+
+I said all what you need to know about my offer. IF you are ready 
+
+to buy and to pay, then feel free to add me to deal. Dont losse 
+
+my time by chatting with me, I don't care about your stories, im 
+
+a carder not a friend ( but we will become friends when we 
+
+dealing) 
+In the case where i fail in my deal, i will kindly refund the 
+
+amount with bonus for time wasting.[/B] 
+
+-.-.-.-.-.-.-.-.-.--.-.-.-.-.-.-.-.-.-.- 
+E-Ticket Carding Service 
+-.-.-.-.-.-.-.-.-.--.-.-.-.-.-.-.-.-.-.- 
+
+I sell most e-tickets, just let me know what you need! 
+
+Rate: 40% of price on the website I use including fees. 
+
+Minimum order: currently set at $100 (you pay 40$ via LR) 
+
+Do you accept escrow: Yes escrow is fine as long as you pay the 
+
+fees. 
+
+Time from order to getting tickets: Sometimes instant but usually 
+
+24-48 hours, 
+on the odd time it may be longer you will be informed if this is 
+
+the case. 
+
+How does it work?: I will card your ticket / tickets to my email, 
+When you are using escrow then send tickets to you 
+via download link then you release funds, so it's safe for both 
+and I have proof that I got them. 
+
+-.-.-.-.-.-.-.-.-.--.-.-.-.-.-.-.-.-.-.- 
+Country's Available 
+-.-.-.-.-.-.-.-.-.--.-.-.-.-.-.-.-.-.-.- 
+
+Argentina 
+Australia 
+Austria 
+Belgium 
+Brazil 
+Denmark 
+Finland 
+France 
+Germany 
+Greece 
+Italy 
+Luxembourg 
+Netherlands 
+Norway 
+Poland 
+Portugal 
+Rep. Of Ireland 
+Russia 
+South Africa 
+Spain 
+Sweden 
+Switzerland 
+Turkey 
+United Kingdom 
+United States 
+_________MY BUSINESS RULE _________
+-Can you buy small cc's or stuff then buy more later,NO FREE,if 
+
+you dont trust can you dont buy.
+- All my stuff and the cc's has tested good before sell so dont 
+
+ask for free test,send first.
+- If not good then i replace and long time for guarantee stuff.
+- And buy in bulk will you get the discount.
+_________ CONTACT_________
+Contact to me via
+ 
+*YMail:j.spaypal@yahoo.com
+ 
+*Y!: j.spaypal
+ 
+*Gmail:j.s.wu.trf@gmial 
+
+*Cell::::::+1 (512) 309-5117
+
+-- quickcash 2015-09-04 03:14 UTC
+
