e5660eed1cd6df8a3c2ab150a1f526a0e33a86de
el.services <anybody@emacswiki.org>
2015-09-07 03:32:53 +0200

    ====> WELCOME ALL OF YOU TO MY SERVICES <==== ===> I'm is a big seller and real hacker here, i have worked for many people around the . . .

--- Comments_on_pastebin.el
+++ Comments_on_pastebin.el
@@ -1,1 +1,270 @@
-DeletedPage
+
+
+----
+
+====> WELCOME ALL OF YOU TO MY SERVICES <====
+===> I'm is a big seller and real hacker here, i have worked for many people around the world, believe in Unity. Trust and Honesty is 100% needed in all field of money making
+===> You can contact me at any time to buy CVV, CC fresh and best
+===> I have some software make the money online good ...
+
+Bank login,Dumps,Fullz,Shopping,Bank TRansfers,Online transfer,Dumps track1&2
+
+
+Mailer,Rdp,Paypal,Plastic Cards,Booking Ticket,Wu Transfer
+
+
+------------Contact------------
+Y!M :  hassan.elhasaan - hassan.elhasaan@yahoo.com
+ICQ :  678365008
+G-MAIL :  hasaan.el7@gmail.com
+JABBER: elhasaan66@xmpp.jp
+
+
+
+------------Western Union Trfs----------------------------
+
+I will Give you MTCN And Sender Info And All Cash-out Info.
+
+ALL I REQUIRE :
+
+1 : Receiver First Name And Last Name
+
+2 : Receiver Address
+
+3 : Receiver Email Address
+
+4 : Payment Info
+
+
+**Prices For Western Union Online Transfer(Eu,Uk,Asia,Canada,Us,France,Germany,Italy and some African Countries):
+3000$ = 300$
+2500$ = 250$
+2000$ = 200$
+1500$ = 150$
+1000$ = 100$
+
+I transfer minimum 500$ with price 50$ first for the time customer
+Western Union Online Software(Western Union Bug(WU Bug)
+Version Latest With an Activation Code :100$
+
+
+---------------------------- CVV--------------------
+1 US (visa,master) = 7 $
+1 US (Amex,dis) = 7 $
+1 UK = 12 $
+1 UK (with DOB ) = 15 $
+1 Ca = 15 $
+1 CA (Amex,dis) = 15 $
+1 EU = 15 $
+1 EU (Amex,dis) = 17 $
+1 US (full info) = 25 $
+1 UK (full info) = 30$
+Australia (AU) = 10 $
+Switzerland (VE) = 15 $
+France (FR) = 15 $
+Germany (GE) = 15 $
+Mexico (MX) = 12 $
+New Zealand (NZ) = 13 $
+ITALY = 15 $
+Asian cvv(all country)=17$
+And many country orther...
+
+------------------------MASTER and VISA BIN-------------------------
+446278 - 446272 - 449352 - 449353 - 498824 - 415929 - 465902 - 492940
+492181 - 492182 - 492942 - 456735 - 454313 - 462785 - 453978
+518675 - 6759 - 5434 - 529930 - 552188 - 543429 - 5505
+
+-------------------Fullz INfos Formate-------------------------
+
+FirstName: ******
+LastLame: ***********
+Address: ****************
+City:********
+State: **
+Email: **********t
+CardType: ******
+ZipCode: *****
+Country: *******
+SSN: ********
+DOB: ** ** ****
+Mother Maiden Name: *********
+Driver License Number: *************H
+Phone: ********
+CardNumber: ***************
+E*piryDate: ** **
+CVV2: ***
+ATM PIN: ****
+
+(3.)
+CC+Cvv2 ( Cvvs)
+Formate:
+
+FirstName: ******
+LastLame: ***********
+Address: ****************
+City:********
+State: **
+Email: **********
+ZipCode: *****
+Country: *******
+Phone: ********
+CardNumber: ***************
+ExpiryDate: ** **
+CVV2: ***
+
+-----------------------DATING SITE-------------
+I CAN PAY ANY DATING SITE IT DEPENDS ON THE SITE YOU WANT
+EG.PEOPLEMEET
+
+---------------SHIPPING-------------------
+I do shopping to any address in US and UK,I shop iphones,laptops,perfums and more,
+Come for more deal
+
+--------------------Payypal Account---------------
+1 Paypal with pass email 1500$ BALANCE = 100 $ /paypal
+1 Paypal don't have pass email = 30 $ /Paypal
+Sell Paypal account US reg 3 month ago: 18$/1 acc: Verified
+Sell Paypal account US : 10 $/1 acc: Verified
+Sell Bank account info US : 7 $
+Sell VPN to -- US : 8 $/month
+Sell Visa Debit US : 120$
+sell sock all country =5$
+
+----------Bank Login --------------- Bank Login From Usa And Eu And Uk And Asia Is Avaiable.
+AVAILABLE BANK LOGIN :
+Scotia OnLine
+Wachovia
+netbank.commbank.com.au
+Abbey
+HSBC
+Bremer Online Banking
+Flagstar Bank
+KBC Bank
+EnterCard
+Postbank
+M&T
+Credit Union
+WaMu
+Landmark
+Orchard
+American Express
+Wells Fargo
+ICICI Bank
+Chase
+Pen Air Federal
+U.S. Bank
+RBC
+First Trust Bank
+Banque Nationale
+HDFC Bank
+**You can contact me for more and many Bank Logins you need.
+
+I make Wire transfer and cheque transfer to
+UK and US banks .. HSBC // Nationwide //Halifax //Abbey // Capital // BOA // watchovia //
+Barclays // FCU / Regions / Wells // etc..
+Cost is 30% upfront of whatever amount you want me to transfer for you ( will accept an offer depending on the amount to be transfered )
+
+--------------------DUMPS---------------------------
+Price: Usual dumps with original track1 and track2 (all dumps hacked)
+track1+track2 ( without holder info )
+USA:
+MasterCard Standart, Visa Classic - $25
+Visa Gold|Platinum|Corporate|Signature|Business – $45
+American Express|Discover|amazon gifts certificates - $25-40
+CANADA:
+MasterCard, Visa Classic - $30
+Visa Gold|Platinum|Corporate|Signature|Business – $55
+EU:
+MasterCard, Visa Classic - $90
+Visa Gold|Platinum|Corporate|Signature|Business – $130
+Other countries:
+MasterCard| Visa Classic - $70
+Visa Gold|Platinum|Corporate|Signature|Business – $90
+
+
+Dumps with CardHolder info
+=====================
+Price: ( Exclusive Dumps )
+track1+track2 ( with CARDHOLDER INFO zip, dob , address, cvv2 )
+USA:
+MasterCard Standart, Visa Classic - $30
+Visa Gold|Platinum|Corporate|Signature|Business – $50
+American Express|Discover|amazon gifts certificates - $30-50
+CANADA:
+MasterCard, Visa Classic - $40
+Visa Gold|Platinum|Corporate|Signature|Business – $60
+EU:
+MasterCard, Visa Classic - $100
+Visa Gold|Platinum|Corporate|Signature|Business – $160
+Other countries:
+MasterCard| Visa Classic - $100
+Visa Gold|Platinum|Corporate|Signature|Business – $120
+
+ATM SKIMMERS
+ATM Skimmer Wincor Nixdorf : $3500
+ATM Skimmer Wincor: 3500
+ATM Skimmer Slimm: $3000
+ATM Skimmer Slim : $2500
+ATM Skimmer NCR: $3000
+ATM Skimmer Diebold Opteva: $2500
+ATM Skimmer Diebold:$2500
+ATM Skimmer Universal:$3500
+ATM Skimmer Small:$2000
+
+MSR MACHINE
+MSR505 = MSR2000 :$600
+MSR505 = MSR300:$450
+MSR505 = TA-48:$400
+MSR206 = MSR3000:$300
+MSR206 = MSR300:$250
+
+Dump Writer and Reader Machine :
+MSR206 Reader/Writer USB
+
+Introduction
+Magnetic Swipe Card Reader/Writer MSR206 is designed to offer a card reading/writing solution for ISO 7811/1~6 formats. It reads and writes up to 3 tracks of data, e.g. decoding/encoding and verifying up to 3 tracks of data simultaneously. Also, MSR206 Reader/Writer provides a standard RS-232 interface to communicate with host system or other terminal computers. That will attractively complement an existing system.
+
+Features
+* Reading/Writing magnetic stripe card complied with ISO 7811/1~6 formats
+* Read/Write High & Low Coercive force of magnetic stripe (300~4000Oe)
+* High/Low Coercivity encoding circuitry selectable on screen
+* Program software for Windows 98/Me/XP
+* Programming software for various read/write performance
+* Programmable leading bit, raw data, DMV/AAMVA, and user defined forma
+* Manual Swipe to read and/or write card with RS-232 output
+* Writing and verifying data on single, dual, or triple track in one swipe
+* 5~35ips operational swipe speed of writing data
+* 5~55ips operational swipe speed of reading data
+* +24VDC+/-10%, 2.0A Max., external power adapter attached
+* Good size with dimensions of 210(L) x 60(W) x 65(H) mm
+* CE, FCC, UL, cUL certified
+Price : 200$
+With Free Shipping.
+
+
+---------------------------------Conditions of sale-------------------------
+
+( read it before add my ICQ & YM in ur contact list! )
+
+1. Dumps are sent to you after full payment for the order...
+
+2. According ur desire staff can be checked up during the moment of purchase.
+
+3. No consultations and advices how where and what for it's possible to take advantage of the given goods.
+
+4. No gifts, "just for test" etc. And I am very tired from beggars and children... do not need wasted my and ur time too..
+
+5 .No minimum orders if you pay with Libertyrreserve (Details for WU|MG orders in icq)
+
+6. Payment method: Perfect Money, Western Union, MoneyGram,Bit-Coin
+
+------------Contact------------
+Y!M :  hassan.elhasaan - hassan.elhasaan@yahoo.com
+ICQ :  678365008
+G-MAIL :  hasaan.el7@gmail.com
+JABBER: elhasaan66@xmpp.jp
+
+Am Here To Get You The Best Stuffs All The Time..!!!!Let Get Paid Together Now...!!!!Am Online Come 4Real Deals...!
+
+-- el.services 2015-09-07 01:32 UTC
+
