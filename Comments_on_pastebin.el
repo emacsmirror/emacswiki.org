87420ad36556d27da6b85375750ca409838fa811
quickcash <anybody@emacswiki.org>
2015-09-08 12:13:40 +0200

    --------- Hello All Customer ---------  I'm a hacker from all over the world.I have experience in information technology for over 7 years,i have . . .

--- Comments_on_pastebin.el
+++ Comments_on_pastebin.el
@@ -1,1 +1,494 @@
-DeletedPage
+
+
+----
+
+--------- Hello All Customer --------- 
+
+I'm a hacker from all over the world.I have experience in information technology for over 7 years,i have team living UK US Asian - We are hacker We have a lot of stuff need sell,we are well specialized in WESTERN UNION,PAYPAL,BANK TRANSFER & LOGINS ..I'm a big seller and i have been a hacker all my life.. I need best buyer and long time business 
+
+  I'm sure you will be happy if you do bussiness with me .Buy Fake Passport ID Cards Drivers License Dumps Cvv CC Track 1/2 ATM Pin Western Union and paypal
+
+Here is a quick overview of Quick Leak
+Changing nationality or making any high quality fake or real documents for work and traveling? then you have come to the right place. we produce the above documents for countries like: USA, Australia,Belgium, Brazil, Canada, Italian, Finland, France, Germany, Israel, Mexico, Netherlands, Switzerland , Sweden,South Africa, Spain, United Kingdom ,All countries in the world . If interested email us at the below email, To get the additional information.To get the additional information and place the order You can address all your questions and demands to this address.
+To get the additional information and place the order just visit our website or you contact us via email or mobile.
+
+---- Yahoo Messenger :yandex_wu
+
+---- E-mail :yandex_wu@yahoo.com
+
+            :yandex.wu@gmail.com
+
+---- ICQ number: 662940267
+
+---- Phone number:(+1) (512) 309-5117 
+
+---------------- My work is online 24/7 ---------------- 
+
+------------western union trfs---------------------------- 
+i will give you mtcn and sender info and all cash-out info. 
+all i require : 
+1 : receiver first name and last name 
+2 : receiver address 
+3 : receiver email address 
+4 : payment info 
+**prices for western union online transfer(eu,uk,asia,canada,us,france,germany,italy and some african countries): 
+3000$ = 300$ 
+2500$ = 250$ 
+2000$ = 200$ 
+1500$ = 150$ 
+1000$ = 100$ 
+i transfer minimum 1000$ with price 100$ first for the time customer 
+western union online software(western union bug(wu bug) 
+version latest with an activation code :100$ 
+Western union bug 2015 with activation code in 350$ :  https://wumt.westernunion.com 
+MTCN PROOF 
+http://i44.tinypic.com/20a2jhj.jpg 
+MTCN DETAILS PROOF 
+http://i43.tinypic.com/b6q39k.jpg 
+WU NEW TRF PROOF 
+http://i40.tinypic.com/x2s2ua.jpg 
+WU NEW TRF DETAILS PROOF 
+http://i39.tinypic.com/4sjmoh.jpg 
+=== Beacause I hacker Really , so i do bug Wu Price Hight 
+* Give me your western union info and payment me fee transfer's. 
+Then i will do transfer's for you, After about 15 mins you'll have MTCN and sender name 
+* Note: My software can only do five times transactions the maximum error for each recipient information
+
+**********DUMPS CCS Sale Online FRESH TRACK 2 DUMPS************** 
+
+VIP DUMPS OFFER - BEST DUMPS FOR ALL YOUR NEEDS 
+
+We are glad to offer you the best dumps 
+
+We are in the market for some years. Our dumps established a reputation as high-qualified and we are following the admissible pricing policy. 
+We are offering high respect and only good dumps to our customers. 
+AVAILABLE NOW 
+Particularities of our: - High-percent valid dumps! - Freshest bases (tr2, tr1+tr2)! - WEEKLY updates! - Lowest prices for dumps of such quality! - We are not resellers of somebody else dumps - that's why all articles will be sold only to you! - Positioning at the US's dumps, but Asian and European dumps are also sometimes available. - We are not checking dumps before a deal, because we are sure of quality of our dumps and you will become an owner of really effective dumps. 
+
+"ONE OF THE FEW HONEST DUMPS VENDORS WITH GOOD DATABASE" 
+
+Plastics and Novelty IDs and checker service available! 
+Stop Wasting your $$ on Rippers/low quality dumps w/o support! 
+Come make Big Money, With The Real Deal! 
+WE DO NOT EXAGGERATE WHEN WE SAY WE ARE THE BEST IN THE BIZ! 
+COME AND SEE FOR YOURSELF!!!
+CONTACT US ON ICQ, after you check our ready busines right ! 
+ USA Dumps : 
+ US Classic/Standart - $50 
+ US Gold/Platinum - $60 
+ US Bussines/Corporate - $55 
+ US MC World - $50 
+ ................................ 
+ EURO Dumps: 
+
+ EU Gold/Classic - $70 
+
+ EU Corporate/Purchasing - $90 
+
+ EU Platinum/Business - $120 
+
+ EU Infinite - $150 
+
+ EU 201 - $80 / EU 101 - $100 
+
+................................ 
+
+ CA Dumps : 
+
+ CA Standart/Classic $50 
+
+ CA Gold/Platinum $60 
+
+ CA Corporate/Signature/Business $80 
+
+ CA 201 - $40 / CA 101 - $50 
+
+ CA INFINITE , MC WORLD - $100 
+
+ ................................ 
+
+ Other countries: 101/121  BRAZIL , AUSTRALIA , CHINA , JAPAN , UK , etc ... 
+
+ MasterCard Visa Classic - $80 
+
+ Visa Gold Platinum Corporate Signature Business ? $100 
+
+ INFINITE , BLACK AMEX , MC WORLD - $200 
+
+ ATM CARDS PLASTIC ....YESCARDS....... ATM 
+
+ Send By DHL 2days . Price 300 EURO or 320$ upfront 150 EURO or 150$ . 
+
+ Sure and u past me ur post code . After 2 days sure but before i past tracking ......... 
+
+
+............................... Dumps Pin/Track 1&2 ............................... 
+
+
+ - Tracks 1&2 US = 60$ per 1 
+
+ - Tracks 1&2 UK = 70$ per 1 
+
+ - Tracks 1&2 CA / AU = 80$ per 1 
+
+ - Tracks 1&2 EU = 90$ per 1 
+
+
+ United States|JPMorgan Chase Bank N.A.|Visa|PLATINUM|101|Track 1/Track 2| 
+
+
+ Track2=5466160067866044=1406101103010555 
+
+ Track1=B5466160067866044^LYONS/WILLIAM^14061010000000000103010555000000 
+
+
+
+5423201104769585=15071060000012430037 
+
+Chile | N/A | SANTIAGO | BANCO SANTANDER CHILE | MASTERCARD | STANDART 
+
+
+
+4966711001306493=13081060000019290035 
+
+ Chile | N/A | Santiago | BANCO SANTANDER-CHILE | VISA | GOLD/PREM | CREDIT | 
+
+
+
+ 4500030105441897=14092010003026090201 
+
+ Canadian Imperial Bank of Commerce CREDIT GOLD/PREM Canada Toronto Ontario ON 
+
+
+
+ Track2=4658591937313000=14022212730000000011 => United Kingdom: PIN 3463 
+
+ Track2=5404500019100759=13052210000002047000 => United Kingdom: PIN 9832 
+
+
+ Track2=4852450003907464=10050114065434000000 => United States : PIN 5845 
+
+ Track2=4185506003290033=11144281300469401011 => United States : PIN 6846 
+
+
+ Pin: 5678,5432,5187,5505,and much pin other 
+
+
+
+ UK Normal with DOB $35 
+
+ UK with BINS with DOB $50 
+
+ UK Normal $20 
+
+ UK with BINS $25 
+
+ UK Amex $30 
+
+...................................... 
+
+ FULLZ FRANçAISE 
+
+ MASTER 25$/1 
+
+ Visa 25$/1 
+
+ 100% valide 
+
+...................................... 
+
+ US visa $5/1 
+
+ US master $7/1 
+
+ US amex $8/1 
+
+ US discover $10/1 
+
+...................................... 
+UK visa $18/1 
+
+UK master $20/1 
+
+UK amex $30/1 
+
+UK discover $35/1 Switch 
+...................................... 
+CA visa $15/1 
+
+CA master $17/1 
+
+CA amex $25/1 
+...................................... 
+AU visa $18/1 
+
+AU master$20/1 
+
+AU amex $30/1 
+...................................... 
+US FULLZ $45/1 
+
+UK FULLZ $65/1 
+
+CA,AU,UK FULLZ 50$/1 
+ ...................................... 
+EU visa $18/1 
+
+EU master $20/1 
+
+EU amex $40/1 
+
+EU Many country : GER,Italy,France,Spain,New Zeland,Mexico,Japan...etc... 
+
+ ...................................... 
+
+ SOME BINS : 
+
+ USA: 517805,488893,492536,408181,542432,482880,374355,374372... 
+
+ CA : 450003,450008,451242,450060,549198,533833,519123,544612... 
+
+ UK : 4547,5506,5569,5404,5031,4921,5505,5506,4921,4550... 
+
+ AU : 543568,450605,494053,450606,456475,521893,519163... 
+
+ FR : 497847,497831,497841,497849,497820,497825,497833... 
+
+ GER: 492942,490762,530127... And others bins for others country 
+
+............................................................................................. 
+
+Exchange Rate To Bitcoins + Perfect Money + Webmoney.....ect 
+
+
+Bitcoin -> Western Union/MoneyGram 
+
+1 BTC = 230.8 USD (Minus the WU/MG fee) 
+Western Union's Fees Table 
+From        To        Fee 
+$400.01        $500.00        $40.00 
+$500.01        $750.00        $45.00 
+$750.01        $1,000        $50.00 
+$1,000.        $1,250.        $76.00 
+
+Money Gram's Fees Table 
+From        To        Fee 
+$400.01        $500.00        $35.00 
+$500.01        $700.00        $39.00 
+$700.01        $1,000        $45.00 
+$1,000        $1,250        $65.00 
+
+Western Union/MoneyGram -> Perfect Money -> WebMoney.... ETC 
+
+USD:10% 
+
+
+ ......... RULES BUSINESS.............................. 
+
+
+- REPLACE among 12 hours after purchase - WE NEVER replace twice same order, try to fool US - INSTANT IGNORE! - WE NEVER provide Test / Sample / Dumps + PIN! - WE NEVER make REFUND, ONLY REPLACE! - WE CAN NOT guarantee limits and amounts on dumps! Orders will be sent via shorttext.com, privnote.com, privatepaste.com, sendspace.com or email. 
+
+REPLACEMENT POLICY : 
+
+- To get replacements customer MUST send back non-working DUMPS (ONLY bad DUMPS) - Customer may use for this purpose shorttext.com, privnote.com, privatepaste.com or sendspace.com, we will will check these pieces ON CHECKER and change the bad ones. - No PROPER report within 12 hours = No Replacement 
+
+Attention - If someone send TRACKS/DUMPS DIRECTLY in icq chat he got: - First time WARN - Next time IGNORE and No replacement! If You don't care about your and my SECURITY - I will do the same with our PARTNERSHIP! Think a little bit, because lots of you guys start thinking only in Jail!  
+
+********** List cvv i have and price i have : 
+
+-------------------------------- 
+
+- Us (Visa,Master) = 4$ per 1 
+
+- Us (Amex,Dis) = 5$ per 1 
+
+- Us Bin 10$ , US Dob 15$ 
+
+- Us fullz info = 25$ per 1 
+
+-------------------------------- 
+
+- Uk (Visa,Master) = 8$ per 1 
+
+- Uk (Amex,Dis) = 10$ per 1 
+
+- Uk Bin 15$ , UK Dob 20$ 
+
+- Uk fullz info = 30$ per 1 
+
+-------------------------------- 
+
+- Ca (Visa,Master) = 10$ per 1 
+
+- Ca (Amex,Dis) = 12$ per 1 
+
+- Ca Bin 15$ , CA Dob 20$ 
+
+- Ca fullz info = 30$ per 1 
+
+-------------------------------- 
+
+- Au (Visa,Master) = 10$ per 1 
+
+- Au (Amex,Dis) = 15$ per 1 
+
+- Au Bin 17$ , AU Dob 20$ 
+
+- Au fullz info = 30$ per 1 
+
+-------------------------------- 
+
+- Eu (Visa,Master) = 20$ per 1 
+
+- Eu (Amex,Dis) = 23$ per 1 
+
+- Eu Bin 25$ , AU Dob 30$ 
+
+- Eu fullz info = 40$ per 1 
+
+-------------------------------- 
+
+- Italy = 20$ per 1 (fullz info = 35$) 
+
+- Spain = 20$ per 1 (fullz info = 35$) 
+
+- Denmark = 25$ per 1 (fullz info = 35$) 
+
+- Sweden = 20$ per 1 (fullz info = 35$) 
+
+- France = 20$ per 1 (fullz info = 35$) 
+
+- Germany = 20$ per 1 (fullz info = 35$) 
+
+- Ireland = 20$ per 1 (fullz info = 35$) 
+
+- Mexico = 15$ per 1 (fullz info = 30$) 
+
+- Asia = 15$ per 1 (fullz info = 30$) 
+
+- And many country orther...let me know if have i will sell for you. 
+
+all cvv very good and fresh , work 100% with hight balance. 
+
+will change if cvv not good or dont work. 
+
+********** BANK LOGIN and TRANFER BANK : (COUNTRY : US,UK,CA,EU,ASIAN...) 
+
+- Bank Us : ( HALIFAX,BOA,CHASE,Wells Fargo...) 
+
+. Balance 6000$ = 500$ 
+
+. Balance 8000$ = 600$ 
+
+. Balance 12000$ = 800$ 
+
+. Balance 15000$ = 1000$ 
+
+. Balance 20000$ = 1200$ 
+
+- Bank UK : ( LLOYDS TSB,BARCLAYS,Standard Chartered,HSBC...) 
+
+. Balance 10000 GBP = 700$ 
+
+. Balance 12000 GBP = 800$ 
+
+. Balance 16000 GBP = 900$ 
+
+. Balance 20000 GBP = 1400$ 
+
+. Balance 30000 GBP = 1600$ 
+
+- You can contact me for more and many Bank Logins you need. 
+
+- Have all details for login and I can transfer balance to your account if you want 
+
+Bank To Bank Transfer To Any Usa Bank 
+
+Bank To Bank Transfer To Any Uk Bank 
+
+Bank To Bank Transfer To Any Euro Country Bank 
+
+Amount To Pay For That Depend On Amount You Want To Transfer 
+
+- chat with me for know more details: (Yahoo Messenger : (yandex_wu) 
+
+********** Ship (Laptop/Iphone/Ipad) : 
+
+- The price of transportation of high-end electronics 
+
+= Laptop Apple = 250$ 
+
+= Laptop HP Dell = 140$ 
+
+= Laptop Toshiba Samsung = 140$ 
+
+= Laptop Vaio = 200$ 
+
+= Iphone 3GS = 130$ 
+
+= Iphone 4G = 160$ 
+
+= Iphone 4GS = 190$ 
+
+= Ipad2 = 180$ 
+
+= Black Berry = 150$ 
+
+=== Transit time from 1 to 2 days 
+
+=== Every 5 hours will send a shipping tracking 
+
+********** Sell Account Paypal Verification : 
+
+[IMG]http://i828.photobucket.com/albums/zz207/blusky4/paypal-logo-1.jpg[/IMG] 
+
+= Account Paypal 1500$ = 150$ 
+
+= Account Paypal 2500$ = 200$ 
+
+= Account Paypal 4000$ = 300$ 
+
+= Account Paypal 7000$ = 500$ 
+
+( Email address PayPal password ) fullz infomation 
+
+- I always check the balance and details before selling 
+
+******************** Business rule ******************** 
+
+Before make business or add my ID let's read carefull my rule because i really hate Spamer,Ripper and Scamer 
+
+1. With me I never give test free or Screenshot . Business with me , first is TRUST me , not trust don't contact 
+
+2. Payment first...it's my rules work... 
+
+3. I always check and update new cvv good and fresh every day 
+
+4. I will change cvv if not good or dont work 
+
+5. Orther stuff you can buy 1 for test ! but for Cvv minimum or is 5 
+
+6. If you buy over 30 stuff, I will discount for you 
+
+7. I accept payment with Bitcoins(BTC), Perfect Money(PM) and Westernunion (WU) 
+
+8. Hope you are best customer and we can to work a long time business 
+
+Thanks all read my post ....... hope to see you soon 
+
+--------------------------------------------------------------------------- 
+
+---- Yahoo Messenger :yandex_wu
+
+---- E-mail :yandex_wu@yahoo.com
+
+            :yandex.wu@gmail.com
+
+---- ICQ number: 662940267
+
+---- Phone number:(+1) (512) 309-5117 
+
+.................. See you soon and hoping to hear from y'll..................
+
+-- quickcash 2015-09-08 10:13 UTC
+
