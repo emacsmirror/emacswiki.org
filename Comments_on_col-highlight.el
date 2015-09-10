2c54399d0e1985f6549dc6242dd4252a9f646fc2
AlexSchroeder <anybody@emacswiki.org>
2015-09-10 12:56:51 +0200

    quoted XML

--- Comments_on_col-highlight.el
+++ Comments_on_col-highlight.el
@@ -1,16 +1,16 @@
-hello,there is a bug,
+hello,there is a bug:
+{{{
 <?xml version="1.0" encoding="UTF-8" standalone="no"?>                                                    
 <?fileVersion 4.0.0?><cproject storage_type_id="org.eclipse.cdt.core.XmlProjectDescriptionStorage">       
     <storageModule moduleId="org.eclipse.cdt.core.settings">                                              
         <cconfiguration id="com.android.toolchain.gcc.1892026188">                                        
-            <storageModule buildSystemId="org.eclipse.cdt.managedbuilder.core.configurationDataProvider" \
-id="com.android.toolchain.gcc.1892026188" moduleId="org.eclipse.cdt.core.settings" name="Default">        
+            <storageModule buildSystemId="org.eclipse.cdt.managedbuilder.core.configurationDataProvider" id="com.android.toolchain.gcc.1892026188" moduleId="org.eclipse.cdt.core.settings" name="Default">        
                 <externalSettings/>                                                                       
                 <extensions>                                                                              
                     <extension id="org.eclipse.cdt.core.ELF" point="org.eclipse.cdt.core.BinaryParser"/>  
-                    <extension id="org.eclipse.cdt.core.VCErrorParser" point="org.eclipse.cdt.core.ErrorP\
--UUU(DOS)----F1  .cproject      Top (1,0)      (nXML Valid Fill) 四  9 10 15:43 3.52 ---------------------
+                    <extension id="org.eclipse.cdt.core.VCErrorParser" point="org.eclipse.cdt.core.ErrorP-UUU(DOS)----F1  .cproject      Top (1,0)      (nXML Valid Fill) 四  9 10 15:43 3.52 ---------------------
 Error in post-command-hook (col-highlight-highlight): (wrong-type-argument characterp -1)
+}}}
 
 -- 匿名者 2015-09-10 07:43 UTC
 
