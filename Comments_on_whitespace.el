2b33c085fbab3ad7e1a143a08cfe690bc24d4880
AlexSchroeder <anybody@emacswiki.org>
2015-09-18 09:25:25 +0200

    Not directly. You could remove the element `empty' from `whitespace-style' using a hook for the major-mode you care about using some elisp?

--- Comments_on_whitespace.el
+++ Comments_on_whitespace.el
@@ -1,1 +1,8 @@
 Is there any way to turn off cleaning up whitespace at the beginning of buffers? It is really annoying in some cases such as editing git commit messages.
+
+----
+
+Not directly. You could remove the element `empty' from `whitespace-style' using a hook for the major-mode you care about using some elisp?
+
+-- AlexSchroeder 2015-09-18 07:25 UTC
+
