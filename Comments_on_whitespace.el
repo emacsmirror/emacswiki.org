28b10580bc14a3a654b12795437163c8699b2722
AlexSchroeder <anybody@emacswiki.org>
2015-09-18 09:27:11 +0200

    no summary available

--- Comments_on_whitespace.el
+++ Comments_on_whitespace.el
@@ -2,7 +2,8 @@ Is there any way to turn off cleaning up whitespace at the beginning of buffers?
 
 ----
 
-Not directly. You could remove the element `empty' from `whitespace-style' using a hook for the major-mode you care about using some elisp?
+Not directly. You could remove the element `empty' from `whitespace-style' using a hook for the Major Mode you care about using some elisp?
+Or customize `whitespace-global-modes` and make sure Whitespace Mode is not used for the Major Modes you care about?
 
 -- AlexSchroeder 2015-09-18 07:25 UTC
 
