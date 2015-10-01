ade90735e9d7ee90c821778e2adf0e1c612d59ee
josteink <anybody@emacswiki.org>
2015-10-01 11:04:28 +0200

    Running cdb-gud.el as currently in the wiki causes constant failures due to bugs in the auto-complete implementation. Unless this bug is fixed, . . .

--- Comments_on_cdb-gud.el
+++ Comments_on_cdb-gud.el
@@ -4,3 +4,12 @@ Also gud mangles syntax for connection to remote host.
 Here is my hacked version of cdb-gud that resolves these issues with kd.exe
 
 https://github.com/4DA/kd-gud
+
+----
+
+Running cdb-gud.el as currently in the wiki causes constant failures due to bugs in the auto-complete implementation.
+
+Unless this bug is fixed, you will have to disable auto-complete-mode after triggering M-x cdb.
+
+-- josteink 2015-10-01 09:04 UTC
+
