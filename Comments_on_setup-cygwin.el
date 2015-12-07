a7d8d871d303e25223dc6d43f72a352eaa1f1389
Anonymous <anybody@emacswiki.org>
2015-12-07 17:19:16 +0100

    This seems to be broken with recent version of cygwin and GNU NT emacs. You get below error when starting shell as cygwin from Win NT emacs . . .

--- Comments_on_setup-cygwin.el
+++ Comments_on_setup-cygwin.el
@@ -13,3 +13,13 @@ Although the current help info for defgroup (24.3.1) doesn't specify this as a r
 cygwin-mount.el defgroups <code>cygwin-mount</code> and places it in the <code>files</code> group.
 
 setup-cygwin, however, seems more appropriately placed in the <code>Processes</code> group.
+
+----
+
+This seems to be broken with recent version of cygwin and GNU NT emacs.
+You get below error when starting shell as cygwin from Win NT emacs
+
+cygwin bash: cannot set terminal process group (-1): inappropriate ioctl for device
+
+-- Anonymous 2015-12-07 16:19 UTC
+
