141e45936ef858886d83841a2f031b7be906eb79
DrewAdams <anybody@emacswiki.org>
2015-12-31 22:21:06 +0100

    no summary available

--- frame+.el
+++ frame+.el
@@ -4,13 +4,13 @@
 ;; Description: Extensions to `frame.el'.
 ;; Author: Drew Adams
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
-;; Copyright (C) 1996-2015, Drew Adams, all rights reserved.
+;; Copyright (C) 1996-2016, Drew Adams, all rights reserved.
 ;; Created: Fri Apr 12 16:42:12 1996
 ;; Version: 0
 ;; Package-Requires: ()
-;; Last-Updated: Thu Jan  1 10:44:33 2015 (-0800)
+;; Last-Updated: Thu Dec 31 13:26:18 2015 (-0800)
 ;;           By: dradams
-;;     Update #: 263
+;;     Update #: 264
 ;; URL: http://www.emacswiki.org/frame+.el
 ;; Doc URL: http://emacswiki.org/OneOnOneEmacs
 ;; Keywords: frames
