b59f294ddfa1bc177d07a0a88942d141971fbf16
DrewAdams <anybody@emacswiki.org>
2015-12-31 21:14:36 +0100

    no summary available

--- bookmark+-bmu.el
+++ bookmark+-bmu.el
@@ -4,12 +4,12 @@
 ;; Description: Bookmark+ code for the `*Bookmark List*' (bmenu).
 ;; Author: Drew Adams, Thierry Volpiatto
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
-;; Copyright (C) 2000-2015, Drew Adams, all rights reserved.
+;; Copyright (C) 2000-2016, Drew Adams, all rights reserved.
 ;; Copyright (C) 2009, Thierry Volpiatto, all rights reserved.
 ;; Created: Mon Jul 12 09:05:21 2010 (-0700)
-;; Last-Updated: Sat Nov  7 09:39:32 2015 (-0700)
+;; Last-Updated: Thu Dec 31 12:19:48 2015 (-0800)
 ;;           By: dradams
-;;     Update #: 3746
+;;     Update #: 3747
 ;; URL: http://www.emacswiki.org/bookmark+-bmu.el
 ;; Doc URL: http://www.emacswiki.org/BookmarkPlus
 ;; Keywords: bookmarks, bookmark+, placeholders, annotations, search, info, url, w3m, gnus
