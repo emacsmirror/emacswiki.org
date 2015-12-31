8f90fab12ab1515d40d47f34c040979fcba87df2
DrewAdams <anybody@emacswiki.org>
2015-12-31 22:30:39 +0100

    no summary available

--- help-macro+.el
+++ help-macro+.el
@@ -4,13 +4,13 @@
 ;; Description: Extensions to `help-macro.el'.
 ;; Author: Drew Adams
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
-;; Copyright (C) 1999-2015, Drew Adams, all rights reserved.
+;; Copyright (C) 1999-2016, Drew Adams, all rights reserved.
 ;; Created: Tue Aug 24 15:36:18 1999
 ;; Version: 0
 ;; Package-Requires: ()
-;; Last-Updated: Thu Jan  1 10:50:17 2015 (-0800)
+;; Last-Updated: Thu Dec 31 13:35:42 2015 (-0800)
 ;;           By: dradams
-;;     Update #: 123
+;;     Update #: 124
 ;; URL: http://www.emacswiki.org/help-macro+.el
 ;; Doc URL: http://emacswiki.org/HelpPlus
 ;; Keywords: help
