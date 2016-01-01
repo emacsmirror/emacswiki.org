81b38c80adbca40abb39bdd9f3151e8c2b27aaee
DrewAdams <anybody@emacswiki.org>
2016-01-01 01:06:11 +0100

    no summary available

--- start.el
+++ start.el
@@ -4,13 +4,13 @@
 ;; Description: Main Emacs startup file: require/autoload other files.
 ;; Author: Drew Adams
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
-;; Copyright (C) 1995-2015, Drew Adams, all rights reserved.
+;; Copyright (C) 1995-2016, Drew Adams, all rights reserved.
 ;; Created: Wed Aug  2 11:12:24 1995
 ;; Version: 0
 ;; Package-Requires: ()
-;; Last-Updated: Sun Sep 20 11:51:36 2015 (-0700)
+;; Last-Updated: Thu Dec 31 16:11:37 2015 (-0800)
 ;;           By: dradams
-;;     Update #: 3060
+;;     Update #: 3061
 ;; URL: http://www.emacswiki.org/start.el
 ;; Keywords: abbrev, internal, local, init
 ;; Compatibility: GNU Emacs: 20.x, 21.x, 22.x, 23.x, 24.x, 25.x
