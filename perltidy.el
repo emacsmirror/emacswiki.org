d1ebe8bf0dd391a96f7fd639cb66cf6075ca1e77
qsimpleq <anybody@emacswiki.org>
2016-01-04 09:34:33 +0100

    Reworked version. Added tramp interaction,            perltidy-program-params            perltidy-dwim-safe fix sub detection

--- perltidy.el
+++ perltidy.el
@@ -1,11 +1,11 @@
 ;;; perltidy.el --- Tidy perl code
 
-;; Copyright (C) 2007 Free Software Foundation, Inc.
+;; Copyright (C) 2007-2015 Free Software Foundation, Inc.
 ;;
 ;; Author: Ye Wenbin <wenbinye@gmail.com>
-;; Maintainer: Ye Wenbin <wenbinye@gmail.com>
+;; Maintainer: Kirill Babikhin <mrakobes86reg@yandex.ru>
 ;; Created: 22 Dec 2007
-;; Version: 0.01
+;; Version: 0.04
 ;; Keywords: tools, convenience, languages
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -27,31 +27,31 @@
 ;; As the PBP(Perl Best Practice) suggest, put this to your ~/.perltidyrc:
 ;; ## .perltidyrc --- configuration for perltidy
 ;; # Max line width is 78 cols
-;; -l=78   
+;; -l=78
 ;; # Indent level is 4 cols
-;; -i=4    
+;; -i=4
 ;; # Continuation indent is 4 cols
-;; -ci=4   
+;; -ci=4
 ;; # Output to STDOUT
-;; -st     
+;; -st
 ;; # Errors to STDERR
-;; -se     
+;; -se
 ;; # Maximal vertical tightness
-;; -vt=2   
+;; -vt=2
 ;; # No extra indentation for closing brackets
-;; -cti=0  
+;; -cti=0
 ;; # Medium parenthesis tightness
-;; -pt=1   
+;; -pt=1
 ;; # Medium brace tightness
-;; -bt=1   
+;; -bt=1
 ;; # Medium square bracket tightness
-;; -sbt=1  
+;; -sbt=1
 ;; # Medium block brace tightness
-;; -bbt=1  
+;; -bbt=1
 ;; # No space before semicolons
-;; -nsfs   
+;; -nsfs
 ;; # Don't outdent long quoted strings
-;; -nolq   
+;; -nolq
 ;; # Break before all operators
 ;; -wbb="% + - * / x != == >= <= =~ !~ < > | & >= < = **= += *= &= <<= &&= -= /= |= >>= ||= .= %= ^= x="
 
@@ -62,50 +62,106 @@
 
 (eval-when-compile
   (require 'cl))
-(defvar perltidy-program "perltidy"
-  "*Program name of perltidy")
-(defvar perltidy-buffer-name "*perltidy*"
-  "*Name of the temporary perltidy buffer.")
-(defvar perltidy-map
-  (let ((map (make-sparse-keymap)))
-    (define-key map "\C-x\C-s" 'perltidy-write)
-    map)
-  "*keymap for *perltidy* buffer.")
 
-(defvar perltidy-last-buffer nil
-  "Internal variable.")
+(defgroup perltidy nil
+  "Tidy perl code using perltidy"
+  :group 'tools
+  :group 'pde)
+
+(defcustom perltidy-program "perltidy"
+  "*Program name of perltidy"
+  :type 'string
+  :group 'perltidy)
+
+(defcustom perltidy-program-params
+  '(;; I/O control
+    "--standard-output"
+    "--standard-error-output"
+    "--force-read-binary"
+    "--quiet"
+
+    ;; FORMATTING OPTIONS
+    "--no-check-syntax"
+    )
+  "*perltidy run options"
+  :type 'list
+  :group 'perltidy)
+
+(defcustom perltidy-rcregex "\\.perltidyrc"
+  "perltidyrc file regex"
+  :type 'string
+  :group 'perltidy)
 
 (defmacro perltidy-save-point (&rest body)
   (declare (indent 0) (debug t))
   `(let ((old-point (point)))
      ,@body
      (goto-char old-point)))
-    
+
+;;;###autoload
 (defun perltidy-region (beg end)
   "Tidy perl code in the region."
   (interactive "r")
+  (or (get 'perltidy-program 'has-perltidy)
+      (if (executable-find perltidy-program)
+          (put 'perltidy-program 'has-perltidy t)
+        (error "Seem perltidy is not installed")))
   (perltidy-save-point
-    (call-process-region beg end perltidy-program t t)))
 
+    (let ((old-perltidy-env (getenv "PERLTIDY"))
+          (remote? (tramp-tramp-file-p buffer-file-name))
+          (perltidyrc (perltidy-find-perltidyrc buffer-file-truename))
+          (pertidyrc-remote (expand-file-name "perltidyrc-remote" temporary-file-directory))
+          (perltidy-run-list perltidy-program-params)
+          )
+
+      (if (and (boundp 'remote?)
+               perltidyrc)
+          (progn
+            (tramp-sh-handle-copy-file perltidyrc pertidyrc-remote t)
+            (setq perltidyrc pertidyrc-remote)
+            (setq perltidy-run-list
+                  (append perltidy-run-list
+                          (list (concat "-pro=" pertidyrc-remote))))))
+
+      (apply #'call-process-region
+             (append (list beg end perltidy-program
+                           t
+                           t
+                           t
+                           )
+                     perltidy-run-list)))
+    t))
+
+;;;###autoload
 (defun perltidy-buffer ()
   "Call perltidy for whole buffer."
   (interactive)
   (perltidy-region (point-min) (point-max)))
 
+;;;###autoload
 (defun perltidy-subroutine ()
   "Call perltidy for subroutine at point."
   (interactive)
-  (perltidy-region (progn (beginning-of-defun) (point))
-                   (progn (end-of-defun) (point))))
 
-;;;###autoload 
-(defun perltidy-dwim (arg)
-  "Perltidy Do What I Mean.
-If with prefix argument, just show the result of perltidy.
-You can use C-x C-s to save the tidy result.
-If region is active call perltidy on the region. If inside
-subroutine, call perltidy on the subroutine, otherwise call
-perltidy for whole buffer."
+  (save-excursion
+    (let ((current-point (point))
+          b e)
+      (setq b (progn (beginning-of-defun) (point)))
+      (when (and
+             (looking-at "\\s-*sub\\s-+")
+             (< b current-point)
+             (> (save-excursion
+                  (setq e (progn (end-of-defun) (point))))
+                current-point))
+        (perltidy-region b e)))))
+
+;;;###autoload
+(defun perltidy-dwim-safe (arg)
+  "Perltidy Do What I Mean safe.
+If region is active call perltidy on the region.
+If inside subroutine, call perltidy on the subroutine,
+otherwise stop."
   (interactive "P")
   (let ((buf (current-buffer))
         beg end)
@@ -113,43 +169,80 @@ perltidy for whole buffer."
            (setq beg (region-beginning)
                  end (region-end)))
           ((save-excursion
-             (beginning-of-defun)
-             (when (looking-at "\\s-*sub\\s-+")
-               (setq beg (point)
-                     end (progn (end-of-defun) (point))))))
+             (let ((current-point (point))
+                   b e)
+               (setq b (progn (beginning-of-defun) (point)))
+               (when (and
+                      (looking-at "\\s-*sub\\s-+")
+                      (< b current-point)
+                      (> (save-excursion
+                           (setq e (progn (end-of-defun) (point))))
+                         current-point))
+                 (setq beg b
+                       end e)))))
+          (t (setq beg nil
+                   end nil)))
+    (if (and beg
+             end)
+        (progn
+          (perltidy-region beg end)
+          (font-lock-fontify-buffer)))))
+
+;;;###autoload
+(defun perltidy-dwim (arg)
+  "Perltidy Do What I Mean.
+If region is active call perltidy on the region.
+If inside subroutine, call perltidy on the subroutine,
+otherwise call perltidy for whole buffer."
+  (interactive "P")
+  (let ((buf (current-buffer))
+        beg end)
+    (cond ((and mark-active transient-mark-mode)
+           (setq beg (region-beginning)
+                 end (region-end)))
+          ((save-excursion
+             (let ((current-point (point))
+                   b e)
+               (setq b (progn (beginning-of-defun) (point)))
+               (when (and
+                      (looking-at "\\s-*sub\\s-+")
+                      (< b current-point)
+                      (> (save-excursion
+                           (setq e (progn (end-of-defun) (point))))
+                         current-point))
+                 (setq beg b
+                       end e)))))
           (t (setq beg (point-min)
                    end (point-max))))
-    (when arg
-      (set-buffer (get-buffer-create perltidy-buffer-name))
-      (erase-buffer)
-      (insert (with-current-buffer buf
-                (buffer-substring beg end)))
-      (setq perltidy-last-buffer (list buf beg end))
-      (setq beg (point-min)
-            end (point-max))
-      (perl-mode)
-      (use-local-map perltidy-map)
-      (pop-to-buffer (current-buffer))
-      (message "Press C-x C-s to apply the tidy result."))
-    (perltidy-region beg end)))
+    (perltidy-region beg end)
+    (font-lock-fontify-buffer)))
 
-(defun perltidy-write ()
-  (interactive)
-  (if perltidy-last-buffer
-      (let ((buf (get-buffer perltidy-buffer-name)))
-        (if (buffer-live-p buf)
-            (if (buffer-live-p (car perltidy-last-buffer))
-                (with-current-buffer (car perltidy-last-buffer)
-                  (goto-char (cadr perltidy-last-buffer))
-                  (perltidy-save-point
-                    (delete-region (cadr perltidy-last-buffer)
-                                   (nth 2 perltidy-last-buffer)))
-                  (insert-buffer-substring buf)
-                  ;; don't write again
-                  (setq perltidy-last-buffer nil))
-              (error "The perltidy associated buffer is not exists!"))
-          (error "No perltidy buffer exists!")))
-    (message "Do you forget perform perltidy or already write to the associated buffer")))
+(defun perltidy-find-perltidyrc (&optional dir rcregex)
+  (unless dir (setq dir (buffer-file-name)))
+  (unless rcregex (setq rcregex perltidy-rcregex))
+  (setq dir (file-name-directory dir))
+
+  (let (rcfile)
+    (catch 'my-tag
+      (locate-dominating-file
+       dir
+       (lambda (parent)
+         (let ((rc (car (ignore-errors (directory-files parent t rcregex))))
+               (pparent (file-name-directory (directory-file-name parent))))
+           (setq rcfile rc)
+           (cond ((equal parent
+                         pparent)
+                  (if (= (length rc) 0)
+                      (throw 'my-tag rc)
+                    (throw 'my-tag nil)))
+
+                 ((and (= (length rc) 0)
+                       (file-exists-p    (expand-file-name "lib" pparent))
+                       (file-directory-p (expand-file-name "lib" pparent)))
+                  (setq rcfile (car (ignore-errors (directory-files pparent t rcregex))))
+                  (throw 'my-tag rcfile))
+                 (t rc))))))
+    rcfile))
 
 (provide 'perltidy)
 ;;; perltidy.el ends here
