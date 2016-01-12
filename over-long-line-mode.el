4505c818e7f178cd0df4ef04152363012b1f0de8
Anonymous <anybody@emacswiki.org>
2016-01-12 04:38:14 +0100

    no summary available

--- over-long-line-mode.el
+++ over-long-line-mode.el
@@ -1,118 +1,150 @@
-#FILE text/x-emacs-lisp 
-OzsgZGVidWctb24tZW50cnkgJ3NldC1hdXRvLW1vZGUgdG8gc2VlIGhvdyB0aGlzIGlzIGZhaWxp
-bmcgdG8gZG8KOzsgdGhlIGV4cGVjdGVkIHRoaW5nIHdoZW4gYSBtb2RlOiBsb2NhbCB2YXJpYWJs
-ZSBpcyBjb25maWd1cmVkLgo7OyBJICp0aGluayogdGhhdCB3aGVuIHRoZSBzZWNvbmQgcm91bmQg
-b2YgaGFjay1sb2NhbC12YXJpYWJsZXMgaXMKOzsgY2FsbGVkLCB3ZSBlbmQgdXAgcHJvY2Vzc2lu
-ZyB0aGUgbW9kZTogc3BlY2lmaWNhdGlvbiBhIHNlY29uZAo7OyB0aW1lPyBOb3QgY2VydGFpbi4g
-TmVlZHMgdG8gYmUgZGVidWdnZWQuCjs7Cjs7IChlZGl0OiBJIGhhdmUgc2luY2UgcmVtb3ZlZCB0
-aGUgYWR2aWNlIHRvIGhhY2stbG9jYWwtdmFyaWFibGVzLAo7OyBzbyB0aGUgYWJvdmUgbWF5IG5v
-IGxvbmdlciBiZSBhIGdvaW5nIGNvbmNlcm4uKQo7Owo7OyBBbHNvOiBmaWd1cmUgb3V0IHdoYXQg
-dG8gc3VibWl0IHRvIEZTRiBmb3IgdGhpcy4KCgoKCjs7OyBvdmVyLWxvbmctbGluZS1tb2RlLmVs
-IC0gU3RvcHMgbWluaWZpZWQgY29kZSBicmluZ2luZyBFbWFjcyB0byBpdHMga25lZXMuCjs7Cjs7
-IEF1dGhvcjogUGhpbCBTLgo7OyBVUkw6IGh0dHA6Ly93d3cuZW1hY3N3aWtpLm9yZy9lbWFjcy9P
-dmVyTG9uZ0xpbmVNb2RlCjs7IFZlcnNpb246IDAuMQoKOzsgQ29tbWVudGFyeToKOzsKOzsgTWFu
-eSBFbWFjcyBtb2RlcyBzdHJ1Z2dsZSB3aXRoIGJ1ZmZlcnMgd2hpY2ggY29udGFpbiBleGNlc3Np
-dmVseSBsb25nIGxpbmVzLAo7OyBhbmQgbWF5IGNvbnNlcXVlbnRseSBjYXVzZSB1bmFjY2VwdGFi
-bGUgcGVyZm9ybWFuY2UgaXNzdWVzLgo7Owo7OyBUaGlzIGlzIGNvbW1vbmx5IG9uIGFjY291bnQg
-b2YgJ21pbmlmaWVkJyBjb2RlIChpLmUuIGNvZGUgaGFzIGJlZW4gY29tcGFjdGVkCjs7IGludG8g
-dGhlIHNtYWxsZXN0IGZpbGUgc2l6ZSBwb3NzaWJsZSwgd2hpY2ggb2Z0ZW4gZW50YWlscyByZW1v
-dmluZyBuZXdsaW5lcwo7OyBzaG91bGQgdGhleSBub3QgYmUgc3RyaWN0bHkgbmVjZXNzYXJ5KS4g
-VGhlc2Uga2luZHMgb2YgZmlsZXMgYXJlIHR5cGljYWxseQo7OyBub3QgaW50ZW5kZWQgdG8gYmUg
-ZWRpdGVkLCBzbyBub3QgcHJvdmlkaW5nIHRoZSB1c3VhbCBlZGl0aW5nIG1vZGUgaW4gdGhlc2UK
-OzsgY2FzZXMgd2lsbCByYXJlbHkgYmUgYW4gaXNzdWUuCjs7Cjs7IFdoZW4gc3VjaCBmaWxlcyBh
-cmUgZGV0ZWN0ZWQsIHdlIGludm9rZSBgb3Zlci1sb25nLWxpbmUtbW9kZScuIFRoaXMgbW9kZSBp
-cwo7OyBhbG1vc3QgZXF1aXZhbGVudCB0byBgZnVuZGFtZW50YWwtbW9kZScsIGFuZCBoZW5jZSBo
-YXMgYSBtaW5pbWFsIGFmZmVjdCBvbgo7OyBwZXJmb3JtYW5jZSBpbiB0aGUgYnVmZmVyLgo7Owo7
-OyBUaGUgdmFyaWFibGVzIGBvdmVyLWxvbmctbGluZS1tb2RlLXRhcmdldC1tb2RlcycsIGBvdmVy
-LWxvbmctbGluZS10aHJlc2hvbGQnLAo7OyBgb3Zlci1sb25nLWxpbmUtbWF4LWxpbmVzJywgYW5k
-IGBvdmVyLWxvbmctbGluZS1tb2RlLWluaGliaXRlZCcgZGV0ZXJtaW5lCjs7IHdoZXRoZXIgdGhp
-cyBtb2RlIHdpbGwgYmUgaW52b2tlZCBmb3IgYSBnaXZlbiBmaWxlLiAgVGhlIHRlc3RzIGFyZSBt
-YWRlIGFmdGVyCjs7IGBzZXQtYXV0by1tb2RlJyBoYXMgc2V0IHRoZSBub3JtYWwgbWFqb3IgbW9k
-ZS4KCjs7OyBDaGFuZ2Vsb2c6Cjs7Cjs7IDAuMSAtIEluaXRpYWwgcmVsZWFzZSB0byBFbWFjc1dp
-a2kuCgo7OzsgQ29kZToKCihkZWZ2YXIgb3Zlci1sb25nLWxpbmUtbW9kZS10YXJnZXQtbW9kZXMK
-ICAnKHByb2ctbW9kZSBjc3MtbW9kZSkKICAiYG92ZXItbG9uZy1saW5lLW1vZGUnIGFmZmVjdHMg
-b25seSB0aGVzZSBtb2RlcyBhbmQgdGhlaXIgZGVyaXZhdGl2ZXMuCgpPdXIgcHJpbWFyeSB1c2Ut
-Y2FzZSBpcyBtaW5pZmllZCBwcm9ncmFtbWluZyBjb2RlLCBzbyBgcHJvZy1tb2RlJyBjb3ZlcnMK
-bW9zdCBjYXNlcywgYnV0IHRoZXJlIGFyZSBzb21lIGV4Y2VwdGlvbnMgdG8gdGhpcy4iKQoKKGRl
-ZnZhciBvdmVyLWxvbmctbGluZS10aHJlc2hvbGQgMjUwCiAgIk51bWJlciBvZiBjb2x1bW5zIGFm
-dGVyIHdoaWNoIHRoZSBub3JtYWwgbW9kZSBmb3IgYSBmaWxlIHdpbGwgbm90IGJlCnVzZWQsIHVu
-bGVzcyBpdCBpcyBzcGVjaWZpZWQgYXMgYSBsb2NhbCB2YXJpYWJsZS4KCmBvdmVyLWxvbmctbGlu
-ZS1tb2RlJyB3aWxsIGJlIHVzZWQgaW5zdGVhZCBpbiB0aGVzZSBjaXJjdW1zdGFuY2VzLgoKU2Vl
-IGBvdmVyLWxvbmctbGluZS1kZXRlY3RlZC1wJyBmb3IgZGV0YWlscy4iKQoKKGRlZnZhciBvdmVy
-LWxvbmctbGluZS1tYXgtbGluZXMgMjAKICAiTnVtYmVyIG9mIG5vbi1ibGFuaywgbm9uLWNvbW1l
-bnQgbGluZXMgdG8gdGVzdCBmb3IgZXhjZXNzaXZlIGxlbmd0aC4KClNlZSBgb3Zlci1sb25nLWxp
-bmUtZGV0ZWN0ZWQtcCcgZm9yIGRldGFpbHMuIikKCihkZWZ2YXIgb3Zlci1sb25nLWxpbmUtbW9k
-ZS1pbmhpYml0ZWQgbmlsCiAgIlNldCBub24tbmlsIHRvIHByZXZlbnQgYG92ZXItbG9uZy1saW5l
-LW1vZGUnIGZyb20gYmVpbmcgdHJpZ2dlcmVkLgoKTWF5IGJlIHVzZWQgZ2xvYmFsbHkgb3IgYnVm
-ZmVyLWxvY2FsbHkgYXMgcmVxdWlyZWQuIikKCihkZWZ2YXItbG9jYWwgb3Zlci1sb25nLWxpbmUt
-b3JpZ2luYWwtbW9kZSBuaWwKICAiU3RvcmVzIHRoZSBvcmlnaW5hbCBgbWFqb3ItbW9kZScgdmFs
-dWUuIikKKHB1dCAnb3Zlci1sb25nLWxpbmUtb3JpZ2luYWwtbW9kZSAncGVybWFuZW50LWxvY2Fs
-IHQpCgooZGVmdW4gb3Zlci1sb25nLWxpbmUtZGV0ZWN0ZWQtcCAoKQogICJGb2xsb3dpbmcgYW55
-IGluaXRpYWwgY29tbWVudHMgYW5kIGJsYW5rIGxpbmVzLCB0aGUgbmV4dCBOIGxpbmVzIG9mIHRo
-ZQpidWZmZXIgd2lsbCBiZSB0ZXN0ZWQgZm9yIGV4Y2Vzc2l2ZSBsZW5ndGggKHdoZXJlIFwiZXhj
-ZXNzaXZlXCIgbWVhbnMgYWJvdmUKYG92ZXItbG9uZy1saW5lLXRocmVzaG9sZCcsIGFuZCBOIGlz
-IGBvdmVyLWxvbmctbGluZS1tYXgtbGluZXMnKS4KClJldHVybnMgbm9uLW5pbCBpZiBhbnkgc3Vj
-aCBleGNlc3NpdmUtbGVuZ3RoIGxpbmUgaXMgZGV0ZWN0ZWQuIgogIChsZXQgKChjb3VudCAwKSkK
-ICAgIChzYXZlLWV4Y3Vyc2lvbgogICAgICAoZ290by1jaGFyIChwb2ludC1taW4pKQogICAgICAo
-d2hpbGUgKGNvbW1lbnQtZm9yd2FyZCkpIDs7IGNsZWFycyB3aGl0ZXNwYWNlIGF0IG1pbmltdW0K
-ICAgICAgKGNhdGNoICdleGNlc3NpdmUKICAgICAgICAod2hpbGUgKDwgY291bnQgb3Zlci1sb25n
-LWxpbmUtbWF4LWxpbmVzKQogICAgICAgICAgKGlmICg+ICgtIChsaW5lLWVuZC1wb3NpdGlvbiAx
-KSAocG9pbnQpKQogICAgICAgICAgICAgICAgIG92ZXItbG9uZy1saW5lLXRocmVzaG9sZCkKICAg
-ICAgICAgICAgICAodGhyb3cgJ2V4Y2Vzc2l2ZSB0KQogICAgICAgICAgICAoZm9yd2FyZC1saW5l
-KQogICAgICAgICAgICAoc2V0cSBjb3VudCAoMSsgY291bnQpKSkpKSkpKQoKKGRlZmluZS1kZXJp
-dmVkLW1vZGUgb3Zlci1sb25nLWxpbmUtbW9kZSBuaWwgIk92ZXItbG9uZyBsaW5lcyIKICAiVGhp
-cyBtb2RlIGlzIHVzZWQgaWYgbGluZSBsZW5ndGhzIGV4Y2VlZCBgb3Zlci1sb25nLWxpbmUtdGhy
-ZXNob2xkJy4KCk1hbnkgRW1hY3MgbW9kZXMgc3RydWdnbGUgd2l0aCBidWZmZXJzIHdoaWNoIGNv
-bnRhaW4gZXhjZXNzaXZlbHkgbG9uZyBsaW5lcywKYW5kIG1heSBjb25zZXF1ZW50bHkgY2F1c2Ug
-dW5hY2NlcHRhYmxlIHBlcmZvcm1hbmNlIGlzc3Vlcy4KClRoaXMgaXMgY29tbW9ubHkgb24gYWNj
-b3VudCBvZiAnbWluaWZpZWQnIGNvZGUgKGkuZS4gY29kZSBoYXMgYmVlbiBjb21wYWN0ZWQKaW50
-byB0aGUgc21hbGxlc3QgZmlsZSBzaXplIHBvc3NpYmxlLCB3aGljaCBvZnRlbiBlbnRhaWxzIHJl
-bW92aW5nIG5ld2xpbmVzCnNob3VsZCB0aGV5IG5vdCBiZSBzdHJpY3RseSBuZWNlc3NhcnkpLiBU
-aGVzZSBraW5kcyBvZiBmaWxlcyBhcmUgdHlwaWNhbGx5Cm5vdCBpbnRlbmRlZCB0byBiZSBlZGl0
-ZWQsIHNvIG5vdCBwcm92aWRpbmcgdGhlIHVzdWFsIGVkaXRpbmcgbW9kZSBpbiB0aGVzZQpjYXNl
-cyB3aWxsIHJhcmVseSBiZSBhbiBpc3N1ZS4KCldoZW4gc3VjaCBmaWxlcyBhcmUgZGV0ZWN0ZWQs
-IHdlIGludm9rZSB0aGlzIG1vZGUuIFRoaXMgaGFwcGVucyBhZnRlcgpgc2V0LWF1dG8tbW9kZScg
-aGFzIHNldCB0aGUgbWFqb3IgbW9kZSwgc2hvdWxkIHRoZSBzZWxlY3RlZCBtYWpvciBtb2RlIGJl
-IGEKbWVtYmVyIChvciBkZXJpdmF0aXZlIG9mIGEgbWVtYmVyKSBvZiBgb3Zlci1sb25nLWxpbmUt
-bW9kZS10YXJnZXQtbW9kZXMnLgoKQnkgZGVmYXVsdCB0aGlzIG1vZGUgaXMgZXNzZW50aWFsbHkg
-ZXF1aXZhbGVudCB0byBgZnVuZGFtZW50YWwtbW9kZScsIGFuZApleGlzdHMgbWFpbmx5IHRvIHBy
-b3ZpZGUgaW5mb3JtYXRpb24gdG8gdGhlIHVzZXIgYXMgdG8gd2h5IHRoZSBleHBlY3RlZCBtb2Rl
-CndhcyBub3QgdXNlZC4KClRvIHJldmVydCB0byB0aGUgb3JpZ2luYWwgbW9kZSBkZXNwaXRlIGFu
-eSBwb3RlbnRpYWwgcGVyZm9ybWFuY2UgaXNzdWVzLAp0eXBlIFxcW292ZXItbG9uZy1saW5lLW1v
-ZGUtcmV2ZXJ0XS4KCkFsdGVybmF0aXZlbHksIHRoZSBub3JtYWwgbW9kZSBjYW4gYmUgbWFudWFs
-bHkgaW52b2tlZCwgb3IgYSBmaWxlLWxvY2FsCmBvdmVyLWxvbmctbGluZS1tb2RlLWluaGliaXRl
-ZCcgbWF5IGJlIHNldCBub24tbmlsIHRvIGVuc3VyZSB0aGF0CnRoZSBub3JtYWwgbW9kZSBpcyBh
-bHdheXMgdXNlZCBmb3IgdGhlIGZpbGUgaW4gcXVlc3Rpb24uIgogIDs7IFRPRE86CiAgOzsgZGVm
-aW5lL3Byb2Nlc3MgYWxpc3Qgb2YgbW9kZSBsaXN0cyAoZm9yIGRlcml2ZWQtbW9kZS1wKSBtYXBw
-ZWQgdG8KICA7OyBiZWhhdmlvdXJzLiBpbmNsdWRlIHR5cGljYWwgdXNlIG9mIHN5bWJvbCBmb3Ig
-Y2FsbGJhY2s7IG90aGVyd2lzZQogIDs7IHNvbWUgc3RhbmRhcmQgdGhpbmdzIHN1Y2ggYXMgZGlz
-YWJsaW5nIGZvbnQtbG9jay1tb2RlLiBQZXJoYXBzIGl0CiAgOzsgc2hvdWxkIGp1c3QgYmUgYSBi
-b2R5IG9mIGZvcm1zIHRvIGV2YWx1YXRlLCBmb3IgbWF4aW11bSBmbGV4aWJpbGl0eT8KICA7OyBi
-dXQgdGhlbiBzeW1ib2w9PmNhbGxiYWNrIGVmZmVjdGl2ZWx5IGNvdmVycyB0aGF0LgogIChzZXRx
-IGZvbnQtbG9jay1tb2RlIDApCiAgKG1lc3NhZ2UgIkNoYW5nZWQgdG8gJXMgKGZyb20gJXMpIG9u
-IGFjY291bnQgb2YgbGluZSBsZW5ndGguICVzIHRvIHVuZG8uIgogICAgICAgICAgIG1ham9yLW1v
-ZGUKICAgICAgICAgICBvdmVyLWxvbmctbGluZS1vcmlnaW5hbC1tb2RlCiAgICAgICAgICAgKHN1
-YnN0aXR1dGUtY29tbWFuZC1rZXlzICJcXFtvdmVyLWxvbmctbGluZS1tb2RlLXJldmVydF0iKSkp
-CgooZGVmdW4gb3Zlci1sb25nLWxpbmUtbW9kZS1yZXZlcnQgKCkKICAiQ2FsbCB0aGUgYG1ham9y
-LW1vZGUnIHdoaWNoIHdhcyBzZWxlY3RlZCBieSBgc2V0LWF1dG8tbW9kZScKYmVmb3JlIGBvdmVy
-LWxvbmctbGluZS1tb2RlJyB3YXMgY2FsbGVkIHRvIHJlcGxhY2UgaXQuIgogIChpbnRlcmFjdGl2
-ZSkKICAoaWYgKGJvdW5kLWFuZC10cnVlLXAgb3Zlci1sb25nLWxpbmUtb3JpZ2luYWwtbW9kZSkK
-ICAgICAgKGZ1bmNhbGwgb3Zlci1sb25nLWxpbmUtb3JpZ2luYWwtbW9kZSkKICAgIChlcnJvciAi
-T3JpZ2luYWwgbW9kZSB1bmtub3duLiIpKSkKCihkZWZpbmUta2V5IG92ZXItbG9uZy1saW5lLW1v
-ZGUtbWFwIChrYmQgIkMtYyBDLWMiKQogICdvdmVyLWxvbmctbGluZS1tb2RlLXJldmVydCkKCihk
-ZWZhZHZpY2Ugc2V0LWF1dG8tbW9kZSAoYWZ0ZXIgb3Zlci1sb25nLWxpbmUtbW9kZS0tc2V0LWF1
-dG8tbW9kZSkKICAiTWF5YmUgdXNlIGBvdmVyLWxvbmctbGluZS1tb2RlJyBmb3IgZmlsZXMgd2l0
-aCB2ZXJ5IGxvbmcgbGluZXMuCgpUaGlzIGFkdmljZSBhY3RzIGFmdGVyIGBzZXQtYXV0by1tb2Rl
-JyBoYXMgbWFkZSBpdHMgZGVjaXNpb24uCgpXZSBjYW4ndCBzZW5zaWJsZSBhY3QgYmVmb3JlIHRo
-aXMgcG9pbnQsIGFzIHNvbWUgbWFqb3IgbW9kZXMgbXVzdApiZSBleGVtcHQgZnJvbSBgb3Zlci1s
-b25nLWxpbmUtbW9kZScgKGJpbmFyeSBmaWxlIG1vZGVzLCBmb3IKZXhhbXBsZSkuICBJbnN0ZWFk
-IG9ubHkgYWN0IHdoZW4gdGhlIHNlbGVjdGVkIG1ham9yIG1vZGUgaXMgYSBtZW1iZXIKb2YgYG92
-ZXItbG9uZy1saW5lLW1vZGUtdGFyZ2V0LW1vZGVzJywgb3IgYSBkZXJpdmF0aXZlIHRoZXJlb2Yu
-CgpJZiBidWZmZXItbG9jYWwgYG92ZXItbG9uZy1saW5lLW1vZGUtaW5oaWJpdGVkJyBpcyBub24t
-bmlsLCB0aGVuCmRvIG5vdGhpbmcuIgogICh1bmxlc3MgKGJvdW5kLWFuZC10cnVlLXAgb3Zlci1s
-b25nLWxpbmUtbW9kZS1pbmhpYml0ZWQpCiAgICAod2hlbiAoYW5kIChhcHBseSAnZGVyaXZlZC1t
-b2RlLXAgb3Zlci1sb25nLWxpbmUtbW9kZS10YXJnZXQtbW9kZXMpCiAgICAgICAgICAgICAgIChv
-dmVyLWxvbmctbGluZS1kZXRlY3RlZC1wKSkKICAgICAgKHNldHEgb3Zlci1sb25nLWxpbmUtb3Jp
-Z2luYWwtbW9kZSBtYWpvci1tb2RlKQogICAgICAob3Zlci1sb25nLWxpbmUtbW9kZSkpKSkKKGFk
-LWFjdGl2YXRlICdzZXQtYXV0by1tb2RlKQoKKHByb3ZpZGUgJ292ZXItbG9uZy1saW5lLW1vZGUp
-Cgo7Ozsgb3Zlci1sb25nLWxpbmUtbW9kZS5lbCBlbmRzIGhlcmUK
+;;; over-long-line-mode.el - Stops minified code bringing Emacs to its knees.
+;;
+;; Author: Phil S.
+;; URL: http://www.emacswiki.org/emacs/OverLongLineMode
+;; Version: 0.1
+
+;; Commentary:
+;;
+;; Many Emacs modes struggle with buffers which contain excessively long lines,
+;; and may consequently cause unacceptable performance issues.
+;;
+;; This is commonly on account of 'minified' code (i.e. code has been compacted
+;; into the smallest file size possible, which often entails removing newlines
+;; should they not be strictly necessary). These kinds of files are typically
+;; not intended to be edited, so not providing the usual editing mode in these
+;; cases will rarely be an issue.
+;;
+;; When such files are detected, we invoke `over-long-line-mode'. This mode is
+;; almost equivalent to `fundamental-mode', and hence has a minimal affect on
+;; performance in the buffer.
+;;
+;; The variables `over-long-line-mode-target-modes', `over-long-line-threshold',
+;; `over-long-line-max-lines', and `over-long-line-mode-inhibited' determine
+;; whether this mode will be invoked for a given file.  The tests are made after
+;; `set-auto-mode' has set the normal major mode.
+
+;;; Changelog:
+;;
+;; 0.1 - Initial release to EmacsWiki.
+
+;;; Code:
+
+(defvar over-long-line-mode-target-modes
+  '(prog-mode css-mode)
+  "`over-long-line-mode' affects only these modes and their derivatives.
+
+Our primary use-case is minified programming code, so `prog-mode' covers
+most cases, but there are some exceptions to this.")
+
+(defvar over-long-line-threshold 250
+  "Number of columns after which the normal mode for a file will not be
+used, unless it is specified as a local variable.
+
+`over-long-line-mode' will be used instead in these circumstances.
+
+See `over-long-line-detected-p' for details.")
+
+(defvar over-long-line-max-lines 20
+  "Number of non-blank, non-comment lines to test for excessive length.
+
+See `over-long-line-detected-p' for details.")
+
+(defvar over-long-line-mode-inhibited nil
+  "Set non-nil to prevent `over-long-line-mode' from being triggered.
+
+May be used globally or buffer-locally as required.")
+
+(defvar-local over-long-line-original-mode nil
+  "Stores the original `major-mode' value.")
+(put 'over-long-line-original-mode 'permanent-local t)
+
+(defun over-long-line-detected-p ()
+  "Following any initial comments and blank lines, the next N lines of the
+buffer will be tested for excessive length (where \"excessive\" means above
+`over-long-line-threshold', and N is `over-long-line-max-lines').
+
+Returns non-nil if any such excessive-length line is detected."
+  (let ((count 0))
+    (save-excursion
+      (goto-char (point-min))
+      (while (comment-forward)) ;; clears whitespace at minimum
+      (catch 'excessive
+        (while (< count over-long-line-max-lines)
+          (if (> (- (line-end-position 1) (point))
+                 over-long-line-threshold)
+              (throw 'excessive t)
+            (forward-line)
+            (setq count (1+ count))))))))
+
+(define-derived-mode over-long-line-mode nil "Over-long lines"
+  "This mode is used if line lengths exceed `over-long-line-threshold'.
+
+Many Emacs modes struggle with buffers which contain excessively long lines,
+and may consequently cause unacceptable performance issues.
+
+This is commonly on account of 'minified' code (i.e. code has been compacted
+into the smallest file size possible, which often entails removing newlines
+should they not be strictly necessary). These kinds of files are typically
+not intended to be edited, so not providing the usual editing mode in these
+cases will rarely be an issue.
+
+When such files are detected, we invoke this mode. This happens after
+`set-auto-mode' has set the major mode, should the selected major mode be a
+member (or derivative of a member) of `over-long-line-mode-target-modes'.
+
+By default this mode is essentially equivalent to `fundamental-mode', and
+exists mainly to provide information to the user as to why the expected mode
+was not used.
+
+To revert to the original mode despite any potential performance issues,
+type \\[over-long-line-mode-revert].
+
+Alternatively, the normal mode can be manually invoked, or a file-local
+`over-long-line-mode-inhibited' may be set non-nil to ensure that
+the normal mode is always used for the file in question."
+  ;; TODO:
+  ;; define/process alist of mode lists (for derived-mode-p) mapped to
+  ;; behaviours. include typical use of symbol for callback; otherwise
+  ;; some standard things such as disabling font-lock-mode. Perhaps it
+  ;; should just be a body of forms to evaluate, for maximum flexibility?
+  ;; but then symbol=>callback effectively covers that.
+  (setq font-lock-mode 0)
+  (message "Changed to %s (from %s) on account of line length. %s to undo."
+           major-mode
+           over-long-line-original-mode
+           (substitute-command-keys "\\[over-long-line-mode-revert]")))
+
+(defun over-long-line-mode-revert ()
+  "Call the `major-mode' which was selected by `set-auto-mode'
+before `over-long-line-mode' was called to replace it."
+  (interactive)
+  (if (bound-and-true-p over-long-line-original-mode)
+      (funcall over-long-line-original-mode)
+    (error "Original mode unknown.")))
+
+(define-key over-long-line-mode-map (kbd "C-c C-c")
+  'over-long-line-mode-revert)
+
+(defadvice set-auto-mode (after over-long-line-mode--set-auto-mode)
+  "Maybe use `over-long-line-mode' for files with very long lines.
+
+This advice acts after `set-auto-mode' has made its decision.
+
+We can't sensible act before this point, as some major modes must
+be exempt from `over-long-line-mode' (binary file modes, for
+example).  Instead only act when the selected major mode is a member
+of `over-long-line-mode-target-modes', or a derivative thereof.
+
+If buffer-local `over-long-line-mode-inhibited' is non-nil, then
+do nothing."
+  (unless (bound-and-true-p over-long-line-mode-inhibited)
+    (when (and (apply 'derived-mode-p over-long-line-mode-target-modes)
+               (over-long-line-detected-p))
+      (setq over-long-line-original-mode major-mode)
+      (over-long-line-mode))))
+(ad-activate 'set-auto-mode)
+
+(provide 'over-long-line-mode)
+
+;;; over-long-line-mode.el ends here
