ac169496621377a5a23bded344a19b69ef9bf4d4
Anonymous <anybody@emacswiki.org>
2016-01-12 05:04:58 +0100

    no summary available

--- over-long-line-mode.el
+++ over-long-line-mode.el
@@ -6,7 +6,11 @@
 ;; Version: 0.1
 
 ;; This file is not part of GNU Emacs.
-;; This file is free software.
+
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version. See <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;;
