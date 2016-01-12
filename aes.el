aaca4bc03f16eba3331ae45d4a1cc95d54eb727e
mhoram <anybody@emacswiki.org>
2016-01-12 14:50:16 +0100

    removed unnecessary statement

--- aes.el
+++ aes.el
@@ -1,4 +1,3 @@
 DeletedPage
 
-;; From now on updates will be published on Github https://github.com/Sauermann/emacs-aes
-;; This version is outdated!
+From now on updates will be published on Github https://github.com/Sauermann/emacs-aes
