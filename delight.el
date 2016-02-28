a2b8c57533735a9bbd0d18a6cf9bbc4c7d67e6f4
Anonymous <anybody@emacswiki.org>
2016-02-28 07:09:33 +0100

    no summary available

--- delight.el
+++ delight.el
@@ -2,7 +2,16 @@
 ;;
 ;; Author: Phil S.
 ;; URL: http://www.emacswiki.org/emacs/DelightedModes
+;; Keywords: convenience
+;; Created: 25 Jun 2013
 ;; Version: 1.04
+
+;; This file is not part of GNU Emacs.
+
+;; This file is free software: you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation, either version 3 of the License, or (at your option) any later
+;; version. See <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;;
