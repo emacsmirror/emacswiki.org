730dc3f70d1bc7e19dec7be8c4f03c586b836bcf
Anonymous <anybody@emacswiki.org>
2016-05-20 17:36:58 +0200

    Compile on current stable (24.5) results in message below. showkey.el:289:19:Warning: `special-display-frame-alist' is an obsolete     variable . . .

---
+++ Comments_on_showkey.el
@@ -0,0 +1,7 @@
+Compile on current stable (24.5) results in message below.
+
+showkey.el:289:19:Warning: `special-display-frame-alist' is an obsolete
+    variable (as of 24.3); use `display-buffer-alist' instead.
+
+-- Anonymous 2016-05-20 15:36 UTC
+
