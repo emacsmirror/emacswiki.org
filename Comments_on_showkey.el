1b5226bebba4a07a98a586b39e5df5556c273c68
DrewAdams <anybody@emacswiki.org>
2016-05-20 22:51:51 +0200

    Pay no attention to such warnings. 1. It is a warning, not an error. 2. This variable should not, IMHO, be deprecated. 3. Although deprecated, . . .

--- Comments_on_showkey.el
+++ Comments_on_showkey.el
@@ -5,3 +5,16 @@ showkey.el:289:19:Warning: `special-display-frame-alist' is an obsolete
 
 -- Anonymous 2016-05-20 15:36 UTC
 
+
+----
+
+Pay no attention to such warnings.
+
+1. It is a warning, not an error.
+2. This variable should not, IMHO, be deprecated.
+3. Although deprecated, it is still supported. And it works fine.
+
+Even when such a warning is appropriate (unlike this case, IMO), it can typically be ignored for any good library.  Most often such a warning is seen for a library that needs to support multiple versions of Emacs, and the library code correctly distinguishes which code paths to use for which versions.
+
+-- DrewAdams 2016-05-20 20:51 UTC
+
