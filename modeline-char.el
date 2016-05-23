a68056962c934f5505095061026497340bce60aa
DrewAdams <anybody@emacswiki.org>
2016-05-24 00:20:29 +0200

    no summary available

--- modeline-char.el
+++ modeline-char.el
@@ -10,7 +10,7 @@
 ;; Package-Requires: ()
 ;; Last-Updated:
 ;;           By:
-;;     Update #: 156
+;;     Update #: 159
 ;; URL: http://www.emacswiki.org/modeline-char.el
 ;; Doc URL: http://www.emacswiki.org/emacs/ModeLineCharacterInfo
 ;; Keywords: mode-line, character
@@ -29,6 +29,33 @@
 ;;  Use minor mode `mlc-char-in-mode-line-mode' to show the value of
 ;;  the character after point.  Use global minor mode
 ;;  `mlc-char-in-mode-line-mode-global' to do this in every buffer.
+;;
+;;  The character is shown (in the mode-line) highlighted with face
+;;  `mlc-mode-line-char-format', followed by `=', followed by the
+;;  hexadecimal Unicode code point for the character, highlighted with
+;;  face `mlc-mode-line-char-format-code'.
+;;
+;;  For example, with point before the character `e' it shows:
+;;
+;;    e=000065
+;;
+;;  with `e' and `000065' highlighted.
+;;
+;;  You thus have a dynamically updated view at all times of what the
+;;  character at the cursor is.
+;;
+;;  In addition:
+;;
+;;   * If you click `mouse-1' on the lighter then full information
+;;     about the character is shown in buffer `*Help*', including the
+;;     font and faces used (for the character in the buffer, not for
+;;     the lighter in the mode-line).
+;;
+;;   * If you click `mouse-2' on the lighter then the character is
+;;     copied to the secondary selection.
+;;
+;;   * If you click `mouse-3' on the lighter then the character is
+;;     shown enlarged in a tooltip.
 ;;
 ;;
 ;;  Commands defined here:
