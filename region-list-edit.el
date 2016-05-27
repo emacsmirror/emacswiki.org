af45d3e696463cf1d34a868459895ae027b2102d
Jianli Zhao <anybody@emacswiki.org>
2016-05-27 06:31:05 +0200

    no summary available

--- region-list-edit.el
+++ region-list-edit.el
@@ -1,4 +1,4 @@
-;;;Updated in 2016.4.12
+;;;Updated in 2016.5.27
 ;;;This package can add/delete a region into/from a region list, such as ‘((4 . 7) (11 . 15) (17 . 17) (20 . 25))
 ;;The member of regioin-list always include the node of the start and end. for example: (3 . 5), then 3 and 5 is both belong to this region.
 
@@ -366,6 +366,174 @@
 
 ;;((1 . 1))
 ;;()=nil
+
+
+(defun region-list-edit-delete-greed (regions-list todo)
+  "Delete a region todo from regions-list, the start and end node of todo, is included, and delete any region of whole that cross with the todo"
+  (interactive)
+  (let (beg-node-to-insert beg-node-included-p end-node-to-insert end-node-included-p beg-have-find-p end-have-find-p temp-node temp-list temp-pre temp-next cartodo cdrtodo)
+    ;; because (copy-marker (1- (car todo))) minimum=1, even (car todo) ==1, so need cartodo just for boundary check.
+    ;; same as cdrtodo
+    (setq cartodo (if (markerp (car todo));;if is marker, then keep as marker
+		      (1- (marker-position (car todo)))
+		    (1- (car todo))))
+    (setq cdrtodo (if (markerp (cdr todo))
+		      (1+ (marker-position (cdr todo)))
+		    (1+ (cdr todo))))
+
+    (setq todo (cons (if (markerp (car todo));;if is marker, then keep as marker
+			 (copy-marker (1- (car todo)))
+		       (1- (car todo)))
+		     (if (markerp (cdr todo))
+			 (copy-marker (1+ (cdr todo)))
+		       (1+ (cdr todo)))))
+    (dotimes (Num (length regions-list))
+      (cond ((and (not beg-have-find-p);;in the inner of Num'th node,include the start and end of Num'th node
+                  (>= cartodo (car (nth Num regions-list)))
+                  (<= cartodo (cdr (nth Num regions-list))))
+             (setq beg-have-find-p t)
+             (setq beg-node-to-insert Num)
+             (setq beg-node-included-p t))
+
+            ((and (not beg-have-find-p);;before the Num'th node
+                  (< cartodo (cdr (nth Num regions-list))))
+             (setq beg-have-find-p t)
+             (setq beg-node-to-insert Num)
+             (setq beg-node-included-p nil))
+            (t nil)))
+
+    (dotimes (Num (length regions-list))
+      (cond ((and (not end-have-find-p);;in the inner of Num'th node
+                  (>= cdrtodo (car (nth Num regions-list)))
+                  (<= cdrtodo (cdr (nth Num regions-list))))
+             (setq end-have-find-p t)
+             (setq end-node-to-insert Num)
+             (setq end-node-included-p t))
+
+            ((and (not end-have-find-p);;before the Num'th node
+                  (< cdrtodo (car (nth Num regions-list))))
+             (setq end-have-find-p t)
+             (setq end-node-to-insert Num)
+             (setq end-node-included-p nil))
+
+            (t nil)))
+
+    (cond ((not beg-have-find-p);;start and end both bigger than the last region in list
+           nil)
+
+          ((and beg-have-find-p;;end is bigger than the last region, start is in one node
+                beg-node-included-p
+                (not end-have-find-p))
+	   (if (= 0 beg-node-to-insert)
+	       (setq regions-list nil)
+	     (setq temp-list (nthcdr (1- beg-node-to-insert) regions-list))
+	     (setcdr temp-list nil)))
+
+          ((and beg-have-find-p;;end is bigger than the last region, start is between nodes
+                (not beg-node-included-p)
+                (> beg-node-to-insert 0)
+                (not end-have-find-p))
+           (setq temp-pre (nthcdr (1- beg-node-to-insert) regions-list))
+           (setcdr temp-pre nil))
+
+          ((and beg-have-find-p;;end is bigger than the last region, start is before all node
+                (not beg-node-included-p)
+                (= beg-node-to-insert 0)
+                (not end-have-find-p))
+           (setq regions-list nil))
+
+          ((and beg-have-find-p ;;start and end both in nodes, even end is in the first node  (4 . 6) - (4 . 6)
+                beg-node-included-p
+                end-have-find-p
+                end-node-included-p)
+	   (if (= 0 beg-node-to-insert)
+	       (setq regions-list (nthcdr (1+ end-node-to-insert) regions-list))
+	     (setq temp-list (nthcdr (1- beg-node-to-insert) regions-list))
+	     (setcdr temp-list (nthcdr (1+ end-node-to-insert) regions-list))))
+
+          ((and beg-have-find-p;;start is between nodes, end is in node, even end is in the last node
+                (not beg-node-included-p)
+                (> beg-node-to-insert 0)
+                end-have-find-p
+                end-node-included-p)
+           (setq temp-pre (nthcdr (1- beg-node-to-insert) regions-list))
+           (setcdr temp-pre (nthcdr (1+ end-node-to-insert) regions-list)))
+
+          ((and beg-have-find-p;;start is before all nodes, end is in node,even end is in the last node
+                (not beg-node-included-p)
+                (= beg-node-to-insert 0)
+                end-have-find-p
+                end-node-included-p)
+           (setq regions-list (nthcdr (1+ end-node-to-insert) regions-list)))
+
+          ((and beg-have-find-p ;;both before the first node
+                end-have-find-p
+                (not end-node-included-p)
+                (= end-node-to-insert 0))
+           nil)
+
+          ((and beg-have-find-p;;start and end both are between nodes
+                (not beg-node-included-p)
+                (> beg-node-to-insert 0)
+                end-have-find-p
+                (not end-node-included-p)
+                (> end-node-to-insert 0))
+           (setq temp-pre (nthcdr (1- beg-node-to-insert) regions-list))
+           (setcdr temp-pre (nthcdr end-node-to-insert regions-list)))
+
+          ((and beg-have-find-p;;start is before all nodes, end is between nodes
+                (not beg-node-included-p)
+                (= beg-node-to-insert 0)
+                end-have-find-p
+                (not end-node-included-p)
+                (> end-node-to-insert 0))
+           (setq regions-list (nthcdr end-node-to-insert regions-list)))
+
+          ((and beg-have-find-p;;start is in node,end is between nodes
+                beg-node-included-p
+                end-have-find-p
+                (not end-node-included-p)
+                (> end-node-to-insert 0))
+	   (if (= 0 beg-node-to-insert)
+	       (setq regions-list (nthcdr end-node-to-insert regions-list))
+	     (setq temp-pre (nthcdr (1- beg-node-to-insert) regions-list))
+	     (setcdr temp-pre (nthcdr end-node-to-insert regions-list))))
+          (t nil))
+    regions-list))
+
+;;(setq region-list-edit-example '((4 . 7) (11 . 15) (17 . 17) (20 . 25)))
+;;start and end both bigger than the last region in list
+;;(region-list-edit-delete-greed region-list-edit-example '(27 . 28))
+;;end is bigger than the last region, start is in one node
+;;(region-list-edit-delete-greed region-list-edit-example '(25 . 27))
+;;(region-list-edit-delete-greed region-list-edit-example '(18 . 27))
+;;(region-list-edit-delete-greed region-list-edit-example '(4 . 27))
+;;(region-list-edit-delete-greed region-list-edit-example '(5 . 25))
+;;end is bigger than the last region, start is between nodes
+;;(region-list-edit-delete-greed region-list-edit-example '(16 . 27))
+;;end is bigger than the last region, start is before all node
+;;(region-list-edit-delete-greed region-list-edit-example '(3 . 27))
+
+;;start and end both in nodes, even end is in the first node
+;;(region-list-edit-delete-greed region-list-edit-example '(6 . 17))
+;;(region-list-edit-delete-greed region-list-edit-example '(4 . 4))
+;;(region-list-edit-delete-greed region-list-edit-example '(4 . 25))
+
+;;start is between nodes, end is in node, even end is in the last node
+;;(region-list-edit-delete-greed region-list-edit-example '(9 . 22))
+;;start is before all nodes, end is in node,even end is in the last node
+;;(region-list-edit-delete-greed region-list-edit-example '(1 . 22))
+;;both before the first node
+;;(region-list-edit-delete-greed region-list-edit-example '(1 . 4))
+;;(region-list-edit-delete-greed region-list-edit-example '(2 . 3))
+
+;;start and end both are between nodes----end is between nodes
+;;(region-list-edit-delete-greed region-list-edit-example '(10 . 18))
+;;(region-list-edit-delete-greed region-list-edit-example '(10 . 11))
+;;start is before all nodes, end is between nodes
+;;(region-list-edit-delete-greed region-list-edit-example '(1 . 14))
+;;start is in node,end is between nodes
+;;(region-list-edit-delete-greed region-list-edit-example '(5 . 19))
 
 
 (provide 'region-list-edit)
