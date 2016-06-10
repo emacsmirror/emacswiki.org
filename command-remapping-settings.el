831ec78d23a42cc3f9234a134ea652a69cacb276
RyanThompson <anybody@emacswiki.org>
2016-06-10 16:39:28 +0200

    Add GPL2+ license

--- command-remapping-settings.el
+++ command-remapping-settings.el
@@ -1,5 +1,18 @@
 ;;; command-remapping-settings.el - Remap commands in all modes from a custom variable
 
+;; This code is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation; either version 3, or (at your
+;; option) any later version.
+;;
+;; This code is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with Emacs. If not, see http://www.gnu.org/licenses.
+
 ;; To Add more remappings, M-x customize-variable command-remapping-alist.
 
 (defun command-remappings-process-mode-map (keymap remap-alist enable)
