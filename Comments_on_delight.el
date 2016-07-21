2e6e8a2438fbc5476a590383e56abcd3f3929c9a
DrewAdams <anybody@emacswiki.org>
2016-07-21 16:38:23 +0200

    no summary available

--- Comments_on_delight.el
+++ Comments_on_delight.el
@@ -5,7 +5,7 @@ Why delete this page? MELPA still fetches its delight.el package's source from h
 
 ----
 
-Dunno why PhilS deleted it. I rolled it back. I'm guessing that Phil had a good reason and can mention that here. It is his library, after all.
+Dunno why PhilS deleted it. I rolled it back, for now. I'm guessing that Phil had a good reason and can mention that here. It is his library, after all.
 
 -- DrewAdams 2016-07-21 14:36 UTC
 
