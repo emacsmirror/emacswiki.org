1570b84711eacf063ebd89b9582dccb9b5ce1cbe
Alex Schroeder <anybody@emacswiki.org>
2016-07-21 16:39:47 +0200

    I'm guessing he wants to host it from GitHub. Here's a copy I found: . . .

--- Comments_on_delight.el
+++ Comments_on_delight.el
@@ -9,3 +9,10 @@ Dunno why PhilS deleted it. I rolled it back, for now. I'm guessing that Phil ha
 
 -- DrewAdams 2016-07-21 14:36 UTC
 
+
+----
+
+I'm guessing he wants to host it from GitHub. Here's a copy I found: https://github.com/phil-s/emacsd/tree/53afb004856c2eaabe86543af7fea196dd04cbc1/el-get/delight
+
+-- [http://alexschroeder.ch/wiki/Emacs Alex Schroeder] 2016-07-21 14:39 UTC
+
