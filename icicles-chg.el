3f824dc4648d53e71638ab30b2b0459aaa0630f7
DrewAdams <anybody@emacswiki.org>
2016-11-19 01:05:52 +0100

    no summary available

--- icicles-chg.el
+++ icicles-chg.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
 ;; Copyright (C) 2007-2016, Drew Adams, all rights reserved.
 ;; Created: Tue Nov 27 07:47:53 2007
-;; Last-Updated: Wed Oct 12 13:43:36 2016 (-0700)
+;; Last-Updated: Fri Nov 18 15:50:40 2016 (-0800)
 ;;           By: dradams
-;;     Update #: 11649
+;;     Update #: 11651
 ;; URL: http://www.emacswiki.org/icicles-chg.el
 ;; Doc URL: http://www.emacswiki.org/Icicles
 ;; Keywords: extensions, help, abbrev, local, minibuffer,
@@ -85,6 +85,8 @@
  
 ;;;(@* "CHANGE LOG FOR `icicles-cmd1.el'")
 ;;
+;; 2016/11/18 dadams
+;;     icicle-bookmark-set: Updated for Bookmark+ EWW support.
 ;; 2016/10/07 dadams
 ;;     icicle-dabbrev-completion, icicle-lisp-complete-symbol:
 ;;       Use icicle-msg-maybe-in-minibuffer, not message.
