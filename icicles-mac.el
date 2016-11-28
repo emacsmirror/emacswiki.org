94ab92a1d646f95107e881456512a5419a75a8a1
DrewAdams <anybody@emacswiki.org>
2016-11-28 02:41:07 +0100

    no summary available

--- icicles-mac.el
+++ icicles-mac.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
 ;; Copyright (C) 1996-2016, Drew Adams, all rights reserved.
 ;; Created: Mon Feb 27 09:24:28 2006
-;; Last-Updated: Thu Dec 31 13:59:23 2015 (-0800)
+;; Last-Updated: Sun Nov 27 17:43:20 2016 (-0800)
 ;;           By: dradams
-;;     Update #: 1269
+;;     Update #: 1270
 ;; URL: http://www.emacswiki.org/icicles-mac.el
 ;; Doc URL: http://www.emacswiki.org/Icicles
 ;; Keywords: internal, extensions, help, abbrev, local, minibuffer,
@@ -73,7 +73,7 @@
 ;;  navigate around the sections of this doc.  Linkd mode will
 ;;  highlight this Index, as well as the cross-references and section
 ;;  headings throughout this file.  You can get `linkd.el' here:
-;;  http://dto.freeshell.org/notebook/Linkd.html.
+;;  http://www.emacswiki.org/emacs/download/linkd.el.
 ;;
 ;;  (@> "Macros")
  
