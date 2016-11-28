ff54242b42da3eb5ca1d6080be967ee81669de75
DrewAdams <anybody@emacswiki.org>
2016-11-28 02:58:09 +0100

    no summary available

--- lacarte.el
+++ lacarte.el
@@ -8,9 +8,9 @@
 ;; Created: Fri Aug 12 17:18:02 2005
 ;; Version: 0
 ;; Package-Requires: ()
-;; Last-Updated: Thu Dec 31 14:15:26 2015 (-0800)
+;; Last-Updated: Sun Nov 27 17:59:16 2016 (-0800)
 ;;           By: dradams
-;;     Update #: 924
+;;     Update #: 925
 ;; URL: http://www.emacswiki.org/lacarte.el
 ;; Doc URL: http://www.emacswiki.org/LaCarte
 ;; Keywords: menu-bar, menu, command, help, abbrev, minibuffer, keys,
@@ -252,7 +252,7 @@
 ;;  navigate around the sections of this doc.  Linkd mode will
 ;;  highlight this Index, as well as the cross-references and section
 ;;  headings throughout this file.  You can get `linkd.el' here:
-;;  http://dto.freeshell.org/notebook/Linkd.html.
+;;  http://www.emacswiki.org/emacs/download/linkd.el.
 ;;
 ;;  (@> "Change log")
 ;;  (@> "User Options")
