cd30df98a0cfce3540e95131e4e1e8474c4efd8e
DrewAdams <anybody@emacswiki.org>
2017-01-01 20:45:10 +0100

    no summary available

--- thumb-frm.el
+++ thumb-frm.el
@@ -4,13 +4,13 @@
 ;; Description: Commands for thumbnail frames.
 ;; Author: Drew Adams
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
-;; Copyright (C) 2004-2016, Drew Adams, all rights reserved.
+;; Copyright (C) 2004-2017, Drew Adams, all rights reserved.
 ;; Created: Fri Dec 10 16:44:55 2004
 ;; Version: 0
 ;; Package-Requires: ((frame-fns "0") (frame-cmds "0"))
-;; Last-Updated: Thu Dec 31 16:16:58 2015 (-0800)
+;; Last-Updated: Sun Jan  1 11:47:41 2017 (-0800)
 ;;           By: dradams
-;;     Update #: 1792
+;;     Update #: 1793
 ;; URL: http://www.emacswiki.org/thumb-frm.el
 ;; Doc URL: http://www.emacswiki.org/FisheyeWithThumbs
 ;; Keywords: frame, icon
