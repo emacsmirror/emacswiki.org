0d5b274eeefa2c8b789f1501d6a1f1db272aee37
DrewAdams <anybody@emacswiki.org>
2017-01-02 18:07:40 +0100

    no summary available

--- bookmark+-chg.el
+++ bookmark+-chg.el
@@ -4,11 +4,11 @@
 ;; Description: Change logs for Bookmark+ libraries.
 ;; Author: Drew Adams
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
-;; Copyright (C) 2000-2016, Drew Adams, all rights reserved.
+;; Copyright (C) 2000-2017, Drew Adams, all rights reserved.
 ;; Created: Fri Sep 15 07:58:41 2000
-;; Last-Updated: Mon Jan  2 08:58:45 2017 (-0800)
+;; Last-Updated: Mon Jan  2 09:10:27 2017 (-0800)
 ;;           By: dradams
-;;     Update #: 16200
+;;     Update #: 16201
 ;; URL: http://www.emacswiki.org/bookmark+-chg.el
 ;; Doc URL: http://www.emacswiki.org/BookmarkPlus
 ;; Keywords: bookmarks, bookmark+
