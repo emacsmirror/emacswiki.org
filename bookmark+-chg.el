4fadaf6aa70408b69591a1c72dc12de7cd8ba900
DrewAdams <anybody@emacswiki.org>
2017-01-29 20:26:39 +0100

    no summary available

--- bookmark+-chg.el
+++ bookmark+-chg.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
 ;; Copyright (C) 2000-2017, Drew Adams, all rights reserved.
 ;; Created: Fri Sep 15 07:58:41 2000
-;; Last-Updated: Sun Jan 29 10:30:10 2017 (-0800)
+;; Last-Updated: Sun Jan 29 11:25:38 2017 (-0800)
 ;;           By: dradams
-;;     Update #: 16246
+;;     Update #: 16251
 ;; URL: http://www.emacswiki.org/bookmark+-chg.el
 ;; Doc URL: http://www.emacswiki.org/BookmarkPlus
 ;; Keywords: bookmarks, bookmark+
@@ -147,7 +147,8 @@
 ;;;(@* "CHANGE LOG FOR `bookmark+-1.el'")
 ;;
 ;; 2017/01/29 dadams
-;;     Added: bmkp-eww-url.
+;;     Added: bmkp-eww-title, bmkp-eww-url.
+;;     bookmark-set, bmkp-this-buffer-p, bmkp-make-eww-record: Use bmkp-eww-title, bmkp-eww-url.
 ;; 2017/01/10 dadams
 ;;     Added:
 ;;       bmkp-eww-allow-multiple-buffers-flag, bmkp-eww-set-new-buffer-name, bmkp-jump-eww-new-buffer,
