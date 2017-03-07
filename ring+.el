0bb66b54735b5dd742f34911d8e67b3657ac56f4
DrewAdams <anybody@emacswiki.org>
2017-03-07 18:43:52 +0100

    no summary available

--- ring+.el
+++ ring+.el
@@ -7,10 +7,10 @@
 ;; Copyright (C) 1996-2017, Drew Adams, all rights reserved.
 ;; Created: Thu Apr 11 16:46:04 1996
 ;; Version: 0
-;; Last-Updated: Sun Jan  1 11:24:57 2017 (-0800)
+;; Last-Updated: Tue Mar  7 09:44:02 2017 (-0800)
 ;;           By: dradams
-;;     Update #: 230
-;; URL: http://www.emacswiki.org/ring%2b.el
+;;     Update #: 231
+;; URL: https://www.emacswiki.org/emacs/download/ring%2b.el
 ;; Doc URL: http://emacswiki.org/RingPlus
 ;; Keywords: extensions, lisp, emacs-lisp
 ;; Compatibility: GNU Emacs: 20.x, 21.x, 22.x
