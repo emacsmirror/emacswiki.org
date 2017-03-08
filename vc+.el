cecc49c657e76b29630ef1d1d0ff9431c7c03b40
DrewAdams <anybody@emacswiki.org>
2017-03-09 00:14:44 +0100

    no summary available

--- vc+.el
+++ vc+.el
@@ -7,10 +7,10 @@
 ;; Copyright (C) 1999-2017, Drew Adams, all rights reserved.
 ;; Created: Thu Mar 11 16:45:20 1999
 ;; Version: 20.0
-;; Last-Updated: Sun Jan  1 11:53:55 2017 (-0800)
+;; Last-Updated: Wed Mar  8 15:15:06 2017 (-0800)
 ;;           By: dradams
-;;     Update #: 1494
-;; URL: http://www.emacswiki.org/vc+.el
+;;     Update #: 1495
+;; URL: https://www.emacswiki.org/emacs/download/vc%2b.el
 ;; Keywords: internal, tools, unix
 ;; Compatibility: GNU Emacs 20.x
 ;;
