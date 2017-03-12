14538b551a660a6c1d99a1fbbd5fa6838fed1996
DrewAdams <anybody@emacswiki.org>
2017-03-12 19:32:17 +0100

    no summary available

--- bookmark+-doc.el
+++ bookmark+-doc.el
@@ -6,10 +6,10 @@
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
 ;; Copyright (C) 2000-2017, Drew Adams, all rights reserved.
 ;; Created: Fri Sep 15 07:58:41 2000
-;; Last-Updated: Mon Feb 27 10:48:48 2017 (-0800)
+;; Last-Updated: Sun Mar 12 11:32:41 2017 (-0700)
 ;;           By: dradams
-;;     Update #: 15220
-;; URL: http://www.emacswiki.org/bookmark+-doc.el
+;;     Update #: 15221
+;; URL: https://www.emacswiki.org/emacs/download/bookmark%2b-doc.el
 ;; Doc URL: http://www.emacswiki.org/BookmarkPlus
 ;; Keywords: bookmarks, bookmark+, placeholders, annotations, search,
 ;;           info, url, eww, w3m, gnus
