47d8bd72daee1af44e50e57aa5067455740c7515
jonasbernoulli <anybody@emacswiki.org>
2017-07-19 13:54:36 +0200

    Bump minimal GPL version "either version 1 -> 3, or (at your option) any later version"

--- glimpse.el
+++ glimpse.el
@@ -8,17 +8,18 @@
 ;; Version: $Revision: 1.2 $ 
 ;; Keywords: 
 
-;; This program is free software; you can redistribute it and/or 
-;; modify it under the terms of the GNU General Public License as 
-;; published by the Free Software Foundation; either version 1, or (at 
-;; your option) any later version.  ;; This program is distributed in 
-;; the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
-;; even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
-;; PARTICULAR PURPOSE.  See the GNU General Public License for more 
-;; details.  ;; A copy of the GNU General Public License can be 
-;; obtained from this program's author (send electronic mail to Alan 
-;; Shutko <ats@acm.org>) or from the Free Software Foundation, Inc., 
-;; 675 Mass Ave, Cambridge, MA 02139, USA.  ;; 
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; LCD Archive Entry: 
 ;;; glimpse|Alan Shutko|ats@acm.org 
