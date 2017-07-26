c67f9887328d6fbce49ebd9ca9b490b2997bf23a
DrewAdams <anybody@emacswiki.org>
2017-07-26 17:43:00 +0200

    no summary available

--- icicles-cmd2.el
+++ icicles-cmd2.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
 ;; Copyright (C) 1996-2017, Drew Adams, all rights reserved.
 ;; Created: Thu May 21 13:31:43 2009 (-0700)
-;; Last-Updated: Fri Jul  7 05:45:23 2017 (-0700)
+;; Last-Updated: Wed Jul 26 08:12:58 2017 (-0700)
 ;;           By: dradams
-;;     Update #: 7436
+;;     Update #: 7437
 ;; URL: https://www.emacswiki.org/emacs/download/icicles-cmd2.el
 ;; Doc URL: https://www.emacswiki.org/emacs/Icicles
 ;; Keywords: extensions, help, abbrev, local, minibuffer,
@@ -297,7 +297,7 @@
 ;;  navigate around the sections of this doc.  Linkd mode will
 ;;  highlight this Index, as well as the cross-references and section
 ;;  headings throughout this file.  You can get `linkd.el' here:
-;;  http://www.emacswiki.org/emacs/download/linkd.el.
+;;  https://www.emacswiki.org/emacs/download/linkd.el.
 ;;
 ;;  (@> "Icicles Commands for Other Packages")
 ;;  (@> "Icicles Top-Level Commands, Part 2")
