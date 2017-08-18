5ef8832a484157854d85ce11a8264e9631780baf
DrewAdams <anybody@emacswiki.org>
2017-08-18 23:13:39 +0200

    [new::] @yPhil: Please try the latest - should be OK now. They changed all of the Emacs-Wiki URLS, and I haven't gotten around to changing them in . . .

--- Comments_on_dired+.el
+++ Comments_on_dired+.el
@@ -2,3 +2,11 @@ There is a 404 error when you click on the link/button "[Dired+ Help on the Web]
 
 -- [http://yassinphilip.bitbucket.io/ yPhil] 2017-08-18 16:28 UTC
 
+
+----
+
+[new:DrewAdams:2017-08-18 21:13 UTC]
+@yPhil: Please try the latest - should be OK now. They changed all of the Emacs-Wiki URLS, and I haven't gotten around to changing them in all of my libraries.
+
+-- DrewAdams 2017-08-18 21:13 UTC
+
