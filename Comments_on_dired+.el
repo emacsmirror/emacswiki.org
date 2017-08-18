5461661516b192bf6b8ad5664dfc728db1aeb726
yPhil <anybody@emacswiki.org>
2017-08-18 18:28:43 +0200

    There is a 404 error when you click on the link/button "[Dired+ Help on the Web]" in the dired+ mode (C-h m) help page.

---
+++ Comments_on_dired+.el
@@ -0,0 +1,4 @@
+There is a 404 error when you click on the link/button "[Dired+ Help on the Web]" in the dired+ mode (C-h m) help page.
+
+-- [http://yassinphilip.bitbucket.io/ yPhil] 2017-08-18 16:28 UTC
+
