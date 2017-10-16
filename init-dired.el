eb7001d2bdf86ea21c0612233abc127fd8093203
AndyStewart <anybody@emacswiki.org>
2017-10-16 17:15:27 +0200

    Add one-key menu for dired-filter

--- init-dired.el
+++ init-dired.el
@@ -6,8 +6,8 @@
 ;; Maintainer: Andy Stewart lazycat.manatee@gmail.com
 ;; Copyright (C) 2008, 2009, Andy Stewart, all rights reserved.
 ;; Created: 2008-10-20 09:38:03
-;; Version: 0.1
-;; Last-Updated: 2008-10-20 09:38:09
+;; Version: 0.2
+;; Last-Updated: 2017-10-16 23:14:09
 ;;           By: Andy Stewart
 ;; URL:
 ;; Keywords: dired
@@ -82,19 +82,19 @@
 
 ;;; Code:
 
-(setq dired-recursive-copies t)               ;可以递归的进行拷贝
-(setq dired-recursive-deletes t)              ;可以递归的删除目录
-(setq dired-recursive-deletes 'always)        ;删除东西时不提示
-(setq dired-recursive-copies 'always)         ;拷贝东西时不提示
-(toggle-dired-find-file-reuse-dir 1)          ;使用单一模式浏览Dired
+(setq dired-recursive-copies t)         ;可以递归的进行拷贝
+(setq dired-recursive-deletes t)        ;可以递归的删除目录
+(setq dired-recursive-deletes 'always)  ;删除东西时不提示
+(setq dired-recursive-copies 'always)   ;拷贝东西时不提示
+(toggle-dired-find-file-reuse-dir 1)    ;使用单一模式浏览Dired
 (setq dired-details-hidden-string "[ ... ] ") ;设置隐藏dired里面详细信息的字符串
 (setq dired-listing-switches "-aluh")         ;传给 ls 的参数
 (setq directory-free-space-args "-Pkh")       ;目录空间选项
 (setq dired-omit-size-limit nil)              ;dired忽略的上限
-(setq dired-dwim-target t)                    ;Dired试着猜处默认的目标目录
-(setq my-dired-omit-status t)                 ;设置默认忽略文件
+(setq dired-dwim-target t)              ;Dired试着猜处默认的目标目录
+(setq my-dired-omit-status t)           ;设置默认忽略文件
 (setq my-dired-omit-regexp "^\\.?#\\|^\\..*") ;设置忽略文件的匹配正则表达式
-(setq my-dired-omit-extensions '(".cache"))   ;设置忽略文件的扩展名列表
+(setq my-dired-omit-extensions '(".cache")) ;设置忽略文件的扩展名列表
 (add-hook 'dired-after-readin-hook '(lambda ()
                                       (progn
                                         (require 'dired-extension)
@@ -103,7 +103,7 @@
                               (progn
                                 (require 'dired-extension)
                                 (dired-omit-method)))) ;隐藏文件的方法
-(setq dired-guess-shell-alist-user                     ;设置文件默认打开的模式
+(setq dired-guess-shell-alist-user      ;设置文件默认打开的模式
       '(
         ;; 图书
         (list "\\.chm$" '(concat
@@ -129,22 +129,22 @@
 ;;; --- 文件浏览器
 (lazy-set-key
  '(
-   ("h" . dired-next-subdir)                   ;下一个子目录
-   ("l" . dired-prev-subdir)                   ;上一个子目录
-   ("n" . dired-next-dirline)                  ;下一个目录
-   ("p" . dired-prev-dirline)                  ;上一个目录
-   ("P" . dired-do-kill-lines)                 ;删除标记的行
-   ("5" . dired-translate-to-html)             ;转换到HTML格式
-   ("9" . auto-install-from-dired)             ;自动从EmacsWiki安装标记的文件
-   ("z" . dired-do-moccur)                     ;搜索dired
-   ("I" . image-dired)                         ;打开浏览模式
-   ("W" . dired-x-find-file)                   ;查找文件
-   ("J" . dired-goto-file)                     ;跳到某个文件
-   ("X" . traverse-cp-or-mv-extfiles-in-dir)   ;拷贝或移动目录下指定扩展名的文件
-   ("V" . traverse-dired-browse-archive)       ;浏览压缩文件
-   ("," . dired-diff)                          ;比较文件
-   ("C-s" . dired-isearch-forward)             ;向后搜索
-   ("C-r" . dired-isearch-backward)            ;向前搜索
+   ("h" . dired-next-subdir)            ;下一个子目录
+   ("l" . dired-prev-subdir)            ;上一个子目录
+   ("n" . dired-next-dirline)           ;下一个目录
+   ("p" . dired-prev-dirline)           ;上一个目录
+   ("P" . dired-do-kill-lines)          ;删除标记的行
+   ("5" . dired-translate-to-html)      ;转换到HTML格式
+   ("9" . auto-install-from-dired)      ;自动从EmacsWiki安装标记的文件
+   ("z" . dired-do-moccur)              ;搜索dired
+   ("I" . image-dired)                  ;打开浏览模式
+   ("W" . dired-x-find-file)            ;查找文件
+   ("J" . dired-goto-file)              ;跳到某个文件
+   ("X" . traverse-cp-or-mv-extfiles-in-dir) ;拷贝或移动目录下指定扩展名的文件
+   ("V" . traverse-dired-browse-archive)     ;浏览压缩文件
+   ("," . dired-diff)                        ;比较文件
+   ("C-s" . dired-isearch-forward)           ;向后搜索
+   ("C-r" . dired-isearch-backward)          ;向前搜索
    ("ESC C-s" . dired-isearch-forward-regexp)  ;向前正则表达式搜索
    ("ESC C-r" . dired-isearch-backward-regexp) ;向后正则表达式搜索
    ("SPC" . scroll-up)                         ;向下翻页
@@ -152,6 +152,7 @@
    ("c" . kill-this-buffer)                    ;关闭当前标签
    ("/" . copy-buffer-file-name-as-kill)       ;显示路径或名称
    ("s" . one-key-menu-dired-sort)             ;排序
+   ("F" . one-key-menu-dired-filter)           ;过滤
    ("w" . wdired-change-to-wdired-mode)        ;切换到dired编辑模式
    )
  dired-mode-map
@@ -221,6 +222,27 @@
   (require 'one-key)
   (require 'dired-sort)                 ;排序 dired 文件
   (one-key-menu "DIRED-SORT" one-key-menu-dired-sort-alist t))
+
+(defvar one-key-menu-dired-filter-alist nil
+  "The `one-key' menu alist for DIRED-FILTER.")
+
+(setq one-key-menu-dired-filter-alist
+      '(
+        (("x" . "Extension") . dired-filter-by-extension)
+        (("f" . "File") . dired-filter-by-file)
+        (("d" . "Directory") . dired-filter-by-directory)
+        (("e" . "Execute") . dired-filter-by-executable)
+        (("." . "Dot files") . dired-filter-by-dot-files)
+        (("r" . "Regex") . dired-filter-by-regexp)
+        (("s" . "Symlink") . dired-filter-by-symlink)
+        (("n" . "Name") . dired-filter-by-name)
+        ))
+
+(defun one-key-menu-dired-filter ()
+  "The `one-key' menu for DIRED-FILTER."
+  (interactive)
+  (require 'dired-filter)
+  (one-key-menu "DIRED-FILTER" one-key-menu-dired-filter-alist t))
 
 (provide 'init-dired)
 
