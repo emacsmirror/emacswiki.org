5d2bceac5cd142a27f0df6b568a173c689fec318
targzeta <anybody@emacswiki.org>
2017-11-10 23:28:41 +0100

    The download button point to https://www.emacswiki.org/emacs-de/download/etags-u.el but the real link is . . .

---
+++ Comments_on_etags-u.el
@@ -0,0 +1,4 @@
+The download button point to https://www.emacswiki.org/emacs-de/download/etags-u.el but the real link is https://www.emacswiki.org/emacs/download/etags-u.el
+
+-- targzeta 2017-11-10 22:28 UTC
+
