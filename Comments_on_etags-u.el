f80090e76cbbeaf0e1f3c3e9a83444b74cb0fbf7
targzeta <anybody@emacswiki.org>
2017-11-10 23:34:05 +0100

    no summary available

--- Comments_on_etags-u.el
+++ Comments_on_etags-u.el
@@ -1,4 +1,2 @@
+[new:targzeta:2017-11-10 22:33 UTC]
 The download button point to https://www.emacswiki.org/emacs-de/download/etags-u.el but the real link is https://www.emacswiki.org/emacs/download/etags-u.el
-
--- targzeta 2017-11-10 22:28 UTC
-
