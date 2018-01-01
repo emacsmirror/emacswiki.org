9a506bc4628a5cace2ff6a5e703574ff6ef95678
DrewAdams <anybody@emacswiki.org>
2018-01-01 23:11:24 +0100

    no summary available

--- icicles-face.el
+++ icicles-face.el
@@ -4,16 +4,16 @@
 ;; Description: Faces for Icicles
 ;; Author: Drew Adams
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
-;; Copyright (C) 1996-2017, Drew Adams, all rights reserved.
+;; Copyright (C) 1996-2018, Drew Adams, all rights reserved.
 ;; Created: Mon Feb 27 09:19:43 2006
-;; Last-Updated: Wed Jul 26 08:18:37 2017 (-0700)
+;; Last-Updated: Mon Jan  1 14:09:58 2018 (-0800)
 ;;           By: dradams
-;;     Update #: 744
+;;     Update #: 745
 ;; URL: https://www.emacswiki.org/emacs/download/icicles-face.el
 ;; Doc URL: https://www.emacswiki.org/emacs/Icicles
 ;; Keywords: internal, extensions, help, abbrev, local, minibuffer,
 ;;           keys, apropos, completion, matching, regexp, command
-;; Compatibility: GNU Emacs: 20.x, 21.x, 22.x, 23.x, 24.x, 25.x
+;; Compatibility: GNU Emacs: 20.x, 21.x, 22.x, 23.x, 24.x, 25.x, 26.x
 ;;
 ;; Features that might be required by this library:
 ;;
