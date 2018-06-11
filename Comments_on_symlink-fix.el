a3c313b7513399ef3eb3e8cdfb5667cd1e4af1bd
TauPan <anybody@emacswiki.org>
2018-06-11 09:16:36 +0200

    I'm still using this with emacs 26.1, however the \N in one of the docstrings triggers "invalid-read-syntax". Replacing it with \<N> (as it's . . .

---
+++ Comments_on_symlink-fix.el
@@ -0,0 +1,4 @@
+I'm still using this with emacs 26.1, however the \N in one of the docstrings triggers "invalid-read-syntax". Replacing it with \<N> (as it's meant to be a metasyntactic variable for a number, I think) helps.
+
+-- [https://github.com/TauPan TauPan] 2018-06-11 07:16 UTC
+
