c23573a866f35e92cc447af4787b6fbbdaf86fab
DrewAdams <anybody@emacswiki.org>
2018-06-29 15:46:20 +0200

    no summary available

--- show-wspace.el
+++ show-wspace.el
@@ -1,3 +1,3 @@
 ;;; show-wspace.el --- OBSOLETE - replaced by library `highlight-chars.el'.
 ;;;
-;;; You can get `highlight-chars.el' from http://www.emacswiki.org/emacs-en/highlight-chars.el
+;;; You can get `highlight-chars.el' from https://www.emacswiki.org/emacs-en/highlight-chars.el
