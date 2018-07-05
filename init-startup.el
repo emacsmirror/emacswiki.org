b9d3366a6c8977f765c56c57c1fb8070a2ad8b82
AndyStewart <anybody@emacswiki.org>
2018-07-05 12:28:37 +0200

    Fixed fullscreen mode works with MacOS

--- init-startup.el
+++ init-startup.el
@@ -6,16 +6,16 @@
 ;; Maintainer: Andy Stewart <lazycat.manatee@gmail.com>
 ;; Copyright (C) 2014, Andy Stewart, all rights reserved.
 ;; Created: 2014-01-20 23:58:38
-;; Version: 0.1
-;; Last-Updated: 2014-01-20 23:58:38
+;; Version: 0.2
+;; Last-Updated: 2018-07-05 18:23:57
 ;;           By: Andy Stewart
 ;; URL: http://www.emacswiki.org/emacs/download/init-startup.el
-;; Keywords: 
+;; Keywords:
 ;; Compatibility: GNU Emacs 24.3.50.1
 ;;
 ;; Features that might be required by this library:
 ;;
-;; 
+;;
 ;;
 
 ;;; This file is NOT part of GNU Emacs
@@ -37,10 +37,10 @@
 ;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
 ;; Floor, Boston, MA 02110-1301, USA.
 
-;;; Commentary: 
-;; 
+;;; Commentary:
+;;
 ;; Config load when startup
-;; 
+;;
 
 ;;; Installation:
 ;;
@@ -57,38 +57,55 @@
 
 ;;; Customize:
 ;;
-;; 
+;;
 ;;
 ;; All of the above can customize by:
 ;;      M-x customize-group RET init-startup RET
 ;;
 
 ;;; Change log:
-;;	
+;;
+;; 2018/07/05
+;;      * Make emacs fullscreen mode works with MacOS.
+;;
 ;; 2014/01/20
 ;;      * First released.
-;; 
+;;
 
 ;;; Acknowledgements:
 ;;
-;; 
+;;
 ;;
 
 ;;; TODO
 ;;
-;; 
+;;
 ;;
 
 ;;; Require
 
-
 ;;; Code:
 
+;; 在Mac平台, Emacs不能进入Mac原生的全屏模式,否则会导致 `make-frame' 创建时也集成原生全屏属性后造成白屏和左右滑动现象.
+;; 所以先设置 `ns-use-native-fullscreen' 和 `ns-use-fullscreen-animation' 禁止Emacs使用Mac原生的全屏模式.
+;; 而是采用传统的全屏模式, 传统的全屏模式, 只会在当前工作区全屏,而不是切换到Mac那种单独的全屏工作区,
+;; 这样执行 `make-frame' 先关代码或插件时,就不会因为Mac单独工作区左右滑动产生的bug.
+;;
+;; Mac平台下,不能直接使用 `set-frame-parameter' 和 `fullboth' 来设置全屏,
+;; 那样也会导致Mac窗口管理器直接把Emacs窗口扔到单独的工作区, 从而对 `make-frame' 产生同样的Bug.
+;; 所以, 启动的时候通过 `set-frame-parameter' 和 `maximized' 先设置Emacs为最大化窗口状态, 启动5秒以后再设置成全屏状态,
+;; Mac就不会移动Emacs窗口到单独的工作区, 最终解决Mac平台下原生全屏窗口导致 `make-frame' 左右滑动闪烁的问题.
+(setq ns-use-native-fullscreen nil)
+(setq ns-use-fullscreen-animation nil)
+(set-frame-parameter (selected-frame) 'fullscreen 'maximized)
+(run-at-time "5sec" nil (lambda () (set-frame-parameter (selected-frame) 'fullscreen 'fullboth)))
+
 (setq ad-redefinition-action 'accept)   ;不要烦人的 redefine warning
-(toggle-frame-fullscreen)               ;全屏
-(tool-bar-mode -1)                      ;禁用工具栏
-(menu-bar-mode -1)                      ;禁用菜单栏
-(scroll-bar-mode -1)                    ;禁用滚动条
+(setq frame-resize-pixelwise t) ;设置缩放的模式,避免Mac平台最大化窗口以后右边和下边有空隙
+(tool-bar-mode -1)              ;禁用工具栏
+(menu-bar-mode -1)              ;禁用菜单栏
+(scroll-bar-mode -1)            ;禁用滚动条
+(server-start)                  ;为emacsclient准备使用场景，比如git
 
 (provide 'init-startup)
 
