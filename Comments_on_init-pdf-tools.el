cf30abe8cdb4e1926200cbfa5036637a1d78500e
Anonymous <anybody@emacswiki.org>
2018-09-03 06:25:34 +0200

    This was great! I am using Parabola (ArchLinux) and I always struggled with the epdfinfo issue. I installed it from AUR (aur.archlinux.org: . . .

---
+++ Comments_on_init-pdf-tools.el
@@ -0,0 +1,4 @@
+This was great! I am using Parabola (ArchLinux) and I always struggled with the epdfinfo issue. I installed it from AUR (aur.archlinux.org: cask-git), and now I don't have issues. I want to say that using make -s, and installing as a TAR also works '''M-x package-install-file RET pdf-tools-${VERSION}.tar RET'''  <--  from http://tech.memoryimprintstudio.com/pdf-annotation-related-tools/) Thanks!
+
+-- Anonymous 2018-09-03 04:25 UTC
+
