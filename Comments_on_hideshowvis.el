a7317c534d776b0bfdf33e47eb6d74c218ec6fda
Anonymous <anybody@emacswiki.org>
2018-09-23 02:19:30 +0200

    Maintainer of hideshowvis here. Please don't use this version, it is outdated and has (minor) changes. I think I've deleted this page in the past but . . .

--- Comments_on_hideshowvis.el
+++ Comments_on_hideshowvis.el
@@ -2,3 +2,10 @@ There's apparently an updated version available at https://github.com/sheijk/hid
 
 -- jimmy 2015-05-18 20:17 UTC
 
+
+----
+
+Maintainer of hideshowvis here. Please don't use this version, it is outdated and has (minor) changes. I think I've deleted this page in the past but apparently it got re-uploaded (history only has one version which it calls revision 10)
+
+-- Anonymous 2018-09-23 00:19 UTC
+
