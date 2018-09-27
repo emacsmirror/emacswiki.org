e43c75ee17bb4530b2ac9ff4c3f29786e5a369ba
chethcoat <anybody@emacswiki.org>
2018-09-27 20:20:48 +0200

    I use bc and I need a bc-mode for Emacs. I went looking, found this one, and downloaded and installed it.  It mostly works, but there are serious . . .

---
+++ Comments_on_bc-mode.el
@@ -0,0 +1,12 @@
+I use bc and I need a bc-mode for Emacs.
+
+I went looking, found this one, and downloaded and installed it.  It mostly works, but there are serious problems with the way comment formatting/layout is implemented.  It is bad enough that I can't see continuing to use this mode without finding a fix or a workaround.  There is next to nothing in the way of instructions for using bc-mode.el.
+
+I note it relies on cc-mode.  That isn't much help for me.  I'm a longtime Emacs user, but not an Elisp hacker, and I don't understand very well how major modes work.  I am not a big-time C programmer, either, although I can write simple C programs when I must.  But I know almost nothing about cc-mode.el.
+
+I tried to contact Kumar, the author, for help.  Well, it turns out that his supplied email address is nonexistent -- not very surprising, since the date on this code is 2005 -- thirteen long years ago!  There is no other way I can see to contact him, and no other person is mentioned.
+
+So here is yet another sample of ancient, abandoned, nonworking and incorrect information that is just left to rot on the WWW.  I guess I'll have to look elsewhere for something useful, because Kumar's bc-mode.el isn't it as it stands right now.
+
+-- chethcoat 2018-09-27 18:20 UTC
+
