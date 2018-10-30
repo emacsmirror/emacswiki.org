7049e69d94679a05d022e4434ddd0910887d77b8
匿名者 <anybody@emacswiki.org>
2018-10-30 09:16:20 +0100

    When I used this in .emacs: (setq sdcv-dictionary-complete-list        ;; a simple dictionary list       '("Oxford Advanced Learner's Dictionary . . .

---
+++ Comments_on_sdcv.el
@@ -0,0 +1,16 @@
+When I used this in .emacs:
+
+(setq sdcv-dictionary-complete-list        ;; a simple dictionary list
+      '("Oxford Advanced Learner's Dictionary 8th Ed."
+"Longman Dictionary of Contemporary English 5th Ed. (En-En)"
+"Cambridge Advanced Learners Dictionary 3th Ed. (En-En)"
+	))
+(setq sdcv-dictionary-data-dir "~/.stardict/dic")
+
+
+I GOT THIS ERROR:
+Nothing similar to /bin/bash: -c: line 0: unexpected EOF while looking for matching `''
+/bin/bash: -c: line 1: syntax error: unexpected end of file
+
+-- 匿名者 2018-10-30 08:16 UTC
+
