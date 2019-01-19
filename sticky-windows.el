9a7083709affc7b9a2f0be94d5660dc5f29f3a09
erjoalgo <anybody@emacswiki.org>
2019-01-19 06:15:56 +0100

    support overriding sticky windows via prefix arg

--- sticky-windows.el
+++ sticky-windows.el
@@ -20,32 +20,36 @@
 (defun sticky-window-keep-window-visible ()
   "Insure the buffer associated with the current window stays visible.
 This is handy for ERC buffers where you would like to see the
-conversation while you work in other windows within the frame. 
+conversation while you work in other windows within the frame.
 This is intended to be used with `sticky-window-delete-window'.
 A prefix arg reverses this operation."
   (interactive)
   (set-window-dedicated-p (selected-window) (not current-prefix-arg)))
 
 ;;;###autoload
-(defun sticky-window-delete-window ()
+(defun sticky-window-delete-window (force)
   "This is intended to be a replacement for `delete-window', but
 that avoids deleting windows that have been marked as dedicated
 with `sticky-window-keep-window-visible'."
-  (interactive)
-  (let ((window (selected-window)))
-	(if (and (not current-prefix-arg) (window-dedicated-p window))
-		(error "This is a dedicated window. Use C-u prefix on this keybinding to really delete it.")
-	  (set-window-dedicated-p (selected-window) nil)
-	  (delete-window window))))
+  (interactive "P")
+  (if force
+      (delete-window window)
+    (let ((window (selected-window)))
+      (if (and (not current-prefix-arg) (window-dedicated-p window))
+	  (error "This is a dedicated window. Use C-u prefix on this keybinding to really delete it.")
+	(set-window-dedicated-p (selected-window) nil)
+	(delete-window window)))))
 
 ;;;###autoload
-(defun sticky-window-delete-other-windows ()
+(defun sticky-window-delete-other-windows (force)
   "Delete all other windows that are not marked to be visible with `sticky-window-keep-window-visible'."
-  (interactive)
-  (mapcar (lambda (window)
-			(if (not (window-dedicated-p window))
-				(delete-window window)))
-		  (cdr (window-list))))
+  (interactive "P")
+  (if force
+      (delete-other-windows)
+    (mapcar (lambda (window)
+	      (if (not (window-dedicated-p window))
+		  (delete-window window)))
+	    (cdr (window-list)))))
 
 (provide 'sticky-windows)
 
