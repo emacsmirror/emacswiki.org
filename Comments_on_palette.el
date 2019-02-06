485b95b5977d196263bf0bbbf6a3b8c6b63d2720
TinaRussell <anybody@emacswiki.org>
2019-02-06 23:16:16 +0100

    Sadly, this package isn’t compatible with Emacs 26, due to removal of `make-variable-frame-local'. https://github.com/nex3/perspective-el/issues/64 :(

---
+++ Comments_on_palette.el
@@ -0,0 +1,4 @@
+Sadly, this package isn’t compatible with Emacs 26, due to removal of `make-variable-frame-local'. https://github.com/nex3/perspective-el/issues/64 :(
+
+-- TinaRussell 2019-02-06 22:16 UTC
+
