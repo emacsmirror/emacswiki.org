83334cb20ffe45d5de72ae4d66e715fd32349693
DrewAdams <anybody@emacswiki.org>
2019-02-07 01:15:52 +0100

    no summary available

--- Comments_on_palette.el
+++ Comments_on_palette.el
@@ -5,7 +5,7 @@ Sadly, this package isn’t compatible with Emacs 26, due to removal of `make-va
 
 ----
 
-TinaRussell: OK, thanks. I fixed that. But there seem to be some other problems that affect the display. I'll have to dig into those later, when I get some time.
+TinaRussell: OK, thanks. I fixed that. But there seem to be some other problems that affect the display, at least on MS Windows 10 (used to be OK, on Windows XP and Windows 7 at least).  I'll have to dig into those later, when I get some time.
 
 -- DrewAdams 2019-02-07 00:08 UTC
 
