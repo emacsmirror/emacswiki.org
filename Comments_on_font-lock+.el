91730c1ce19f3e63d6f8bab13e5b20ab50316059
TinaRussell <anybody@emacswiki.org>
2019-02-20 08:13:13 +0100

    @DrewAdams: Aha! I’ve found the culprit. The issue is that Org has its own unfontify-region function, `org-unfontify-region'… <pre> . . .

--- Comments_on_font-lock+.el
+++ Comments_on_font-lock+.el
@@ -13,3 +13,55 @@ Dunno. There's no explicit occurrence of `keymap' in the code. What happens if y
 
 -- DrewAdams 2019-01-30 15:55 UTC
 
+
+----
+
+@DrewAdams:
+
+Aha! I’ve found the culprit. The issue is that Org has its own unfontify-region function, `org-unfontify-region'…
+
+<pre>
+(defun org-unfontify-region (beg end &optional _maybe_loudly)
+  "Remove fontification and activation overlays from links."
+  (font-lock-default-unfontify-region beg end)
+  (let* ((buffer-undo-list t)
+	 (inhibit-read-only t) (inhibit-point-motion-hooks t)
+	 (inhibit-modification-hooks t)
+	 deactivate-mark buffer-file-name buffer-file-truename)
+    (decompose-region beg end)
+    (remove-text-properties beg end
+			    '(mouse-face t keymap t org-linked-text t
+					 invisible t intangible t
+					 org-emphasis t))
+    (org-remove-font-lock-display-properties beg end)))
+</pre>
+
+…and it doesn’t have a redefinition in font-lock+.el. Do not fear, however! I have written one:
+
+<pre>
+(defun org-unfontify-region (beg end &optional _maybe_loudly)
+  "Remove fontification and activation overlays from links.
+Ignore text with property `font-lock-ignore'."
+  (font-lock-default-unfontify-region beg end)
+  (let* ((buffer-undo-list t)
+	 (inhibit-read-only t) (inhibit-point-motion-hooks t)
+	 (inhibit-modification-hooks t)
+	 (here (min beg end))
+	 (end1 (max beg end))
+	 deactivate-mark buffer-file-name buffer-file-truename chg)
+	(decompose-region here end1)
+	(while (< here end1)
+	  (setq chg  (next-single-property-change here 'font-lock-ignore nil end1))
+	  (unless (get-text-property here 'font-lock-ignore)
+		(remove-text-properties here chg
+					'(mouse-face t keymap t org-linked-text t
+						 invisible t intangible t
+						 org-emphasis t)))
+      (setq here  chg))
+	(org-remove-font-lock-display-properties beg end))))
+</pre>
+
+Now my link-text dynamic block thing works! Hooray!
+
+-- TinaRussell 2019-02-20 07:13 UTC
+
