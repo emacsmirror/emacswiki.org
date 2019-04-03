f5d99be70e609eb041ffb09f115d021ab56ab95f
JohnL4 <anybody@emacswiki.org>
2019-04-03 19:32:56 +0200

    Borked on emacs 26.1.  I changed "holiday-face" to just "holiday", but now I'm getting "cond: Symbol’s function definition is void: . . .

---
+++ Comments_on_calendar+.el
@@ -0,0 +1,4 @@
+Borked on emacs 26.1.  I changed "holiday-face" to just "holiday", but now I'm getting "cond: Symbol’s function definition is void: extract-calendar-month" and I've already spent enough time at work not doing work. :/
+
+-- JohnL4 2019-04-03 17:32 UTC
+
