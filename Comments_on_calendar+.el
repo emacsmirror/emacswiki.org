6dd32baddbf7d1797959e8554a1f7e1d2d6c35b1
DrewAdams <anybody@emacswiki.org>
2019-04-04 01:27:17 +0200

    `calendar+.el` is *very* old code. The file header states the compatibility explicitly as Emacs 20.x. If you're not using Emacs 20 then it probably . . .

--- Comments_on_calendar+.el
+++ Comments_on_calendar+.el
@@ -2,3 +2,12 @@ Borked on emacs 26.1.  I changed "holiday-face" to just "holiday", but now I'm g
 
 -- JohnL4 2019-04-03 17:32 UTC
 
+
+----
+
+`calendar+.el` is *very* old code. The file header states the compatibility explicitly as Emacs 20.x. If you're not using Emacs 20 then it probably won't work/help out of the box.
+
+Sorry for your waste of time. (On the other hand, playing with Elisp is always a learning experience. ;-))
+
+-- DrewAdams 2019-04-03 23:27 UTC
+
