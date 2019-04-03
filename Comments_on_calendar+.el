2ad1617a1eeb2aeccaaac4b3d842788268f14f5e
DrewAdams <anybody@emacswiki.org>
2019-04-04 01:27:39 +0200

    no summary available

--- Comments_on_calendar+.el
+++ Comments_on_calendar+.el
@@ -5,7 +5,7 @@ Borked on emacs 26.1.  I changed "holiday-face" to just "holiday", but now I'm g
 
 ----
 
-`calendar+.el` is *very* old code. The file header states the compatibility explicitly as Emacs 20.x. If you're not using Emacs 20 then it probably won't work/help out of the box.
+##calendar+.el## is *very* old code. The file header states the compatibility explicitly as Emacs 20.x. If you're not using Emacs 20 then it probably won't work/help out of the box.
 
 Sorry for your waste of time. (On the other hand, playing with Elisp is always a learning experience. ;-))
 
