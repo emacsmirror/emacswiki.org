e6362c5ee553c376beb6b3cb6c022cf05da87578
npostavs <anybody@emacswiki.org>
2019-06-12 14:57:14 +0200

    As of Emacs 26.1, this can probably be replaced with {{{ (setq maximum-scroll-margin 0.5       scroll-margin 99999 . . .

--- Comments_on_centered-cursor-mode.el
+++ Comments_on_centered-cursor-mode.el
@@ -2,3 +2,17 @@ To make this work correctly with visual-line-mode when scrolling up/down, replac
 
 -- Andy 2019-02-01 06:03 UTC
 
+
+----
+
+As of Emacs 26.1, this can probably be replaced with
+
+{{{
+(setq maximum-scroll-margin 0.5
+      scroll-margin 99999
+      scroll-preserve-screen-position t
+      scroll-conservatively 0)
+}}}
+
+-- npostavs 2019-06-12 12:57 UTC
+
