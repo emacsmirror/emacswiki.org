1a47a077a41d13bce9584ffd4527e9f48a589c49
JeanLouis <anybody@emacswiki.org>
2019-08-06 21:52:14 +0200

    It does not work on git version. There is also one function wrongly defined. And menu key does not work, in general, it will block but not open . . .

---
+++ Comments_on_one-key.el
@@ -0,0 +1,5 @@
+It does not work on git version. There is also one function wrongly defined.
+And menu key does not work, in general, it will block but not open menus.
+
+-- [https://gnu.support JeanLouis] 2019-08-06 19:52 UTC
+
