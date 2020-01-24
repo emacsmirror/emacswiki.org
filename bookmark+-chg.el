18f2d9334eef0202c0b6e734741146599c847371
DrewAdams <anybody@emacswiki.org>
2020-01-24 19:37:26 +0100

    no summary available

--- bookmark+-chg.el
+++ bookmark+-chg.el
@@ -6,9 +6,9 @@
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
 ;; Copyright (C) 2000-2020, Drew Adams, all rights reserved.
 ;; Created: Fri Sep 15 07:58:41 2000
-;; Last-Updated: Fri Jan 24 09:31:19 2020 (-0800)
+;; Last-Updated: Fri Jan 24 10:34:18 2020 (-0800)
 ;;           By: dradams
-;;     Update #: 16605
+;;     Update #: 16609
 ;; URL: https://www.emacswiki.org/emacs/download/bookmark%2b-chg.el
 ;; Doc URL: https://www.emacswiki.org/emacs/BookmarkPlus
 ;; Keywords: bookmarks, bookmark+
@@ -2105,7 +2105,10 @@
 ;;;(@* "CHANGE LOG FOR `bookmark+-key.el'")
 ;;
 ;; 2020/01/24 dadams
-;;     bmkp-bookmarks-here-menu-command-entries: Added bmkp-light-bookmarks.
+;;     bmkp-bookmarks-here-menu-command-entries, bmkp-highlight-menu:
+;;       Added: bmkp-describe-bookmark-lighted-here.
+;;     bmkp-bookmarks-here-menu-command-entries:
+;;       Added: bmkp-light-bookmarks, bmkp-bookmarks-lighted-at-point.
 ;; 2020/01/22 dadams
 ;;     Bind bmkp-switch-to-bookmark-file-this-file/buffer to C-x p C-l,
 ;;          bmkp-save-bookmarks-this-file/buffer to C-x p C-s.
