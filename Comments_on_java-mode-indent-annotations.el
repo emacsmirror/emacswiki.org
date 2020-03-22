fab5d6312ddca922991b32225e2aadff65b5606a
madanswer.ask <anybody@emacswiki.org>
2020-03-22 15:29:06 +0100

    Thanks for sharing such informative and helpful blog post and you are doing a good job so keep posting such amazing articles.  Just if you need more . . .

---
+++ Comments_on_java-mode-indent-annotations.el
@@ -0,0 +1,7 @@
+Thanks for sharing such informative and helpful blog post and you are doing a good job so keep posting such amazing articles.
+ Just if you need more details on the topic you can follow us on <a href="https://madanswer.com/java> Java Interview Questions and Answers </a>
+
+thank you as always!
+
+-- [https://madanswer.com madanswer.ask] 2020-03-22 14:29 UTC
+
