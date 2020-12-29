2bb43a74fd3dd292b6d85d47a12785b93bdd55d5
DrewAdams <anybody@emacswiki.org>
2020-12-29 21:50:19 +0100

    no summary available

--- bookmark+-1.el
+++ bookmark+-1.el
@@ -4,12 +4,12 @@
 ;; Description: First part of package Bookmark+.
 ;; Author: Drew Adams, Thierry Volpiatto
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
-;; Copyright (C) 2000-2020, Drew Adams, all rights reserved.
+;; Copyright (C) 2000-2021, Drew Adams, all rights reserved.
 ;; Copyright (C) 2009, Thierry Volpiatto.
 ;; Created: Mon Jul 12 13:43:55 2010 (-0700)
-;; Last-Updated: Sat Nov 28 21:06:33 2020 (-0800)
+;; Last-Updated: Tue Dec 29 12:36:09 2020 (-0800)
 ;;           By: dradams
-;;     Update #: 9223
+;;     Update #: 9224
 ;; URL: https://www.emacswiki.org/emacs/download/bookmark%2b-1.el
 ;; Doc URL: https://www.emacswiki.org/emacs/BookmarkPlus
 ;; Keywords: bookmarks, bookmark+, placeholders, annotations, search, info, url, eww, w3m, gnus
