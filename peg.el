8af27112dbe491e94d9db0831fcbd744b3ca1e73
Helmut Eller <anybody@emacswiki.org>
2021-09-29 16:40:00 +0200

    Drop the link to META, as most Emacs programmers probably have never heard of it.

--- peg.el
+++ peg.el
@@ -1,8 +1,7 @@
-Parsing Expression Grammars (PEG) are a formalism in the spirit of
+[https://bford.info/packrat/ Parsing Expression Grammars (PEGs)] are a formalism in the spirit of
 Context Free Grammars (CFG) with some simplifications which makes
 the implementation of PEGs as recursive descent parsers particularly
-simple and easy to understand [[https://bford.info/packrat/ Ford], [http://web.archive.org/web/20200109080251/http://home.pipeline.com/~hbaker1/Prag-Parse.html Baker]].
-PEGs are more expressive than regexps and potentially easier to use.
+simple and easy to understand.
 
 The [https://elpa.gnu.org/packages/peg.html peg.el] library implements the macros `define-peg-rule', `with-peg-rules', and
 `peg-parse' which parses the current buffer according to a PEG.
