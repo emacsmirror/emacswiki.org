a3fcf7a6404e8c46559c35d27bdfa9f17ead0f86
DrewAdams <anybody@emacswiki.org>
2022-01-26 20:06:14 +0100

    no summary available

--- icicles-doc1.el
+++ icicles-doc1.el
@@ -4,11 +4,11 @@
 ;; Description: Minibuffer completion and cycling.
 ;; Author: Drew Adams
 ;; Maintainer: Drew Adams (concat "drew.adams" "@" "oracle" ".com")
-;; Copyright (C) 1996-2021, Drew Adams, all rights reserved.
+;; Copyright (C) 1996-2022, Drew Adams, all rights reserved.
 ;; Created: Tue Aug  1 14:21:16 1995
-;; Last-Updated: Sun Apr 11 08:38:46 2021 (-0700)
+;; Last-Updated: Wed Jan 26 10:36:28 2022 (-0800)
 ;;           By: dradams
-;;     Update #: 28672
+;;     Update #: 28677
 ;; URL: https://www.emacswiki.org/emacs/download/icicles-doc1.el
 ;; Doc URL: https://www.emacswiki.org/emacs/Icicles
 ;; Keywords: internal, extensions, help, abbrev, local, minibuffer,
