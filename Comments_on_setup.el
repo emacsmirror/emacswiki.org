7e215d370fd42334d33cef84cc2ad0da4c660b7d
Joe <anybody@emacswiki.org>
2022-03-27 07:13:51 +0200

    This package seems to be something different from the official setup.el on ELPA[0]. Could it please be renamed? Thank you! [0] . . .

---
+++ Comments_on_setup.el
@@ -0,0 +1,6 @@
+This package seems to be something different from the official setup.el on ELPA[0]. Could it please be renamed? Thank you!
+
+[0] https://elpa.gnu.org/packages/setup.html
+
+-- Joe 2022-03-27 05:13 UTC
+
