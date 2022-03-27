8ca93dbf89a5c1318ec704d4d9d55ac129f52517
shrysr <anybody@emacswiki.org>
2022-03-28 00:21:40 +0200

    It appears this has already been discussed on another page Comments_on_SetupEl ?

--- Comments_on_setup.el
+++ Comments_on_setup.el
@@ -4,3 +4,10 @@ This package seems to be something different from the official setup.el on ELPA[
 
 -- Joe 2022-03-27 05:13 UTC
 
+
+----
+
+It appears this has already been discussed on another page [[Comments_on_SetupEl]] ?
+
+-- shrysr 2022-03-27 22:21 UTC
+
