2954f21c746fbea2aaa9ec949d8a3bd21f70c90e
Anonymous <anybody@emacswiki.org>
2022-05-06 19:16:48 +0200

    Use `cl-lib' in place of the depracated `cl' package and use declare forms for indentation

--- macro-utils.el
+++ macro-utils.el
@@ -1,29 +1,30 @@
 ;;; macro-utils.el --- Utilities for writing macros.
 
-;; Copyright (C) 2005 Lars Brinkhoff.
+;; Copyright (C) 2022 Umut Tuna Akgül <thuna.cing@gmail.com>
+;; Copyright (C) 2005 Lars Brinkhoff. (Original Author)
 ;; This file is in the public domain.
 
-(require 'cl)
+(require 'cl-lib)
 
 (defmacro with-gensyms (symbols &rest body)
   "Execute BODY in a context where the variables in SYMBOLS are bound to
 fresh gensyms."
-  (assert (every #'symbolp symbols))
-  `(let ,(mapcar* #'list symbols '#1=((gensym) . #1#))
-    ,@body))
+  (declare (indent 1))
+  (cl-assert (cl-every #'symbolp symbols))
+  `(let ,(cl-mapcar #'list symbols '#1=((gensym) . #1#))
+     ,@body))
 
 (defmacro once-only (symbols &rest body)
   "Execute BODY in a context where the values bound to the variables in
 SYMBOLS are bound to fresh gensyms, and the variables in SYMBOLS are bound
 to the corresponding gensym."
-  (assert (every #'symbolp symbols))
-  (let ((gensyms (mapcar (lambda (x) (gensym)) symbols)))
+  (declare (indent 1))
+  (cl-assert (cl-every #'symbolp symbols))
+  (let ((gensyms (mapcar #'gensym symbols)))
     `(with-gensyms ,gensyms
-       (list 'let (mapcar* #'list (list ,@gensyms) (list ,@symbols))
-        ,(list* 'let (mapcar* #'list symbols gensyms)
-           body)))))
-
-(put 'once-only 'lisp-indent-function 1)
+       (list 'let (cl-mapcar #'list (list ,@gensyms) (list ,@symbols))
+             ,(cl-list* 'let (cl-mapcar #'list symbols gensyms)
+			body)))))
 
 ;;; A small test.
 (when nil
@@ -32,16 +33,15 @@ to the corresponding gensym."
          (sym1 (caaadr form1))
          (form2 (eval `(let ((x '(bar))) ,form)))
          (sym2 (caaadr form2)))
-    (assert (equal form1
-                   `(let ((,sym1 (gensym)))
-                     (list 'let (mapcar* #'list (list ,sym1) (list x))
-                       (let ((x ,sym1))
-                         (list 'foo x))))))
-    (assert (equal form2
-                   `(let ((,sym2 (bar)))
-                     (foo ,sym2))))))
+    (cl-assert
+     (equal form1
+            `(let ((,sym1 (gensym)))
+	       (list 'let (cl-mapcar #'list (list ,sym1) (list x))
+		     (let ((x ,sym1))
+		       (list 'foo x))))))
+    (cl-assert
+     (equal form2
+            `(let ((,sym2 (bar)))
+               (foo ,sym2))))))
 
 (provide 'macro-utils)
-
-;; macro-utils.el ends here.
-
