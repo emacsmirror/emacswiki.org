d6bc8eb9239faa20a548f9354da9558991ed0f1a
Johan Bolmsjö <anybody@emacswiki.org>
2023-06-03 09:48:05 +0200

    I tried to upload a version of ppindent.el that works with later Emacs versions. In doing so I inadvertently changed the page type from "text entry" . . .

---
+++ Comments_on_ppindent.el
@@ -0,0 +1,7 @@
+I tried to upload a version of ppindent.el that works with later Emacs versions.
+In doing so I inadvertently changed the page type from "text entry" to "uploaded file";
+it may have upset the Wiki as it still shows the old version of the file, although claiming it's the new version.
+Still the new version can be accessed through https://www.emacswiki.org/emacs?action=browse;id=ppindent.el;revision=2
+
+-- Johan Bolmsjö 2023-06-03 07:48 UTC
+
