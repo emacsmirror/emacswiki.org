a1c597621c790b8b75e18f7f3f856141abf0494a
Alex <anybody@emacswiki.org>
2023-06-06 16:35:39 +0200

    From the "Edit this page" link you can change the page type from "text" to "file" and back. So I downloaded the file, changed the page type, and . . .

--- Comments_on_ppindent.el
+++ Comments_on_ppindent.el
@@ -5,3 +5,10 @@ Still the new version can be accessed through https://www.emacswiki.org/emacs?ac
 
 -- Johan Bolmsjö 2023-06-03 07:48 UTC
 
+
+----
+
+From the "Edit this page" link you can change the page type from "text" to "file" and back. So I downloaded the file, changed the page type, and pasted the file content. Looking at the diff between revisions 1 and 3 I think that this is now resolved.
+
+-- Alex 2023-06-06 14:35 UTC
+
