add342cc6faef4fc866039e88fc6c70bbd0141cc
Anonymous <anybody@emacswiki.org>
2023-10-03 19:13:04 +0200

    Hi Adam, First thanks for your great work on Bookmark+. Would you mind using let instead of lexical-let ? It's obsolete since emacs 24.1 and was . . .

---
+++ Comments_on_bookmark+-1.el
@@ -0,0 +1,7 @@
+Hi Adam,
+First thanks for your great work on Bookmark+.
+Would you mind using let instead of lexical-let ?
+It's obsolete since emacs 24.1 and was removed on emacs 29.1.
+
+-- Anonymous 2023-10-03 17:13 UTC
+
