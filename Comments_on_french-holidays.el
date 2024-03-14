7170dee8e0755411c3aa4ddce053c6cf31ae57f0
Alex Schroeder <anybody@emacswiki.org>
2024-03-14 07:19:56 +0100

    Deleted

--- Comments_on_french-holidays.el
+++ Comments_on_french-holidays.el
@@ -1,8 +1,1 @@
-Replace	(holiday-float 5 0 4 "Fête des mères") by 
-
-(if (not (equal (calendar-nth-named-day -1 0 5 displayed-year) (caar (holiday-easter-etc 49))))
-    (holiday-float 5 0 -1 "Fête des mères")
-  (holiday-float 6 0 1 "Fête des mères"))
-
--- RwN 2024-03-05 16:21 UTC
-
+DeletedPage
