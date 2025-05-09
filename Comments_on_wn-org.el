ff261020ca7513eececa6a24c45992e3d716a60e
Cena <anybody@emacswiki.org>
2025-05-09 20:33:29 +0200

    The link provided 404s, and the current single repository which exists at this user's GH account (https://github.com/Henry/EuLisp) doesn't mention . . .

---
+++ Comments_on_wn-org.el
@@ -0,0 +1,4 @@
+The link provided 404s, and the current single repository which exists at this user's GH account (https://github.com/Henry/EuLisp) doesn't mention wn-org.el.
+
+-- Cena 2025-05-09 18:33 UTC
+
