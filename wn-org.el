6e58494219d7abb97902d3cfd875e983a39e0d83
Cena <anybody@emacswiki.org>
2025-07-22 18:45:43 +0200

    no summary available

--- wn-org.el
+++ wn-org.el
@@ -1,3 +1,3 @@
-This is an org-mode based interface for the Word Net dictionary, by Henry Weller (2010).
+This is an org-mode based interface for the Word Net dictionary, by Henry G. Weller (2016).
 
 This package can be found in the [https://github.com/emacsattic/wn-org/blob/master/wn-org.el EmacsAttic Github repository.]
