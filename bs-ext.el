80479157a39deed2ca7883600c9352655ff28172
JoeBloggs <anybody@emacswiki.org>
2025-09-01 21:36:05 +0200

    testing yaoddmuse.el

--- bs-ext.el
+++ bs-ext.el
@@ -1,4 +1,4 @@
-;;; bs-ext.el --- Extensions to emacs buffer-selection library (bs.el) TEST
+;;; bs-ext.el --- Extensions to emacs buffer-selection library (bs.el)
 
 ;; Filename: bs-ext.el
 ;; Description: Extensions to emacs buffer-selection library (bs.el)
