2c5249742d6d0319fabaddd3ffbba99d6fc8e82f
Cena <anybody@emacswiki.org>
2026-02-05 03:42:16 +0100

    adds ell.el

---
+++ ell.el
@@ -0,0 +1,5 @@
+This file allows a user to connect to and download files from the [https://www.emacswiki.org/emacs/EmacsLispList EmacsLispList], an early package listing site which dates to [https://www.damtp.cam.ac.uk/user/sje30/emacs/ellcover.html at least 1999].  Its goal was to "...provide one compact list with links to all of the current emacs lisp files out there on the internet". 
+
+The Emacs Lisp List is still accessible as of this writing in early February 2026, and last updated in 2014.
+
+Lisp:ell.el
