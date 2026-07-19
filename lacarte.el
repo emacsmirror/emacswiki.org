47d59520f942f1e888da20d0de2a80cb6fbf565b
DrewAdams <anybody@emacswiki.org>
2026-07-20 01:00:14 +0200

    no summary available

--- lacarte.el
+++ lacarte.el
@@ -8,9 +8,9 @@
 ;; Created: Fri Aug 12 17:18:02 2005
 ;; Version: 0
 ;; Package-Requires: ()
-;; Last-Updated: Thu Jan 20 15:04:44 2022 (-0800)
-;;           By: dradams
-;;     Update #: 1117
+;; Last-Updated: Sun Jul 19 15:59:35 2026 (-0700)
+;;           By: drew0
+;;     Update #: 1118
 ;; URL: https://www.emacswiki.org/emacs/download/lacarte.el
 ;; Doc URL: https://www.emacswiki.org/emacs/LaCarte
 ;; Keywords: menu-bar, menu, command, help, abbrev, minibuffer, keys,
@@ -460,10 +460,12 @@
   "Execute menu items as commands, with completion."
   :prefix "lacarte-" :group 'menu
   :link `(url-link :tag "Send Bug Report"
-          ,(concat "mailto:" "drew.adams" "@" "oracle" ".com?subject=
+          ,(format (concat "mailto:" "drew" "0000" "0001" "@gm" "ail" ".com?subject=\
 lacarte.el bug: \
-&body=Describe bug here, starting with `emacs -q'.  \
-Don't forget to mention your Emacs and library versions."))
+&body=Describe bug below, using a precise recipe that starts with `emacs -Q' or `emacs -q'.  \
+Be sure to mention the `Update #' from the file header.\
+%%0A%%0AEmacs version: %s")
+          (emacs-version)))
   :link '(url-link :tag "Other Libraries by Drew"
           "https://www.emacswiki.org/emacs/DrewsElispLibraries")
   :link '(url-link :tag "Download" "https://www.emacswiki.org/emacs/download/lacarte.el")
