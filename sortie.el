ec0960bca3428d442d0029b2398e366f39844aaf
DrewAdams <anybody@emacswiki.org>
2026-07-20 01:18:03 +0200

    no summary available

--- sortie.el
+++ sortie.el
@@ -4,13 +4,13 @@
 ;; Description: Sorting of completion candidates.
 ;; Author: Drew Adams
 ;; Maintainer: Drew Adams
-;; Copyright (C) 2020-2022, Drew Adams, all rights reserved.
+;; Copyright (C) 2020-2026, Drew Adams, all rights reserved.
 ;; Created: Wed Jun  3 13:54:04 2020 (-0700)
 ;; Version: 0
 ;; Package-Requires: ()
-;; Last-Updated: Wed Jan 12 14:43:51 2022 (-0800)
-;;           By: dradams
-;;     Update #: 217
+;; Last-Updated: Sun Jul 19 16:17:06 2026 (-0700)
+;;           By: drew0
+;;     Update #: 220
 ;; URL: https://www.emacswiki.org/emacs/download/sortie.el
 ;; Doc URL: https://www.emacswiki.org/emacs/Sortie
 ;; Keywords: completion sorting cycling
@@ -130,7 +130,7 @@
 ;;  Non-interactive functions defined here:
 ;;
 ;;    `sorti-bind-cycle-key', `sorti-bind-cycle-key-and-complete',
-;;    `sorti-cycle-sort-order', `sorti-reverse-order'.
+;;    `sorti-reverse-order'.
 ;;
 ;;  Internal variables defined here:
 ;;
@@ -175,9 +175,12 @@
   "Preferences for sorting of completion candidates."
   :prefix "sorti-" :group 'completion
   :link `(url-link :tag "Send Bug Report"
-                   ,(concat "mailto:" "drew.adams" "@" "oracle" ".com?subject=sortie.el bug: \
-&body=Describe bug here, starting with `emacs -q'.  \
-Don't forget to mention your Emacs and Keysee library versions."))
+                   ,(format (concat "mailto:" "drew" "0000" "0001" "@gm" "ail" ".com?subject=\
+sortie.el bug: \
+&body=Describe bug below, using a precise recipe that starts with `emacs -Q' or `emacs -q'.  \
+Be sure to mention the `Update #' from the file header.\
+%%0A%%0AEmacs version: %s")
+          (emacs-version)))
   :link '(url-link :tag "Other Libraries by Drew" "https://www.emacswiki.org/emacs/DrewsElispLibraries")
   :link '(url-link :tag "Download" "https://www.emacswiki.org/emacs/download/sortie.el")
   :link '(url-link :tag "Description" "https://www.emacswiki.org/emacs/Sortie"))
